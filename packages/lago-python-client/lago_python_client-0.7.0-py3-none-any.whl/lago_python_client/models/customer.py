bc56b017e6f2bcf4b9c475017ebdbb98616c534b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:59:07 +0000

    Add package lago-python-client/0.7.0/lago_python_client-0.7.0-py3-none-any.whl

---
+++ packages/lago-python-client/lago_python_client-0.7.0-py3-none-any.whl/lago_python_client/models/customer.py
@@ -0,0 +1,50 @@
+from pydantic import BaseModel, Field
+from typing import Optional
+
+
+class BillingConfiguration(BaseModel):
+    payment_provider: Optional[str]
+    provider_customer_id: Optional[str]
+    sync: Optional[bool]
+
+
+class Customer(BaseModel):
+    external_id: str
+    address_line1: Optional[str]
+    address_line2: Optional[str]
+    city: Optional[str]
+    country: Optional[str]
+    currency: Optional[str]
+    email: Optional[str]
+    legal_name: Optional[str]
+    legal_number: Optional[str]
+    logo_url: Optional[str]
+    name: str
+    phone: Optional[str]
+    state: Optional[str]
+    url: Optional[str]
+    vat_rate: Optional[float]
+    zipcode: Optional[str]
+    billing_configuration: Optional[BillingConfiguration]
+
+
+class CustomerResponse(BaseModel):
+    lago_id: str
+    external_id: str
+    address_line1: Optional[str]
+    address_line2: Optional[str]
+    city: Optional[str]
+    country: Optional[str]
+    currency: Optional[str]
+    email: Optional[str]
+    created_at: str
+    legal_name: Optional[str]
+    legal_number: Optional[str]
+    logo_url: Optional[str]
+    name: str
+    phone: Optional[str]
+    state: Optional[str]
+    url: Optional[str]
+    vat_rate: Optional[float]
+    zipcode: Optional[str]
+    billing_configuration: Optional[BillingConfiguration]
