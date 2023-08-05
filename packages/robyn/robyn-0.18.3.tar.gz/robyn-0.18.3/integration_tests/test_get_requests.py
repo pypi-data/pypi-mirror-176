74cb4cac6ada0a972a2914d75dc168af4aa07fbb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:40 +0000

    Add package robyn/0.18.3/robyn-0.18.3.tar.gz

---
+++ packages/robyn/robyn-0.18.3.tar.gz/robyn-0.18.3/integration_tests/test_get_requests.py
@@ -0,0 +1,40 @@
+import requests
+
+BASE_URL = "http://127.0.0.1:5000"
+
+
+def test_index_request(session):
+    res = requests.get(f"{BASE_URL}")
+    assert res.status_code == 200
+
+
+def test_jsonify(session):
+    r = requests.get(f"{BASE_URL}/jsonify")
+    assert r.json() == {"hello": "world"}
+    assert r.status_code == 200
+
+
+def test_html(session):
+    r = requests.get(f"{BASE_URL}/test/123")
+    assert "Hello world. How are you?" in r.text
+
+
+def test_queries(session):
+    r = requests.get(f"{BASE_URL}/query?hello=robyn")
+    assert r.json() == {"hello": "robyn"}
+
+    r = requests.get(f"{BASE_URL}/query")
+    assert r.json() == {}
+
+
+def test_const_request(session):
+    r = requests.get(f"{BASE_URL}/const_request")
+    assert "Hello world" in r.text
+    assert r.status_code == 200
+
+
+def test_const_request_json(session):
+    r = requests.get(f"{BASE_URL}/const_request_json")
+    assert r.status_code == 200
+    assert r.json() == {"hello": "world"}
+
---
+++ packages/robyn/robyn-0.18.3.tar.gz/robyn-0.18.3/src/routers/middleware_router.rs
@@ -0,0 +1,90 @@
+use std::collections::HashMap;
+use std::sync::RwLock;
+// pyo3 modules
+use crate::types::PyFunction;
+use pyo3::prelude::*;
+use pyo3::types::PyAny;
+
+use matchit::Router;
+
+use anyhow::{bail, Error, Result};
+
+use crate::routers::types::MiddlewareRoute;
+
+/// Contains the thread safe hashmaps of different routes
+
+pub struct MiddlewareRouter {
+    before_request: RwLock<Router<(PyFunction, u8)>>,
+    after_request: RwLock<Router<(PyFunction, u8)>>,
+}
+
+impl MiddlewareRouter {
+    pub fn new() -> Self {
+        Self {
+            before_request: RwLock::new(Router::new()),
+            after_request: RwLock::new(Router::new()),
+        }
+    }
+
+    #[inline]
+    fn get_relevant_map(
+        &self,
+        route: MiddlewareRoute,
+    ) -> Option<&RwLock<Router<(PyFunction, u8)>>> {
+        match route {
+            MiddlewareRoute::BeforeRequest => Some(&self.before_request),
+            MiddlewareRoute::AfterRequest => Some(&self.after_request),
+        }
+    }
+
+    // Checks if the functions is an async function
+    // Inserts them in the router according to their nature(CoRoutine/SyncFunction)
+    pub fn add_route(
+        &self,
+        route_type: MiddlewareRoute,
+        route: &str,
+        handler: Py<PyAny>,
+        is_async: bool,
+        number_of_params: u8,
+    ) -> Result<(), Error> {
+        let table = match self.get_relevant_map(route_type) {
+            Some(table) => table,
+            None => bail!("No relevant map"),
+        };
+
+        let function = if is_async {
+            PyFunction::CoRoutine(handler)
+        } else {
+            PyFunction::SyncFunction(handler)
+        };
+
+        table
+            .write()
+            .unwrap()
+            .insert(route.to_string(), (function, number_of_params))?;
+
+        Ok(())
+    }
+
+    pub fn get_route(
+        &self,
+        route_method: MiddlewareRoute,
+        route: &str, // check for the route method here
+    ) -> Option<((PyFunction, u8), HashMap<String, String>)> {
+        // need to split this function in multiple smaller functions
+        let table = self.get_relevant_map(route_method)?;
+
+        match table.read().unwrap().at(route) {
+            Ok(res) => {
+                let mut route_params = HashMap::new();
+
+                for (key, value) in res.params.iter() {
+                    route_params.insert(key.to_string(), value.to_string());
+                }
+
+                Some((res.value.clone(), route_params))
+            }
+            Err(_) => None,
+        }
+    }
+}
