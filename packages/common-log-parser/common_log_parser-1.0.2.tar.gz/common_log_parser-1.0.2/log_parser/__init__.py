1a14aab3c1f96103c29dd112e168ee1571c3fc85
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:56:01 +0000

    Add package common-log-parser/1.0.2/common_log_parser-1.0.2.tar.gz

---
+++ packages/common-log-parser/common_log_parser-1.0.2.tar.gz/common_log_parser-1.0.2/log_parser/__init__.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# This file is part of log_parser.
+# https://github.com/adeelahmad/log_parser
+
+# Licensed under the MIT license:
+# http://www.opensource.org/licenses/MIT-license
+# Copyright (c) 2022, Adeel Ahmad <230291+adeelahmad@users.noreply.github.com>
+
+from log_parser.version import __version__  # NOQA
+from .log_parser import *  # NOQA
