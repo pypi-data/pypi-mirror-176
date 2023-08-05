acfd6c79c0f8999c02ac3600c114a6af3f4392c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:54:34 +0000

    Add package ParserLexicalAnalyzer/1.0.0/ParserLexicalAnalyzer-1.0.0-py3-none-any.whl

---
+++ packages/ParserLexicalAnalyzer/ParserLexicalAnalyzer-1.0.0-py3-none-any.whl/ParserLexicalAnalyzer/myParser.py
@@ -0,0 +1,9 @@
+import LexicalAnalyzer
+import Parser
+
+def parse(string):
+    lex = LexicalAnalyzer()
+    parser = Parser()
+    tokens = lex.get_tokens(string)
+    parsed_string = parser.parse(tokens)
+    return parsed_string
