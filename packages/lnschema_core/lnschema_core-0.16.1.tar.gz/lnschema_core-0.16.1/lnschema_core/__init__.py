5dff13ffc6e4e792547fc6b44652aabd1d9bee5f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:15:04 +0000

    Add package lnschema_core/0.16.1/lnschema_core-0.16.1.tar.gz

---
+++ packages/lnschema_core/lnschema_core-0.16.1.tar.gz/lnschema_core-0.16.1/lnschema_core/__init__.py
@@ -0,0 +1,61 @@
+"""Data lineage (`yvzi`).
+
+.. note::
+   Please see the documented API reference `here <https://lamin.ai/docs/db/lamindb.schema>`__.
+
+   This page just provides a list of entities.
+
+Import the package::
+
+   import lnschema_core
+
+Entities:
+
+.. autosummary::
+   :toctree: .
+
+   DObject
+   DTransform
+   DSet
+   DTransformIn
+   Jupynb
+   Pipeline
+   Run
+   User
+   Storage
+   Usage
+   Project
+
+Development tools:
+
+.. autosummary::
+   :toctree: .
+
+   dev
+
+"""
+# This is lnschema-module yvzi.
+_schema_id = "yvzi"
+_name = "core"
+_migration = "4ee426b656bb"
+__version__ = "0.16.1"
+
+from . import _core, dev
+from ._core import (
+    DObject,
+    DSet,
+    DSetDObject,
+    DTransform,
+    DTransformIn,
+    Jupynb,
+    Pipeline,
+    Project,
+    ProjectDSet,
+    Run,
+    Storage,
+    Usage,
+    User,
+)
+from .dev import id  # backward compat
+
+storage = Storage  # backward compat
