ef36f94c431c4ebfa57cf3609f3bb923f0342572
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:16:07 +0000

    Add package avocado-framework/99.0/avocado-framework-99.0.tar.gz

---
+++ packages/avocado-framework/avocado-framework-99.0.tar.gz/avocado-framework-99.0/examples/jobs/multiple_suites_from_config.py
@@ -0,0 +1,32 @@
+#!/usr/bin/env python3
+
+import sys
+
+from avocado.core.job import Job
+from avocado.core.suite import TestSuite
+
+ORDERLY_CONFIG = {
+    "resolver.references": ["/bin/true", "/bin/true", "/bin/last"],
+    "nrunner.max_parallel_tasks": 1,
+}
+
+RANDOM_CONFIG = {
+    "resolver.references": [
+        "/bin/true",
+        "/bin/true",
+        "/bin/true",
+        "/bin/true",
+        "/bin/true",
+        "/bin/last",
+    ],
+    "nrunner.shuffle": True,
+    "nrunner.max_parallel_tasks": 3,
+}
+
+with Job(
+    test_suites=[
+        TestSuite.from_config(ORDERLY_CONFIG, name="orderly"),
+        TestSuite.from_config(RANDOM_CONFIG, name="random"),
+    ]
+) as j:
+    sys.exit(j.run())
