48a0b013ace686c3b0d464a88bf1dea7071b144f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:15:50 +0000

    Add package sharingiscaring/0.0.3/sharingiscaring-0.0.3.tar.gz

---
+++ packages/sharingiscaring/sharingiscaring-0.0.3.tar.gz/sharingiscaring-0.0.3/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+
+setuptools.setup(
+    name='sharingiscaring',
+    version='0.0.3',
+    author='Sander de Ruiter',
+    author_email='sdr@concordium-explorer.nl',
+    description='Initial version',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url='https://github.com/sderuiter/sharingiscaring',
+    project_urls={
+    },
+    license='MIT',
+    packages=['sharingiscaring'],
+    install_requires=['rich', 'python-dateutil', 'base58', 'sha3'],
+)
