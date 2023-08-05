e522dc62854890db40275bb36307aac0391f5a42
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:56:46 +0000

    Add package deepchem/2.6.2.dev20221110084045/deepchem-2.6.2.dev20221110084045-py3-none-any.whl

---
+++ packages/deepchem/deepchem-2.6.2.dev20221110084045-py3-none-any.whl/deepchem/models/torch_models/__init__.py
@@ -0,0 +1,19 @@
+# flake8:noqa
+import logging
+
+logger = logging.getLogger(__name__)
+
+from deepchem.models.torch_models.torch_model import TorchModel
+from deepchem.models.torch_models.attentivefp import AttentiveFP, AttentiveFPModel
+from deepchem.models.torch_models.cgcnn import CGCNN, CGCNNModel
+from deepchem.models.torch_models.gat import GAT, GATModel
+from deepchem.models.torch_models.gcn import GCN, GCNModel
+from deepchem.models.torch_models.mpnn import MPNN, MPNNModel
+from deepchem.models.torch_models.lcnn import LCNN, LCNNModel
+from deepchem.models.torch_models.pagtn import Pagtn, PagtnModel
+from deepchem.models.torch_models.mat import MAT, MATModel
+from deepchem.models.torch_models.megnet import MEGNetModel
+from deepchem.models.torch_models.normalizing_flows_pytorch import NormalizingFlow
+from deepchem.models.torch_models.layers import CNNModule, CombineMeanStd, WeightedLinearCombo, AtomicConvolution, NeighborList
+from deepchem.models.torch_models.cnn import CNN
+from deepchem.models.torch_models.dmpnn import DMPNN, DMPNNModel
