b0d742ad11d446b75e34dfb99565641e5508ac7c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:30:10 +0000

    Add package saas-co/5.4/saas_co-5.4.tar.gz

---
+++ packages/saas-co/saas_co-5.4.tar.gz/saas_co-5.4/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='saas_co',
+    version='5.4',
+    license='MIT',
+    author="David Schwartz",
+    author_email='david.schwartz@devfactory.com',
+    packages=['saas_co'],
+    url='https://github.com/gmyrianthous/example-publish-pypi',
+    keywords='cost optimization cur',
+    install_requires=[
+          'boto3',
+          'botocore',
+          'pandas',
+          'awswrangler',
+          'jsonpath-ng'
+      ],
+)
