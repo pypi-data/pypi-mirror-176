9f29c4cbbde4fc577e3fb2af6de199372fede4b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:30 +0000

    Add package open-gopro/0.11.2/open_gopro-0.11.2.tar.gz

---
+++ packages/open-gopro/open_gopro-0.11.2.tar.gz/open_gopro-0.11.2/open_gopro/api/__init__.py
@@ -0,0 +1,21 @@
+# __init__.py/Open GoPro, Version 2.0 (C) Copyright 2021 GoPro, Inc. (http://gopro.com/OpenGoPro).
+# This copyright was auto-generated on Thu Feb  3 00:30:25 UTC 2022
+
+"""Top level API module definition"""
+
+from .api import Api
+from .ble_commands import BleCommands, BleSettings, BleStatuses
+from .wifi_commands import WifiCommands, WifiSettings
+from .builders import (
+    BleSetting,
+    BleStatus,
+    BleReadCommand,
+    BleAsyncResponse,
+    BleProtoCommand,
+    BleWriteCommand,
+    RegisterUnregisterAll,
+    WifiSetting,
+    WifiGetBinary,
+    WifiGetJsonCommand,
+)
+from . import params as Params
