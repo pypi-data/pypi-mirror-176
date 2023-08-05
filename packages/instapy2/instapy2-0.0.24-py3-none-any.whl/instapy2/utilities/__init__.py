dda649cec94e310e2bd589288321862cc5b89e08
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:53 +0000

    Add package instapy2/0.0.24/instapy2-0.0.24-py3-none-any.whl

---
+++ packages/instapy2/instapy2-0.0.24-py3-none-any.whl/instapy2/utilities/__init__.py
@@ -0,0 +1,5 @@
+from .comments import CommentsUtility
+from .follows import FollowsUtility
+from .interactions import InteractionsUtility
+from .likes import LikesUtility
+from .media import MediaUtility
