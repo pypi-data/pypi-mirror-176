386316800b586eea6a2f38b74b590690f6a6f743
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:10:35 +0000

    Add package pymemuc/0.1.5/pymemuc-0.1.5-py3-none-any.whl

---
+++ packages/pymemuc/pymemuc-0.1.5-py3-none-any.whl/pymemuc/_memuc.py
@@ -0,0 +1,102 @@
+"""This module contains functions for directly interacting with memuc.exe."""
+from contextlib import suppress
+from os.path import join, normpath
+from subprocess import (
+    PIPE,
+    STARTF_USESHOWWINDOW,
+    STARTUPINFO,
+    SW_HIDE,
+    CalledProcessError,
+    Popen,
+    TimeoutExpired,
+)
+from tempfile import NamedTemporaryFile
+
+from ._constants import DEBUG, WINREG_EN
+from .exceptions import PyMemucError, PyMemucTimeoutExpired
+
+if WINREG_EN:
+    from winreg import HKEY_LOCAL_MACHINE, ConnectRegistry, OpenKey, QueryValueEx
+
+
+@staticmethod
+def _get_memu_top_level() -> str:
+    """locate the path of the memu directory using windows registry keys
+
+    :return: the path of the memu directory
+    :rtype: str
+    :raises PyMemucError: an error if memu is not installed
+    """
+    if not WINREG_EN:
+        raise PyMemucError("Windows Registry is not supported on this platform")
+    for key in [  # keys to search for memu
+        r"SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\MEmu",
+        r"SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall\MEmu",
+    ]:
+        with suppress(FileNotFoundError):
+            akey = OpenKey(ConnectRegistry(None, HKEY_LOCAL_MACHINE), key)
+            return str(join(normpath(QueryValueEx(akey, "InstallLocation")[0]), "Memu"))
+    raise PyMemucError("MEmuc not found, is it installed?")
+
+
+def memuc_run(
+    self, args: list[str], non_blocking=False, timeout=None
+) -> tuple[int, str]:
+    """run a command with memuc.exe.
+    Memuc can support non-blocking commands, returning a task id.
+    A timeout can be specified if memuc is expected to hang, but this will not work with non-blocking commands.
+
+    :param args: a list of arguments to pass to memuc.exe
+    :type args: list[str]
+    :param non_blocking: whether to run the command in the background. Defaults to False.
+    :type non_blocking: bool, optional
+    :param timeout: the timeout in seconds. Defaults to None for no timeout.
+    :type timeout: int, optional
+    :return: the return code and the output of the command
+    :rtype: tuple[int, str]
+    :raises PyMemucError: an error if the command failed
+    """
+    # sourcery skip: extract-method
+    args.insert(0, self.memuc_path)
+    if timeout is not None and non_blocking:
+        raise PyMemucError("Cannot use timeout and non_blocking at the same time")
+    if timeout is None:
+        args += "-t" if non_blocking else ""
+    try:
+        startupinfo = STARTUPINFO()
+        startupinfo.dwFlags = STARTF_USESHOWWINDOW
+        startupinfo.wShowWindow = SW_HIDE
+        with NamedTemporaryFile(mode="r+", delete=False) as stdout_file:
+            with Popen(
+                args,
+                stdin=PIPE,
+                stdout=stdout_file,
+                stderr=PIPE,
+                shell=False,
+                startupinfo=startupinfo,
+            ) as process:
+                return_code = process.wait(timeout)
+                stdout_file.flush()
+                stdout_file.seek(0)
+                result = stdout_file.read()
+                if DEBUG:
+                    print("pymemuc._memuc.memuc_run:")
+                    print(f"\tCommand: {' '.join(args)}")
+                    print(f"\tOutput [{return_code}]: {result}")
+                return (return_code, result)
+    except CalledProcessError as err:
+        raise PyMemucError(err) from err
+    except TimeoutExpired as err:
+        raise PyMemucTimeoutExpired(err) from err
+
+
+# TODO: add output parsing
+def check_task_status(self, task_id):
+    """Check the status of a task
+
+    :param task_id: Asynchronous task ID
+    :type task_id: str
+    :return: the return code and the output of the command.
+    :rtype: tuple[int, str]
+    """
+    return self.memuc_run(["taskstatus"], task_id)
