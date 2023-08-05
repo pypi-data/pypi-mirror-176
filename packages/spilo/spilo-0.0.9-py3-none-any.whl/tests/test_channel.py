fff6d44b277119a45080f0210ba05f144ef32589
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:30:27 +0000

    Add package spilo/0.0.9/spilo-0.0.9-py3-none-any.whl

---
+++ packages/spilo/spilo-0.0.9-py3-none-any.whl/tests/test_channel.py
@@ -0,0 +1,26 @@
+import pytest
+
+from spilo.channel import Channel
+
+
+def test_channel_instance(channel):
+    isinstance(channel.clients, set)
+    isinstance(channel.dict_clients, dict)
+
+
+def test_adding_new_client_in_channel(client, channel):
+    channel.add_client(client)
+
+    assert len(channel.clients) == 1
+    for client_1 in channel.clients:
+        assert client_1 is client
+
+
+@pytest.mark.asyncio
+async def test_remove_client_from_channel(client, channel):
+    channel.add_client(client)
+
+    await channel.remove_client(client)
+    assert len(channel.clients) == 0
+    assert channel.dict_clients.get(client.client_id) is None
+
