dd2c375c7d3990bb0bba5cfdafec6e4bbd2a8ce9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:11:06 +0000

    Add package fijiwashere123/0.0.0/fijiwashere123-0.0.0.tar.gz

---
+++ packages/fijiwashere123/fijiwashere123-0.0.0.tar.gz/fijiwashere123-0.0.0/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup, find_packages
+# :)
+VERSION = '1.0.0'
+
+# Setting up
+setup(
+    name='fijiwashere123', 
+    author="",
+    author_email="",
+    packages=find_packages(),
+)
