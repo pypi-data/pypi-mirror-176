1484507710689f59c27f4091a292fd75d1e8146d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:11:13 +0000

    Add package pynumwork/0.1.2/pynumwork-0.1.2.tar.gz

---
+++ packages/pynumwork/pynumwork-0.1.2.tar.gz/pynumwork-0.1.2/setup.py
@@ -0,0 +1,21 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+modules = \
+['pynumwork']
+setup_kwargs = {
+    'name': 'pynumwork',
+    'version': '0.1.2',
+    'description': 'A simple Python package for simple math problems. Dedicated to Ksenia :)',
+    'long_description': 'Dedicated to Ksenia :)\n\nA simple Python package for simple math operations.\n',
+    'author': 'spacecultengineer',
+    'author_email': 'spacecultengineer@gmail.com',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'py_modules': modules,
+    'python_requires': '>=3.9,<4.0',
+}
+
+
+setup(**setup_kwargs)
