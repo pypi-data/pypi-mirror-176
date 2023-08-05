b9ba9e983e66b0af4b3efca231e783449b4fd20d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:34 +0000

    Add package apify-client/0.7.0b16/apify_client-0.7.0b16-py3-none-any.whl

---
+++ packages/apify-client/apify_client-0.7.0b16-py3-none-any.whl/apify_client/clients/resource_clients/schedule_collection.py
@@ -0,0 +1,73 @@
+from typing import Any, Dict, List, Optional
+
+from ..._utils import ListPage, _filter_out_none_values_recursively
+from ..base import ResourceCollectionClient
+
+
+class ScheduleCollectionClient(ResourceCollectionClient):
+    """Sub-client for manipulating schedules."""
+
+    def __init__(self, *args: Any, **kwargs: Any) -> None:
+        """Initialize the ScheduleCollectionClient with the passed arguments."""
+        resource_path = kwargs.pop('resource_path', 'schedules')
+        super().__init__(*args, resource_path=resource_path, **kwargs)
+
+    def list(
+        self,
+        *,
+        limit: Optional[int] = None,
+        offset: Optional[int] = None,
+        desc: Optional[bool] = None,
+    ) -> ListPage:
+        """List the available schedules.
+
+        https://docs.apify.com/api/v2#/reference/schedules/schedules-collection/get-list-of-schedules
+
+        Args:
+            limit (int, optional): How many schedules to retrieve
+            offset (int, optional): What schedules to include as first when retrieving the list
+            desc (bool, optional): Whether to sort the schedules in descending order based on their modification date
+
+        Returns:
+            ListPage: The list of available schedules matching the specified filters.
+        """
+        return self._list(limit=limit, offset=offset, desc=desc)
+
+    def create(
+        self,
+        *,
+        cron_expression: str,
+        is_enabled: bool,
+        is_exclusive: bool,
+        name: Optional[str] = None,
+        actions: List[Dict] = [],
+        description: Optional[str] = None,
+        timezone: Optional[str] = None,
+    ) -> Dict:
+        """Create a new schedule.
+
+        https://docs.apify.com/api/v2#/reference/schedules/schedules-collection/create-schedule
+
+        Args:
+            cron_expression: The cron expression used by this schedule
+            is_enabled: True if the schedule should be enabled
+            is_exclusive: When set to true, don't start actor or actor task if it's still running from the previous schedule.
+            name: The name of the schedule to create.
+            actions: Actors or tasks that should be run on this schedule. See the API documentation for exact structure.
+            description: Description of this schedule
+            timezone: Timezone in which your cron expression runs (TZ database name from https://en.wikipedia.org/wiki/List_of_tz_database_time_zones)
+
+        Returns:
+            dict: The created schedule.
+        """
+        schedule = {
+            'cronExpression': cron_expression,
+            'isEnabled': is_enabled,
+            'isExclusive': is_exclusive,
+            'name': name,
+            'actions': actions,
+            'description': description,
+            'timezone': timezone,
+        }
+
+        return self._create(_filter_out_none_values_recursively(schedule))
