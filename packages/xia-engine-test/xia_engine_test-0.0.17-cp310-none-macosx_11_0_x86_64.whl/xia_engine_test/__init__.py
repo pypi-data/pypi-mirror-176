1efd8aa40236f5206dd0bcf93e124ae4b0a91231
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:31:36 +0000

    Add package xia-engine-test/0.0.17/xia_engine_test-0.0.17-cp310-none-macosx_11_0_x86_64.whl

---
+++ packages/xia-engine-test/xia_engine_test-0.0.17-cp310-none-macosx_11_0_x86_64.whl/xia_engine_test/__init__.py
@@ -0,0 +1,10 @@
+from xia_engine_test.field_test import FieldTest
+from xia_engine_test.document_example import MessageHello, DocumentSimple, External
+
+__all__ = [
+    "FieldTest",
+    "MessageHello", "DocumentSimple", "External"
+]
+
+
+__version__ = "0.0.17"
