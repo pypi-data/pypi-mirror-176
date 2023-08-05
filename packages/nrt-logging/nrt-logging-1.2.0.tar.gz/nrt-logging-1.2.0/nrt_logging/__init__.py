6d2937cc7a344a1a5ddceedb4463e689444821fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:11:13 +0000

    Add package nrt-logging/1.2.0/nrt-logging-1.2.0.tar.gz

---
+++ packages/nrt-logging/nrt-logging-1.2.0.tar.gz/nrt-logging-1.2.0/nrt_logging/__init__.py
@@ -0,0 +1,10 @@
+"""
+Version 1.1.0: Add support for configuration file.
+Version 1.2.0:
+    Add support for:
+        Add the same Stream Handler in multiple loggers.
+        Limit logger file size, split and zip log files.
+
+"""
+
+__version__ = '1.2.0'
