61988e65459864cc6f4353cc9c1764d3658fb07f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:59:12 +0000

    Add package kappadata/1.0.30/kappadata-1.0.30-py3-none-any.whl

---
+++ packages/kappadata/kappadata-1.0.30-py3-none-any.whl/kappadata/transforms/patchwise_random_rotation.py
@@ -0,0 +1,17 @@
+import torch
+from torchvision.transforms.functional import rotate
+import numpy as np
+from .base.kd_stochastic_transform import KDStochasticTransform
+
+
+class PatchwiseRandomRotation(KDStochasticTransform):
+    def __call__(self, x, ctx=None):
+        c, l, patch_h, patch_w = x.shape
+        # sample rotations
+        rotations = self.rng.integers(0, 4, size=l) * 90
+        if ctx is not None:
+            ctx["patchwise_random_rotation"] = rotations
+        # rotate patches
+        for i in range(l):
+            x[:, i] = rotate(x[:, i], angle=float(rotations[i]))
+        return x
