5111c784273f50a8ee1b1d3f08a4bf435c44c4f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:39 +0000

    Add package ivprefecttools/0.0.2/ivprefecttools-0.0.2.tar.gz

---
+++ packages/ivprefecttools/ivprefecttools-0.0.2.tar.gz/ivprefecttools-0.0.2/setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+setuptools.setup(
+    name="ivprefecttools",
+    version="0.0.2",
+    author="stevef",
+    author_email="",
+    description="package for reusable functionality across flows",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.9',
+)
