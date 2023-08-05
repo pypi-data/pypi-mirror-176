3dd78c59a89b1799330d3e50c75262ba313a80cb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:48 +0000

    Add package Gaussian-Binomial-rv/0.1.1/Gaussian-Binomial-rv-0.1.1.tar.gz

---
+++ packages/Gaussian-Binomial-rv/Gaussian-Binomial-rv-0.1.1.tar.gz/Gaussian-Binomial-rv-0.1.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='Gaussian-Binomial-rv',
+      version='0.1.1',
+      description='Gaussian distributions',
+      packages=['Gaussian-Binomial-rv'],
+      zip_safe=False)
