b911e74f5019ce741099d5d8a2b958cb699d51e5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:12:08 +0000

    Add package simpot/0.2.0/simpot-0.2.0.tar.gz

---
+++ packages/simpot/simpot-0.2.0.tar.gz/simpot-0.2.0/simpot/__init__.py
@@ -0,0 +1,3 @@
+from simpot.BNamespace import BNamespace
+from simpot.RdfsClass import RdfsClass
+from simpot.serialize import graph, mapper_all, serialize_to_rdf, serialize_to_rdf_file, serialize_all_to_rdf
