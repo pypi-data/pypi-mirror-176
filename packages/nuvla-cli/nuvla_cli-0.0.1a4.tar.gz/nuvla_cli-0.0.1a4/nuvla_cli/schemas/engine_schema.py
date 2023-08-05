3f3be49a8d92abc9340837377bd87572fdeefd8e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:58 +0000

    Add package nuvla-cli/0.0.1a4/nuvla_cli-0.0.1a4.tar.gz

---
+++ packages/nuvla-cli/nuvla_cli-0.0.1a4.tar.gz/nuvla_cli-0.0.1a4/nuvla_cli/schemas/engine_schema.py
@@ -0,0 +1,31 @@
+"""
+"""
+from typing import List
+
+from pydantic import BaseModel
+
+from nuvla_cli.common.common import NuvlaID
+
+
+class EngineConstants:
+    project_base_name: str = 'nuvlaedge_'
+    started_engine_tag: str = 'cli.engine.started=True'
+    cli_engine_name_tag: str = 'cli.engine.name='
+    local_engine_type: str = 'cli.engine.type=local'
+    remote_engine_type: str = 'cli.engine.type=remote'
+    BASE_DEPLOYMENT_COMMAND: str = 'docker-compose -p {project_name} -f {files} {action}'
+
+
+engine_cte: EngineConstants = EngineConstants()
+
+
+class EngineSchema(BaseModel):
+    JOB_PORT: int = 5000
+    AGENT_PORT: int = 5080
+    NUVLABOX_UUID: NuvlaID = ''
+    VPN_INTERFACE_NAME: str = 'vpn_'
+    COMPOSE_PROJECT_NAME: str = 'cli_nuvlaedge_'
+    EXCLUDED_MONITORS: str = 'geolocation'
+
+    # Engine deployment files
+    engine_files: List[str] = []
