62236b8259a53ccba1afe5c8421fe7380192a259
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:16:05 +0000

    Add package raypyc/0.1.4/raypyc-0.1.4-py3-none-any.whl

---
+++ packages/raypyc/raypyc-0.1.4-py3-none-any.whl/raypyc/colors/__init__.pyi
@@ -0,0 +1,29 @@
+from raypyc.structures import Color
+
+LIGHTGRAY: Color  # Light Gray
+GRAY: Color  # Gray
+DARKGRAY: Color  # Dark Gray
+YELLOW: Color  # Yellow
+GOLD: Color  # Gold
+ORANGE: Color  # Orange
+PINK: Color  # Pink
+RED: Color  # Red
+MAROON: Color  # Maroon
+GREEN: Color  # Green
+LIME: Color  # LIME
+DARKGREEN: Color  # Lime
+SKYBLUE: Color  # Sky Blue
+BLUE: Color  # Blue
+DARKBLUE: Color  # Dark Blue
+PURPLE: Color  # Purple
+VIOLET: Color  # Violet
+DARKPURPLE: Color  # Dark Purple
+BEIGE: Color  # Beige
+BROWN: Color  # Brown
+DARKBROWN: Color  # Dark Brown
+
+WHITE: Color  # White
+BLACK: Color  # Black
+BLANK: Color  # Blank (Transparent)
+MAGENTA: Color  # Magenta
+RAYWHITE: Color  # My own White (raylib logo)
