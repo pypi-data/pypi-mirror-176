06f312610d626d0ddbc0c156387dd9680075f8aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:54:59 +0000

    Add package common-log-parser/0.1.0/common_log_parser-0.1.0-py3-none-any.whl

---
+++ packages/common-log-parser/common_log_parser-0.1.0-py3-none-any.whl/tests/base.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# This file is part of log_parser.
+# https://github.com/adeelahmad/log_parser
+
+# Licensed under the MIT license:
+# http://www.opensource.org/licenses/MIT-license
+# Copyright (c) 2022, Adeel Ahmad <230291+adeelahmad@users.noreply.github.com>
+
+from unittest import TestCase as PythonTestCase
+
+
+class TestCase(PythonTestCase):
+    pass
