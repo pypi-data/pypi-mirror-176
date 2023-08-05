9975e9403a33b9b702ee5d46d3ebd7f143d9493a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:57:54 +0000

    Add package click-logging-config/0.0.0.post9/click_logging_config-0.0.0.post9.tar.gz

---
+++ packages/click-logging-config/click_logging_config-0.0.0.post9.tar.gz/click_logging_config-0.0.0.post9/click_logging_config/_default_values.py
@@ -0,0 +1,23 @@
+#  Copyright (c) 2020 Russell Smiley
+#
+#  This file is part of click_logging_config.
+#
+#  You should have received a copy of the MIT License along with build_harness.
+#  If not, see <https://opensource.org/licenses/MIT>.
+
+"""Default values."""
+
+import pathlib
+
+DEFAULT_RELEASE_ID = "0.0.0"
+
+DEFAULT_CONSOLE_LOGGING_ENABLED = False
+
+DEFAULT_FILE_LOGGING_ENABLED = True
+DEFAULT_FILE_ROTATION_BACKUPS = 10
+DEFAULT_FILE_ROTATION_SIZE_MB = 1
+
+DEFAULT_LOG_FILE = pathlib.Path("this.log")
+DEFAULT_LOG_LEVEL = "warning"
+
+VALID_LOG_LEVELS = ["critical", "error", "warning", "info", "debug", "notset"]
