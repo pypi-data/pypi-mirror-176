899d34586ee7c8b670aa56622c7bef0d871f539e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:57:37 +0000

    Add package houhou/0.0.3/houhou-0.0.3.tar.gz

---
+++ packages/houhou/houhou-0.0.3.tar.gz/houhou-0.0.3/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages,setup
+
+setup(name='houhou',
+      py_modules=['.etsy'],
+      version='0.0.3',
+      description='This is for my own use',
+      author='hhh',
+      author_email='',
+      packages=find_packages(),
+      )
