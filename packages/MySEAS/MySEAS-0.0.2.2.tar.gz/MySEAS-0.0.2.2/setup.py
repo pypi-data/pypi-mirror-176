97bcaf140c77c678f7453f483646d30d7815f05e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:15:46 +0000

    Add package MySEAS/0.0.2.2/MySEAS-0.0.2.2.tar.gz

---
+++ packages/MySEAS/MySEAS-0.0.2.2.tar.gz/MySEAS-0.0.2.2/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+
+VERSION = '0.0.2.2'
+DESCRIPTION = 'A Simple Game Engine Libary'
+LONG_DESCRIPTION = 'Chech github: https://github.com/coding610/SEAS/tree/master'
+
+
+# Setting up
+setup(
+    name="MySEAS",
+    version=VERSION,
+    author="Sixten Bohman",
+    author_email="sixten.bohman.08@gmail.com",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=LONG_DESCRIPTION,
+
+    packages=find_packages(),
+    
+    install_requires=['pygame'],
+    keywords=['python', 'pygame', 'Game Engine', 'Simple', 'Beginner', 'Learning', 'Funny?'],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
