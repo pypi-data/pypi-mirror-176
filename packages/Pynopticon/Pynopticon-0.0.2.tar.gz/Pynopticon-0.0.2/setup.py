eaa2d19ac222568863b2d6cb2a52ce4c8bf66564
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:12:02 +0000

    Add package Pynopticon/0.0.2/Pynopticon-0.0.2.tar.gz

---
+++ packages/Pynopticon/Pynopticon-0.0.2.tar.gz/Pynopticon-0.0.2/setup.py
@@ -0,0 +1,16 @@
+from distutils.core import setup
+
+with open("README.md", "r") as f:
+  long_description = f.read()
+
+setup(name='Pynopticon',
+      version='0.0.2',
+      description='Pynopticon',
+      long_description=long_description,
+      long_description_content_type='text/markdown',
+      author='Rick Wierenga',
+      author_email='rick_wierenga@icloud.com',
+      url='https://www.github.com/rickwierenga/pynopticon/',
+      packages=['pynopticon'],
+   )
+
