4e0080999eaf84a6d2e329c058de1f1ef6d9e074
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:57:39 +0000

    Add package houhou/0.0.4/houhou-0.0.4.tar.gz

---
+++ packages/houhou/houhou-0.0.4.tar.gz/houhou-0.0.4/setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages,setup
+
+setup(name='houhou',
+      py_modules=['houhou.etsy','houhou.sql_handler'],
+      version='0.0.4',
+      description='This is for my own use',
+      author='hhh',
+      author_email='',
+      packages=find_packages(),
+      )
