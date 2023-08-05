21b8a44fac11c955c849de200570deb83a6f4da5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:56:55 +0000

    Add package BaseML/0.0.2/BaseML-0.0.2-py3-none-any.whl

---
+++ packages/BaseML/BaseML-0.0.2-py3-none-any.whl/BaseML/__init__.py
@@ -0,0 +1,14 @@
+from .base import baseml
+from .Classification import cls
+from .Regression import reg
+from .Cluster import clt
+from .DimentionReduction import rdc
+
+
+__all__ = [
+    'baseml',
+    'cls',
+    'reg',
+    'clt',
+    'rdc',
+    ]
