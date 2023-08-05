e6736f6d9be8ce5fc70036a2a79d4ca00f8dcc94
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:07:59 +0000

    Add package finam-trade-api/0.2.5/finam-trade-api-0.2.5.tar.gz

---
+++ packages/finam-trade-api/finam-trade-api-0.2.5.tar.gz/finam-trade-api-0.2.5/finam/portfolio/portfolio.py
@@ -0,0 +1,24 @@
+from typing import Union
+
+from finam.base_client.base import BaseClient
+from finam.models import ErrorBodyModel
+from finam.portfolio.model import PortfolioRequestModel, PortfolioResponseModel
+
+
+class PortfolioClient(BaseClient):
+    def __init__(self, token: str):
+        super().__init__(token)
+        self._url = "/api/v1/portfolio"
+
+    async def get_portfolio(self, params: PortfolioRequestModel) -> Union[PortfolioResponseModel, ErrorBodyModel]:
+        p = {
+            "clientId": params.clientId,
+            "content.IncludeCurrencies": params.includeCurrencies,
+            "content.IncludeMoney": params.includeMoney,
+            "content.IncludePositions": params.includePositions,
+            "content.IncludeMaxBuySell": params.includeMaxBuySell,
+        }
+        response, ok = await self._exec_request(self.RequestMethod.GET, self._url, params=p)
+        if not ok:
+            return ErrorBodyModel(**response)
+        return PortfolioResponseModel(**response)
