32a54657b560fe19e924f81555481454c38ebd95
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:59:27 +0000

    Add package netbox-initializers/3.3.3/netbox_initializers-3.3.3-py3-none-any.whl

---
+++ packages/netbox-initializers/netbox_initializers-3.3.3-py3-none-any.whl/netbox_initializers/__init__.py
@@ -0,0 +1,14 @@
+from extras.plugins import PluginConfig
+
+
+class NetBoxInitializersConfig(PluginConfig):
+    name = "netbox_initializers"
+    verbose_name = "NetBox Initializers"
+    description = "Load initial data into Netbox"
+    version = "3.3.3"
+    base_url = "initializers"
+    min_version = "3.3.0"
+    max_version = "3.3.99"
+
+
+config = NetBoxInitializersConfig
