c65cf862808969e1cc340a1a17fab223f4f0a62d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:10:57 +0000

    Add package hspylib-vault/0.9.51/hspylib-vault-0.9.51.tar.gz

---
+++ packages/hspylib-vault/hspylib-vault-0.9.51.tar.gz/hspylib-vault-0.9.51/vault/exception/exceptions.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+   TODO Purpose of the file
+   @project: HSPyLib
+   @package: hspylib.app.vault.exception
+      @file: exceptions.py
+   @created: Tue, 4 May 2021
+    @author: <B>H</B>ugo <B>S</B>aporetti <B>J</B>unior"
+      @site: https://github.com/yorevs/hspylib
+   @license: MIT - Please refer to <https://opensource.org/licenses/MIT>
+
+   Copyright 2022, HSPyLib team
+"""
+from hspylib.core.exception.exceptions import HSBaseException
+
+class VaultCloseError(HSBaseException):
+    """Raised when closing the vault"""
+
+
+class VaultOpenError(HSBaseException):
+    """Raised when opening the vault"""
+
+
+class VaultSecurityException(HSBaseException):
+    """Raised when something unexpected happened to vault file"""
