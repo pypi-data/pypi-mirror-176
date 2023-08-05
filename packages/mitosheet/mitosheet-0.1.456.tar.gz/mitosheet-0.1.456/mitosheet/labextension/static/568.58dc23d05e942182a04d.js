097b4407530a93967f9bc43c68b92c5911fa18d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:56:05 +0000

    Add package mitosheet/0.1.456/mitosheet-0.1.456.tar.gz

---
+++ packages/mitosheet/mitosheet-0.1.456.tar.gz/mitosheet-0.1.456/mitosheet/labextension/static/568.58dc23d05e942182a04d.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkmitosheet=self.webpackChunkmitosheet||[]).push([[568],{1568:(e,l,a)=>{a.r(l),a.d(l,{ExampleModel:()=>s.ExampleModel,ExampleView:()=>s.ExampleView,MODULE_NAME:()=>t.o,MODULE_VERSION:()=>t.Y,default:()=>E});var t=a(8657),s=a(1106);const E=a(9333).default}}]);
