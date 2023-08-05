2d4d6cf69cb01f33acfe7f9e2a220444ede646e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:56:05 +0000

    Add package mitosheet3/0.3.243/mitosheet3-0.3.243-py2.py3-none-any.whl

---
+++ packages/mitosheet3/mitosheet3-0.3.243-py2.py3-none-any.whl/mitosheet3-0.3.243.data/data/share/jupyter/labextensions/mitosheet3/static/568.0890fd227bdbb6bf8eb5.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkmitosheet3=self.webpackChunkmitosheet3||[]).push([[568],{1568:(e,l,a)=>{a.r(l),a.d(l,{ExampleModel:()=>s.ExampleModel,ExampleView:()=>s.ExampleView,MODULE_NAME:()=>t.o,MODULE_VERSION:()=>t.Y,default:()=>E});var t=a(8657),s=a(1106);const E=a(9333).default}}]);
