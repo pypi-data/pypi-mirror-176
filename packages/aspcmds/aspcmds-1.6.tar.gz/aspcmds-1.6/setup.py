8875fb2531d5d6e49be7490fb7a2991dd4066bb3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:45 +0000

    Add package aspcmds/1.6/aspcmds-1.6.tar.gz

---
+++ packages/aspcmds/aspcmds-1.6.tar.gz/aspcmds-1.6/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+# read the contents of your README file
+from pathlib import Path
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.txt").read_text()
+
+setup(
+    name='aspcmds',
+    version='1.6',
+    # other arguments omitted
+    long_description=long_description,
+    long_description_content_type='text/markdown'
+)
