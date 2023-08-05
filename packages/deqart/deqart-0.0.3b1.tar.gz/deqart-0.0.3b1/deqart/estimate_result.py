e3865532476f02553cb96d64cecd35fc412d8450
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:26 +0000

    Add package deqart/0.0.3b1/deqart-0.0.3b1.tar.gz

---
+++ packages/deqart/deqart-0.0.3b1.tar.gz/deqart-0.0.3b1/deqart/estimate_result.py
@@ -0,0 +1,12 @@
+class EstimateResult:
+    """This class contains information about the estimated runtime/cost that
+    will be incurred from running the given circuit on the given quantum
+    machine/simulator. WARNING: this is just an estimate, the actual runtime
+    may be less or more."""
+
+    def __init__(self, data):
+        self.device = data.get("device")
+        self.estimate_ms = data.get("estimate_ms")
+        self.num_qubits = data.get("num_qubits")
+        self.circuit = data.get("qc")
+        self.warning_message = data.get("warning_message")
