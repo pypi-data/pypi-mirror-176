46b82e5bb13232a4e89cb38ff9056ec2638dc16c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:54 +0000

    Add package nasbench-pytorch/1.2.3/nasbench_pytorch-1.2.3.tar.gz

---
+++ packages/nasbench-pytorch/nasbench_pytorch-1.2.3.tar.gz/nasbench_pytorch-1.2.3/Contributors.md
@@ -0,0 +1,4 @@
+# Contributors
+
+- Abhash Jha (@abhash-er)
+    - modified the model code so that cast to double is possible
---
+++ packages/nasbench-pytorch/nasbench_pytorch-1.2.3.tar.gz/nasbench_pytorch-1.2.3/setup.py
@@ -0,0 +1,9 @@
+import setuptools
+
+setuptools.setup(
+    name='nasbench_pytorch',
+    version='1.2.3',
+    license='Apache License 2.0',
+    author='Romulus Hong, Gabriela Suchopárová',
+    packages=setuptools.find_packages()
+)
