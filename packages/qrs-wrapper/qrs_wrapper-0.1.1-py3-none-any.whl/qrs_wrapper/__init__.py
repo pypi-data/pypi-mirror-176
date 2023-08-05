61c9368c39a8978b75667f27d268347c01eab236
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:11:49 +0000

    Add package qrs-wrapper/0.1.1/qrs_wrapper-0.1.1-py3-none-any.whl

---
+++ packages/qrs-wrapper/qrs_wrapper-0.1.1-py3-none-any.whl/qrs_wrapper/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Nov 10 18:25:36 2022
+
+@author: globi
+"""
+from qrs_wrapper.wrapper_map import WRAPPER_MAP, CATALOGUE_DETECTORS
+from qrs_wrapper.qrs_wrapper import *
+
+print("-- imported qrs-wrapper successfully --")
