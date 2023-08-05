48080fa8432281983a0069ca3673fbcf5f5a3cb3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:56:08 +0000

    Add package authk/1.0.4/authk-1.0.4.tar.gz

---
+++ packages/authk/authk-1.0.4.tar.gz/authk-1.0.4/src/authk/add.py
@@ -0,0 +1,36 @@
+"""
+Adds key to authorized_keys
+"""
+
+from typing import Optional
+
+from sshpubkeys import SSHKey
+
+from authk._authorized_keys import AuthorizedKeys
+
+
+def add(key_txt: str) -> Optional[str]:
+    """
+    Add key to authorized_keys list
+
+    Args:
+        key_txt (str): key payload in text form
+
+    Returns:
+        None
+
+    """
+    return_value = None
+    key = SSHKey(key_txt, strict=True)
+    with AuthorizedKeys() as aks:
+        existing_key = aks.get(key.comment)
+        return_value = ""
+        if existing_key:
+            aks.setdefault(key.comment, key)
+            print(f"{key.comment} exists")
+            return_value = "exists"
+        else:
+            aks[key.comment] = key
+            print(f"{key.comment} succesfully added")
+            return_value = "sucessfully added"
+    return f"{key.comment} {return_value}"
