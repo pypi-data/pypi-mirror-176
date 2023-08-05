afde65be9f5c886fa8871e0bcac115414c3a816b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:23 +0000

    Add package indico/3.2.1/indico-3.2.1-py3-none-any.whl

---
+++ packages/indico/indico-3.2.1-py3-none-any.whl/indico/modules/events/persons/forms.py
@@ -0,0 +1,55 @@
+# This file is part of Indico.
+# Copyright (C) 2002 - 2022 CERN
+#
+# Indico is free software; you can redistribute it and/or
+# modify it under the terms of the MIT License; see the
+# LICENSE file for more details.
+
+from flask import request
+from wtforms.fields import BooleanField, HiddenField, SelectField, StringField, TextAreaField
+from wtforms.validators import DataRequired
+
+from indico.core.auth import multipass
+from indico.util.i18n import _
+from indico.util.placeholders import render_placeholder_info
+from indico.web.forms.base import IndicoForm
+from indico.web.forms.fields import HiddenFieldList, IndicoEmailRecipientsField
+from indico.web.forms.widgets import CKEditorWidget, SwitchWidget
+
+
+class EmailEventPersonsForm(IndicoForm):
+    from_address = SelectField(_('From'), [DataRequired()])
+    subject = StringField(_('Subject'), [DataRequired()])
+    body = TextAreaField(_('Email body'), [DataRequired()], widget=CKEditorWidget())
+    recipients = IndicoEmailRecipientsField(_('Recipients'))
+    copy_for_sender = BooleanField(_('Send copy to me'), widget=SwitchWidget(),
+                                   description=_('Send copy of each email to my mailbox'))
+    person_id = HiddenFieldList()
+    user_id = HiddenFieldList()
+    role_id = HiddenFieldList()
+    submitted = HiddenField()
+
+    def __init__(self, *args, **kwargs):
+        register_link = kwargs.pop('register_link')
+        event = kwargs.pop('event')
+        super().__init__(*args, **kwargs)
+        self.from_address.choices = list(event.get_allowed_sender_emails().items())
+        self.body.description = render_placeholder_info('event-persons-email', event=None, person=None,
+                                                        register_link=register_link)
+
+    def is_submitted(self):
+        return super().is_submitted() and 'submitted' in request.form
+
+
+class ManagePersonListsForm(IndicoForm):
+    disallow_custom_persons = BooleanField(_('Disallow manually entering persons'), widget=SwitchWidget(),
+                                           description=_('Prohibit submitters from adding new speakers/authors '
+                                                         'manually and only allow searching for existing users.'))
+    default_search_external = BooleanField(_('Include users with no Indico account by default'), widget=SwitchWidget(),
+                                           description=_('If enabled, searching people for speakers/authors will '
+                                                         'include those with no Indico account by default.'))
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        if not any(auth.supports_search for auth in multipass.identity_providers.values()):
+            del self.default_search_external
