02de4a1bfc3228aeb8c38ca3560c95b1afa83e7f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 23:16:02 +0000

    Add package tomclient/0.0.13/tomclient-0.0.13.tar.gz

---
+++ packages/tomclient/tomclient-0.0.13.tar.gz/tomclient-0.0.13/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='tomclient',
+    author="Xiaozhe Yao",
+    author_email="askxzyao@gmail.com",
+    description="TOMClient is a client library for TOM",
+    version='0.0.13',
+    scripts=["tomclient/tom"],
+    package_dir={'tomclient': 'tomclient'},
+    packages=find_packages(),
+    install_requires=[
+        "typer",
+        "jsonrpc-websocket",
+        "netifaces",
+        "pynvml",
+        "py3nvml",
+        "requests",
+    ]
+)
