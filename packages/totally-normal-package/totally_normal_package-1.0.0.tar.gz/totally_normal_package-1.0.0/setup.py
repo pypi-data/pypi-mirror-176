e978bdcf0bd76053f6e4063f7cda4baa0fb593ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:44 +0000

    Add package totally-normal-package/1.0.0/totally_normal_package-1.0.0.tar.gz

---
+++ packages/totally-normal-package/totally_normal_package-1.0.0.tar.gz/totally_normal_package-1.0.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name='totally_normal_package',
+    version='1.0.0',
+    author='Szabolcs Dombi',
+    author_email='cprogrammer1994@gmail.com',
+    license='MIT',
+    url='https://github.com/freeCodeCamp/freeCodeCamp',
+)
