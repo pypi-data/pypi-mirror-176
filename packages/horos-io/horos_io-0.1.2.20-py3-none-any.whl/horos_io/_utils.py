3dcbbede7e2c81d044f5543158a245412a23b8b2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:56:29 +0000

    Add package horos-io/0.1.2.20/horos_io-0.1.2.20-py3-none-any.whl

---
+++ packages/horos-io/horos_io-0.1.2.20-py3-none-any.whl/horos_io/_utils.py
@@ -0,0 +1,211 @@
+import glob
+import os
+import os.path
+import pathlib
+import sys
+import warnings
+from typing import List, Dict, Union, Optional, Tuple
+
+import cv2
+import numpy as np
+import pandas as pd
+import pydicom
+from decorator import decorator
+from pydicom.errors import InvalidDicomError
+from scipy import interpolate
+
+from horos_io import _config
+
+Path = Union[str, os.PathLike]
+
+
+def __always_true(*args, **kwargs) -> bool: return True
+
+
+@decorator
+def handle_1D(f, *args, **kwargs):
+    def _handle_1D(item):
+        if isinstance(item, np.ndarray):
+            if len(item.shape) < 2:
+                return item[:, np.newaxis]
+            else:
+                return item
+        elif isinstance(item, dict):
+            return {k: _handle_1D(v) for k, v in item.items()}
+        elif isinstance(item, tuple):
+            return tuple([_handle_1D(v) for v in item])
+        else:
+            return item
+
+    return f(*_handle_1D(args), **_handle_1D(kwargs))
+
+
+def _to_str(i):
+    if not 99 >= i >= 0 or not round(i) == i:
+        raise ValueError(f"i should be 2 digits, but instead it is {i}")
+    return str(i) if i >= 10 else f"0{i}"
+
+
+def get_log(log: str, root: str):
+    if log is None:
+        log = os.path.join(root, "val_contour_log.csv")
+    if os.path.isfile(log):
+        df = pd.read_csv(log, index_col=0)
+    else:
+        df = pd.DataFrame(columns=["ID", "contour_type", "result", "by", "frame", "slice", "remark", "time_stamp"])
+    return df
+
+
+def _has_dicom(pth: Path) -> bool:
+    for f in glob.glob(os.path.join(pth, "*")):
+        try:
+            pydicom.dcmread(f)
+            return True
+        except InvalidDicomError:
+            continue
+        except PermissionError:
+            continue
+    return False
+
+
+def get_seq_paths(root: Path) -> List[str]:
+    """
+    checks for sequences, which are assumed to  be 3 levels into the root;
+
+    Args:
+        root: path to the Horos Dataset
+    Returns:
+        list of sequence names
+    """
+    return [pth for pth in globSSF("*/*/*/", root_dir=root) if _has_dicom(os.path.join(root, pth))]
+
+
+def get_ids(root: Path):
+    """
+    gets a list of IDs; not as trivial, as there can be other folders, too;
+    checks for sequences, which are assumed to  be 3 levels into the root;
+    splits the first directory of it and returns non duplicates of it
+    Args:
+        root: path to the Horos Dataset
+    Returns:
+        list of IDs
+    """
+    seqs = get_seq_paths(root)
+    result = [pathlib.Path(seq).parts[0] for seq in seqs]
+    return sorted(list(set(result)))
+
+
+def get_log_path(log: str, root: str):
+    if log is None:
+        log = os.path.join(root, "val_contour_log.csv")
+    return log
+
+
+def globSSF(path_name, /, root_dir=None, **kwargs) -> List[str]:
+    """mimcks behaviour of future globs"""
+    if root_dir is None:
+        return glob.glob(path_name, **kwargs)
+
+    return [os.path.relpath(pth, root_dir) for pth in
+            glob.glob(os.path.normpath(os.path.join(root_dir, path_name)), **kwargs)]
+
+
+####
+def mask_from_omega_contour(cines: np.ndarray, contours: Dict[str, np.ndarray], loc: Union[int, Tuple]) -> np.ndarray:
+    # TODO: raises error when passing 1D sequences
+    # simply havin the user pass the location as tuple or int should be much more flexible
+    mask = np.zeros_like(cines[loc].pixel_array)
+    for i, (name, c) in enumerate(contours.items()):
+        if name != "aroot":
+            tck, _ = interpolate.splprep([*zip(*c[loc])], s=0, per=True)
+            xnew, ynew = interpolate.splev(np.linspace(0, 1, 500), tck, der=0)
+            smooth_contour = np.array([(round(px), round(py)) for px, py in zip(xnew, ynew)])
+            mask = cv2.drawContours(mask, [smooth_contour], 0, i + 1, - 1)
+        else:
+            aroot_c = interpolate_aroot(c[loc])
+            mask = cv2.drawContours(mask, [np.array(aroot_c, dtype=int)], 0, i + 1, -1)
+    return mask
+
+
+def interpolate_aroot(landmarks: List[tuple]) -> List[tuple]:
+    """
+    an aortic root selection of landmarks consists of exactly 6 points; starting with 2 distal points;
+    and then going clockwise;
+
+    interpolates the lateral segments, while using linspace for the horizontal sections;
+
+    Args:
+        landmarks:
+
+    Returns:
+
+    """
+    if not len(landmarks) == 6:
+        raise ValueError(f"aroot needs to have exactly 6 landmarks; instead there are {len(landmarks)}")
+
+    def linspace_interpolation(p1, p2, n=10):
+        result = np.linspace(0, 1, n)[:, np.newaxis] * (np.array(p2) - p1) + p1
+        result = [(x, y) for x, y in result]
+        return result
+
+    def spline_interpolation(seq: List[tuple], n=10) -> List[tuple]:
+        """use basis shift and do a natural cubic spline interp. and shift back"""
+        new_base = np.column_stack([
+            v1 := np.array(seq[-1]) - seq[0],
+            [-v1[1], v1[0]]
+        ])
+        transformed_seq = (np.linalg.inv(new_base) @ np.array(seq).T).T
+        sp = interpolate.CubicSpline(*transformed_seq.T, bc_type="natural")
+        xs = np.linspace(transformed_seq[0, 0], transformed_seq[-1, 0], n)
+        ys = sp(xs)  # take x coords of seq endpoints
+        result = [(x, y) for x, y in (new_base @ [xs, ys]).T]
+
+        return result
+
+    dist = linspace_interpolation(landmarks[0], landmarks[1])
+    inner = spline_interpolation(landmarks[1:4])
+    prox = linspace_interpolation(landmarks[3], landmarks[4])
+    outer = spline_interpolation(landmarks[-2:] + [landmarks[0]])
+    result = dist + inner + prox + outer
+    return result
+
+
+def masks2spline(masks: np.ndarray):
+    """converts an integer mask into a list of contours"""
+    return [masks2spline(masks == i) for i in np.unique(masks)]
+
+
+def mask2spline(mask, ):
+    """
+    Convert a binary mask to a spline.
+
+    Args:
+        mask (ndarray): Binary mask.
+        epsilon (float): The epsilon value for RDP algorithm.
+
+    Returns:
+        x (ndarray): x coordinates of the spline.
+        y (ndarray): y coordinates of the spline.
+    """
+    # Prepare the mask
+    warnings.warn(DeprecationWarning("dont use horos_io.mask2spline for now..."))
+    if not mask.any():
+        return np.nan
+    _, thrs = cv2.threshold(mask, 0, 255, cv2.THRESH_OTSU)
+
+    # Find the contour
+    contours, _ = cv2.findContours(thrs, mode=cv2.RETR_TREE, method=cv2.CHAIN_APPROX_NONE)
+    sorted_contours = sorted(contours, key=cv2.contourArea, reverse=True)
+    largest_contour = sorted_contours[0]
+    largest_contour = [(p[0], p[1]) for p in largest_contour]
+
+    return largest_contour
+
+
+def get_tag(tag: Tuple[str, str], ID: str, root: str) -> str:
+    dicom_paths = globSSF(f"{os.path.join(root, ID, '*/*/*.dcm')}")
+    return pydicom.dcmread(dicom_paths[0])[tag].value
+
+
+def get_study_date(ID: str, root: str) -> str:
+    return get_tag(_config.study_date_tag, ID, root)
---
+++ packages/horos-io/horos_io-0.1.2.20-py3-none-any.whl/horos_io/cli.py
@@ -0,0 +1,277 @@
+"""
+Console script for horos io;
+will incorporate functionality for running visual sanity checks on contours overlayed on top of the data;
+
+generating .csv files for data
+"""
+import os
+import sys
+import typing
+from typing import Optional, NoReturn
+
+import click
+import pandas as pd
+from tqdm import tqdm
+
+from horos_io import load_horos_contour
+from horos_io._utils import globSSF, mask_from_omega_contour, get_log, get_log_path
+from horos_io.cmr import Path, get_image_info, get_contour_info, get_combined_info
+from horos_io.ui import shitty_manual_confirm
+from horos_io.validation import visually_confirm_omega_iter, write_log, last_validation_was_successful, \
+    contour_is_valid, changed_since_last_validation, last_validation
+import matplotlib
+if sys.platform == "darwin":
+    matplotlib.use("macosx")
+
+from matplotlib import pyplot as plt
+
+@click.group()
+def main():
+    """Console script for horos_io."""
+    return 0
+
+
+@click.command("image_info",
+               help="saves information about the horos dataset as csv, such that it can be later used for efficient"
+                    "loading of the data")
+@click.option("--root", default=".", help="directory to the Horos Dataset")
+@click.option("--silent", default=False, is_flag=True, help="if set, will not print out the result")
+@click.option("--out", default=None,
+              help="how the image_info,csv should be saved; if not specified, will save an image_info.csv in root")
+def make_image_info_csv(root: Path,
+                        out: Optional[Path],
+                        silent: bool) -> NoReturn:
+    """
+    one-time operation to update the information of the dataset;
+    columns:
+    ("seq_path",  <relative path starting from the root of the data>
+     "location",  joins the rootdir with seq path
+     "ID",        <Impression_Cmr{ID}>
+     "slice_type", one of config.slice_types
+     "n_frames",
+     "n_slices",
+
+    seq_path is relative to the root
+    location is joint path of root and seq_path
+
+    creates a csv file containing location of the sequence.
+    sequence can be loaded with load_cine_sequence
+    Args:
+        silent: if silent, will not print out the result to console
+        root: root of the data; contents should be directories of Impression Studies
+        out: optional parameter to specify, what and where to save; if None, will save image_info.csv inside root
+    Returns:
+    """
+    click.echo(f"retrieving image info from {root}")
+    result = get_image_info(root)
+    if not silent:
+        click.echo("Findings:\n\n")
+        with pd.option_context('display.max_rows', None, 'display.max_columns',
+                               None):  # more options can be specified also
+            click.echo(result)
+    if out is None:
+        out = os.path.normpath(os.path.join(root, "image_info.csv"))
+    click.echo(f"saving result to {out}")
+    result.to_csv(out)
+
+
+@click.command("contour_info",
+               help="saves information about the horos dataset as csv, such that it can be later used for efficient"
+                    "loading of the data")
+@click.option("--root", default=".", help="directory to the Horos Dataset")
+@click.option("--silent", default=False, is_flag=True, help="if set, will not print out the result")
+@click.option("--out", default=None,
+              help="how the image_info,csv should be saved; if not specified, will save an image_info.csv in root")
+def make_contour_info_csv(root: Path, out: Optional[Path], silent: bool) -> NoReturn:
+    """
+    one-time operation to update the information of the dataset;
+
+    ("ID": <Impression_Cmr{ID}>,
+     "contour_path": relative path to the contour file, starting from root of the data,
+     "contour_type": one of config.contour_types,
+     "location": joins the root dir with the sequence path
+     "slice_type": one of config.slice_types,
+    )
+
+    creates a csv file containing location of all contour files; or whether they exist at all.
+    Args:
+        root: root of the data; contents should be directories of Impression Studies
+    Returns:
+    """
+    root = os.path.normpath(root)
+    click.echo(f"creating contour info for {root}")
+    result = get_contour_info(root)
+    if not silent:
+        click.echo("Findings:\n\n")
+        with pd.option_context('display.max_rows', None, 'display.max_columns',
+                               None):  # more options can be specified also
+            click.echo(result)
+
+    if out is None:
+        out = os.path.normpath(os.path.join(root, "contour_info.csv"))
+    click.echo(f"saving contour info to: {out}")
+    result.to_csv(out)
+
+
+@click.command()
+@click.option("--by", required=True, help="the name that will be recorded in the log_")
+@click.option("--root", default=".", help="source root of the Horos dataset")
+@click.option("--only_unconfirmed", is_flag=True, default=False,
+              help="will only prompt you cases, that have not been confirmed by the user yet (registered in log_)")
+@click.option("--log", default=None,
+              help="if None, will save or update a val_contour_log.csv in root, else checks your specified log_ instead")
+@click.option("--dry_run", is_flag=True, default=False,
+              help="if this flag is set, run through the procedure without displaying or logging anything")
+def validate(by: str, root: Path, only_unconfirmed: bool, log: Path, dry_run: bool):
+    """
+    visually confirm literally every existing omega contour... (might need to change with params to return
+    an adequate iterator)
+
+    will also save a dataframe / update a dataframe in <root>/val_contour_log.csv,
+    that logs review result with timestamp
+    """
+    click.echo(f"starting validation process in root: {root}")
+
+    for ID, omega, f, s, cines, contours, mt_contour in tqdm(
+        visually_confirm_omega_iter(combined_info=get_combined_info(root))):
+        if only_unconfirmed and last_validation_was_successful(get_log(log, root), ID=ID, frame=f, slice=s,
+                                                               contour_type=omega) and not changed_since_last_validation(
+            mt_contour, last_validation(get_log(log, root), ID=ID, frame=f, slice=s,
+                                        contour_type=omega)):  # CAVE: hard coded
+            click.echo(f"skipping {ID}, ({f}, {s}), {omega}")
+            continue
+        loc = (f, s)
+        fig, ax = plt.subplots()
+        ax.imshow(cines[loc].pixel_array)
+        ax.imshow(mask_from_omega_contour(cines, contours, loc), cmap="jet", alpha=0.5)
+        ax.figure.suptitle(f"{ID}: {omega} f: {f}, s: {s}, ")
+
+        ##################### ideall y this is not part of the cli logic #######################################
+        if omega == "omega_3ch":
+            ax.axes.scatter(*zip(*contours["aroot"][loc]),
+                            c=["black", "red", "green", "blue", "cyan", "purple"])
+        if not dry_run:
+            ok, remark = shitty_manual_confirm(ax)
+            write_log(log, **locals())
+        else:
+            plt.close()
+
+    click.echo("batch finished validation!")
+
+
+@click.command("rename", help="renames all contours with .xml and .rois_series attached to it")
+@click.argument("old", nargs=1)
+@click.argument("target", nargs=1)
+@click.option("--root", help="Data root")
+def rename_contour_files(root: str, old: str, target: str) -> typing.NoReturn:
+    """
+    renames .xml and .roi_series files by dictionary, that lie in the impression folders:
+    root |--- Impression_CmrXXXX
+                    |---- lv_epi.xml
+                    |---- lv_epi.roi_series
+    Args:
+        target:
+        old:
+        root:
+    Returns:
+    """
+    files = globSSF(f"*/{old}.xml", root_dir=root) + globSSF(f"*/{old}.rois_series", root_dir=root)
+    for f in files:
+        path, f_name = os.path.split(f)
+        f_name, f_ending = f_name.split(".")
+        src = os.path.normpath(os.path.join(root, path, f"{old}.{f_ending}"))
+        dst = os.path.normpath(os.path.join(root, path, f"{target}.{f_ending}"))
+        if os.path.isfile(dst):
+            raise FileExistsError(f"{dst} for {src} already exists")
+        os.rename(src, dst)
+        click.echo(f"renamed {src} to {dst}")
+    click.echo("finished")
+
+
+@click.group("check", help="given a root and a log file, checks if the user (--by) has seen all the samples")
+def check():
+    pass
+
+
+@click.option("--by", required=True, help="the name to check for in the logs")
+@click.option("--root", default=".", help="source root to the Horos dataset")
+@click.option("--log", default=None,
+              help="if None, will check for val_contour_log.csv in root, else checks your specified log instead")
+@click.command("failed", help="check to log for which contours did not and in validation by the user")
+def check_failed(by: str, root: str, log: str):
+    click.echo(
+        f"checking for failed validations by {by} for dataset in {root}, using logfile {get_log_path(log, root)}")
+    log_df = get_log(log, root)
+    failed = log_df.apply(lambda row: not last_validation_was_successful(log_df,
+                                                                         by=row["by"],
+                                                                         ID=row["ID"],
+                                                                         contour_type=row["contour_type"],
+                                                                         frame=row["frame"],
+                                                                         slice=row["slice"]), axis=1)
+    result = log_df[(log_df["by"] == by) & failed]
+    result = result[["by", "ID", "contour_type", "frame", "slice", "remark"]].drop_duplicates()
+    with pd.option_context('display.max_rows', None, 'display.max_columns', None):  # more options can be specified also
+        click.echo(result)
+        click.echo("----------------")
+        click.echo(f"These samples did not end with confirmation by user {by}")
+
+
+@click.command("unobserved",
+               help="looks at the data and the logs to determine, which contours by which ID still needs to,"
+                    "be evalutated by a given user;")
+@click.option("--root", default=".", help="source root to the Horos dataset")
+@click.option("--log", default=None,
+              help="if None, will check for val_contour_log.csv in root, else checks your specified log instead")
+@click.option("--by", required=True, help="the name to check for in the logs")
+def check_unobserved(by: str, root: str, log: str):
+    click.echo(
+        f"checking, which contours have not been seen by user {by} yet in datasat at {root}, using log {get_log_path(log, root)}")
+    log_df = get_log(log, root)
+    combined_info_ = get_combined_info(root)
+    # combined_info_["ID", "contour_type"]
+    click.echo("Under construction")
+    return
+
+    # for g_n, group in combined_info_.groupby(["ID", "slice_type", "contour_type", "location_contour", "location_images"]):
+    #     click.echo(group)
+    #
+    # # TODO: open the contour to look for which frame / slice needs to be seen
+    # # TODO: filter by omega
+
+
+@click.command("existing", help="look at exsiting contours in root, given a number of contour types; will check"
+                                "for existing <contour_type>.xml files that are in child directories of root")
+@click.argument("contour_types", nargs=-1)
+@click.option("--n", default=None, type=int,
+              help="If passed, will check if a contour file has at least that many contours")
+@click.option("--root", default=".", help="source root to the Horos dataset")
+def check_existing_contours(contour_types: typing.List[str], root: str, n: Optional[int], ):
+    """currently terrible and unhelpful vaidation process... honestly using pytest was pretty king"""
+    click.echo(f"checking contour types {contour_types} in root: {root}...")
+    combined_info = get_combined_info(root)
+
+    click.echo("the following contours need to be rechecked: ")
+    # TODO: improve logging...
+    # really, pytest for this kinda stuff wasnt half bad
+    for contour_type in contour_types:
+        eligible = combined_info[
+            (combined_info["contour_type"] == contour_type) & (combined_info["location_contour"].notna())]
+        eligible.loc[:, "valid"] = eligible.apply(
+            lambda row: contour_is_valid(load_horos_contour(row["location_contour"], row["location_images"]), n),
+            axis=1)
+        click.echo(eligible[eligible["valid"] != True][["ID", "contour_path", "contour_type"]])
+
+    click.echo("finished checking")
+
+
+main.add_command(make_image_info_csv)
+main.add_command(make_contour_info_csv)
+main.add_command(rename_contour_files)
+main.add_command(validate)
+main.add_command(check)
+check.add_command(check_failed)
+check.add_command(check_unobserved)
+check.add_command(check_existing_contours)
+
+if __name__ == "__main__":
+    sys.exit(main())  # pragma: no cover
