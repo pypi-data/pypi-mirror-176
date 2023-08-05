baaa49c02a9e00551002e9c2ef73a7dd6b522077
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:14 +0000

    Add package hstream/0.0.11/hstream-0.0.11.tar.gz

---
+++ packages/hstream/hstream-0.0.11.tar.gz/hstream-0.0.11/hstream/runner.py
@@ -0,0 +1,23 @@
+import os
+import uvicorn
+from uvicorn import Server
+import click
+from pathlib import Path
+
+
+@click.command()
+@click.argument("user_filename")
+def run(user_filename="main"):
+    uvicorn.run(
+        f"{Path(user_filename).stem}:hs",
+        host="127.0.0.1",
+        port=8083,
+        reload="True",
+        factory=True,
+        app_dir=os.getcwd(),
+        reload_dirs=[os.getcwd()],
+    )
+
+
+if __name__ == "__main__":
+    run()
