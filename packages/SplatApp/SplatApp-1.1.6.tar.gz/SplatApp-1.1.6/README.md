2e639917d9269826cd0660d2b11f9135367b8547
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:55:56 +0000

    Add package SplatApp/1.1.6/SplatApp-1.1.6.tar.gz

---
+++ packages/SplatApp/SplatApp-1.1.6.tar.gz/SplatApp-1.1.6/README.md
@@ -0,0 +1,2 @@
+This module is created dy SplatCraft#5972 (check DISCORD)
+It enables you to create text based games, with customizable background, player mouvements (and soon CAMERA MOUVEMENTS)
---
+++ packages/SplatApp/SplatApp-1.1.6.tar.gz/SplatApp-1.1.6/SplatTextGame/splatcode.py
@@ -0,0 +1,83 @@
+import os, time, threading
+
+GAME = False
+
+class Screen:
+  class set:
+    def cam_size(weidth:int, height:int):
+      global camw, camh
+      camw = weidth
+      camh = height
+
+    def cam_pos(x:int, y:int):
+      global camx, camy
+      camx = x
+      camy = y
+
+    def background(bck_grnd:list):
+      global bck_sc
+      bck_sc = bck_grnd
+
+    def player_pos(x:int,y:int):
+      global playerx, playery
+      playerx = x
+      playery = y
+      
+  class background:
+    def show():
+      if not GAME:
+        pass
+      global playerx, playery ,bck_sc
+      by = 1
+      bx = 1
+      bck_lvl = ""
+      for todo1 in bck_sc:
+        for todo2 in todo1:
+          if bx == playerx and by == playery:
+            bck_lvl += "P"
+          else:
+            bck_lvl += todo2
+          bx += 1
+        bck_lvl += "\n"
+        by += 1
+        bx = 1
+      os.system("clear")
+      print(bck_lvl)
+  
+class Player:
+  def GoUp():
+    global playery
+    playery -= 1
+  def GoDown():
+    global playery
+    playery += 1
+  def GoLeft():
+    global playerx
+    playerx -= 1
+  def GoRight():
+    global playerx
+    playerx += 1
+
+class Game:  
+  def START():
+    global GAME
+    GAME = True
+    gl = threading.Thread(target=Game.do_not_open.game_loop)
+    gl.start()
+  
+  def STOP():
+    global GAME
+    GAME = False
+
+  class get:
+    def background():
+      global bck_sc
+      return bck_sc
+  
+  class do_not_open:
+    def game_loop():
+      while GAME:
+        time.sleep(0.6)
+        Screen.background.show()
+      print("Game has stopped")
+  
