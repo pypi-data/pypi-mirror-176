f37828d9918d05917a9b8dfbb87881141a5a7a23
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:28:51 +0000

    Add package duality/5.4.1/duality-5.4.1.tar.gz

---
+++ packages/duality/duality-5.4.1.tar.gz/duality-5.4.1/_testassets/D_inputdict.py
@@ -0,0 +1,12 @@
+# MISC PARTICLES AND A TEST FIELD.
+
+xy = {
+    'x' : int(input('Unesite broj: ')),
+    'y' : float(input('Unesite broj: ')),
+}
+
+
+def zbroj(x,y):
+    return print(x+y)
+
+zbroj(**xy)
---
+++ packages/duality/duality-5.4.1.tar.gz/duality-5.4.1/_testassets/T_staticWheel.py
@@ -0,0 +1,22 @@
+### TEST ENV ###
+import duality
+import time
+from logistics.plugins.coloring import paint_text
+
+app = duality.DualityApp()
+
+class Car:
+
+    @app.entry(option_name = 'change speed', option_description = 'this changes the speed of the car.', print_val = True)
+    def set_name(speed = 0, amount = app.store('amount', 'int', 'AMOUNT')):
+        speed += amount
+        return speed
+
+    @app.entry('show info', option_description = 'describes the car.', print_val = True)
+    def show_info():
+        return 'This would show some text in general.'
+
+if __name__ == '__main__':
+    paint_text(text = '\n>>> Executed on: ' + str(duality.__version__) + ' <<<', color = 'Fy', print_trigger = True)
+    time.sleep(3)
+    app.wheel(type = 'static')
