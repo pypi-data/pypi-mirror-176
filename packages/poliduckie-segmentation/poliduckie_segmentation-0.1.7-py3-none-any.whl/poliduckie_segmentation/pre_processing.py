df4963a9007e0f07e26ae144f30083255b9fc06d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:29:27 +0000

    Add package poliduckie-segmentation/0.1.7/poliduckie_segmentation-0.1.7-py3-none-any.whl

---
+++ packages/poliduckie-segmentation/poliduckie_segmentation-0.1.7-py3-none-any.whl/poliduckie_segmentation/pre_processing.py
@@ -0,0 +1,24 @@
+import cv2
+import pickle
+
+'''
+BirdEyeView HOW TO USE
+
+- Set the FILE_NAME to the pickle file containing the transformation matrix
+- Instance an object
+- Compute the transformation by calling obj.computeBirdEye(desired image)
+
+'''
+
+
+class BirdEyeView():
+
+    def __init__(self):
+        pkg_path = os.path.abspath(__file__)
+        model_path = os.path.join(os.path.dirname(pkg_path), 'BirdEyeCalibration', 'BirdEye.pkl')
+        file = open(model_path, 'rb')
+        self.Matrix = pickle.load(file)
+        pass
+
+    def computeBirdEye(self,img):
+        return cv2.warpPerspective(img, self.Matrix, img.shape[:2][::-1])
