4c3c3d5784c075bacba06d2d658f1545056f883a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:31:39 +0000

    Add package PySDM-examples/2.14/PySDM-examples-2.14.tar.gz

---
+++ packages/PySDM-examples/PySDM-examples-2.14.tar.gz/PySDM-examples-2.14/PySDM_examples/deJong_Mackay_2022/settings_0D.py
@@ -0,0 +1,41 @@
+import numpy as np
+from PySDM.dynamics.collisions.breakup_efficiencies import ConstEb
+from PySDM.dynamics.collisions.breakup_fragmentations import ExponFrag
+from PySDM.dynamics.collisions.coalescence_efficiencies import Berry1967
+from PySDM.dynamics.collisions.collision_kernels import Geometric
+from PySDM.formulae import Formulae
+from PySDM.initialisation.spectra import Exponential
+from PySDM.physics.constants import si
+from pystrict import strict
+
+
+@strict
+class Settings0D:
+    def __init__(self, seed: int = None):
+        self.formulae = Formulae(**({} if seed is None else {"seed": seed}))
+        self.n_sd = 2**10
+        self.n_part = 100 / si.cm**3
+        self.X0 = self.formulae.trivia.volume(radius=30.531 * si.micrometres)
+        self.frag_scale = self.formulae.trivia.volume(radius=100 * si.micrometres)
+        self.dv = 1 * si.m**3
+        self.norm_factor = self.n_part * self.dv
+        self.rho = 1000 * si.kilogram / si.metre**3
+        self.dt = 1 * si.seconds
+        self.adaptive = True
+        self.warn_overflows = True
+        self.seed = 44
+        self._steps = [0]
+        self.kernel = Geometric()
+        self.coal_eff = Berry1967()
+        self.fragmentation = ExponFrag(scale=self.frag_scale)
+        self.vmin = 0.0
+        self.break_eff = ConstEb(1.0)  # no "bouncing"
+        self.spectrum = Exponential(norm_factor=self.norm_factor, scale=self.X0)
+        self.radius_bins_edges = np.logspace(
+            np.log10(0.01 * si.um), np.log10(5000 * si.um), num=64, endpoint=True
+        )
+        self.radius_range = [0 * si.um, 1e6 * si.um]
+
+    @property
+    def output_steps(self):
+        return [int(step / self.dt) for step in self._steps]
---
+++ packages/PySDM-examples/PySDM-examples-2.14.tar.gz/PySDM-examples-2.14/setup.py
@@ -0,0 +1,42 @@
+import platform
+
+from setuptools import find_packages, setup
+
+
+def get_long_description():
+    with open("README.md", "r", encoding="utf8") as file:
+        long_description = file.read()
+    return long_description
+
+
+setup(
+    name="PySDM-examples",
+    description="PySDM usage examples reproducing results from literature "
+    "and depicting how to use PySDM from Python Jupyter notebooks",
+    use_scm_version={"local_scheme": lambda _: "", "version_scheme": "post-release"},
+    setup_requires=["setuptools_scm"],
+    install_requires=[
+        "PySDM",
+        "PyMPDATA",
+        "atmos-cloud-sim-uj-utils",
+        "pystrict",
+        "matplotlib",
+        "joblib",
+        "ipywidgets",
+        "seaborn",
+        "ghapi",
+    ]
+    + (["pyvinecopulib", "vtk"] if platform.architecture()[0] != "32bit" else []),
+    author="https://github.com/orgs/atmos-cloud-sim-uj/people",
+    author_email="sylwester.arabas@uj.edu.pl",
+    long_description=get_long_description(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/atmos-cloud-sim-uj/PySDM-examples",
+    license="GPL-3.0",
+    packages=find_packages(include=["PySDM_examples", "PySDM_examples.*"]),
+    project_urls={
+        "Tracker": "https://github.com/atmos-cloud-sim-uj/PySDM/issues",
+        "Documentation": "https://atmos-cloud-sim-uj.github.io/PySDM-examples",
+        "Source": "https://github.com/atmos-cloud-sim-uj/PySDM-examples",
+    },
+)
