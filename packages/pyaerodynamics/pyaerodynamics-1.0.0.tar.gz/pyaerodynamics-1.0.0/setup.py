64ec8235c222513a504bb9c817db7a57f133436e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:08:13 +0000

    Add package pyaerodynamics/1.0.0/pyaerodynamics-1.0.0.tar.gz

---
+++ packages/pyaerodynamics/pyaerodynamics-1.0.0.tar.gz/pyaerodynamics-1.0.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='pyaerodynamics',
+      version='1.0.0',
+      description='Aerodynamic calculation',
+      packages=['pyaerodynamics'],
+      author_email='rvasilikhin@gmail.com',
+      zip_safe=False)
