6d2b689a36c395d61c8f890172b6b9c9f90349ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:57:25 +0000

    Add package modal-client/0.40.250/modal_client-0.40.250-py3-none-any.whl

---
+++ packages/modal-client/modal_client-0.40.250-py3-none-any.whl/client_test/function_test.py
@@ -0,0 +1,233 @@
+# Copyright Modal Labs 2022
+import asyncio
+
+import cloudpickle
+import pytest
+import time
+
+from modal import Proxy, Stub
+from modal.functions import FunctionCall, gather
+from modal.stub import AioStub
+from modal_proto import api_pb2
+
+stub = Stub()
+
+
+@stub.function()
+def foo():
+    pass  # not actually used in test (servicer returns sum of square of all args)
+
+
+def test_run_function(client):
+    with stub.run(client=client):
+        assert foo(2, 4) == 20
+
+
+def test_map(client):
+    stub = Stub()
+
+    @stub.function
+    def dummy():
+        pass  # not actually used in test (servicer returns sum of square of all args)
+
+    with stub.run(client=client):
+        assert list(dummy.map([5, 2], [4, 3])) == [41, 13]
+        assert set(dummy.map([5, 2], [4, 3], order_outputs=False)) == {13, 41}
+
+
+def test_for_each(client, servicer):
+    stub = Stub()
+
+    res = 0
+
+    @stub.function
+    @servicer.function_body
+    def side_effect(_):
+        nonlocal res
+        res += 1
+
+    with stub.run(client=client):
+        side_effect.for_each(range(10))
+
+    assert res == 10
+
+
+def test_map_none_values(client, servicer):
+    stub = Stub()
+
+    @stub.function
+    @servicer.function_body
+    def custom_function(x):
+        if x % 2 == 0:
+            return x
+
+    with stub.run(client=client):
+        assert list(custom_function.map(range(4))) == [0, None, 2, None]
+
+
+def test_starmap(client):
+    stub = Stub()
+
+    @stub.function
+    def dummy():
+        pass  # not actually used in test (servicer returns sum of square of all args)
+
+    with stub.run(client=client):
+        assert list(dummy.starmap([[5, 2], [4, 3]])) == [29, 25]
+
+
+def test_function_memory_request(client):
+    stub = Stub()
+
+    @stub.function(memory=2048)
+    def f1():
+        pass
+
+
+def test_function_cpu_request(client):
+    stub = Stub()
+
+    @stub.function(cpu=2.0)
+    def f1():
+        pass
+
+
+def test_function_future(client, servicer):
+    stub = Stub()
+
+    @stub.function()
+    @servicer.function_body
+    def later():
+        return "hello"
+
+    with stub.run(client=client):
+        future = later.submit()
+        assert isinstance(future, FunctionCall)
+
+        servicer.function_is_running = True
+        assert future.object_id == "fc-1"
+
+        with pytest.raises(TimeoutError):
+            future.get(0.01)
+
+        servicer.function_is_running = False
+        assert future.get(0.01) == "hello"
+
+
+@pytest.mark.asyncio
+async def test_function_future_async(client, servicer):
+    stub = AioStub()
+
+    @stub.function()
+    @servicer.function_body
+    def later():
+        return "foo"
+
+    async with stub.run(client=client):
+        future = await later.submit()
+        servicer.function_is_running = True
+
+        with pytest.raises(TimeoutError):
+            await future.get(0.01)
+
+        servicer.function_is_running = False
+        assert await future.get(0.01) == "foo"
+
+
+@pytest.mark.asyncio
+async def test_generator_future(client, servicer):
+    stub = Stub()
+
+    @stub.generator()
+    def later():
+        yield "foo"
+
+    with stub.run(client=client):
+        assert later.submit() is None  # until we have a nice interface for polling generator futures
+
+
+def test_sync_parallelism(client, servicer):
+    stub = Stub()
+
+    @stub.function()
+    @servicer.function_body
+    async def slo1(sleep_seconds):
+        # need to use async function body in client test to run stuff in parallel
+        # but calling interface is still non-asyncio
+        await asyncio.sleep(sleep_seconds)
+        return sleep_seconds
+
+    with stub.run(client=client):
+        t0 = time.time()
+        # NOTE tests breaks in macOS CI if the smaller time is smaller than ~300ms
+        res = gather(slo1.submit(0.31), slo1.submit(0.3))
+        t1 = time.time()
+        assert res == [0.31, 0.3]  # results should be ordered as inputs, not by completion time
+        assert t1 - t0 < 0.6  # less than the combined runtime, make sure they run in parallel
+
+
+def test_proxy(client, servicer):
+    stub = Stub()
+
+    @stub.function(proxy=Proxy.from_name("my-proxy"))
+    def f():
+        pass
+
+    with stub.run(client=client):
+        pass
+
+
+class CustomException(Exception):
+    pass
+
+
+def test_function_exception(client, servicer):
+    stub = Stub()
+
+    @stub.function
+    @servicer.function_body
+    def failure():
+        raise CustomException("foo!")
+
+    with stub.run(client=client):
+        with pytest.raises(CustomException) as excinfo:
+            failure()
+        assert "foo!" in str(excinfo.value)
+
+
+def test_function_relative_import_hint(client, servicer):
+    stub = Stub()
+
+    @stub.function
+    @servicer.function_body
+    def failure():
+        raise ImportError("attempted relative import with no known parent package")
+
+    with stub.run(client=client):
+        with pytest.raises(ImportError) as excinfo:
+            failure()
+        assert "HINT" in str(excinfo.value)
+
+
+lifecycle_stub = Stub()
+
+
+class Foo:
+    bar = "hello"
+
+    @lifecycle_stub.function(serialized=True)
+    def run(self):
+        return self.bar
+
+
+def test_serialized_function_includes_lifecycle_class(client, servicer):
+    with lifecycle_stub.run(client=client):
+        pass
+
+    assert len(servicer.app_functions) == 1
+    func_def = next(iter(servicer.app_functions.values()))
+    assert func_def.definition_type == api_pb2.Function.DEFINITION_TYPE_SERIALIZED
+
+    func = cloudpickle.loads(func_def.function_serialized)
+    cls = cloudpickle.loads(func_def.class_serialized)
+    assert func(cls()) == "hello"
---
+++ packages/modal-client/modal_client-0.40.250-py3-none-any.whl/modal/cli/__init__.py
@@ -0,0 +1,10 @@
+# Copyright Modal Labs 2022
+from .entry_point import entrypoint_cli
+
+
+def main():
+    entrypoint_cli()
+
+
+if __name__ == "__main__":
+    main()
