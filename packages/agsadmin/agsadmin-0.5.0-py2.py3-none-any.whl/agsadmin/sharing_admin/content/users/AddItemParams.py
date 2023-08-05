eb14b00816fe670a6c55ebc23f03ac43f01febb8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 16:58:35 +0000

    Add package agsadmin/0.5.0/agsadmin-0.5.0-py2.py3-none-any.whl

---
+++ packages/agsadmin/agsadmin-0.5.0-py2.py3-none-any.whl/agsadmin/sharing_admin/content/users/AddItemParams.py
@@ -0,0 +1,20 @@
+from __future__ import (absolute_import, division, print_function, unicode_literals)
+from builtins import (ascii, bytes, chr, dict, filter, hex, input, int, map, next, oct, open, pow, range, round, str,
+                      super, zip)
+
+from agsadmin.sharing_admin.content.users.UpdateItemParams import UpdateItemParams
+
+# local imports
+from ..ItemType import ItemType
+from .UpdateItemParams import UpdateItemParams
+
+
+class AddItemParams(UpdateItemParams):
+    """Holds parameter values for the "addItems" user content operation."""
+    @property
+    def type(self):
+        self._get_nullable_enum("type", ItemType)
+
+    @type.setter
+    def type(self, value):
+        self._set_nullable_enum("type", value, ItemType)
