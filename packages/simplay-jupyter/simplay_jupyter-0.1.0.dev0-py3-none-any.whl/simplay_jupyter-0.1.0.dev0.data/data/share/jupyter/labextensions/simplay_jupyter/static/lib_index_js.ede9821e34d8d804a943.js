f6db5a52e7ac4f605c603e0435e17ae105557d77
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:25 +0000

    Add package simplay-jupyter/0.1.0.dev0/simplay_jupyter-0.1.0.dev0-py3-none-any.whl

---
+++ packages/simplay-jupyter/simplay_jupyter-0.1.0.dev0-py3-none-any.whl/simplay_jupyter-0.1.0.dev0.data/data/share/jupyter/labextensions/simplay_jupyter/static/lib_index_js.ede9821e34d8d804a943.js
@@ -0,0 +1,31 @@
+"use strict";
+(self["webpackChunksimplay_jupyter"] = self["webpackChunksimplay_jupyter"] || []).push([["lib_index_js"],{
+
+/***/ "./lib/index.js":
+/*!**********************!*\
+  !*** ./lib/index.js ***!
+  \**********************/
+/***/ (function(__unused_webpack_module, exports, __webpack_require__) {
+
+
+// Copyright (c) team-simplay
+// Distributed under the terms of the Modified BSD License.
+var __createBinding = (this && this.__createBinding) || (Object.create ? (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    Object.defineProperty(o, k2, { enumerable: true, get: function() { return m[k]; } });
+}) : (function(o, m, k, k2) {
+    if (k2 === undefined) k2 = k;
+    o[k2] = m[k];
+}));
+var __exportStar = (this && this.__exportStar) || function(m, exports) {
+    for (var p in m) if (p !== "default" && !Object.prototype.hasOwnProperty.call(exports, p)) __createBinding(exports, m, p);
+};
+Object.defineProperty(exports, "__esModule", ({ value: true }));
+__exportStar(__webpack_require__(/*! ./version */ "./lib/version.js"), exports);
+__exportStar(__webpack_require__(/*! ./widget */ "./lib/widget.js"), exports);
+//# sourceMappingURL=index.js.map
+
+/***/ })
+
+}]);
+//# sourceMappingURL=lib_index_js.ede9821e34d8d804a943.js.map
