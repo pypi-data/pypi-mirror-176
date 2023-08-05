190d5a98961b0eee4c7c426673d2438f826eca07
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:09:58 +0000

    Add package vector-quantize-pytorch/0.10.9/vector_quantize_pytorch-0.10.9-py3-none-any.whl

---
+++ packages/vector-quantize-pytorch/vector_quantize_pytorch-0.10.9-py3-none-any.whl/vector_quantize_pytorch/residual_vq.py
@@ -0,0 +1,111 @@
+from functools import partial
+from random import randrange
+
+import torch
+from torch import nn
+from vector_quantize_pytorch.vector_quantize_pytorch import VectorQuantize
+
+from einops import rearrange, repeat
+
+class ResidualVQ(nn.Module):
+    """ Follows Algorithm 1. in https://arxiv.org/pdf/2107.03312.pdf """
+    def __init__(
+        self,
+        *,
+        num_quantizers,
+        shared_codebook = False,
+        heads = 1,
+        quantize_dropout = False,
+        quantize_dropout_cutoff_index = 0,
+        **kwargs
+    ):
+        super().__init__()
+        assert heads == 1, 'residual vq is not compatible with multi-headed codes'
+
+        self.num_quantizers = num_quantizers
+
+        self.layers = nn.ModuleList([VectorQuantize(**kwargs) for _ in range(num_quantizers)])
+
+        self.quantize_dropout = quantize_dropout
+
+        assert quantize_dropout_cutoff_index >= 0
+        self.quantize_dropout_cutoff_index = quantize_dropout_cutoff_index
+
+        if not shared_codebook:
+            return
+
+        first_vq, *rest_vq = self.layers
+        codebook = first_vq._codebook
+
+        for vq in rest_vq:
+            vq._codebook = codebook
+
+    @property
+    def codebooks(self):
+        codebooks = [layer._codebook.embed for layer in self.layers]
+        codebooks = torch.stack(codebooks, dim = 0)
+        codebooks = rearrange(codebooks, 'q 1 c d -> q c d')
+        return codebooks
+
+    def get_codes_from_indices(self, indices):
+        batch = indices.shape[0]
+        codebooks = repeat(self.codebooks, 'q c d -> q b c d', b = batch)
+        gather_indices = repeat(indices, 'b n q -> q b n d', d = codebooks.shape[-1])
+
+        # take care of quantizer dropout
+        mask = gather_indices == -1.
+        gather_indices = gather_indices.masked_fill(mask, 0) # have it fetch a dummy code to be masked out later
+
+        all_codes = codebooks.gather(2, gather_indices) # gather all codes
+
+        # mask out any codes that were dropout-ed
+        all_codes = all_codes.masked_fill(mask, 0.)
+        return all_codes
+
+    def forward(
+        self,
+        x,
+        return_all_codes = False
+    ):
+        b, n, *_, num_quant, device = *x.shape, self.num_quantizers, x.device
+
+        quantized_out = 0.
+        residual = x
+
+        all_losses = []
+        all_indices = []
+
+        should_quantize_dropout = self.training and self.quantize_dropout
+
+        if should_quantize_dropout:
+            rand_quantize_dropout_index = randrange(self.quantize_dropout_cutoff_index, num_quant)
+
+        for quantizer_index, layer in enumerate(self.layers):
+
+            if should_quantize_dropout and quantizer_index > rand_quantize_dropout_index:
+                null_indices = torch.full((b, n), -1., device = device, dtype = torch.long)
+                null_loss = torch.full((b,), 0., device = device, dtype = x.dtype)
+
+                all_indices.append(null_indices)
+                all_losses.append(null_loss)
+                continue
+
+            quantized, indices, loss = layer(residual)
+            residual = residual - quantized
+            quantized_out = quantized_out + quantized
+
+            all_indices.append(indices)
+            all_losses.append(loss)
+
+        all_losses, all_indices = map(partial(torch.stack, dim = -1), (all_losses, all_indices))
+
+        ret = (quantized_out, all_indices, all_losses)
+
+        if return_all_codes:
+            # whether to return all codes from all codebooks across layers
+            all_codes = self.get_codes_from_indices(all_indices)
+
+            # will return all codes in shape (quantizer, batch, sequence length, codebook dimension)
+            ret = (*ret, all_codes)
+
+        return ret
