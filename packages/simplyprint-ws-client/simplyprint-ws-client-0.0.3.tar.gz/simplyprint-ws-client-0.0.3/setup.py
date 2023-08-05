1e9e1b987594399ca565d338abe8e5eefc08bc43
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:59 +0000

    Add package simplyprint-ws-client/0.0.3/simplyprint-ws-client-0.0.3.tar.gz

---
+++ packages/simplyprint-ws-client/simplyprint-ws-client-0.0.3.tar.gz/simplyprint-ws-client-0.0.3/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+from simplyprint_ws_client import VERSION
+
+setup(
+    name="simplyprint-ws-client",
+    version=VERSION,
+    description="SimplyPrint WebSocket Client",
+    url="https://github.com/SimplyPrint/printer-ws-client",
+    author="SimplyPrint",
+    license="AGPLv3",
+    packages=["simplyprint_ws_client"],
+    install_requires=[
+        "tornado",
+        "psutil",
+        "sentry-sdk",
+        "netifaces",
+        "requests",
+    ]
+)
