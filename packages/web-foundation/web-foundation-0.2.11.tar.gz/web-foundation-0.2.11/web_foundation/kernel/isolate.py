d80646a71aff6e8f40eae5f75db2b0f3a38f61af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:11:02 +0000

    Add package web-foundation/0.2.11/web-foundation-0.2.11.tar.gz

---
+++ packages/web-foundation/web-foundation-0.2.11.tar.gz/web-foundation-0.2.11/web_foundation/kernel/isolate.py
@@ -0,0 +1,51 @@
+import asyncio
+import signal
+from abc import ABCMeta
+from typing import Dict, Generic
+
+import loguru
+from sanic import Sanic
+from sanic.worker.multiplexer import WorkerMultiplexer
+
+from web_foundation.extentions.sanic_ext_worker import ExtWorker
+from web_foundation.kernel.container import GenericDependencyContainer
+from web_foundation.kernel.messaging.channel import IChannel
+
+
+class Isolate(Generic[GenericDependencyContainer], metaclass=ABCMeta):
+    name: str
+    channel: IChannel | None
+    container: GenericDependencyContainer
+    init_kwargs: Dict
+
+    def __init__(self, name: str, container: GenericDependencyContainer, **kwargs):
+        self.name = name
+        self.container = container
+        self.channel = IChannel(f"Channel_{name}")
+        self.init_kwargs = kwargs
+
+    async def work(self, **kwargs) -> None:
+        raise NotImplementedError
+
+    def run_forked(self) -> None:
+        async def call():
+            nonlocal self
+            await self.container.init_resources()
+            asyncio.create_task(self.channel.listen_consume())
+            await self.work(**self.init_kwargs)
+            await self.container.shutdown_resources()
+
+        def signal_handler(sig, frame):
+            for task in asyncio.all_tasks():
+                task.cancel()
+
+        signal.signal(signal.SIGINT, signal_handler)
+
+        try:
+            asyncio.run(call())
+        except KeyboardInterrupt:
+            asyncio.run(self.container.shutdown_resources())
+            self.close()
+
+    def close(self):
+        pass
