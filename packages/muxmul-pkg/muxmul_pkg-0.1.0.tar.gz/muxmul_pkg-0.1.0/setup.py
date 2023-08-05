49fdce9e9a20a3c8a9e729118e8b5512faf9e8eb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-05 18:10:45 +0000

    Add package muxmul-pkg/0.1.0/muxmul_pkg-0.1.0.tar.gz

---
+++ packages/muxmul-pkg/muxmul_pkg-0.1.0.tar.gz/muxmul_pkg-0.1.0/setup.py
@@ -0,0 +1,13 @@
+import setuptools
+
+with open("README.md","r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="muxmul_pkg",
+    version="0.1.0",
+    author="Qiu Chao",
+    author_email="2644185154@qq.com",
+    packages=setuptools.find_packages(),
+
+)
