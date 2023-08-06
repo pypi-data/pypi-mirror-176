86b5b652e6561ee5e842ccf304dbc7523008c9f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:43:59 +0000

    Add package selenium-captcha-solver/0.0.3/selenium-captcha-solver-0.0.3.tar.gz

---
+++ packages/selenium-captcha-solver/selenium-captcha-solver-0.0.3.tar.gz/selenium-captcha-solver-0.0.3/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="selenium-captcha-solver",
+    version="0.0.3",
+    author="Improvs Tech",
+    author_email="improvs.tech@gmail.com",
+    description="Selenium captcha solver",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/KofeinTech/selenium-captcha",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    packages=setuptools.find_packages(),
+)
