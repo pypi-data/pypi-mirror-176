d3948dbe9c22e1f33570821e243fc83d2a460523
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:38:24 +0000

    Add package visual-sponge/0.1/visual_sponge-0.1.tar.gz

---
+++ packages/visual-sponge/visual_sponge-0.1.tar.gz/visual_sponge-0.1/setup.py
@@ -0,0 +1,20 @@
+from setuptools import setup, Extension
+from setuptools.command.egg_info import egg_info
+
+class MyEggInfo(egg_info):
+    def run(self):
+        super().run()
+        self.filelist.include("visual_sponge/static/*")
+        self.filelist.include("visual_sponge/templates/*")
+
+setup(name="visual_sponge",
+      version="0.1",
+      description="A python package to do the visualization for molecular simulations",
+      author="Yijie Xia",
+      author_email="yijiexia@pku.edu.cn",
+      install_requires = [""],
+      cmdclass={"egg_info": MyEggInfo},
+      long_description=open('README.md').read(),
+      entry_points = {
+        "console_scripts": ["vsponge = visual_sponge.__main__:main"]},
+      )
---
+++ packages/visual-sponge/visual_sponge-0.1.tar.gz/visual_sponge-0.1/visual_sponge/backend.py
@@ -0,0 +1,67 @@
+"""
+This module starts the backend of the web
+"""
+import os
+import sys
+from io import StringIO
+import traceback
+
+from flask import Flask, render_template, send_from_directory, request, jsonify
+import webview
+
+from . import MACROS
+from .commands import *
+
+
+def run():
+    app = Flask(MACROS.PACKAGE,
+                template_folder=os.path.join(os.path.dirname(__file__), "templates"),
+                static_folder=os.path.join(os.path.dirname(__file__), "static"))
+
+    @app.template_filter('localization')
+    def localization(key):
+        return MACROS.CURRENT_LANGUAGE.get(key, key)
+
+    @app.route('/favicon.ico')
+    def favicon():
+        return send_from_directory(os.path.join(os.path.dirname(__file__), "static"),
+                                   path='favicon.ico',
+                                   mimetype='image/vnd.microsft.icon')
+
+    @app.route('/')
+    def index():
+        return render_template("index.html")
+
+    @app.route("/cmd", methods=['POST'])
+    def cmd():
+        MACROS.CMD = None
+        MACROS.TEXT = ""
+        MACROS.TEMP = None
+        value = request.form["value"]
+        if not value:
+            return jsonify({"text":""})
+        try:
+            try:
+                exec(f"MACROS.TEMP = {value}", globals())
+                if MACROS.TEMP is not None:
+                    MACROS.TEXT += f"{MACROS.TEMP}"
+            except SyntaxError:
+                exec(value, globals())
+            return jsonify({"text":MACROS.TEXT, "cmd":MACROS.CMD})
+        except:
+            return jsonify({"text":f"{traceback.format_exc()}"})
+
+    MACROS.APP = app
+    app.json_encoder = MACROS.JSONEncoder
+
+    if not MACROS.DEBUG_MODE:
+        stream = StringIO()
+        sys.stderr = stream
+        webview.create_window(title = MACROS.PACKAGE, 
+                              url = app)
+        webview.start(localization=MACROS.CURRENT_LANGUAGE)
+    else:
+        webview.create_window(title = MACROS.PACKAGE, 
+                      url = app)
+        webview.start(localization=MACROS.CURRENT_LANGUAGE,
+                      debug=True)
