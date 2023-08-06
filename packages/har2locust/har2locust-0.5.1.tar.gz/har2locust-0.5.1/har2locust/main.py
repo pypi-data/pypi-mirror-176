8b2bdfebc1e706235141dc4428c4e122b03086ff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:04:48 +0000

    Add package har2locust/0.5.1/har2locust-0.5.1.tar.gz

---
+++ packages/har2locust/har2locust-0.5.1.tar.gz/har2locust-0.5.1/har2locust/main.py
@@ -0,0 +1,170 @@
+import importlib
+import json
+import logging
+import os
+import pathlib
+import ast
+import sys
+from typing import List
+from urllib.parse import urlsplit
+import jinja2
+from .argument_parser import get_parser
+from .plugin import astprocessor, entriesprocessor, valuesprocessor, outputstringprocessor
+
+
+def cli():
+    args = get_parser().parse_args()
+    logging.basicConfig(level=args.loglevel.upper())
+    main(
+        args.input,
+        plugins=args.plugins.split(",") if args.plugins else [],
+        resource_type=args.resource_types.split(","),
+        template_name=args.template,
+    )
+
+
+def main(
+    har_file: str,
+    plugins: List[str] = [],
+    resource_type=["xhr", "document", "other"],
+    template_name="locust.jinja2",
+):
+    har_path = pathlib.Path(har_file)
+
+    # build safe class name from filename
+    name = pathlib.Path(har_file).stem.replace("-", "_").replace(".", "_")
+
+    with open(har_path, encoding="utf8", errors="ignore") as f:
+        har = json.load(f)
+    logging.debug(f"loaded {har_path}")
+
+    default_plugins = [
+        "har2locust/plugins/rest.py",
+        "har2locust/plugins/urlignore.py",
+        "har2locust/plugins/headerignore.py",
+        "har2locust/plugins/black.py",
+    ]
+    default_and_extra_plugins = default_plugins + plugins
+    for plugin in default_and_extra_plugins:
+        sys.path.append(os.path.curdir)
+        import_path = plugin.replace("/", ".").rstrip(".py")
+        importlib.import_module(import_path)
+    logging.debug(f"loaded plugins {default_and_extra_plugins}")
+
+    pp_dict = process(har, resource_type=resource_type)
+    py = rendering(template_name, {"name": name, **pp_dict})
+    print(py)
+
+
+def process(
+    har: dict,
+    resource_type=["xhr", "document", "other"],
+) -> dict:
+    """Scan the har dict for common headers
+
+    In doing so request and reponse variables are organized in a useful format:
+    from [[{'name': key, 'value': value}, ...], ...] list of list of dict
+    to   [{(key, value), ...}, ...] list of set of tuple.
+    Moreover requests can be filter by resource type.
+
+    Args:
+        har (dict): the dict obtain by parsing har file with json
+        resource_type (list): list of resource type to include in the python
+            generated code. Supported type are `xhr`, `script`, `stylesheet`,
+            `image`, `font`, `document`, `other`.
+    """
+    supported_resource_type = {
+        "xhr",
+        "script",
+        "stylesheet",
+        "image",
+        "font",
+        "document",
+        "other",
+    }
+    if unsupported := set(resource_type) - supported_resource_type:
+        raise NotImplementedError(f"{unsupported} resource types are not supported")
+
+    har_version = har["log"]["version"]
+    logging.debug(f'har log version is "{har_version}"')
+    if har_version != "1.2":
+        logging.warning(f"Untested har version {har_version}")
+
+    entries = har["log"]["entries"]
+    logging.debug(f"found {len(entries)} entries")
+
+    # filtering entries
+    entries = [e for e in har["log"]["entries"] if e["_resourceType"] in resource_type]
+
+    for e in entries:
+        # set defaults
+        e["request"]["fname"] = "client.request"
+        e["request"]["extraparams"] = [("catch_response", True)]
+
+    for p in entriesprocessor.processors:
+        p(entries)
+
+    logging.debug(f"{resource_type=}")
+    logging.debug(f"{len(entries)} entries after filtering by resource_type")
+
+    headers_req, headers_res = [], []
+    for e in entries:
+        headers_req.append({(h["name"], h["value"]) for h in e["request"]["headers"]})
+        headers_res.append({(h["name"], h["value"]) for h in e["response"]["headers"]})
+
+    # collect headers common to all requests
+    default_headers = set.intersection(*headers_req)
+
+    urlparts = urlsplit(entries[0]["request"]["url"])
+    host = f"{urlparts.scheme}://{urlparts.netloc}/"
+    for i, e in enumerate(entries):
+        r = e["request"]
+        r["url"] = r["url"].removeprefix(host)
+        r["headers"] = sorted(headers_req[i] - default_headers, key=lambda item: item[0])
+
+    logging.debug("preprocessed har dict")
+
+    # return the "values"-dict for use in rendering
+    return dict(host=host, default_headers=sorted(default_headers), entries=entries)
+
+
+def rendering(template_name: str, values: dict) -> str:
+    for p in valuesprocessor.processors:
+        p(values)
+    logging.debug("valueprocessors applied")
+
+    logging.debug(f'about to load template "{template_name}"')
+    if pathlib.Path(template_name).exists():
+        template_path = pathlib.Path(template_name)
+    else:
+        template_path = pathlib.Path(__file__).parents[0].joinpath(pathlib.Path(template_name))
+        if not template_path.exists():
+            raise Exception(f"Template {template_name} does not exist, neither in current directory nor as built in")
+
+    template_dir = template_path.parents[0]
+
+    env = jinja2.Environment(loader=jinja2.FileSystemLoader(template_dir))
+    template = env.get_template(template_path.name)
+    logging.debug("template loaded")
+
+    py = template.render(values)
+    logging.debug("template rendered")
+
+    try:
+        tree = ast.parse(py, type_comments=True)
+    except SyntaxError as e:
+        logging.debug(py)
+        levelmessage = " (set log level DEBUG to see the whole output)" if logging.DEBUG < logging.root.level else ""
+        logging.error(f"{e.msg} when parsing rendered template{levelmessage}")
+        raise
+
+    for p in astprocessor.processors:
+        p(tree, values)
+    py = ast.unparse(ast.fix_missing_locations(tree))
+    logging.debug("astprocessors applied")
+
+    for p in outputstringprocessor.processors:
+        py = p(py)
+    logging.debug("outputstringprocessors applied")
+
+    return py
---
+++ packages/har2locust/har2locust-0.5.1.tar.gz/har2locust-0.5.1/tests/test_har2locust.py
@@ -0,0 +1,113 @@
+# pylint: disable=redefined-outer-name
+import json
+import pathlib
+import subprocess
+import os
+import pytest
+import re
+from har2locust.main import main, process, rendering
+
+inputs_dir = pathlib.Path(__file__).parents[0] / "inputs"
+outputs_dir = pathlib.Path(__file__).parents[0] / "outputs"
+
+har_files = list(inputs_dir.glob("*.har"))
+py_files = [outputs_dir / f.with_suffix(".py").name for f in har_files]
+
+
+with open(inputs_dir / "login.har") as f:
+    har = json.load(f)
+
+
+def test_har_file_not_found():
+    har_file_foo = inputs_dir / "foo.har"
+    with pytest.raises(FileNotFoundError):
+        main(str(har_file_foo))
+
+
+def test_preprocessing_unsupported_resource_type():
+    with pytest.raises(NotImplementedError, match="are not supported"):
+        process(har, resource_type=["xhr", "foo", "bar"])
+
+
+def test_rendering_syntax_error():
+    with pytest.raises(
+        SyntaxError,
+        match=re.escape("invalid syntax (<unknown>, line 1)"),
+    ):
+        main(str(inputs_dir / "login.har"), template_name="tests/broken_template.jinja2")
+
+
+def test_rendering_missing_template():
+    with pytest.raises(
+        Exception,
+        match="Template this_doesnt_exist.jinja2 does not exist, neither in current directory nor as built in",
+    ):
+        rendering("this_doesnt_exist.jinja2", process(har))
+
+
+# writing py file in tests/output for manual inspection
+@pytest.mark.parametrize("har_file, py_file", zip(har_files, py_files))
+def test_output(har_file, py_file):
+    with open(py_file, encoding="utf-8") as f:
+        expected_output = f.read()
+    proc = subprocess.Popen(
+        ["har2locust", har_file],
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        text=True,
+        encoding="utf-8",
+        cwd=os.path.join(os.path.dirname(__file__), "../"),
+    )
+    stdout, stderr = proc.communicate()
+    assert proc.returncode == 0, f"Bad return code {proc.returncode}, stderr: {stderr}"
+    assert stdout.strip() == expected_output.strip()
+
+
+def test_helptext():
+    proc = subprocess.Popen(
+        ["har2locust", "--help"],
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        text=True,
+        encoding="utf-8",
+    )
+    stdout, stderr = proc.communicate()
+    assert proc.returncode == 0, f"Bad return code {proc.returncode}, stderr: {stderr}"
+    assert "usage: har2locust" in stdout
+
+
+def test_plugins():
+    har_file = "tests/inputs/login.har"
+    py_file = "tests/outputs/login_plugin.py"
+    with open(py_file, encoding="utf-8") as f:
+        expected_output = f.read()
+    proc = subprocess.Popen(
+        ["har2locust", har_file, "--plugins", "tests/plugin_example.py"],
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        text=True,
+        encoding="utf-8",
+        cwd=os.path.join(os.path.dirname(__file__), "../"),
+    )
+    stdout, stderr = proc.communicate()
+    assert proc.returncode == 0, f"Bad return code {proc.returncode}, stderr: {stderr}"
+    assert stdout.strip() == expected_output.strip()
+    assert "self.reader.user" in stdout
+    assert "self.customer" in stdout
+    # test url timestamp rewriting function
+    assert "self.rest_" in stdout
+    assert "&_" not in stdout
+
+
+# this test is intended to be run AFTER regenerating the output using make update_tests
+def test_locust_run():
+    proc = subprocess.Popen(
+        ["locust", "-f", "outputs/reqres.in.py", "-i", "1", "--headless"],
+        stderr=subprocess.PIPE,
+        text=True,
+        encoding="utf-8",
+        cwd=os.path.join(os.path.dirname(__file__)),
+    )
+    _, stderr = proc.communicate()
+    assert proc.returncode == 0, f"Bad return code {proc.returncode}, stderr: {stderr}"
+    assert "Iteration limit reached" in stderr, stderr
