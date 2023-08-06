1a928f931a74370ff6a04f25f3cc816bc6012f2b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:11:42 +0000

    Add package document-insighter/0.0.7/document-insighter-0.0.7.tar.gz

---
+++ packages/document-insighter/document-insighter-0.0.7.tar.gz/document-insighter-0.0.7/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="document-insighter",
+    version="0.0.7",
+    author="Wang Kai",
+    author_email="wangkai@godeepsite.com",
+    description="Document Insighter Python Client",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/deepsite/document-insighter-python",
+    project_urls={
+        "Bug Tracker": "https://github.com/deepsite/document-insighter-python/issues"
+    },
+    license="MIT",
+    packages=find_packages(exclude=("tests*",)),
+    install_requires=["requests-oauthlib==1.3.1", "dataclasses-json==0.5.7", "polling2==0.5.0"],
+)
