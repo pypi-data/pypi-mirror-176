779694709bae603328f1de0fd6172496fdd75b18
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:11:31 +0000

    Add package survlime/0.1.4/survlime-0.1.4.tar.gz

---
+++ packages/survlime/survlime-0.1.4.tar.gz/survlime-0.1.4/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="survlime",
+    version="0.1.4",
+    description="Survival adaptation of the LIME algorithm",
+    package_dir={"": "."},
+    packages=find_packages(),
+    install_requires=["numpy", "cvxpy", "scikit-survival", "scikit-learn", "pandas"],
+    extras_require={"dev": ["pytest"]},
+    test_suite="tests",
+    url='https://github.com/imatge-upc/SurvLIME',
+    include_package_data=True,
+    author='Carlos Hernández Pérez, Cristian Pachón García',
+    author_email='crherperez95@gmail.com, cc.pachon@gmail.com'
+)
