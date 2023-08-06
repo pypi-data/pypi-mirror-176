ad03322c1c23fa543acdf1adbd351895f445ffac
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:18:39 +0000

    Add package multidimio/0.2.4/multidimio-0.2.4.tar.gz

---
+++ packages/multidimio/multidimio-0.2.4.tar.gz/multidimio-0.2.4/src/mdio/__init__.py
@@ -0,0 +1,22 @@
+"""MDIO library."""
+
+
+from importlib import metadata
+
+from mdio.api import MDIOReader
+from mdio.api import MDIOWriter
+from mdio.api.convenience import copy_mdio
+from mdio.converters import mdio_to_segy
+from mdio.converters import segy_to_mdio
+
+
+__all__ = [
+    "MDIOReader",
+    "MDIOWriter",
+    "copy_mdio",
+    "mdio_to_segy",
+    "segy_to_mdio",
+]
+
+
+__version__ = metadata.version("multidimio")
