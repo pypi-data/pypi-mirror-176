6a2a17d4b3c76974cae9b908effc802df94d0df0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:23:52 +0000

    Add package makefilepy/0.0.1/makefilepy-0.0.1.tar.gz

---
+++ packages/makefilepy/makefilepy-0.0.1.tar.gz/makefilepy-0.0.1/makefilepy/Variable.py
@@ -0,0 +1,11 @@
+from typing import Any
+
+from pydantic import BaseModel
+
+
+class Variable(BaseModel):
+    key: str
+    value: Any
+
+    def build(self) -> str:
+        return f'{self.key}={self.value}'
