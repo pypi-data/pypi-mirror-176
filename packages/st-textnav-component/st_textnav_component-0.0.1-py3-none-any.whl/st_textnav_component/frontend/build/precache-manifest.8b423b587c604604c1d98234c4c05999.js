062755fa0ebe05f29a880991d0ef5cf11af8b8ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:33:23 +0000

    Add package st-textnav-component/0.0.1/st_textnav_component-0.0.1-py3-none-any.whl

---
+++ packages/st-textnav-component/st_textnav_component-0.0.1-py3-none-any.whl/st_textnav_component/frontend/build/precache-manifest.8b423b587c604604c1d98234c4c05999.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4afe42d420a6858ff916507f9a80a95f",
+    "url": "./index.html"
+  },
+  {
+    "revision": "9da47ac6bbd27b15fc4b",
+    "url": "./static/js/2.ec6e7411.chunk.js"
+  },
+  {
+    "revision": "129725115fe778ca4356862cdbd81508",
+    "url": "./static/js/2.ec6e7411.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8f860150960159641b3b",
+    "url": "./static/js/main.c0c5ac3d.chunk.js"
+  },
+  {
+    "revision": "788696aa9cc71262340a",
+    "url": "./static/js/runtime-main.5875292e.js"
+  }
+]);
