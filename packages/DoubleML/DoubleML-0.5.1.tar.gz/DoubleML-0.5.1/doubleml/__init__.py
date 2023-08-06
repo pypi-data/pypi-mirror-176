1cfff8f98873c5970299ddf44b2ec84fc1b71c68
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:12:59 +0000

    Add package DoubleML/0.5.1/DoubleML-0.5.1.tar.gz

---
+++ packages/DoubleML/DoubleML-0.5.1.tar.gz/DoubleML-0.5.1/doubleml/__init__.py
@@ -0,0 +1,16 @@
+from pkg_resources import get_distribution
+
+from .double_ml_plr import DoubleMLPLR
+from .double_ml_pliv import DoubleMLPLIV
+from .double_ml_irm import DoubleMLIRM
+from .double_ml_iivm import DoubleMLIIVM
+from .double_ml_data import DoubleMLData, DoubleMLClusterData
+
+__all__ = ['DoubleMLPLR',
+           'DoubleMLPLIV',
+           'DoubleMLIRM',
+           'DoubleMLIIVM',
+           'DoubleMLData',
+           'DoubleMLClusterData']
+
+__version__ = get_distribution('doubleml').version
