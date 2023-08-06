412d323acdd9f5994a15020f35f2f9ed8ee907b9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:05:16 +0000

    Add package sklearn-evaluation/0.7.9/sklearn_evaluation-0.7.9-py3-none-any.whl

---
+++ packages/sklearn-evaluation/sklearn_evaluation-0.7.9-py3-none-any.whl/sklearn_evaluation/__init__.py
@@ -0,0 +1,18 @@
+__version__ = '0.7.9'
+
+from .evaluator import ClassifierEvaluator
+from .nb.NotebookIntrospector import NotebookIntrospector
+from .nb.NotebookCollection import NotebookCollection
+from .nb.NotebookDatabase import NotebookDatabase
+from .SQLiteTracker import SQLiteTracker
+from .telemetry import telemetry
+
+__all__ = [
+    'ClassifierEvaluator',
+    'NotebookIntrospector',
+    'SQLiteTracker',
+    'NotebookCollection',
+    'NotebookDatabase',
+]
+
+telemetry.log_api('imported')
