4a95ef81705c9325f320f6718acb60023d82236f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:13:07 +0000

    Add package kadaptest7/0.0.1/kadaptest7-0.0.1-py3-none-any.whl

---
+++ packages/kadaptest7/kadaptest7-0.0.1-py3-none-any.whl/kadaptest6/data.py
@@ -0,0 +1,14 @@
+from kadaptest6.vin_decoder.decoder import Decode
+from kadaptest6.retained.retained import Retained
+
+from kadaptest6.linked.bicycle import Bicycle
+from kadaptest6.linked.child import Child
+from kadaptest6.linked.city import City
+from kadaptest6.linked.freezing import Freezing
+from kadaptest6.linked.holiday import Holiday
+from kadaptest6.linked.jaywalking import Jaywalking
+from kadaptest6.linked.local import Local
+from kadaptest6.linked.motorcycle import Motorcycle
+from kadaptest6.linked.oldman import Oldman
+from kadaptest6.linked.schoolzone import Schoolzone
+from kadaptest6.linked.violation import Violation
---
+++ packages/kadaptest7/kadaptest7-0.0.1-py3-none-any.whl/kadaptest6/linked/oldman.py
@@ -0,0 +1,78 @@
+import pandas as pd
+import requests
+import logging
+
+"""
+고령자사고 다발 지역
+"""
+class Oldman :    
+    def __init__(self, debug=False) :
+        """
+        - debug: True 이면 모든 로깅 메시지 출력, False 이면 에러 로깅 메시지 출력
+        """
+
+        self.logger = logging.getLogger("root")
+
+        if debug :
+            self.logger.setLevel(logging.INFO)
+        else :
+            self.logger.setLevel(logging.ERROR)
+
+        formatter = logging.Formatter("[%(levelname)s] %(message)s")
+
+        if len(self.logger.handlers) == 0 :
+            stream_handler = logging.StreamHandler()
+            stream_handler.setFormatter(formatter)
+            self.logger.addHandler(stream_handler)
+
+        self.url = "https://taas.koroad.or.kr/data/rest/koroadkatech/frequentzone/oldman?authKey="
+        # self.key = False
+        # self.servicekey = "PPjO73QvOu67lL5z%2Bfkfmu6ZASEXqi1yn8ZOJXeocdUQcL%2BXyvyfVDWijWSrYbh%2F"
+
+        # 시, 군, 구 코드 변수에 저장
+        response = requests.get("https://raw.githubusercontent.com/kr-juso/administrationCode/main/administrationCode.tsv")
+        self.results = response.content.decode('utf-8').split("\n")[1:-1]
+
+    def yearCodes(self) :
+        yearStrs = ["12년도", "13년도", "14년도", "15년도", "16년도", "17년도", "18년도", "19년도", "20년도"]
+        searchYearCodes = ["2013098", "2014105", "2015048", "2016146", "2017029", "2018029", "2019036", "2020027", "2021024"]
+      
+        result = {key: value for key, value in zip(yearStrs, searchYearCodes)}
+        # df = pd.DataFrame(result.items(), columns=['Year', 'Code'])
+        # df = df.style.hide_index()
+        # df = df.style.hide(axis='index')
+        # df = df.to_json(force_ascii=False)
+
+        return result
+
+    def getData(self, yearCode, cityCode, key) :
+        """
+        - API를 request 함수 통해서 호출 및 데이터 획득, pandas 포맷으로 출력
+        """
+        return self.request(yearCode, cityCode, key)
+
+    def request(self, yearCode, cityCode, servicekey) :
+        """
+        - 실질적으로 데이터를 받아오는 함수
+        """
+
+        df = pd.DataFrame()
+
+        json_index = ["items", "item"]
+        param = {'searchYearCd': yearCode, 'sido': cityCode[:2], 'guGun': cityCode[2:], 'type': 'json'}
+        response = requests.get(self.url+servicekey, params=param)
+
+        if not response.ok :
+            return print("ERROR: Response not OK")
+        
+        print("Response ok")
+
+        items = response.json()
+
+        for index in json_index :
+            items = items[index]
+
+        # df = pd.concat([df, pd.json_normalize(items)])
+        # df = df.to_json(force_ascii=False)
+
+        return items
