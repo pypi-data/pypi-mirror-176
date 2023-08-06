9c16afbc452a02e4b63c0369c3c4fd184db4e47c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:11:12 +0000

    Add package gtas-python-core/0.5.11/gtas_python_core-0.5.11-py3-none-any.whl

---
+++ packages/gtas-python-core/gtas_python_core-0.5.11-py3-none-any.whl/library/gtastest.py
@@ -0,0 +1,9 @@
+class GtasTest:
+    def SayHello():
+        print("Welcome To Python!!")
+
+def SayHello():
+    print("Welcome To Python!!")
+
+def test_package():
+    print("This is package test")
