cf8dda4583bdd107276b5bbb85cc65d9afc7b92b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:13:15 +0000

    Add package psyki/0.2.7/psyki-0.2.7.tar.gz

---
+++ packages/psyki/psyki-0.2.7.tar.gz/psyki-0.2.7/psyki/logic/datalog/grammar/__init__.py
@@ -0,0 +1,261 @@
+from __future__ import annotations
+from abc import ABC
+from typing import Iterable
+from psyki.logic import Formula
+import psyki.logic.datalog as datalog
+
+
+# TODO: refactoring
+def optimize_datalog_formula(formula: Formula):
+    if isinstance(formula, datalog.grammar.Expression):
+        lhs = formula.lhs
+        rhs = formula.rhs
+        op = formula.op
+        if op in (',', ';', '+') and len(formula.nary) == 0:
+            if isinstance(lhs, datalog.grammar.Expression):
+                if lhs.op == op:
+                    optimize_datalog_formula(lhs)
+                    for clause in lhs.nary:
+                        formula.nary.append(clause)
+                    formula.lhs = None
+                else:
+                    formula.nary.append(lhs)
+            else:
+                formula.nary.append(lhs)
+            if isinstance(rhs, datalog.grammar.Expression):
+                if rhs.op == op:
+                    optimize_datalog_formula(rhs)
+                    for clause in rhs.nary:
+                        formula.nary.append(clause)
+                    formula.rhs = None
+                else:
+                    formula.nary.append(rhs)
+            else:
+                formula.nary.append(rhs)
+        else:
+            optimize_datalog_formula(lhs)
+            optimize_datalog_formula(rhs)
+    else:
+        if hasattr(formula, 'lhs'):
+            optimize_datalog_formula(formula.lhs)
+        if hasattr(formula, 'rhs'):
+            optimize_datalog_formula(formula.rhs)
+        if hasattr(formula, 'predicate'):
+            optimize_datalog_formula(formula.predicate)
+
+
+class DatalogFormula(Formula):
+
+    def __init__(self, lhs: DefinitionClause, rhs: Clause, op: str = '<-'):
+        self.lhs: DefinitionClause = lhs
+        self.rhs: Clause = rhs
+        self.op: str = op
+
+    def __str__(self) -> str:
+        return str(self.lhs) + self.op + str(self.rhs)
+
+    def copy(self) -> Formula:
+        return DatalogFormula(self.lhs.copy(), self.rhs.copy(), self.op)
+
+
+class DefinitionClause(Formula):
+
+    def __init__(self, predication: str, arg: Argument):
+        self.predication: str = predication
+        self.arg: Argument = arg
+
+    def __str__(self) -> str:
+        return self.predication + '(' + str(self.arg) + ')'
+
+    def copy(self) -> Formula:
+        return DefinitionClause(self.predication, self.arg)
+
+
+class Clause(Formula, ABC):
+
+    def copy(self) -> Clause:
+        pass
+
+
+class Expression(Clause):
+
+    def __init__(self, lhs: Clause, rhs: Clause, op: str, nary: Iterable[Clause] = []):
+        self.lhs: Clause = lhs
+        self.rhs: Clause = rhs
+        self.nary: list[Clause] = list(nary)
+        self.op: str = op
+
+    def __str__(self) -> str:
+        if len(self.nary) == 0:
+            return '(' + str(self.lhs) + ')' + self.op + '(' + str(self.rhs) + ')'
+        else:
+            return "'" + self.op + "'(" + ','.join(str(clause) for clause in self.nary) + ')'
+
+    def copy(self) -> Formula:
+        return Expression(self.lhs.copy(), self.rhs.copy(), self.op, [c.copy() for c in self.nary])
+
+
+class Literal(Clause, ABC):
+    pass
+
+
+class Negation(Literal):
+
+    def __init__(self, predicate: Clause):
+        self.predicate: Clause = predicate
+
+    def __str__(self) -> str:
+        return 'neg(' + str(self.predicate) + ')'
+
+    def copy(self) -> Formula:
+        return Negation(self.predicate.copy())
+
+
+class Predicate(Clause, ABC):
+    pass
+
+
+class Unary(Predicate):
+
+    def __init__(self, name: str):
+        self.name: str = name
+
+    def __str__(self) -> str:
+        return self.name
+
+    def copy(self) -> Formula:
+        return Unary(self.name)
+
+
+class MofN(Predicate):
+    def __init__(self, name: str, m: Number, arg: ComplexArgument):
+        self.name: str = name
+        self.m: Number = m
+        self.arg: ComplexArgument = arg
+
+    def __str__(self) -> str:
+        return self.name + '(' + str(self.m) + ', ' + str(self.arg) + ')'
+
+    def copy(self) -> Formula:
+        return MofN(self.name, self.m, self.arg)
+
+
+class Nary(Predicate):
+
+    def __init__(self, name: str, arg: Argument):
+        self.name: str = name
+        self.arg: Argument = arg
+
+    def __str__(self) -> str:
+        return self.name + '(' + str(self.arg) + ')'
+
+    def copy(self) -> Formula:
+        return Nary(self.name, self.arg)
+
+
+class Term(Predicate, ABC):
+    pass
+
+
+class Constant(Term, ABC):
+    pass
+
+
+class Predication(Constant):
+
+    def __init__(self, name: str):
+        self.name: str = name
+
+    def __str__(self) -> str:
+        return self.name
+
+    def copy(self) -> Formula:
+        return Predication(self.name)
+
+
+class Boolean(Constant):
+
+    def __init__(self, value: bool = True):
+        self.value: bool = value
+
+    def __str__(self) -> str:
+        return str(self.value)
+
+    @property
+    def is_true(self) -> bool:
+        return self.value
+
+    def copy(self) -> Formula:
+        return Boolean(self.value)
+
+
+class Number(Constant):
+
+    def __init__(self, value: str):
+        self.value: float = float(value)
+
+    def __str__(self) -> str:
+        return str(self.value)
+
+    def copy(self) -> Formula:
+        return Number(self.value)
+
+
+class Variable(Term):
+
+    def __init__(self, name: str):
+        self.name: str = name
+
+    def __str__(self) -> str:
+        return self.name
+
+    def copy(self) -> Formula:
+        return Variable(self.name)
+
+
+class Argument(Formula):
+
+    def __init__(self, term: Term, arg: Argument = None):
+        self.term: Term = term
+        self.arg: Argument = arg
+
+    def __str__(self) -> str:
+        return str(self.term) + (',' + str(self.arg) if self.arg is not None else '')
+
+    def copy(self) -> Formula:
+        return Argument(self.term.copy(), self.arg)
+
+    @property
+    def unfolded(self) -> list[Term]:
+        if self.arg is None:
+            return [self.term]
+        else:
+            return [self.term] + self.arg.unfolded
+
+    @property
+    def last(self) -> Term:
+        return self.unfolded[-1]
+
+
+class ComplexArgument(Formula):
+
+    def __init__(self, clause: Clause, arg: ComplexArgument = None):
+        self.clause: Clause = clause
+        self.arg: ComplexArgument = arg
+
+    def __str__(self) -> str:
+        return str(self.clause) + (',' + str(self.arg) if self.arg is not None else '')
+
+    def copy(self) -> Formula:
+        return ComplexArgument(self.clause.copy(), self.arg)
+
+    @property
+    def unfolded(self):
+        if self.arg is None:
+            return [self.clause]
+        else:
+            return [self.clause] + self.arg.unfolded
+
+    @property
+    def last(self):
+        return self.unfolded[-1]
