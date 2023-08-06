1eec1a8ac1bf2e02110da80b8f5588c6315c9467
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:06:16 +0000

    Add package essexpropds/0.0.2/essexpropds-0.0.2.tar.gz

---
+++ packages/essexpropds/essexpropds-0.0.2.tar.gz/essexpropds-0.0.2/setup.py
@@ -0,0 +1,11 @@
+# Import required functions
+from setuptools import setup, find_packages
+
+# Call setup function
+setup(
+    author="Jung Song",
+    description="A package for utils",
+    name="essexpropds",
+    packages=find_packages(include=["essexpropds", "essexpropds.*"]),
+    version="0.0.2",
+)
