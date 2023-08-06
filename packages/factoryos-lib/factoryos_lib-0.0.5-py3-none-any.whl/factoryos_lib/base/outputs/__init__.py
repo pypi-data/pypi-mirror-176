37a86df3bdd7ee7f0b8e8f5afdbf226330032d5c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:09:52 +0000

    Add package factoryos-lib/0.0.5/factoryos_lib-0.0.5-py3-none-any.whl

---
+++ packages/factoryos-lib/factoryos_lib-0.0.5-py3-none-any.whl/factoryos_lib/base/outputs/__init__.py
@@ -0,0 +1,12 @@
+# -*- coding: utf-8 -*-
+# Copyright 2022 Valiot. | All Rights Reserved
+
+""" Define the components for a reusable neural network model."""
+
+__author__ = ["alejandro.pasos@valiot.io"]
+__all__ = [
+
+    "PrintOutput",
+    "UpdateDatumOutput"
+]
+
