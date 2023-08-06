e820d71629f51f4391f3eaf76c97f9cfd9995d4d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:14:38 +0000

    Add package peakrdl/0.4.0/peakrdl-0.4.0.tar.gz

---
+++ packages/peakrdl/peakrdl-0.4.0.tar.gz/peakrdl-0.4.0/src/peakrdl/main.py
@@ -0,0 +1,75 @@
+import argparse
+import sys
+from typing import TYPE_CHECKING, List, Dict
+
+from systemrdl import RDLCompileError
+
+from .__about__ import __version__
+from .plugins.exporter import get_exporter_plugins
+from .cmd.dump import Dump
+from .cmd.list_globals import ListGlobals
+
+
+if TYPE_CHECKING:
+    from .subcommand import Subcommand
+
+
+DESCRIPTION = """
+PeakRDL is a control & status register model automation toolchain.
+
+For help about a specific subcommand, try:
+    peakrdl <command> --help
+
+For more documentation, visit https://peakrdl.readthedocs.io
+
+"""
+
+
+class SubcommandHelpFormatter(argparse.RawDescriptionHelpFormatter):
+    def _format_action(self, action): # type: ignore
+        parts = super(argparse.RawDescriptionHelpFormatter, self)._format_action(action)
+        if action.nargs == argparse.PARSER:
+            parts = "\n".join(parts.split("\n")[1:])
+        return parts
+
+
+def main() -> None:
+    # Collect all subcommands
+    subcommands = [
+        Dump(),
+        ListGlobals(),
+    ] # type: List[Subcommand]
+    subcommands += get_exporter_plugins()
+
+    # Check for duplicates
+    sc_dict = {} # type: Dict[str, Subcommand]
+    for sc in subcommands:
+        if sc.name in sc_dict:
+            raise RuntimeError(f"More than one exporter plugin was registered with the same name '{sc.name}': \n\t{sc_dict[sc.name]}\n\t{sc}")
+        sc_dict[sc.name] = sc
+
+    # Initialize top-level arg parser
+    parser = argparse.ArgumentParser(
+        description=DESCRIPTION,
+        formatter_class=SubcommandHelpFormatter,
+    )
+    parser.add_argument("--version", action="version", version=__version__)
+
+    # Initialize subcommand arg parsers
+    subgroup = parser.add_subparsers(
+        title="subcommands",
+        metavar="<subcommand>",
+    )
+    for subcommand in subcommands:
+        subcommand._init_subparser(subgroup)
+
+    # Execute!
+    options = parser.parse_args()
+    if not hasattr(options, 'subcommand'):
+        parser.print_usage()
+        print(f"{parser.prog}: error the following arguments are required: <subcommand>")
+        sys.exit(1)
+    try:
+        options.subcommand.main(options)
+    except RDLCompileError:
+        sys.exit(1)
---
+++ packages/peakrdl/peakrdl-0.4.0.tar.gz/peakrdl-0.4.0/src/peakrdl/plugins/importer.py
@@ -0,0 +1,87 @@
+from typing import TYPE_CHECKING, Any, List
+
+from . import entry_points
+from ..importer import Importer
+
+if TYPE_CHECKING:
+    import argparse
+    from systemrdl import RDLCompiler
+
+class ImporterPluginWrapper(Importer):
+    """
+    Importers external to this package can register an implementation that can
+    be loaded into PeakRDL
+
+    The importer definition is provided by a class that mimics the
+    members/methods of the Importer class without actually extending it
+
+    .. code:: python
+
+        class MyImporter:
+            file_extensions = ["foo"]
+
+            def is_compatible(self, path: str) -> bool:
+                raise NotImplementedError
+
+            def add_importer_arguments(self, arg_group: 'argparse.ArgumentParser') -> None:
+                pass
+
+            def do_import(self, rdlc: 'RDLCompiler', options: 'argparse.Namespace', path: str):
+                raise NotImplementedError
+    """
+
+    def __init__(self, name: str, importer_plugin_cls: Any) -> None:
+        self.plugin = importer_plugin_cls()
+        self.name = name
+        self.file_extensions = getattr(self.plugin, "file_extensions", [])
+
+    def __repr__(self) -> str:
+        return "<%s '%s' from %s at 0x%x>" % (
+            self.__class__.__qualname__,
+            self.name,
+            self.plugin.__class__,
+            id(self)
+        )
+
+    def is_compatible(self, path: str) -> bool:
+        func = getattr(self.plugin, "is_compatible", None)
+        if callable(func):
+            return func(path)
+        else:
+            raise NotImplementedError
+
+
+    def add_importer_arguments(self, arg_group: 'argparse._ActionsContainer') -> None:
+        func = getattr(self.plugin, "add_importer_arguments", None)
+        if callable(func):
+            func(arg_group)
+
+
+    def do_import(self, rdlc: 'RDLCompiler', options: 'argparse.Namespace', path: str) -> None:
+        func = getattr(self.plugin, "do_import", None)
+        if callable(func):
+            func(rdlc, options, path)
+        else:
+            raise NotImplementedError
+
+
+
+def get_importer_plugins() -> List[Importer]:
+    """
+    Load any plugins that advertise themselves in their setup.py via the following:
+
+    setup(
+        ...
+        entry_points = {
+            "peakrdl.importers": [
+                'my_importer_name = module.path.to:MyImporter'
+            ]
+        },
+    )
+    """
+    importers = []
+    for ep in entry_points.get_entry_points("peakrdl.importers"): # type: ignore
+        importer = ImporterPluginWrapper(ep.name, ep.load())
+        importers.append(importer)
+
+    return importers # type: ignore
