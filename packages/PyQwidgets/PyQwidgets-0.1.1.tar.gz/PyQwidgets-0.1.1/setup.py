755c452a0432391a26ca13f812170bed7afd5c4b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:24:17 +0000

    Add package PyQwidgets/0.1.1/PyQwidgets-0.1.1.tar.gz

---
+++ packages/PyQwidgets/PyQwidgets-0.1.1.tar.gz/PyQwidgets-0.1.1/setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+setuptools.setup(
+	name='PyQwidgets',
+	version='0.1.1',
+	author='Georg8528',
+	author_email='zadvornow2908@gmail.com',
+	description='This mini-framework will help you create widgets',
+	packages=['PyQwidgets'],
+	classifiers=[
+		"Programming Language :: Python :: 3",
+		"License :: OSI Approved :: MIT License",
+		"Operating System :: OS Independent",
+	],
+	python_requires='>=3.6',
+)
