cb021724ffa13a7196a2c908dde223749e03f793
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:40:24 +0000

    Add package generate-docstring/0.1.0/generate_docstring-0.1.0-py3-none-any.whl

---
+++ packages/generate-docstring/generate_docstring-0.1.0-py3-none-any.whl/generate_docstring/parser.py
@@ -0,0 +1,43 @@
+"""
+
+parser module
+"""
+from typing import Tuple
+
+import libcst as cst
+from jinja2 import Environment, PackageLoader, select_autoescape
+
+from generate_docstring.transformer import DocstringTransformer, Templates
+from generate_docstring.visitor import Visitor
+
+
+def parse(code: str, module_name: str) -> Tuple[cst.Module, cst.Module]:
+    """
+
+    parse function
+
+    Args:
+        code (str): TODO: to complete
+        module_name (str): TODO: to complete
+        override (bool): TODO: to complete
+
+    Returns:
+        str: TODO: to complete
+
+    """
+    tree = cst.parse_module(code)
+    env = Environment(
+        loader=PackageLoader("docstring", "templates"), autoescape=select_autoescape()
+    )
+    templates: Templates = {
+        cst.FunctionDef: env.get_template("function_def.py.jinja2"),
+        cst.ClassDef: env.get_template("class_def.py.jinja2"),
+        cst.Module: env.get_template("module.py.jinja2"),
+    }
+    visitor = Visitor()
+    tree.visit(visitor)
+    transformer = DocstringTransformer(
+        templates, visitor.indents_func, visitor.indents_class, visitor.raises, module_name
+    )
+    modified_tree = tree.visit(transformer)
+    return tree, modified_tree
