d29693b84902605adca9bfd87c1f27b398d58646
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:10:47 +0000

    Add package redshift_client/1.2.0/redshift_client-1.2.0.tar.gz

---
+++ packages/redshift_client/redshift_client-1.2.0.tar.gz/redshift_client-1.2.0/build/pkg/default.nix
@@ -0,0 +1,31 @@
+{
+  lib,
+  metadata,
+  python_pkgs,
+  src,
+}: let
+  runtime_deps = with python_pkgs; [
+    psycopg2
+    types-psycopg2
+    fa-purity
+  ];
+  build_deps = with python_pkgs; [flit-core];
+  test_deps = with python_pkgs; [
+    import-linter
+    mypy
+    pytest
+  ];
+  pkg = (import ./build.nix) {
+    inherit lib src metadata runtime_deps build_deps test_deps;
+  };
+  build_env = extraLibs:
+    lib.buildEnv {
+      inherit extraLibs;
+      ignoreCollisions = false;
+    };
+in {
+  inherit pkg;
+  env.runtime = build_env runtime_deps;
+  env.dev = build_env (runtime_deps ++ test_deps ++ build_deps);
+  env.bin = build_env [pkg];
+}
