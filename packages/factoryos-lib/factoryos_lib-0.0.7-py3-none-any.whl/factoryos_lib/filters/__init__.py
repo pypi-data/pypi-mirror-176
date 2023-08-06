3c8a9faf8d3eb79d911906aaa1806b3b4e811fb0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:09:57 +0000

    Add package factoryos-lib/0.0.7/factoryos_lib-0.0.7-py3-none-any.whl

---
+++ packages/factoryos-lib/factoryos_lib-0.0.7-py3-none-any.whl/factoryos_lib/filters/__init__.py
@@ -0,0 +1,13 @@
+# Copyright 2022 Valiot. | All Rights Reserved
+
+""" Define the components for filter jobs."""
+
+__author__ = ["alejandro.pasos@valiot.io"]
+__all__ = [
+    "MovingAverage",
+    "ExponentialMovingAverage",
+    "Stats"
+
+]
+
+from factoryos_lib.filters._stats import Stats
