aa4a8835192863f099b0156664e5f108f05e6852
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:23:05 +0000

    Add package MathcadPy/0.3/MathcadPy-0.3-py3-none-any.whl

---
+++ packages/MathcadPy/MathcadPy-0.3-py3-none-any.whl/MathcadPy/__init__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+"""
+__init__.py
+~~~~~~~~~~~~~~
+MathcadPy
+https://github.com/MattWoodhead/MathcadPy
+
+Copyright 2022 Matt Woodhead
+
+Requirements:
+
+Mathcad Prime ( https://www.mathcad.com )
+PyWin32 ( https://github.com/mhammond/pywin32 )
+"""
+
+from ._application import *
+from . import __version__
+
+__author__ = __version__.__author__
+__version__ = __version__.__version__
