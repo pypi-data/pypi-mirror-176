c6438ab095d0e9e7e9e48470da4abba9e24cc3a6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:16:24 +0000

    Add package djoserfoodgram/1.2.1/djoserfoodgram-1.2.1.tar.gz

---
+++ packages/djoserfoodgram/djoserfoodgram-1.2.1.tar.gz/djoserfoodgram-1.2.1/setup.py
@@ -0,0 +1,50 @@
+from setuptools import setup
+
+packages = \
+['djoser',
+ 'djoser.social',
+ 'djoser.social.backends',
+ 'djoser.social.token',
+ 'djoser.urls']
+
+package_data = \
+{'': ['*'],
+ 'djoser': ['locale/de/LC_MESSAGES/django.po',
+            'locale/es/LC_MESSAGES/django.po',
+            'locale/fr/LC_MESSAGES/django.po',
+            'locale/ka/LC_MESSAGES/django.po',
+            'locale/pl/LC_MESSAGES/django.po',
+            'locale/pt_BR/LC_MESSAGES/django.po',
+            'locale/ru_RU/LC_MESSAGES/django.po',
+            'templates/email/*']}
+
+install_requires = \
+['asgiref>=3.2.10,<4.0.0',
+ 'coreapi>=2.3.3,<3.0.0',
+ 'django-templated-mail>=1.1.1,<2.0.0',
+ 'djangorestframework-simplejwt>=4.3.0,<5.0.0',
+ 'social-auth-app-django>=4.0.0,<5.0.0']
+ 
+ 
+extras_require = \
+{':python_version < "3.8"': ['importlib-metadata>=1.0,<2.0'],
+ 'test': ['pytest>=6.0.2,<7.0.0',
+          'codecov>=2.0.16,<3.0.0',
+          'coverage>=5.3,<6.0',
+          'pytest-cov>=2.10.1,<3.0.0',
+          'pytest-django>=3.10.0,<4.0.0',
+          'pytest-pythonpath>=0.7.3,<0.8.0',
+          'djet>=0.2.2,<0.3.0']}
+
+setup_kwargs = {
+	'name':'djoserfoodgram',
+	'version':'1.2.1',
+	'author':'avnosov',
+	'author_email':'nosov1995@gmail.com',
+	'description':'remove path',
+	'packages':packages,
+    'package_data':package_data,
+    'extras_require': extras_require,
+	'python_requires': '>=3.6.1,<4.0.0',
+}
+setup(**setup_kwargs)
