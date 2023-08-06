df86faa38e1c9a00c82bcfc0ed2eafba0d3b74ae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:12:04 +0000

    Add package dtuprosys/0.1.19/dtuprosys-0.1.19.tar.gz

---
+++ packages/dtuprosys/dtuprosys-0.1.19.tar.gz/dtuprosys-0.1.19/dtuprosys/chemometrics/plotting/plot_spectra.py
@@ -0,0 +1,33 @@
+import matplotlib.colors as colors
+import matplotlib.pyplot as plt
+import pandas as pd
+
+
+def plot_spectra(spectra: pd.DataFrame, title: str, xlabel: str, ylabel: str, reference: pd.DataFrame = None):
+    """
+    Plots spectra.
+    @param spectra dataframe containing the spectra with the wavenumbers as columns
+    @param title title of plot
+    @param xlabel x-axis label
+    @param ylabel y-axis label
+    @param reference dataframe containing the reference spectra with the wavenumbers as columns
+    """
+
+    if reference is not None:
+        reference = reference.values
+        norm = plt.Normalize(reference.min(), reference.max())
+        cmap = plt.get_cmap('viridis')
+        colormap = cmap(norm(reference))
+        plt.figure(figsize=(10, 3))
+        for i, row, in spectra.iterrows():
+            plt.plot(spectra.columns, row.values, color=colormap[i])
+    else:
+        plt.figure(figsize=(10, 3))
+        for i, row, in spectra.iterrows():
+            plt.plot(spectra.columns, row.values)
+
+    plt.title(title)
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)
+    return None
+
