eba8bbd0d85831be9731fd0af1657b9dd5d1684b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:14:21 +0000

    Add package mybhadd/0.0.1/mybhadd-0.0.1.tar.gz

---
+++ packages/mybhadd/mybhadd-0.0.1.tar.gz/mybhadd-0.0.1/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="mybhadd",
+    version="0.0.1",
+    description="test pack for addition",
+    py_modules=["bhadd"],
+    package_dir={"": "src"},
+    classifiers=["Programming Language :: Python :: 3",
+                 "License :: OSI Approved :: MIT License", "Operating System :: OS Independent"],
+)
