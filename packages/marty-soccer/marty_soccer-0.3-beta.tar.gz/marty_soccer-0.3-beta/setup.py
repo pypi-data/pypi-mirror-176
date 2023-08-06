332496fa413ec43f14d1b1e84af8af22cdc23457
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:23:31 +0000

    Add package marty-soccer/0.3b0/marty_soccer-0.3-beta.tar.gz

---
+++ packages/marty-soccer/marty_soccer-0.3-beta.tar.gz/marty_soccer-0.3-beta/setup.py
@@ -0,0 +1,33 @@
+from distutils.core import setup
+setup(
+    name='marty_soccer',
+    packages=['marty_soccer'],
+    package_dir = {'': 'src'},
+    version='0.3-beta',
+    license='MIT',
+    description='marty AI soccer library for education created by DGS',
+    author='DGS',
+    author_email='dgs.python@dgs.edu.hk',
+    url='https://github.com/DGSpython/marty-package',
+    download_url='https://github.com/DGSpython/marty-package/archive/refs/tags/v0.1-beta.tar.gz',
+    keywords=['DGS', 'Marty', 'Soccer'],
+    install_requires=[
+        'nums_from_string',
+        'martypy',
+        'datetime',
+        'pyserial'
+    ],
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9'
+    ],
+)
