821b1562d520ebeb1ff42a06bb90b48022bf1e35
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:10:23 +0000

    Add package ExtremePluginManager/1.1.1/ExtremePluginManager-1.1.1-py3-none-any.whl

---
+++ packages/ExtremePluginManager/ExtremePluginManager-1.1.1-py3-none-any.whl/ExtremePluginManager/constants.py
@@ -0,0 +1,4 @@
+import pathlib
+import typing
+
+PATH_TYPEHINT = typing.Union[str, pathlib.Path, bytes]
---
+++ packages/ExtremePluginManager/ExtremePluginManager-1.1.1-py3-none-any.whl/ExtremePluginManager/converter/__init__.py
@@ -0,0 +1,2 @@
+from ExtremePluginManager.converter.directory import directory_to_zip
+from ExtremePluginManager.converter.zip import zip_to_directory
