4d8d6a26fc706db414ee48cd8cda09b0065a4fe1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:22:01 +0000

    Add package streamlit-doubutsushogi/0.0.15/streamlit-doubutsushogi-0.0.15.tar.gz

---
+++ packages/streamlit-doubutsushogi/streamlit-doubutsushogi-0.0.15.tar.gz/streamlit-doubutsushogi-0.0.15/streamlit_doubutsushogi/frontend/build/precache-manifest.38f984033bc33fe3d9ce2390d8540bbc.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00f7867ff6471905dd88e8588646ad05",
+    "url": "./index.html"
+  },
+  {
+    "revision": "f5bbfd08a2634ab77177",
+    "url": "./static/js/2.dcc6c2e3.chunk.js"
+  },
+  {
+    "revision": "3dc2da520b7611f998bec3f8b32ee24e",
+    "url": "./static/js/2.dcc6c2e3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7d80cd1062397b2381d7",
+    "url": "./static/js/main.1333c33f.chunk.js"
+  },
+  {
+    "revision": "7e9d84e346ce158d1e50",
+    "url": "./static/js/runtime-main.44d30fc2.js"
+  }
+]);
