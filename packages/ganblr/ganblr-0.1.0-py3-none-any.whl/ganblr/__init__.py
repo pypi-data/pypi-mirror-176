b2f8690d1215f4fa48d5f41a3cd25728255badae
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:34:31 +0000

    Add package ganblr/0.1.0/ganblr-0.1.0-py3-none-any.whl

---
+++ packages/ganblr/ganblr-0.1.0-py3-none-any.whl/ganblr/__init__.py
@@ -0,0 +1,8 @@
+"""Top-level package for Ganblr."""
+
+__author__ = """Tulip Lab"""
+__email__ = 'jhzhou@tuliplab.academy'
+__version__ = '0.1.0'
+
+from .models.ganblr import GANBLR
+from .models.ganblrpp import GANBLRPP
