b198803f0f5439b1be642a512d81203bcfc0e9a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:23:52 +0000

    Add package makefilepy/0.0.1/makefilepy-0.0.1-py3-none-any.whl

---
+++ packages/makefilepy/makefilepy-0.0.1-py3-none-any.whl/tests/test_Variable.py
@@ -0,0 +1,18 @@
+import unittest
+
+from makefilepy import Variable
+
+
+class TestVariable(unittest.TestCase):
+    def test_init(self):
+        key = 'test'
+        value = 'testValue'
+        variable = Variable(key=key, value=value)
+        self.assertEqual(variable.key, key)
+        self.assertEqual(variable.value, value)
+
+    def test_build(self):
+        key = 'test'
+        value = 'testValue'
+        variable = Variable(key=key, value=value)
+        self.assertEqual(variable.build(), f'{key}={value}')
