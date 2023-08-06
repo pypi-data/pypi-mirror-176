17d434367cd8edda0512b5129b8b15b5825294d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:18:15 +0000

    Add package pyDE1/1.5.2/pyDE1-1.5.2-py3-none-any.whl

---
+++ packages/pyDE1/pyDE1-1.5.2-py3-none-any.whl/pyDE1/scale/__init__.py
@@ -0,0 +1,12 @@
+"""
+Copyright © 2021 Jeff Kletsky. All Rights Reserved.
+
+License for this software, part of the pyDE1 package, is granted under
+GNU General Public License v3.0 only
+SPDX-License-Identifier: GPL-3.0-only
+"""
+
+from .scale import (
+    Scale, scale_factory, recognized_scale_prefixes
+)
+from .atomax_skale_ii import AtomaxSkaleII
---
+++ packages/pyDE1/pyDE1-1.5.2-py3-none-any.whl/pyDE1/source_data.py
@@ -0,0 +1,11 @@
+# DO NOT EDIT -- This file automatically generated -- DO NOT EDIT
+git_data = {
+    "hash": "fa89735a",
+    "subject": "Don't reset the estimator on restoring period estimate from DB",
+    "ref_names": "HEAD -> devel-2022-11",
+    "author_date": "Sun Oct 30 10:22:07 2022 -0700",
+    "commit_date": "Sun Oct 30 11:40:46 2022 -0700",
+}
+source_data = {
+    "git": git_data,
+}
