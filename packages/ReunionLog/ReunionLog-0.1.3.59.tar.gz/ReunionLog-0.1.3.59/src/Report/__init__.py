f1466875345ae535a6695917bf204de14f4b690a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:40:13 +0000

    Add package ReunionLog/0.1.3.59/ReunionLog-0.1.3.59.tar.gz

---
+++ packages/ReunionLog/ReunionLog-0.1.3.59.tar.gz/ReunionLog-0.1.3.59/src/Report/__init__.py
@@ -0,0 +1,25 @@
+import ReportAbilities.
+import ReportEventsDamageDone
+import ReportEventsDeath
+import ReportEventsInterrupts
+import ReportNames
+
+
+"""
+#Abilities
+from .ReportAbilities import Get_AbilityList
+from .ReportAbilities import Get_Data_Ability
+
+#Damage Done
+from .ReportEventsDamageDone import Get_Data_EventDamageDone
+
+#Death
+from .ReportEventsDeath import Get_Data_EventDeath
+
+#Interrupts
+from .ReportEventsInterrupts import Get_Data_EventInterrupts
+
+#Names
+from .ReportNames import Get_Data_Name
+from .ReportNames import Get_NameList
+"""
