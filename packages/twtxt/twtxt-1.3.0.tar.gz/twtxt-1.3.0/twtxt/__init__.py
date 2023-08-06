353cce716523479ec30b2ab51405f88f2a749adb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:44:21 +0000

    Add package twtxt/1.3.0/twtxt-1.3.0.tar.gz

---
+++ packages/twtxt/twtxt-1.3.0.tar.gz/twtxt-1.3.0/twtxt/__init__.py
@@ -0,0 +1,12 @@
+"""
+    twtxt
+    ~~~~~
+
+    Decentralised, minimalist microblogging service for hackers.
+
+    :copyright: (c) 2016-2022 by buckket.
+    :license: MIT, see LICENSE for more details.
+"""
+
+
+__version__ = '1.3.0'
