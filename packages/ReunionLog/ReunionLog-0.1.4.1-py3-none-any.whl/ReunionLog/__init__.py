955889e4d2153fe80f6375de14f26a9b900e0162
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:22:30 +0000

    Add package ReunionLog/0.1.4.1/ReunionLog-0.1.4.1-py3-none-any.whl

---
+++ packages/ReunionLog/ReunionLog-0.1.4.1-py3-none-any.whl/ReunionLog/__init__.py
@@ -0,0 +1,24 @@
+from ReunionLog.OAuth.Authentication import Retrieve_Headers
+from ReunionLog.Report.ReportAbilities import Get_AbilityList
+from ReunionLog.Report.ReportAbilities import Get_Data_Ability
+from ReunionLog.Report.ReportEventsDamageDone import Get_Data_EventDamageDone
+from ReunionLog.Report.ReportEventsDeath import Get_Data_EventDeath
+from ReunionLog.Report.ReportEventsInterrupts import Get_Data_EventInterrupts
+from ReunionLog.Report.ReportNames import Get_Data_Name
+from ReunionLog.Report.ReportNames import Get_NameList
+
+"""
+#Abilities
+from .ReportAbilities import Get_AbilityList
+from .ReportAbilities import Get_Data_Ability
+#Damage Done
+from .ReportEventsDamageDone import Get_Data_EventDamageDone
+#Death
+from .ReportEventsDeath import Get_Data_EventDeath
+#Interrupts
+from .ReportEventsInterrupts import Get_Data_EventInterrupts
+
+#Names
+from .ReportNames import Get_Data_Name
+from .ReportNames import Get_NameList
+"""
