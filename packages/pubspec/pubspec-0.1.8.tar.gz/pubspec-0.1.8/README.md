cd18d955aebf55bde837d6b39d1221a147fcc396
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:09:35 +0000

    Add package pubspec/0.1.8/pubspec-0.1.8.tar.gz

---
+++ packages/pubspec/pubspec-0.1.8.tar.gz/pubspec-0.1.8/README.md
@@ -0,0 +1,27 @@
+# Pubspec
+
+Update all package versions in flutter pubspec.yaml
+
+[Python Package Index](https://pypi.org/project/pubspec/)
+
+## Installation and Usage
+
+1. [Python3](https://www.python.org/downloads/) required
+2. Install pubspec
+```
+pip3 install pubspec
+```
+3. Run pubspec in same directory as pubspec.yaml file
+```
+pubspec
+```
+
+---
+## Demo
+![Screenshot](https://user-images.githubusercontent.com/17068559/201468054-a8af7bcb-db53-408a-ba18-6cbf5208900e.png)
+
+https://user-images.githubusercontent.com/17068559/201467967-8fc4706d-83a4-4f17-a0ee-58eda9fdb178.mov
+
+
+## Suggestions
+Open to any suggestions.
---
+++ packages/pubspec/pubspec-0.1.8.tar.gz/pubspec-0.1.8/setup.py
@@ -0,0 +1,26 @@
+import setuptools
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+setuptools.setup(
+    name='pubspec',
+    version='0.1.8',
+    scripts=['pubspec'],
+    author="Preetam",
+    author_email="contact@preetam.dev",
+    description="Python script to update pubspec.yaml in flutter",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/prtm/pubspec",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: Apache Software License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=[
+        'requests>=2.24.0',
+        'ruamel.yaml>=0.16.10',
+        'lxml>=4.5.1',
+        'pyyaml>=6.0',
+    ]
+)
