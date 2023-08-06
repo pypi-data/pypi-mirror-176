e3a78793fef24cf4ef6bc5a201e82d739dbf8c6c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:45:25 +0000

    Add package mindspore-ascend-dev/2.0.0.dev20221113/mindspore_ascend_dev-2.0.0.dev20221113-cp38-cp38-manylinux1_x86_64.whl

---
+++ packages/mindspore-ascend-dev/mindspore_ascend_dev-2.0.0.dev20221113-cp38-cp38-manylinux1_x86_64.whl/mindspore/experimental/map_parameter.py
@@ -0,0 +1,227 @@
+# Copyright 2022 Huawei Technologies Co., Ltd
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ============================================================================
+"""MapParameter implementation."""
+from __future__ import absolute_import
+
+__all__ = ['MapParameter']
+
+import sys
+from copy import copy
+import numbers
+import mindspore as ms
+from mindspore.common.parameter import Tensor, Parameter
+from mindspore._c_expression import Tensor as Tensor_
+from mindspore._c_expression import MapTensor_
+
+
+class MapParameter(Parameter):
+    """
+    MapParameter is a parameter that stores a map like data structure.
+
+    .. warning::
+        This is an experimental prototype that is subject to change and/or deletion.
+
+    Args:
+        key_dtype (:class:`mindspore.dtype`): The data type of the key. The argument should be defined in
+            `mindspore.dtype`, currently only integer types are supported. Default: int32.
+        value_dtype (:class:`mindspore.dtype`): The data type of the value Tensor. The argument should
+            be defined in `mindspore.dtype`. Default: float32.
+        value_shape (Union[tuple, list, int]): Used to indicate the shape of the value Tensor. The argument should be
+            a list of integers, a tuple of integers or an integer. Default: 1.
+        default_value (Union[numbers.Number, str]): The default value number or initializer name. Default: 'normal'.
+        permit_filter_value (numbers.Number): The permit filter value number. Default: 1.
+        evict_filter_value (numbers.Number): The evict filter value number. Default: MAX_SIZE.
+        name (str): Name of the map parameter. Default: None.
+        requires_grad (bool): True if the parameter requires gradient. Default: True.
+
+
+    Examples:
+        >>> import mindspore as ms
+        >>> from mindspore import Tensor
+        >>> from mindspore.experimental import MapParameter
+        >>>
+        >>> m = MapParameter(key_dtype=ms.int32, value_dtype=ms.float32, value_shape=(3), default_value='zeros')
+        >>> t = m.get(Tensor([1, 2, 3], dtype=ms.int32))
+        [[0. 0. 0.]
+         [0. 0. 0.]
+         [0. 0. 0.]]
+        >>> m.put(Tensor([1, 2], dtype=ms.int32), Tensor([[1, 1, 1], [2, 2, 2]], dtype=ms.float32))
+        >>> t = m.get(Tensor([1, 2, 3], dtype=ms.int32))
+        >>> print(t)
+        [[1. 1. 1.]
+         [2. 2. 2.]
+         [0. 0. 0.]]
+        >>> m.erase(Tensor([2, 3], dtype=ms.int32))
+        >>> print(t)
+        [[1. 1. 1.]]
+    """
+
+    def __new__(cls, key_dtype=ms.int32, value_dtype=ms.float32, value_shape=1, default_value='normal',
+                permit_filter_value=1, evict_filter_value=sys.maxsize, **kwargs):
+        if isinstance(value_shape, numbers.Number):
+            value_shape = (value_shape,)
+        data = Tensor_(value_dtype, value_shape)
+        obj = Tensor_.__new__(cls)
+        Tensor_.__init__(obj, data)
+        # Compatible attributes with Parameter.
+        obj.has_init = False
+        obj.init_mode = None
+        obj.is_default_input_init = False
+        # MapParameter added attributes.
+        obj.key_dtype = key_dtype
+        obj.value_dtype = value_dtype
+        obj.value_shape = value_shape
+        obj.default_value = default_value
+        obj.permit_filter_value = permit_filter_value
+        obj.evict_filter_value = evict_filter_value
+        return obj
+
+    def __init__(self, name=None, requires_grad=True, **kwargs):
+        Parameter.__init__(self, self, name=name, requires_grad=requires_grad)
+        self._map_tensor = MapTensor_(self.key_dtype, self.value_dtype, self.value_shape, self.default_value,
+                                      self.permit_filter_value, self.evict_filter_value)
+
+    def __getitem__(self, key_tensor):
+        return self.get(key_tensor, True)
+
+    def __setitem__(self, key_tensor, value_tensor):
+        return self.put(key_tensor, value_tensor)
+
+    def __str__(self):
+        return 'MapParameter(' + str(self._map_tensor) + ')'
+
+    def __copy__(self):
+        x = type(self)()
+        x.__dict__.update(self.__dict__)
+        return x
+
+    def clone(self, init='same'):
+        """
+        Clone the MapParameter.
+
+        Args:
+            init (Union[str, numbers.Number]): Initialize the default value of the new map parameter.
+                If `init` is a `numbers.Number`, clone a new map parameter with the same key value shape
+                and dtype, and the default value of the new map parameter will be set according to `init`.
+                If `init` is a `str`, the `init` should be the alias of the class inheriting from `Initializer`.
+                If `init` is 'same', clone a new map parameter with the same default value. Default: 'same'.
+
+        Returns:
+            MapParameter, the new map parameter.
+        """
+        x = copy(self)
+        x.param_info = self.param_info.clone()
+        info = self.param_info
+        if hasattr(info, "cloned_obj"):
+            info.cloned_obj.append(x)
+        else:
+            info.cloned_obj = [x]
+        self.param_info = info
+        if init != 'same':
+            x.default_value = init  # pylint: disable=W0201
+        x._map_tensor = MapTensor_(x.key_dtype, x.value_dtype, x.value_shape, x.default_value, x.permit_filter_value,  # pylint: disable=W0212
+                                   x.evict_filter_value)
+        return x
+
+    def get(self, key_tensor, insert_default_value=True):
+        """
+        Get value tensor according the key tensor, fill and return the default value in map parameter if key is not
+        existed.
+
+        Args:
+            key_tensor (Tensor): The key tensor.
+            insert_default_value (bool): The flag of insert default_value.
+
+        Returns:
+            Tensor, the value tensor for the key tensor.
+        """
+        result_tensor = self._map_tensor.get(key_tensor, insert_default_value)
+        return Tensor(result_tensor, internal=True)
+
+    def get_keys(self):
+        """
+        Get all keys as a tensor.
+
+        Returns:
+            Tensor, the tensor contains all keys.
+        """
+        return None
+
+    def get_values(self):
+        """
+        Get all values as a tensor.
+
+        Returns:
+            Tensor, the tensor contains all values.
+        """
+        return None
+
+    def get_data(self):
+        """
+        Get all keys and values as a tensor.
+
+        Returns:
+            Tensor, the tensor contains all keys and values.
+        """
+        return None
+
+
+    def put(self, key_tensor, value_tensor):
+        """
+        Insert or update records according the given key tensor and value tensor.
+
+        Args:
+            key_tensor (Tensor): The key tensor.
+            value_tensor (Tensor): The value tensor.
+
+        Returns:
+            MapParameter, the MapParameter object itself.
+        """
+        self._map_tensor.put(key_tensor, value_tensor)
+        return self
+
+    def erase(self, key_tensor):
+        """
+        Remove records according the given key tensor.
+
+        Args:
+            key_tensor (Tensor): The key tensor.
+
+        Returns:
+            MapParameter, the MapParameter object itself.
+        """
+        self._map_tensor.erase(key_tensor)
+        return self
+
+    def export(self, full=False):
+        """
+        Export data from this map parameter.
+
+        Args:
+            full (bool): True for full export, otherwise for incremental export. Default: False.
+
+        Returns:
+            Tuple(key_array, value_array, status_array), The exported data as a tuple.
+        """
+        return self._map_tensor.export(full)
+
+    def update(self, data):
+        """
+        Update this map parameter from exported data.
+
+        Args:
+            data (Tuple): The data tuple with key_array, value_array and status_array.
+        """
+        self._map_tensor.update(data)
---
+++ packages/mindspore-ascend-dev/mindspore_ascend_dev-2.0.0.dev20221113-cp38-cp38-manylinux1_x86_64.whl/mindspore/profiler/envprofiling.py
@@ -0,0 +1,150 @@
+# Copyright 2022 Huawei Technologies Co., Ltd
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ============================================================================
+"""Whether to enable profiler using environment variables."""
+import os
+import json
+import time
+from mindspore import log as logger
+from mindspore.profiler import Profiler
+from mindspore.profiler.common.validator.validate_path import validate_and_normalize_path
+
+
+def construct_profiling_options():
+    """Construct profiling options to determine which profiling data should be collected."""
+    try:
+        profiling_options = json.loads(os.environ.get("MS_PROFILER_OPTIONS", "{}"))
+    except json.JSONDecodeError as err:
+        raise RuntimeError('The format of PROFILING_OPTIONS is incorrect.') from err
+    options = combine_profile_options(profiling_options)
+    if not isinstance(options.get("start"), bool):
+        options["start"] = False
+        logger.warning(
+            "The 'start' parameter of the environment variable MS_PROFILE_OPTIONS is an invalid value,"
+            " it will be set to False.")
+    if not isinstance(options.get("profile_memory"), bool):
+        options["profile_memory"] = False
+        logger.warning(
+            "The 'memory' parameter of the environment variable MS_PROFILE_OPTIONS is an invalid value,"
+            " it will be set to False.")
+    if not isinstance(options.get("profile_communication"), bool):
+        options["profile_communication"] = False
+        logger.warning(
+            "The 'hccl' parameter of the environment variable MS_PROFILE_OPTIONS is an invalid value,"
+            " it will be set to False.")
+    if not isinstance(options.get("aicore_metrics"), int):
+        options["aicore_metrics"] = 0
+        logger.warning(
+            "The 'aicore_metrics' parameter of the environment variable MS_PROFILE_OPTIONS is an invalid value,"
+            " it will be set to 0.")
+    if not isinstance(options.get("l2_cache"), bool):
+        options["l2_cache"] = False
+        logger.warning(
+            "The 'l2_cache' parameter of the environment variable MS_PROFILE_OPTIONS is an invalid value,"
+            " it will be set to False.")
+    if not isinstance(options.get("sync_enable"), bool):
+        options["sync_enable"] = True
+        logger.warning(
+            "The 'sync_enable' parameter of the environment variable MS_PROFILE_OPTIONS is an invalid value,"
+            " it will be set to True.")
+    if options.get("start"):
+        output_path = profiling_options.get("output_path")
+        if not output_path or not os.path.isabs(output_path):
+            output_path = os.path.join(os.getcwd(), "data")
+        if not os.path.exists(output_path):
+            os.makedirs(output_path, exist_ok=True)
+        options["output_path"] = validate_and_normalize_path(output_path)
+        options["profiler_path"] = os.path.join(output_path, "profiler")
+        return options
+    return options
+
+
+def combine_profile_options(profiling_options):
+    """Combined profiling options."""
+    factor_s_to_us = 1e7
+    options = {
+        "start": profiling_options.get('start', False),
+        "start_time": int(time.time() * factor_s_to_us),
+        "pid": os.getpid(),
+        "output_path": "",
+        "profiler_path": "",
+        "profile_memory": profiling_options.get("memory", False),
+        "profile_communication": profiling_options.get("hccl", False),
+        "aicore_metrics": profiling_options.get("aicore_metrics", 0),
+        "l2_cache": profiling_options.get("l2_cache", False),
+        "sync_enable": profiling_options.get("sync_enable", True)
+    }
+    return options
+
+
+class EnvProfiler:
+    """Collect and analyze training performance data, support calls during and after training."""
+
+    def __init__(self):
+        self._profiling_options = ''
+        self._environ_enable = False
+        self._output_path = False
+        self.memory = False
+        self.hccl = False
+        self.aicore_metrics = 0
+        self.l2_cache = False
+        self.sync_enable = True
+        self.start_time = 0
+
+    def analyse(self):
+        """Determine whether to stop collecting and parsing performance data based on environment variables."""
+        options = construct_profiling_options()
+        self._environ_enable = options.get("start")
+        self._output_path = options.get("profiler_path")
+        self.memory = options.get("profile_memory")
+        self.hccl = options.get("profile_communication")
+        self.aicore_metrics = options.get("aicore_metrics")
+        self.l2_cache = options.get("l2_cache")
+        self.sync_enable = options.get("sync_enable")
+        if not self._environ_enable:
+            return
+        env_options = json.loads(os.getenv("MS_PROFILER_RUN_CONFIG", "{}"))
+        if not env_options.get("pid", 0) == os.getpid():
+            return
+        self.start_time = env_options.get("start_time")
+        options = {
+            "output_path": self._output_path,
+            "profile_memory": self.memory,
+            "profile_communication": self.hccl,
+            "aicore_metrics": self.aicore_metrics,
+            "l2_cache": self.l2_cache,
+            "start_time": self.start_time,
+            "sync_enable": self.sync_enable
+        }
+        profiler = Profiler(env_enable=options)
+        profiler.analyse()
+
+
+def profiler_check_env():
+    """Profiler initialization according to environment."""
+    if os.getenv("MS_PROFILER_RUN_CONFIG"):
+        return
+    config = construct_profiling_options()
+    if not config.get("start"):
+        return
+    os.environ["MS_PROFILER_RUN_CONFIG"] = json.dumps(config)
+    Profiler(output_path=config.get("output_path"),
+             profile_memory=config.get("profile_memory"),
+             profile_communication=config.get("profile_communication"),
+             aicore_metrics=config.get("aicore_metrics"),
+             l2_cache=config.get("l2_cache"),
+             sync_enable=config.get("sync_enable"))
+
+
+profiler_check_env()
