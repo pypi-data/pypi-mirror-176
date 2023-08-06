8b76128caad41ab60d736048963086efc4908ff5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:11:50 +0000

    Add package programlib/3.0.0/programlib-3.0.0-py3-none-any.whl

---
+++ packages/programlib/programlib-3.0.0-py3-none-any.whl/programlib/programs/1f24dd61-ea5a-4c4a-96bc-5e0c81123d4c.cpp
@@ -0,0 +1,38 @@
+using namespace std;
+#include <vector>
+#include <iostream>
+#include <string>
+#include <cstring>
+#include <queue>
+#include <stdio.h>
+#include <math.h>
+#include <map>
+#include <set>
+#include <stack>
+#include <climits>
+
+/*
+Hello World
+
+Write a program that prints "Hello World"
+
+For example: 
+H
+e
+l
+l
+o
+ 
+W
+o
+r
+l
+d
+
+
+*/
+
+int main() {
+
+	cout << "Hello World" << endl;
+	
