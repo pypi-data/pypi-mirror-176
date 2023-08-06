a377b4db39a64c47a8993bed62616435c302e97d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:12:02 +0000

    Add package jackyleeutils/0.0.10/jackyleeutils-0.0.10.tar.gz

---
+++ packages/jackyleeutils/jackyleeutils-0.0.10.tar.gz/jackyleeutils-0.0.10/setup.py
@@ -0,0 +1,11 @@
+import setuptools
+from pathlib import Path
+
+setuptools.setup(
+    name="jackyleeutils",
+    author="jackyleeutils",
+    version="0.0.10",
+    long_description=Path("README.md").read_text(encoding="utf-8"),
+    packages=setuptools.find_packages(
+        exclude=["data", "tests", "scripts", "log"])
+)
