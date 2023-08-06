857cca77d62b7f8950bfb78fec90dd433f6650e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:23:47 +0000

    Add package hawktuner/0.0.2/HawkTuner-0.0.2.tar.gz

---
+++ packages/hawktuner/HawkTuner-0.0.2.tar.gz/HawkTuner-0.0.2/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+# 若Discription.md中有中文 須加上 encoding="utf-8"
+with open("Discription.md", "r",encoding="utf-8") as f:
+    long_description = f.read()
+    
+setuptools.setup(
+    name = "HawkTuner",
+    version = "0.0.2",
+    author = "hawktorng",
+    author_email="cool870427@gmail.com",
+    description="ML DL tuner",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com",                                         packages=setuptools.find_packages(),     
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6'
+    )
