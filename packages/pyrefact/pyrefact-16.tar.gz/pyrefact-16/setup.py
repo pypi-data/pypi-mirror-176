0d90031ce830514e35e556eb7d48dc0872b84a3c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:09:21 +0000

    Add package pyrefact/16/pyrefact-16.tar.gz

---
+++ packages/pyrefact/pyrefact-16.tar.gz/pyrefact-16/setup.py
@@ -0,0 +1,19 @@
+"""Installer for pyrefact"""
+import sys
+import warnings
+
+import setuptools
+
+if tuple(sys.version_info) < (3, 9):
+    warnings.warn("Pyrefact is not tested with python < 3.9, and may not work.")
+
+setuptools.setup(
+    name="pyrefact",
+    version="16",
+    description="Automatic python refactoring",
+    author="Olle Lindgren",
+    author_email="olle.ln@outlook.com",
+    packages=["pyrefact"],
+    install_requires=["black>=22.1.0", "pylint>=2.12.2", "rmspace>=6"],
+    url="https://github.com/OlleLindgren/pyrefact",
+)
