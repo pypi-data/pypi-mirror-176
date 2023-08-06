4eb88b444bfe6375e135c252337a49adfc7cf27f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:11:38 +0000

    Add package dogejrslib/0.0.1/dogejrslib-0.0.1.tar.gz

---
+++ packages/dogejrslib/dogejrslib-0.0.1.tar.gz/dogejrslib-0.0.1/dogejrslib/__init__.py
@@ -0,0 +1,1 @@
+from dogejrsfuncs import test
---
+++ packages/dogejrslib/dogejrslib-0.0.1.tar.gz/dogejrslib-0.0.1/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+VERSION = '0.0.1'
+DESCRIPTION = 'dogejrs first pypi lib!'
+LONG_DESCRIPTION = 'dogejrs first pypi lib!'
+
+# Setting up
+setup(
+    name="dogejrslib",
+    version=VERSION,
+    author="Doge Jr The 1st",
+    author_email="<DogeJuniorOnADisc@hotmail.com>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    packages=find_packages(),
+    install_requires=[],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
