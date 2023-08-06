a84ed600d3173a9285a875e2f38fee95b13f2ef5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:14:10 +0000

    Add package aiges/0.5.0.dev0/aiges-0.5.0.dev0.tar.gz

---
+++ packages/aiges/aiges-0.5.0.dev0.tar.gz/aiges-0.5.0.dev0/setup.py
@@ -0,0 +1,41 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['aiges',
+ 'aiges.aiges_inner',
+ 'aiges.backup',
+ 'aiges.client',
+ 'aiges.client.utils',
+ 'aiges.cmd',
+ 'aiges.core',
+ 'aiges.examples.once.mmocr',
+ 'aiges.examples.stream.mmocr',
+ 'aiges.examples.stream.mock',
+ 'aiges.serve',
+ 'aiges.utils']
+
+package_data = \
+{'': ['*'], 'aiges': ['test_data/*', 'tpls/*']}
+
+install_requires = \
+['jinja2>=2.0']
+
+setup_kwargs = {
+    'name': 'aiges',
+    'version': '0.5.0.dev0',
+    'description': "A module for test aiges's python wrapper.py",
+    'long_description': None,
+    'author': 'maybaby',
+    'author_email': 'ybyang7@iflytek.com',
+    'maintainer': None,
+    'maintainer_email': None,
+    'url': None,
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'python_requires': '>=3.6',
+}
+
+
+setup(**setup_kwargs)
