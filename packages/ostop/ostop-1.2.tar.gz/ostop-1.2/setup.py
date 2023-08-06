b5f6821656d844d4308f2b7702db00d2722d5f48
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:17:38 +0000

    Add package ostop/1.2/ostop-1.2.tar.gz

---
+++ packages/ostop/ostop-1.2.tar.gz/ostop-1.2/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name="ostop",
+    version="1.2",
+    license="MIT",
+    author="Katherine Burgess",
+    author_email="20burgessk@gmail.com",
+    packages=find_packages("src"),
+    package_dir={"": "src"},
+    url="https://github.com/burgess01/top",
+    keywords="top",
+    install_requires=[
+        "schedule",
+        "psutil",
+        "datetime",
+        "hurry.filesize",
+        "platform",
+    ],
+)
