639b70f8288ad32ec1b6a8e8120fefcc679a8183
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:24:09 +0000

    Add package codiga/0.1.0/codiga-0.1.0.tar.gz

---
+++ packages/codiga/codiga-0.1.0.tar.gz/codiga-0.1.0/codiga/constants.py
@@ -0,0 +1,11 @@
+GRAPHQL_ENDPOINT_PROD_URL = 'https://api.codiga.io/graphql'
+GRAPHQL_ENDPOINT_STAGING_URL = 'https://api-staging.codiga.io/graphql'
+
+DEFAULT_TIMEOUT = 600  # 20 minutes
+VALID_SCM_KINDS = ["Bitbucket", "Git", "Github", "Gitlab"]
+
+BLANK_SHA = "0000000000000000000000000000000000000000"
+
+API_TOKEN_ENVIRONMENT_VARIABLE = "CODIGA_API_TOKEN"
+
+API_TOKEN_HEADER = "X-Api-Token"
