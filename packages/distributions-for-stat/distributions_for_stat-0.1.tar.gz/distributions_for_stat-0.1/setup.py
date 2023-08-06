e7efe48ffd19026566d1878e31eccde2f9f42be6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:36:04 +0000

    Add package distributions-for-stat/0.1/distributions_for_stat-0.1.tar.gz

---
+++ packages/distributions-for-stat/distributions_for_stat-0.1.tar.gz/distributions_for_stat-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distributions_for_stat',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions_for_stat'],
+      zip_safe=False)
