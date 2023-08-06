c0e85d0bb4cb6c0d226445f99d3e09a9e585641e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:36:02 +0000

    Add package joj-submitter/0.0.10/joj-submitter-0.0.10.tar.gz

---
+++ packages/joj-submitter/joj-submitter-0.0.10.tar.gz/joj-submitter-0.0.10/joj_submitter/__init__.py
@@ -0,0 +1,9 @@
+from joj_submitter.__main__ import Detail as Detail
+from joj_submitter.__main__ import JOJSubmitter as JOJSubmitter
+from joj_submitter.__main__ import Language as Language
+from joj_submitter.__main__ import Record as Record
+from joj_submitter.__main__ import app
+
+
+def main() -> None:
+    app()
