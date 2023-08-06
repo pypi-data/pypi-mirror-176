50a6ff5e7879d8d5ac5f48a3f06b6ec9e48a63ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:04:16 +0000

    Add package gurobi-machinelearning/1.0.1/gurobi-machinelearning-1.0.1.tar.gz

---
+++ packages/gurobi-machinelearning/gurobi-machinelearning-1.0.1.tar.gz/gurobi-machinelearning-1.0.1/src/gurobi_ml/sklearn/pipeline.py
@@ -0,0 +1,139 @@
+# Copyright © 2022 Gurobi Optimization, LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+
+""" Module for embedding a :external+sklearn:py:class:`sklearn.pipeline.Pipeline`
+into a :gurobipy:`model`.
+"""
+
+
+from ..exceptions import NoModel
+from ..modeling.base_predictor_constr import AbstractPredictorConstr
+from ..modeling.get_convertor import get_convertor
+from ..register_user_predictor import user_predictors
+from .predictors_list import sklearn_predictors, sklearn_transformers
+from .skgetter import SKgetter
+
+
+def add_pipeline_constr(gp_model, pipeline, input_vars, output_vars=None, **kwargs):
+    """Embed pipeline into gp_model
+
+    Predict the values of output_vars using input_vars
+
+    Parameters
+    ----------
+    gp_model: :gurobipy:`model`
+        The gurobipy model where the predictor should be inserted.
+    pipeline: :external+sklearn:py:class:`sklearn.pipeline.Pipeline`
+        The pipeline to insert as predictor.
+    input_vars: :gurobipy:`mvar` or :gurobipy:`var` array like
+        Decision variables used as input for regression in model.
+    output_vars: :gurobipy:`mvar` or :gurobipy:`var` array like, optional
+        Decision variables used as output for regression in model.
+
+    Returns
+    -------
+    PipelineConstr
+        Object containing information about what was added to gp_model to embed the
+        predictor into it
+
+    Raises
+    ------
+    NoModel
+        If the translation to Gurobi of one of the elements in the pipeline
+        is not implemented or recognized.
+
+    Note
+    ----
+    |VariablesDimensionsWarn|
+    """
+    return PipelineConstr(gp_model, pipeline, input_vars, output_vars, **kwargs)
+
+
+class PipelineConstr(SKgetter, AbstractPredictorConstr):
+    """Class to model trained :external+sklearn:py:class:`sklearn.pipeline.Pipeline` with gurobipy
+
+    Stores the changes to :gurobipy:`model` when embedding an instance into it."""
+
+    def __init__(self, gp_model, pipeline, input_vars, output_vars=None, **kwargs):
+        self._steps = []
+        self._default_name = "pipe"
+        SKgetter.__init__(self, pipeline, **kwargs)
+        AbstractPredictorConstr.__init__(self, gp_model, input_vars, output_vars, **kwargs)
+
+    def _mip_model(self, **kwargs):
+        pipeline = self.predictor
+        gp_model = self._gp_model
+        input_vars = self._input
+        output_vars = self._output
+        steps = self._steps
+        transformers = sklearn_transformers()
+        for transformer in pipeline[:-1]:
+            convertor = get_convertor(transformer, transformers)
+            if convertor is None:
+                raise NoModel(
+                    self.predictor, f"I don't know how to deal with that object: {transformer}"
+                )
+            steps.append(convertor(gp_model, transformer, input_vars, **kwargs))
+            input_vars = steps[-1].output
+
+        predictor = pipeline[-1]
+        predictors = sklearn_predictors() | user_predictors()
+        convertor = get_convertor(predictor, predictors)
+        if convertor is None:
+            raise NoModel(self.predictor, f"I don't know how to deal with that object: {predictor}")
+        steps.append(convertor(gp_model, predictor, input_vars, output_vars, **kwargs))
+        if self._output is None:
+            self._output = steps[-1].output
+
+    def print_stats(self, file=None):
+        """Print statistics on model additions stored by this class
+
+        This function prints detailed statistics on the variables
+        and constraints that where added to the model.
+
+        The pipeline version includes a summary of the steps that it contains.
+
+        Arguments
+        ---------
+
+        file: None, optional
+            Text stream to which output should be redirected. By default sys.stdout.
+        """
+        super().print_stats(file=file)
+        print(file=file)
+        print(f"Pipeline has {len(self._steps)} steps:", file=file)
+        print(file=file)
+
+        header = f"{'Step':13} {'Output Shape':>14} {'Variables':>12} {'Constraints':^38}"
+        print("-" * len(header), file=file)
+        print(header, file=file)
+        print(f"{' '*41} {'Linear':>12} {'Quadratic':>12} {'General':>12}", file=file)
+        print("=" * len(header), file=file)
+        for step in self:
+            step.print_stats(abbrev=True, file=file)
+            print(file=file)
+        print("-" * len(header), file=file)
+
+    def __getitem__(self, key):
+        """Get an item from the pipeline steps"""
+        return self._steps[key]
+
+    def __iter__(self):
+        """Iterate through pipeline steps"""
+        return self._steps.__iter__()
+
+    def __len__(self):
+        """Get number of pipeline steps"""
+        return self._steps.__len__()
---
+++ packages/gurobi-machinelearning/gurobi-machinelearning-1.0.1.tar.gz/gurobi-machinelearning-1.0.1/src/gurobi_ml/sklearn/skgetter.py
@@ -0,0 +1,77 @@
+# Copyright © 2022 Gurobi Optimization, LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+
+""" Implements some utility tools for all scikit-learn objects """
+import warnings
+
+import numpy as np
+from sklearn.utils.validation import check_is_fitted
+
+from ..exceptions import NoSolution
+
+
+class SKgetter:
+    """Utility class for sklearn convertors
+
+    Implement some common functionalities: check predictor is fitted, output dimension, get error
+
+    Attributes
+    ----------
+    predictor
+        Scikit-Learn predictor embedded into Gurobi model.
+
+    """
+
+    def __init__(self, predictor, output_type="regular", **kwargs):
+        check_is_fitted(predictor)
+        self.predictor = predictor
+        self.output_type = output_type
+        try:
+            self.n_outputs_ = predictor.n_outputs_
+        except AttributeError:
+            pass
+
+    def get_error(self):
+        """Returns error in Gurobi's solution with respect to prediction from input
+
+        Returns
+        -------
+        error: ndarray of same shape as :py:attr:`gurobi_ml.modeling.base_predictor_constr.AbstractPredictorConstr.output`
+            Assuming that we have a solution for the input and output variables
+            `x, y`. Returns the absolute value of the differences between `predictor.predict(x)` and
+            `y`. Where predictor is the regression this object is modeling.
+
+        Raises
+        ------
+        NoSolution
+            If the Gurobi model has no solution (either was not optimized or is infeasible).
+        """
+        if self._has_solution():
+            if self.output_type == "probability_1":
+                with warnings.catch_warnings():
+                    warnings.simplefilter("ignore", category=UserWarning)
+                    predicted = self.predictor.predict_proba(self.input.X)[:, 1]
+            elif self.output_type == "probability":
+                with warnings.catch_warnings():
+                    warnings.simplefilter("ignore", category=UserWarning)
+                    predicted = self.predictor.predict_proba(self.input.X)
+            else:
+                with warnings.catch_warnings():
+                    warnings.simplefilter("ignore", category=UserWarning)
+                    predicted = self.predictor.predict(self.input.X)
+            if len(predicted.shape) == 1:
+                predicted = predicted.reshape(-1, 1)
+            return np.abs(predicted - self.output.X)
+        raise NoSolution()
