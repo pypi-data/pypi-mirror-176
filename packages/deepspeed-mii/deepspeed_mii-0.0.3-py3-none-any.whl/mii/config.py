f573d205195aa33909bbd63ee00676afa3fcd5f9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:38:57 +0000

    Add package deepspeed-mii/0.0.3/deepspeed_mii-0.0.3-py3-none-any.whl

---
+++ packages/deepspeed-mii/deepspeed_mii-0.0.3-py3-none-any.whl/mii/config.py
@@ -0,0 +1,78 @@
+import torch
+from typing import Union, List
+from pydantic import BaseModel, validator
+
+
+class MIIConfig(BaseModel):
+    tensor_parallel: int = 1
+    port_number: int = 50050
+    dtype: str = "float"
+    enable_cuda_graph: bool = False
+    checkpoint_dict: Union[dict, None] = None
+    deploy_rank: Union[int, List[int]] = -1
+    torch_dist_port: int = 29500
+    hf_auth_token: str = None
+    replace_with_kernel_inject: bool = True
+    profile_model_time: bool = False
+    skip_model_check: bool = False
+
+    @validator('dtype')
+    def dtype_valid(cls, value):
+        # parse dtype value to determine torch dtype
+        MIIConfig._torch_dtype(value)
+        return value.lower()
+
+    @validator("deploy_rank")
+    def deploy_valid(cls, field_value, values):
+        if "tensor_parallel" not in values:
+            raise ValueError(
+                "'tensor_parallel' must be defined in the pydantic model before 'deploy_rank'"
+            )
+
+        # if deploy rank is not given, default to align with TP value
+        if field_value == -1:
+            field_value = list(range(values["tensor_parallel"]))
+
+        # ensure deploy rank type is always list for easier consumption later
+        if not isinstance(field_value, list):
+            field_value = [field_value]
+
+        # number of ranks provided must be equal to TP size, DP is handled outside MII currently
+        assert values["tensor_parallel"] == len(field_value), \
+            f"{len(field_value)} rank(s) provided in 'deploy_rank' does not align with tensor_parallel size of {values['tensor_parallel']}"
+        return field_value
+
+    @validator('checkpoint_dict')
+    def checkpoint_dict_valid(cls, value):
+        if value is None:
+            return value
+        if value.get('base_dir', ''):
+            raise ValueError(
+                "please unset 'base_dir' it will be set w.r.t. the deployment 'model_path'"
+            )
+        for k in ['checkpoints', 'parallelization', 'version', 'type']:
+            if not value.get(k, ''):
+                raise ValueError(f"Missing key={k} in checkpoint_dict")
+        return value
+
+    @staticmethod
+    def _torch_dtype(value):
+        value = value.lower()
+        if value == "float" or value == "fp32" or value == "float32":
+            dtype = torch.float
+        elif value == "half" or value == "fp16" or value == "float16":
+            dtype = torch.half
+        elif value == "int8":
+            dtype = torch.int8
+        else:
+            raise ValueError(f"unknown dtype={value}")
+        return dtype
+
+    def torch_dtype(self):
+        return self._torch_dtype(self.dtype)
+
+    class Config:
+        validate_all = True
+        validate_assignment = True
+        use_enum_values = True
+        extra = 'forbid'
---
+++ packages/deepspeed-mii/deepspeed_mii-0.0.3-py3-none-any.whl/mii/models/load_models.py
@@ -0,0 +1,97 @@
+'''
+Copyright 2022 The Microsoft DeepSpeed Team
+'''
+import os
+import mii
+import json
+import torch
+import inspect
+import deepspeed
+from deepspeed.runtime.config import DeepSpeedConfig
+from deepspeed.runtime.zero.config import ZeroStageEnum
+
+
+def load_models(task_name,
+                model_name,
+                model_path,
+                ds_optimize,
+                ds_zero,
+                provider,
+                mii_config,
+                ds_config_path=None):
+    global generator
+    world_size = int(os.getenv('WORLD_SIZE', '1'))
+
+    ds_kwargs = {
+        "checkpoint": None,
+        "mpu": None,
+        "args": None,
+        "training_mp_size": 1,
+        "replace_with_kernel_inject": mii_config.replace_with_kernel_inject
+    }
+
+    if provider == mii.constants.ModelProvider.HUGGING_FACE:
+        from mii.models.providers.huggingface import hf_provider
+        inference_pipeline = hf_provider(model_path, model_name, task_name, mii_config)
+    elif provider == mii.constants.ModelProvider.ELEUTHER_AI:
+        from mii.models.providers.eleutherai import eleutherai_provider
+        assert mii_config.torch_dtype() == torch.half, "gpt-neox only support fp16"
+        assert mii_config.enable_cuda_graph == False, "Provider EleutherAI not supported with Cuda Graphs"
+        from megatron import mpu
+        ds_kwargs["mpu"] = mpu
+        inference_pipeline = eleutherai_provider(model_path,
+                                                 model_name,
+                                                 task_name,
+                                                 mii_config)
+        ds_kwargs["training_mp_size"] = 2
+        ds_kwargs["args"] = inference_pipeline.neox_args
+    elif provider == mii.constants.ModelProvider.HUGGING_FACE_LLM:
+        from mii.models.providers.llm import load_hf_llm
+        assert mii_config.torch_dtype() == torch.half or mii_config.torch_dtype() == torch.int8, "Bloom models only support fp16/int8"
+        assert mii_config.enable_cuda_graph == False, "Bloom models do no support Cuda Graphs"
+        inference_pipeline = load_hf_llm(model_path, model_name, task_name, mii_config)
+        ds_kwargs["checkpoint"] = inference_pipeline.checkpoint_dict
+        if mii_config.torch_dtype() == torch.int8:
+            if "enable_qkv_quantization" in inspect.signature(
+                    deepspeed.init_inference).parameters:
+                ds_kwargs["enable_qkv_quantization"] = True
+    elif provider == mii.constants.ModelProvider.DIFFUSERS:
+        from mii.models.providers.diffusers import diffusers_provider
+        inference_pipeline = diffusers_provider(model_path,
+                                                model_name,
+                                                task_name,
+                                                mii_config)
+        ds_kwargs["replace_with_kernel_inject"] = False  #not supported yet
+        mii_config.enable_cuda_graph = True
+    else:
+        raise ValueError(f"Unknown model provider {provider}")
+
+    print(
+        f"> --------- MII Settings: ds_optimize={ds_optimize}, replace_with_kernel_inject={mii_config.replace_with_kernel_inject}, enable_cuda_graph={mii_config.enable_cuda_graph} "
+    )
+    if ds_optimize:
+        engine = deepspeed.init_inference(getattr(inference_pipeline,
+                                                  "model",
+                                                  inference_pipeline),
+                                          mp_size=world_size,
+                                          dtype=mii_config.torch_dtype(),
+                                          replace_method='auto',
+                                          enable_cuda_graph=mii_config.enable_cuda_graph,
+                                          **ds_kwargs)
+        if mii_config.profile_model_time:
+            engine.profile_model_time()
+        if hasattr(inference_pipeline, "model"):
+            inference_pipeline.model = engine
+
+    elif ds_zero:
+        ds_config = DeepSpeedConfig(ds_config_path)
+        #TODO: don't read ds-config from disk, we should pass this around as a dict instead
+        ds_config_dict = json.load(open(ds_config_path, 'r'))
+        assert ds_config.zero_optimization_stage == ZeroStageEnum.weights, "DeepSpeed ZeRO inference is only supported for ZeRO-3"
+
+        # initialise Deepspeed ZeRO and store only the engine object
+        ds_engine = deepspeed.initialize(model=inference_pipeline.model,
+                                         config_params=ds_config_dict)[0]
+        ds_engine.module.eval()  # inference
+        inference_pipeline.model = ds_engine.module
+    return inference_pipeline
