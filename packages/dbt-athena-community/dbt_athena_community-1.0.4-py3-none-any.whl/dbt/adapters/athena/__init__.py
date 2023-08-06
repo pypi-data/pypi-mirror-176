19b37bab1ac6226917d77763737c1f898ff50024
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:18:57 +0000

    Add package dbt-athena-community/1.0.4/dbt_athena_community-1.0.4-py3-none-any.whl

---
+++ packages/dbt-athena-community/dbt_athena_community-1.0.4-py3-none-any.whl/dbt/adapters/athena/__init__.py
@@ -0,0 +1,14 @@
+from dbt.adapters.athena.connections import AthenaConnectionManager
+from dbt.adapters.athena.connections import AthenaCredentials
+from dbt.adapters.athena.impl import AthenaAdapter
+import dbt.adapters.athena.query_headers
+
+from dbt.adapters.base import AdapterPlugin
+from dbt.include import athena
+
+
+Plugin = AdapterPlugin(
+    adapter=AthenaAdapter,
+    credentials=AthenaCredentials,
+    include_path=athena.PACKAGE_PATH
+)
