f6c20cb8525d089f89fa6a98f1fe2086865ac3bc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:38:42 +0000

    Add package ep-launch/3.5.0/ep-launch-3.5.0.tar.gz

---
+++ packages/ep-launch/ep-launch-3.5.0.tar.gz/ep-launch-3.5.0/eplaunch/workflows/manager.py
@@ -0,0 +1,14 @@
+import inspect
+import os
+import platform
+
+
+
+
+# class FailedWorkflowDetails:
+#     def __init__(self, workflow_file_path, message):
+#         self.workflow_file_path = workflow_file_path
+#         self.message = message
+
+
+
