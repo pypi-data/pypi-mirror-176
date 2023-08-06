e5423977c5d84cb78c3ea250d2aff1a584824d22
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:09:34 +0000

    Add package leopards/0.10.2/leopards-0.10.2.tar.gz

---
+++ packages/leopards/leopards-0.10.2.tar.gz/leopards-0.10.2/leopards/Query.py
@@ -0,0 +1,43 @@
+from functools import partial
+try:
+    from Q import get_key_op, convert_value,check
+except ModuleNotFoundError:  # pragma: no cover
+    from .Q import get_key_op, convert_value,check # pragma: no cover
+
+def Q(iterable:list, query:dict=None, convert_types=True, **kwargs):
+    """
+     Query a list of dictionary or objects by a query dict.
+    :param iterable: the iterable of dicts
+    :param query: dictionary holding your query
+    :param convert_types: try to convert the field in data to match query type
+    :return: Iterable of type filter
+    """
+    def filter_list(item, **kwargs):
+        if type(item)  in (str, int, float, list, tuple):
+            raise TypeError("The item in the list shall be dict or object")
+        if type(item) is not dict:
+            item= item.__dict__
+        for k,v in kwargs.items():
+            if k in("OR" , "__or__"):
+                for q in v:
+                    if filter_list(item,**q):
+                        return True
+                return False
+            elif k in("AND","__and__"):
+                for q in v:
+                    if not filter_list(item,**q):
+                        return False
+                return True
+            elif k in ("NOT", "__not__"):
+               return  not filter_list(item,**v)
+            key, op = get_key_op(k)
+            value = item[key]
+            if convert_types and type(v) != type(value):
+                value = convert_value(value, type(v))
+            if not check(value, op, v):
+                return False
+        return True
+    if query is None: query={}
+    query.update(kwargs)
+    p = partial(filter_list, **query)
+    return filter(p, iterable)
