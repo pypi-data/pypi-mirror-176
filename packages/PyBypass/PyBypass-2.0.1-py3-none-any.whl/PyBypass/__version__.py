7a4e87f3a0b28be407e559901a41ec28742a0b22
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:18:48 +0000

    Add package PyBypass/2.0.1/PyBypass-2.0.1-py3-none-any.whl

---
+++ packages/PyBypass/PyBypass-2.0.1-py3-none-any.whl/PyBypass/__version__.py
@@ -0,0 +1,7 @@
+__title__ = "PyBypass"
+__description__ = "Python module for bypassing different kind of links."
+__version__ = "2.0.1"
+__author__ = "Sanjit Sinha"
+__author_email__ = "sanjitsinha@anipirates.eu.org"
+__license__ = "MIT"
+__copyright__ = "Copyright 2022 @ Sanjit Sinha"
