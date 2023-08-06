eb0defa5dc03bdb083d371b75f3b18cfc43cf4ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:44:52 +0000

    Add package csify/1.0.6/csify-1.0.6.tar.gz

---
+++ packages/csify/csify-1.0.6.tar.gz/csify-1.0.6/demo/main.py
@@ -0,0 +1,28 @@
+# import sys
+# sys.path.insert(0, '../src/csify')
+from src.csify import CSify
+import google_translate_args
+import deepl_args
+import demo
+
+#  This is a demo on how to use the Csify class
+if __name__ == '__main__':
+    #  Initiate an EN-JA code-switcher.
+    code_switcher = CSify(**deepl_args.EN_TO_ENJA)
+    print(code_switcher.generate("your last report was more than two weeks ago."))
+    print(code_switcher.generate("our lives are not our own, from womb to tomb, we're bound to others."))
+    #  Initiate an KO-JA code-switcher.
+    code_switcher = CSify(**google_translate_args.KO_TO_KOJA)
+    print(code_switcher.generate("저는 어제 약국에 가서 약을 많이 샀어요."))
+    """
+    This demo function below is defined at ./demo.py
+    It downloads and extracts the JESC split corpus, a parallel Japanese-English monolingual corpus.
+    Of the extraction results located at ./data/split, we will take the test data (./data/split/test) that contains
+    2000 lines and generate code-switched data from it.
+    The result will be in 2 files:
+    English sentences and code-switched sentences generated from it will be stored in ./data/CSified/EN-Code-Switched
+    Japanese sentences and code-switched sentences generated from it will be stored in ./data/CSified/JA-Code-Switched
+    This demo also features a progress bar that tracks how many sentences it has generated and its speed in 
+    it/s (sentences per second).
+    """
+    demo.generate_jesc_cs()
