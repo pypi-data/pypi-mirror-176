489de668c0d50d05d42fbdccfd9ff43807cafd7f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:36:05 +0000

    Add package sqlglot/10.0.3/sqlglot-10.0.3.tar.gz

---
+++ packages/sqlglot/sqlglot-10.0.3.tar.gz/sqlglot-10.0.3/sqlglot/executor/env.py
@@ -0,0 +1,36 @@
+import datetime
+import re
+import statistics
+
+from sqlglot.helper import PYTHON_VERSION
+
+
+class reverse_key:
+    def __init__(self, obj):
+        self.obj = obj
+
+    def __eq__(self, other):
+        return other.obj == self.obj
+
+    def __lt__(self, other):
+        return other.obj < self.obj
+
+
+ENV = {
+    "__builtins__": {},
+    "datetime": datetime,
+    "locals": locals,
+    "re": re,
+    "bool": bool,
+    "float": float,
+    "int": int,
+    "str": str,
+    "desc": reverse_key,
+    "SUM": sum,
+    "CONCAT": lambda *args: "".join(v for v in args if v),
+    "AVG": statistics.fmean if PYTHON_VERSION >= (3, 8) else statistics.mean,  # type: ignore
+    "COUNT": lambda acc: sum(1 for e in acc if e is not None),
+    "MAX": max,
+    "MIN": min,
+    "POW": pow,
+}
