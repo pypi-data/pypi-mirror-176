ea1de1f3fe1472e50a3e8496d6a62ad4127a7f55
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:11:32 +0000

    Add package cjdb/0.1.0/cjdb-0.1.0.tar.gz

---
+++ packages/cjdb/cjdb-0.1.0.tar.gz/cjdb-0.1.0/cjdb_api/app/__init__.py
@@ -0,0 +1,21 @@
+from cjdb_api.app.vars import DEBUG, CONN_STRING
+from cjdb_api.app.db import db
+from cjdb_api.app.ma import ma
+from cjdb_api.app.routes import api_blueprint
+from flask import Flask
+
+
+def make_app():
+    flask_app = Flask(__name__)
+    flask_app.config["DEBUG"] = DEBUG
+    flask_app.config["SQLALCHEMY_DATABASE_URI"] = CONN_STRING
+    flask_app.config['JSON_SORT_KEYS'] = False
+
+    db.init_app(flask_app)
+    ma.init_app(flask_app)
+
+    flask_app.register_blueprint(api_blueprint, url_prefix="/api")
+
+    return flask_app
+
+
