ffcd5a55eb11725af12c7ed878739009d5a90bff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:34:58 +0000

    Add package foxes/0.2.3/foxes-0.2.3.tar.gz

---
+++ packages/foxes/foxes-0.2.3.tar.gz/foxes-0.2.3/foxes/core/point_data_model.py
@@ -0,0 +1,109 @@
+from abc import abstractmethod
+
+from .data_calc_model import DataCalcModel
+import foxes.variables as FV
+
+
+class PointDataModel(DataCalcModel):
+    """
+    Abstract base class for models that modify
+    point based data.
+    """
+
+    @abstractmethod
+    def output_point_vars(self, algo):
+        """
+        The variables which are being modified by the model.
+
+        Parameters
+        ----------
+        algo : foxes.core.Algorithm
+            The calculation algorithm
+
+        Returns
+        -------
+        output_vars : list of str
+            The output variable names
+
+        """
+        return []
+
+    @abstractmethod
+    def calculate(self, algo, mdata, fdata, pdata):
+        """ "
+        The main model calculation.
+
+        This function is executed on a single chunk of data,
+        all computations should be based on numpy arrays.
+
+        Parameters
+        ----------
+        algo : foxes.core.Algorithm
+            The calculation algorithm
+        mdata : foxes.core.Data
+            The model data
+        fdata : foxes.core.Data
+            The farm data
+        pdata : foxes.core.Data
+            The point data
+
+        Returns
+        -------
+        results : dict
+            The resulting data, keys: output variable str.
+            Values: numpy.ndarray with shape (n_states, n_points)
+
+        """
+        pass
+
+    def run_calculation(self, algo, *data, out_vars, **calc_pars):
+        """
+        Starts the model calculation in parallel, via
+        xarray's `apply_ufunc`.
+
+        Typically this function is called by algorithms.
+
+        Parameters
+        ----------
+        algo : foxes.core.Algorithm
+            The calculation algorithm
+        *data : tuple of xarray.Dataset
+            The input data
+        out_vars: list of str
+            The calculation output variables
+        **calc_pars : dict, optional
+            Additional arguments for the `calculate` function
+
+        Returns
+        -------
+        results : xarray.Dataset
+            The calculation results
+
+        """
+        return super().run_calculation(
+            algo,
+            *data,
+            out_vars=out_vars,
+            loop_dims=[FV.STATE, FV.POINT],
+            out_core_vars=[FV.VARS],
+            **calc_pars
+        )
+
+    def finalize(self, algo, results, clear_mem=False, verbosity=0):
+        """
+        Finalizes the model.
+
+        Parameters
+        ----------
+        algo : foxes.core.Algorithm
+            The calculation algorithm
+        results : xarray.Dataset
+            The calculation results
+        clear_mem : bool
+            Flag for deleting model data and
+            resetting initialization flag
+        verbosity : int
+            The verbosity level
+
+        """
+        super().finalize(algo, clear_mem=clear_mem, verbosity=verbosity)
---
+++ packages/foxes/foxes-0.2.3.tar.gz/foxes-0.2.3/foxes/models/turbine_types/__init__.py
@@ -0,0 +1,3 @@
+from .PCt_file import PCtFile
+from .PCt_two_files import PCtTwoFiles
+from .null_type import NullType
