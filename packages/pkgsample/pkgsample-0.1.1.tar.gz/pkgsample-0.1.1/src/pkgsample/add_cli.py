093480b308229c3525f9957fba031835e8a38405
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:36:46 +0000

    Add package pkgsample/0.1.1/pkgsample-0.1.1.tar.gz

---
+++ packages/pkgsample/pkgsample-0.1.1.tar.gz/pkgsample-0.1.1/src/pkgsample/add_cli.py
@@ -0,0 +1,21 @@
+# COMMANDS. An example of a function to run as a command-line command.
+# If you aren't providing a command, you don't need this file.
+
+import sys
+
+import rich
+
+# This file and function are mentioned in pyproject.toml in the
+# [project.scripts] section, like this:
+#
+#   [project.scripts]
+#   pkgsample_add = "pkgsample.add_cli:add_main"
+#
+
+def add_main():
+    """Add together the numbers on the command line."""
+    nums = [int(a) for a in sys.argv[1:]]
+    rich.print(f"Your numbers are: [bold]{nums}[/bold]")
+
+    total = sum(nums)
+    rich.print(f"They add up to: [bold]{total}[/bold]")
