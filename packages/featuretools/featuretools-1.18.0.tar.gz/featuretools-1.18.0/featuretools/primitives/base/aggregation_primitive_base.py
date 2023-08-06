7e90acfe46e94a9a757a8e8027ea7d76c423ad7e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:13:46 +0000

    Add package featuretools/1.18.0/featuretools-1.18.0.tar.gz

---
+++ packages/featuretools/featuretools-1.18.0.tar.gz/featuretools-1.18.0/featuretools/primitives/base/aggregation_primitive_base.py
@@ -0,0 +1,45 @@
+from featuretools.primitives.base.primitive_base import PrimitiveBase
+
+
+class AggregationPrimitive(PrimitiveBase):
+    stack_on = None  # whitelist of primitives that can be in input_types
+    stack_on_exclude = None  # blacklist of primitives that can be in signature
+    base_of = None  # whitelist of primitives this primitive can be input for
+    base_of_exclude = None  # primitives this primitive can't be input for
+    stack_on_self = True  # determines if primitive can be in input_types for self
+
+    def generate_name(
+        self,
+        base_feature_names,
+        relationship_path_name,
+        parent_dataframe_name,
+        where_str,
+        use_prev_str,
+    ):
+        base_features_str = ", ".join(base_feature_names)
+        return "%s(%s.%s%s%s%s)" % (
+            self.name.upper(),
+            relationship_path_name,
+            base_features_str,
+            where_str,
+            use_prev_str,
+            self.get_args_string(),
+        )
+
+    def generate_names(
+        self,
+        base_feature_names,
+        relationship_path_name,
+        parent_dataframe_name,
+        where_str,
+        use_prev_str,
+    ):
+        n = self.number_output_features
+        base_name = self.generate_name(
+            base_feature_names,
+            relationship_path_name,
+            parent_dataframe_name,
+            where_str,
+            use_prev_str,
+        )
+        return [base_name + "[%s]" % i for i in range(n)]
