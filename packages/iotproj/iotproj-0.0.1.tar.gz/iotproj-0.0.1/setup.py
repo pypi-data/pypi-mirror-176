d1527d4df6704cebebf5b2afd117a14cf972fb81
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:34:54 +0000

    Add package iotproj/0.0.1/iotproj-0.0.1.tar.gz

---
+++ packages/iotproj/iotproj-0.0.1.tar.gz/iotproj-0.0.1/setup.py
@@ -0,0 +1,29 @@
+
+from setuptools import setup
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name='iotproj',
+    version='0.0.1',
+    description='Test project',
+    packages=['iotproj'],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent"
+    ],
+    python_requires='>=3',
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="Felipe Bombardelli",
+    author_email="felipebombardelli@gmail.com",
+    url="https://github.com/bombark/iotproj",
+    entry_points={'console_scripts': [
+        'iotproj = iotproj:app',
+    ]},
+)
