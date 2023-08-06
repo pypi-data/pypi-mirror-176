399f93a81bd7c63879d60fff9e158b71b3b78720
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:05:43 +0000

    Add package jsonjsc/1.2.0/jsonjsc-1.2.0-py3-none-any.whl

---
+++ packages/jsonjsc/jsonjsc-1.2.0-py3-none-any.whl/jsonjsc/decoder.py
@@ -0,0 +1,9 @@
+import json
+
+from jsonjsc.parser import parse
+
+
+class JSONCommentDecoder(json.JSONDecoder):
+    def decode(self, s, **kwargs):
+        s = parse(s)
+        return super(JSONCommentDecoder, self).decode(s, **kwargs)
