d1e4c636081efde88edf7a492e95ea108578a52e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:37:20 +0000

    Add package weathersunday/1.0.1/weathersunday-1.0.1.tar.gz

---
+++ packages/weathersunday/weathersunday-1.0.1.tar.gz/weathersunday-1.0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='weathersunday',
+      version='1.0.1',
+      description='Weather forcast data',
+      author='bbqbb',
+      author_email='kc.bbqbb@gmail.com',
+      install_requires=[''],
+      classifiers=[]
+     )
