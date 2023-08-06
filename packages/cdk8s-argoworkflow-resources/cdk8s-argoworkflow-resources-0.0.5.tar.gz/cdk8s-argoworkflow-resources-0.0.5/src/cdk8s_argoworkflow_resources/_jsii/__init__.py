da5a3ee4cc5dbef21132b63d1fd488d4fb7b18a0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:11:01 +0000

    Add package cdk8s-argoworkflow-resources/0.0.5/cdk8s-argoworkflow-resources-0.0.5.tar.gz

---
+++ packages/cdk8s-argoworkflow-resources/cdk8s-argoworkflow-resources-0.0.5.tar.gz/cdk8s-argoworkflow-resources-0.0.5/src/cdk8s_argoworkflow_resources/_jsii/__init__.py
@@ -0,0 +1,25 @@
+import abc
+import builtins
+import datetime
+import enum
+import typing
+
+import jsii
+import publication
+import typing_extensions
+
+import cdk8s._jsii
+import constructs._jsii
+
+__jsii_assembly__ = jsii.JSIIAssembly.load(
+    "@opencdk8s/cdk8s-argoworkflow-resources",
+    "0.0.5",
+    __name__[0:-6],
+    "cdk8s-argoworkflow-resources@0.0.5.jsii.tgz",
+)
+
+__all__ = [
+    "__jsii_assembly__",
+]
+
+publication.publish()
