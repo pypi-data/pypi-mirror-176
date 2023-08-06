135520e2d97e114d51b82a0c1420f4f99b0d1a91
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:13:48 +0000

    Add package distributions-from-mercy/0.1/distributions_from_mercy-0.1.tar.gz

---
+++ packages/distributions-from-mercy/distributions_from_mercy-0.1.tar.gz/distributions_from_mercy-0.1/setup.py
@@ -0,0 +1,7 @@
+from setuptools import setup
+
+setup(name='distributions_from_mercy',
+      version='0.1',
+      description='Gaussian distributions',
+      packages=['distributions_from_mercy'],
+      zip_safe=False)
