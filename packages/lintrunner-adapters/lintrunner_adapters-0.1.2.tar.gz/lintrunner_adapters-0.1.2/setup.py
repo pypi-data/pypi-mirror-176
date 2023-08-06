0bf63f38401de3db5a5f91b6d7669641a570680d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:36:38 +0000

    Add package lintrunner-adapters/0.1.2/lintrunner_adapters-0.1.2.tar.gz

---
+++ packages/lintrunner-adapters/lintrunner_adapters-0.1.2.tar.gz/lintrunner_adapters-0.1.2/setup.py
@@ -0,0 +1,37 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['lintrunner_adapters',
+ 'lintrunner_adapters._common',
+ 'lintrunner_adapters.adapters',
+ 'lintrunner_adapters.tools']
+
+package_data = \
+{'': ['*']}
+
+install_requires = \
+['click>=8.1.3,<9.0.0']
+
+entry_points = \
+{'console_scripts': ['lintrunner_adapters = lintrunner_adapters.__main__:cli']}
+
+setup_kwargs = {
+    'name': 'lintrunner-adapters',
+    'version': '0.1.2',
+    'description': 'Adapters and tools for lintrunner',
+    'long_description': '# lintrunner-adapters\n\n[![Test](https://github.com/justinchuby/lintrunner-adapters/actions/workflows/test.yml/badge.svg)](https://github.com/justinchuby/lintrunner-adapters/actions/workflows/test.yml) [![Lint](https://github.com/justinchuby/lintrunner-adapters/actions/workflows/lint.yml/badge.svg)](https://github.com/justinchuby/lintrunner-adapters/actions/workflows/lint.yml) [![PyPI version](https://badge.fury.io/py/lintrunner-adapters.svg)](https://badge.fury.io/py/lintrunner-adapters)\n\nAdapters for [lintrunner](https://github.com/suo/lintrunner)\n\n## Usage\n\n```text\nUsage: python -m lintrunner_adapters [OPTIONS] COMMAND [ARGS]...\n\nOptions:\n  --help  Show this message and exit.\n\nCommands:\n  run       Run an adapter.\n  to-sarif  Convert the output of lintrunner json (INPUT) to SARIF (OUTPUT).\n```\n',
+    'author': 'Justin Chu',
+    'author_email': 'justinchu@microsoft.com',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'https://github.com/justinchuby/lintrunner-adapters',
+    'packages': packages,
+    'package_data': package_data,
+    'install_requires': install_requires,
+    'entry_points': entry_points,
+    'python_requires': '>=3.7,<4.0',
+}
+
+
+setup(**setup_kwargs)
