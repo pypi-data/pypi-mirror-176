f1d126e8ea3c84cf86db3b622ea1c64c97f727f5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:05:09 +0000

    Add package zlibrary-module/0.0.3/zlibrary_module-0.0.3.tar.gz

---
+++ packages/zlibrary-module/zlibrary_module-0.0.3.tar.gz/zlibrary_module-0.0.3/src/zlibrary_module/zlib.py
@@ -0,0 +1,61 @@
+from bs4 import BeautifulSoup as bs
+import requests
+
+baseurl="https://zlibrary.to/"
+
+class zlib:
+    def search(query: str):
+        return searchbook(query)
+    def select(query: str, selection):
+        return searchbook(query, selection)
+
+
+def searchbook(query:str):
+    query=query.lower()
+    query=query.replace(" ","-")
+    query=query.replace("(","")
+    query=query.replace(")","")
+    query=query.replace(".","")
+    search=baseurl+"top-"+query+"-books"
+    url = requests.get(search)
+    url=url.text
+    soup=bs(url,"html.parser")
+    texts = soup.find_all('a')
+    searchresults=[]
+    for text in texts:
+        names=(text.get_text())
+        searchresults.append(names)
+    del searchresults[0:8]
+    del searchresults[-8:-1]
+    for i in range(0,10):
+        number=i+1
+        number=str(number)
+        print(number+"."+searchresults[i])
+    return searchresults
+
+def selectbook(query:str,selection:int):
+    query=query.lower()
+    query=query.replace(" ","-")
+    query=query.replace("(","")
+    query=query.replace(")","")
+    query=query.replace(".","")
+    search=baseurl+"top-"+query+"-books"
+    url = requests.get(search)
+    url=url.text
+    soup=bs(url,"html.parser")
+    texts = soup.find_all('a')
+    searchresults=[]
+    for text in texts:
+        names=(text.get_text())
+        searchresults.append(names)
+    del searchresults[0:8]
+    del searchresults[-8:-1]
+    book=searchresults[selection-1]
+    book=book.lower()
+    book=book.replace("(","")
+    book=book.replace(" - ","-")
+    book=book.replace(" ","-")
+    book=book.replace(")","")
+    book=book.replace(".","")
+    url="https://zlibrary.to/pdfs/"+book
+    print(url)
