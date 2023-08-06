0e60b33eb21b499f40125f473adc213fffeb1987
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:20:18 +0000

    Add package trivialscan/0.3.9/trivialscan-0.3.9.tar.gz

---
+++ packages/trivialscan/trivialscan-0.3.9.tar.gz/trivialscan-0.3.9/src/trivialscan/evaluations/certificate/trust_libcurl.py
@@ -0,0 +1,24 @@
+from typing import Union
+
+from tlstrust import TrustStore, context
+
+from ...exceptions import EvaluationNotRelevant, NoLogEvaluation
+from ...transport import TLSTransport
+from ...certificate import BaseCertificate, RootCertificate
+from .. import BaseEvaluationTask
+
+
+class EvaluationTask(BaseEvaluationTask):
+    def __init__(  # pylint: disable=useless-super-delegation
+        self, transport: TLSTransport, metadata: dict, config: dict
+    ) -> None:
+        super().__init__(transport, metadata, config)
+
+    def evaluate(self, certificate: BaseCertificate) -> Union[bool, None]:
+        if not isinstance(certificate, RootCertificate):
+            raise EvaluationNotRelevant
+        if not certificate.subject_key_identifier:
+            raise NoLogEvaluation
+        return TrustStore(certificate.subject_key_identifier).check_trust(
+            context_type=context.SOURCE_CURL
+        )
