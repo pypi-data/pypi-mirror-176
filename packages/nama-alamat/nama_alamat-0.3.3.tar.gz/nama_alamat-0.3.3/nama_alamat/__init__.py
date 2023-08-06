031048042b98b274f4d06290e88fc0a379f34cf8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:14:34 +0000

    Add package nama-alamat/0.3.3/nama_alamat-0.3.3.tar.gz

---
+++ packages/nama-alamat/nama_alamat-0.3.3.tar.gz/nama_alamat-0.3.3/nama_alamat/__init__.py
@@ -0,0 +1,6 @@
+"""Top-level package for Indonesia Name and Address Preprocessing."""
+from __future__ import annotations
+
+__author__ = """Esha Indra"""
+__email__ = "esha.indra@gmail.com"
+__version__ = "0.3.0"
