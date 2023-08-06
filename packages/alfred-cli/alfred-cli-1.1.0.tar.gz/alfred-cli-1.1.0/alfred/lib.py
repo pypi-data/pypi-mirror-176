31bc3cf6ac49666b5639ed1ff5064ba8f1773d13
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:14:40 +0000

    Add package alfred-cli/1.1.0/alfred-cli-1.1.0.tar.gz

---
+++ packages/alfred-cli/alfred-cli-1.1.0.tar.gz/alfred-cli-1.1.0/alfred/lib.py
@@ -0,0 +1,61 @@
+import io
+import os
+import sys
+from typing import List, Iterator
+
+import click
+
+from alfred.type import path
+
+ROOT_DIR = os.path.realpath(os.path.join(__file__, '..'))
+
+
+class InvalidPythonModule(Exception):
+    pass
+
+
+def import_python(python_path: path) -> dict:
+
+    module = {"__file__": python_path}
+    with io.open(python_path, encoding="utf8") as file:
+        content = file.read()
+        try:
+            code = compile(content, python_path, 'exec')
+            eval(code, module, module)  # pylint: disable=eval-used
+        except Exception as exception:
+            rows = content.split("\n")
+            exc_type, _, exc_tb = sys.exc_info()
+            line = "N/D"
+            if hasattr(exc_tb, "tb_next"):
+                line = exc_tb.tb_next.tb_lineno
+            raise InvalidPythonModule(f"""Invalid command module : "{python_path}", Line {line}
+  {rows[line - 1]}\n
+  {exc_type.__name__} : {exception}\n""") from exception
+
+    return module
+
+
+def list_python_modules(folder_path: path) -> Iterator[path]:
+    for filename in os.listdir(folder_path):
+        if filename.endswith('.py') and filename != '__init__.py':
+            python_path = path(os.path.join(folder_path, filename))
+            yield python_path
+
+
+def list_hierarchy_directory(workingdir: path) -> List[path]:
+    workingdir = os.path.realpath(workingdir)
+    all_parts = workingdir.split(os.sep)
+
+    result = [workingdir]
+    while len(all_parts) != 1:
+        all_parts.pop()
+        if len(all_parts) == 1:
+            result.append(os.sep)
+        else:
+            result.append(os.sep.join(all_parts))
+
+    return result
+
+
+def print_error(msg: str):
+    click.echo(click.style(msg, fg="red", bold=True), err=True)
---
+++ packages/alfred-cli/alfred-cli-1.1.0.tar.gz/alfred-cli-1.1.0/setup.py
@@ -0,0 +1,43 @@
+from setuptools import setup
+from setuptools import find_packages
+
+setup(
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Intended Audience :: Developers",
+        "Environment :: Console"
+    ],
+    extras_require={
+        'dev': [
+            'pylint',
+            'coverage',
+            'twine'
+        ],
+        "dev_windows": [
+
+        ]
+    },
+    license='MIT license',
+    long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
+    entry_points = {
+        'console_scripts': [
+            'alfred = alfred.cli:cli',
+        ],
+    },
+    include_package_data=True,
+    install_requires = [
+        "click",
+        "plumbum",
+        "PyYAML"
+    ],
+    name='alfred-cli',
+    package_data={
+        'resources': ['alfred/resources/*'],
+    },
+    packages=find_packages(exclude=["tests.*", "tests"]),
+    version='1.1.0',
+    url='https://github.com/FabienArcellier/alfred-cli#alfred'
+)
