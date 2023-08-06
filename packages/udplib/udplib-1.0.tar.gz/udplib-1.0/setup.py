c38c6a6903c607e5ee66e39d6cc7e8dab8fde0ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:18:57 +0000

    Add package udplib/1.0/udplib-1.0.tar.gz

---
+++ packages/udplib/udplib-1.0.tar.gz/udplib-1.0/setup.py
@@ -0,0 +1,15 @@
+import setuptools
+
+setuptools.setup(
+    name="udplib",
+    version="1.0",
+    author="Huizixuan",
+    long_description_content_type="text/markdown",
+    url="https://github.com/",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+)
