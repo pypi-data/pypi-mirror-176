97784a76484691f4bcaf2f322039e7ff9b402ee3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:17:45 +0000

    Add package mage-ai/0.7.5/mage_ai-0.7.5-py3-none-any.whl

---
+++ packages/mage-ai/mage_ai-0.7.5-py3-none-any.whl/mage_ai/data_integrations/sources/constants.py
@@ -0,0 +1,12 @@
+SOURCES = [
+    dict(name='Amplitude'),
+    dict(name='BigQuery'),
+    dict(name='Google Search Console'),
+    dict(name='Google Sheets'),
+    dict(name='Intercom'),
+    dict(name='MySQL'),
+    dict(name='PostgreSQL'),
+    dict(name='Redshift'),
+    dict(name='Salesforce'),
+    dict(name='Stripe'),
+]
