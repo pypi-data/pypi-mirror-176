d8fabf2abd9c39efb7b5166f5e80a7f0dff1062b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:09:25 +0000

    Add package sparrow-zoo/0.1.2.dev1668604397/sparrow-zoo-0.1.2.dev1668604397.tar.gz

---
+++ packages/sparrow-zoo/sparrow-zoo-0.1.2.dev1668604397.tar.gz/sparrow-zoo-0.1.2.dev1668604397/sparrow_zoo/detector/base.py
@@ -0,0 +1,7 @@
+from ..base import SparrowModel
+
+
+class SparrowDetector(SparrowModel):
+    """Sparrow object detection model."""
+
+    output_names: tuple[str, str, str] = ("boxes", "scores", "labels")
