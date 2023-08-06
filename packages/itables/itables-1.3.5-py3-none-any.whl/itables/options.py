514ecec975938e52846714af2225a414cfbc5fcd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:42:34 +0000

    Add package itables/1.3.5/itables-1.3.5-py3-none-any.whl

---
+++ packages/itables/itables-1.3.5-py3-none-any.whl/itables/options.py
@@ -0,0 +1,37 @@
+"""Global options for the Interactive Tables"""
+from .utils import read_package_file
+
+"""Show the index? Possible values: True, False and 'auto'. In mode 'auto', the index is not shown
+if it has no name and its content is range(N)"""
+showIndex = "auto"
+
+"""Default styling options. See https://datatables.net/manual/styling/classes"""
+classes = ["display"]
+
+"""Default table style"""
+style = "width:auto;"
+
+"""Additional tags like e.g. caption"""
+tags = ""
+
+"""Maximum bytes for displaying a table"""
+maxBytes = 2**16
+
+"""Maximum number of rows or columns"""
+# maxRows = 10000
+# maxColumns = 1000
+
+"""Pre dt code"""
+pre_dt_code = ""
+
+"""Optional table footer"""
+footer = False
+
+"""Column filters"""
+column_filters = False
+
+"""Table CSS"""
+css = read_package_file("html/itables.css")
+
+"""Should a warning appear when we have to encode an unexpected type?"""
+warn_on_unexpected_types = True
