21cab0657048378865b73ee55aa8e17b789c7b0d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:39:53 +0000

    Add package teddecor/1.1.2/teddecor-1.1.2-py3-none-any.whl

---
+++ packages/teddecor/teddecor-1.1.2-py3-none-any.whl/teddecor/__init__.py
@@ -0,0 +1,10 @@
+"""TEDDecor
+Inline markown parsing and printing
+
+This is a easy to use library that gives a user access to colored text, bold text,
+underlined text, hyperlinks and much more.
+"""
+
+__version__ = "1.1.2"
+from .TED import TED
+from .Logger import LL, Log, Logger
