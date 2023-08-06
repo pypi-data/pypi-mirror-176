831c9d882a7922a2c301cd3a201bf0d83fa437c7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:13:52 +0000

    Add package nrt-logging/1.2.1/nrt-logging-1.2.1.tar.gz

---
+++ packages/nrt-logging/nrt-logging-1.2.1.tar.gz/nrt-logging-1.2.1/setup.py
@@ -0,0 +1,35 @@
+import os
+from setuptools import setup
+import nrt_logging
+
+
+PATH = os.path.dirname(__file__)
+
+with open(os.path.join(PATH, 'requirements.txt')) as f:
+    requirements = f.read().splitlines()
+
+with open(os.path.join(PATH, 'README.md')) as f:
+    readme = f.read()
+
+setup(
+    name='nrt-logging',
+    version=nrt_logging.__version__,
+    author='Eyal Tuzon',
+    author_email='eyal.tuzon.dev@gmail.com',
+    description='Hierarchical logging in yaml format',
+    keywords='logging logger log hierarchical hierarchy yaml',
+    long_description_content_type='text/markdown',
+    long_description=readme,
+    url='https://github.com/etuzon/Python-NRT-Logging',
+    packages=['nrt_logging'],
+    classifiers=[
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    install_requires=[requirements],
+    data_files=[('', ['requirements.txt'])],
+)
