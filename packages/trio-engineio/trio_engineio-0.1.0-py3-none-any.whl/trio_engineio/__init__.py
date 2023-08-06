ff49e26ee9261580fca71583d35423da3bb74334
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:23:44 +0000

    Add package trio-engineio/0.1.0/trio_engineio-0.1.0-py3-none-any.whl

---
+++ packages/trio-engineio/trio_engineio-0.1.0-py3-none-any.whl/trio_engineio/__init__.py
@@ -0,0 +1,12 @@
+# Copyright (c) 2022, Eric Lemoine
+# All rights reserved.
+#
+# This source code is licensed under the BSD-style license found in the
+# LICENSE file in the root directory of this source tree.
+
+from .exceptions import EngineIoConnectionError
+from .trio_client import EngineIoClient
+
+__version__ = "0.1.4"
+
+__all__ = ["__version__", "EngineIoClient", "EngineIoConnectionError"]
