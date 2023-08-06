4bc11376367bc6754a4e894101e4bdf3914485c9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:34:38 +0000

    Add package inspector-python/0.1.2/inspector-python-0.1.2.tar.gz

---
+++ packages/inspector-python/inspector-python-0.1.2.tar.gz/inspector-python-0.1.2/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='inspector-python',
+    version='0.1.2',
+    extras_require=dict(tests=['pytest']),
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
+    url='https://inspector.dev/',
+    license='MIT',
+    author='Antonio Bruno',
+    author_email='antoniobruno82@gmail.com',
+    description='Connect your Python application to Inspector'
+)
