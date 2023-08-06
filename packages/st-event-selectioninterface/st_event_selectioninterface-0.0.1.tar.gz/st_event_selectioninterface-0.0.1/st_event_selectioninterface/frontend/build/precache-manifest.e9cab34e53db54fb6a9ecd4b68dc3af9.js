cca145d6b0009e94504a3c1d610294f0a81a846a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:35:49 +0000

    Add package st-event-selectioninterface/0.0.1/st_event_selectioninterface-0.0.1.tar.gz

---
+++ packages/st-event-selectioninterface/st_event_selectioninterface-0.0.1.tar.gz/st_event_selectioninterface-0.0.1/st_event_selectioninterface/frontend/build/precache-manifest.e9cab34e53db54fb6a9ecd4b68dc3af9.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e28c268af6f6b61d8c9c1e75eff92f5d",
+    "url": "./index.html"
+  },
+  {
+    "revision": "9963ea6a4362a8983416",
+    "url": "./static/js/2.524d9ab4.chunk.js"
+  },
+  {
+    "revision": "129725115fe778ca4356862cdbd81508",
+    "url": "./static/js/2.524d9ab4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8bc20c32bd0279e1af2e",
+    "url": "./static/js/main.5677e1d7.chunk.js"
+  },
+  {
+    "revision": "7e9d84e346ce158d1e50",
+    "url": "./static/js/runtime-main.44d30fc2.js"
+  }
+]);
