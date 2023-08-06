c2a76ddd324af57410798131f65e74f3db867051
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:41:05 +0000

    Add package discord-cooldown/0.1.4/discord_cooldown-0.1.4-py3-none-any.whl

---
+++ packages/discord-cooldown/discord_cooldown-0.1.4-py3-none-any.whl/discord_cooldown/__init__.py
@@ -0,0 +1,18 @@
+"""
+Package Name: [discord_cooldown]
+HomePage: https://pypi.org/project/discord-cooldown/
+Github: https://github.com/Modern-Realm/discord_cooldown
+
+Short Description:
+• A responsive package for command cooldowns.
+• With this package you can create the command cooldowns which will not get reset whenever the bot re-run.
+
+"""
+
+__title__ = "discord-cooldown"
+__author__ = "P. Sai Keerthan reddy, Modern Realm"
+__license__ = "MIT"
+__copyright__ = "Copyright (c) 2022 skrphenix & Modern Realm"
+__version__ = "0.1.4"
+
+from .cooldown import Cooldown
