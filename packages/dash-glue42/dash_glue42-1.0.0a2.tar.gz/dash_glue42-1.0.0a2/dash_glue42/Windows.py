d80fd399c8886597c0ba265f264f046be3387376
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:10:57 +0000

    Add package dash-glue42/1.0.0a2/dash_glue42-1.0.0a2.tar.gz

---
+++ packages/dash-glue42/dash_glue42-1.0.0a2.tar.gz/dash_glue42-1.0.0a2/dash_glue42/Windows.py
@@ -0,0 +1,49 @@
+# AUTO GENERATED FILE - DO NOT EDIT
+
+from dash.development.base_component import Component, _explicitize_args
+
+
+class Windows(Component):
+    """A Windows component.
+A component exposing the Glue42 Window Management API.
+
+Keyword arguments:
+
+- id (optional):
+    The ID of this component. Used to identify Dash components in
+    callbacks.  The ID must be unique across all components in an app.
+
+- open (dict; optional):
+    Property to open a new Glue42 Window.
+
+    `open` is a dict with keys:
+
+    - name (string; required):
+        A unique window name.
+
+    - options (dict; optional):
+        Options for creating a window.
+
+    - url (string; required):
+        The window URL.
+
+- setProps (optional):
+    Dash-assigned callback that should be called to report property
+    changes to Dash in order to make them available for callbacks."""
+    @_explicitize_args
+    def __init__(self, id=Component.UNDEFINED, open=Component.UNDEFINED, **kwargs):
+        self._prop_names = ['id', 'open', 'setProps']
+        self._type = 'Windows'
+        self._namespace = 'dash_glue42'
+        self._valid_wildcard_attributes =            []
+        self.available_properties = ['id', 'open', 'setProps']
+        self.available_wildcard_properties =            []
+        _explicit_args = kwargs.pop('_explicit_args')
+        _locals = locals()
+        _locals.update(kwargs)  # For wildcard attrs
+        args = {k: _locals[k] for k in _explicit_args if k != 'children'}
+        for k in []:
+            if k not in args:
+                raise TypeError(
+                    'Required argument `' + k + '` was not specified.')
+        super(Windows, self).__init__(**args)
