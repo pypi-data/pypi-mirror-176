53f426506123560c8050d2d617d0f863a8312485
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:36:19 +0000

    Add package txapi/1.0.0/txapi-1.0.0.tar.gz

---
+++ packages/txapi/txapi-1.0.0.tar.gz/txapi-1.0.0/setup.py
@@ -0,0 +1,18 @@
+# -*- coding:utf-8 -*-
+# from distutils.core import setup
+from setuptools import find_packages
+from setuptools import setup
+
+setup(
+    name='txapi',
+    version="1.0.0",
+    description='api project for v2&v3',
+    author='coreylam',
+    author_email='coreylam@163.com',
+    url='https://github.com/coreylam/txapi',
+    license='',
+    include_package_data=True,
+    packages=find_packages(),
+    zip_safe=False,
+    install_requires=[]
+)
---
+++ packages/txapi/txapi-1.0.0.tar.gz/txapi-1.0.0/txapi/demo/config.py
@@ -0,0 +1,6 @@
+api_domain = "tencentcloudapi.com"
+sid = ""
+skey = ""
+region = "ap-chongqing"
+is_debug = True
+is_ssl = True
