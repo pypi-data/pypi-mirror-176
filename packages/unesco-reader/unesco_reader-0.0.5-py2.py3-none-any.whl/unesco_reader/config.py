e8ec09dabd31805fda66843a44e7c6b013086dbc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:09:33 +0000

    Add package unesco-reader/0.0.5/unesco_reader-0.0.5-py2.py3-none-any.whl

---
+++ packages/unesco-reader/unesco_reader-0.0.5-py2.py3-none-any.whl/unesco_reader/config.py
@@ -0,0 +1,16 @@
+"""Configuration file for unesco_reader"""
+
+from pathlib import Path
+
+
+class PATHS:
+    """Paths"""
+
+    package = Path(__file__).resolve().parent.parent
+
+    BASE_URL = "https://apimgmtstzgjpfeq2u763lag.blob.core.windows.net/content/MediaLibrary/bdds/"
+
+    DATASETS = package / 'unesco_reader' / 'glossaries'
+
+    TEST_FILES = package / 'tests' / 'test_files'
+
