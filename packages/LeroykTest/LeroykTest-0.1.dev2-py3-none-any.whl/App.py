8e2e28b2d8760b5d13c82b45afe8dfb6995f88bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:11:57 +0000

    Add package LeroykTest/0.1.dev2/LeroykTest-0.1.dev2-py3-none-any.whl

---
+++ packages/LeroykTest/LeroykTest-0.1.dev2-py3-none-any.whl/App.py
@@ -0,0 +1,9 @@
+from one import one
+from two import two
+
+print("hello setuptools")
+O = one.One()
+O.run()
+
+T = two.Two()
+T.run()
---
+++ packages/LeroykTest/LeroykTest-0.1.dev2-py3-none-any.whl/__init__.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+__doc__ = "注释"
+# 导入其他文件
+__all__ = ["App"]
+
+
+def function():
+    print("Hello LeroykTest")
