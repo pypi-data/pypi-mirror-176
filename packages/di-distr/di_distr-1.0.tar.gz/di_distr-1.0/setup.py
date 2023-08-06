838643f75446d5c8f848f5380fb859c9ca96395e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:45:26 +0000

    Add package di-distr/1.0/di_distr-1.0.tar.gz

---
+++ packages/di-distr/di_distr-1.0.tar.gz/di_distr-1.0/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='di_distr',
+      version='1.0',
+      description='Gaussian and Binomial distributions',
+      packages=['di_distr'],
+      zip_safe=False,
+      py_modules=[],
+      author="Diana")
