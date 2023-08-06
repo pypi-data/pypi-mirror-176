06e51ace86483d27ef2b5b13c8172c8688ce5518
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:10:54 +0000

    Add package some-crap/0.0.1/some-crap-0.0.1.tar.gz

---
+++ packages/some-crap/some-crap-0.0.1.tar.gz/some-crap-0.0.1/setup.py
@@ -0,0 +1,41 @@
+from os import path
+from setuptools import setup
+import some_crap
+
+# read the README for long_description
+cwd = path.abspath(path.dirname(__file__))
+with open(path.join(cwd, 'README.md'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='some-crap',
+    description='Python client for evidently nothing',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    version=some_crap.__version__,
+    license='MIT',
+    author='Some Random guy',
+    author_email='some-random-guy@gmail.com',
+    # url='https://github.com/percy/percy-appium-python',
+    keywords='nothing crap',
+    packages=['something'],
+    include_package_data=True,
+    install_requires=[
+        'Appium-Python-Client'
+        'requests'
+    ],
+    python_requires='>=3.6',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Natural Language :: English',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+    test_suite='tests',
+    tests_require=['Appium-Python-Client', 'httpretty', 'requests'],
+    zip_safe=False
+)
