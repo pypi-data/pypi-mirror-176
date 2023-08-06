ef00469b803047afabda7c28ce5f7f38f3d62eb3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:34:51 +0000

    Add package ggce/0.1.2/ggce-0.1.2-py3-none-any.whl

---
+++ packages/ggce/ggce-0.1.2-py3-none-any.whl/ggce/__init__.py
@@ -0,0 +1,9 @@
+from .logger import logger  # noqa
+from .model import Model  # noqa
+from .engine.system import System  # noqa
+from .executors.solvers import SparseSolver, DenseSolver  # noqa
+
+
+# DO NOT CHANGE ANYTHING BELOW THIS
+__version__ = '0.1.2'
+# DO NOT CHANGE ANYTHING ABOVE THIS
