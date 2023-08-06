d7613b1eaee5a90d9d68f5b84cf9901fb7b80415
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:05:32 +0000

    Add package rssbot/121/rssbot-121.tar.gz

---
+++ packages/rssbot/rssbot-121.tar.gz/rssbot-121/rssbot/run.py
@@ -0,0 +1,37 @@
+# This file is placed in the Public Domain.
+# pylint: disable=R,C,W,C0302
+
+
+"runtime"
+
+
+## import
+
+
+import inspect
+import os
+import sys
+import traceback
+
+
+from .obj import Default
+
+
+## define
+
+
+def __dir__():
+    return (
+            'Cfg',
+           )
+
+
+__all__ = __dir__()
+
+
+## runtime
+
+
+Cfg = Default()
+
+
