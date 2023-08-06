a8a24add13e72852d773d4971d754e360b90027f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:41:26 +0000

    Add package py-json-config/0.1.1/py_json_config-0.1.1.tar.gz

---
+++ packages/py-json-config/py_json_config-0.1.1.tar.gz/py_json_config-0.1.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='py_json_config',
+      version='0.1.1',
+      description='Module to use .json as config',
+      long_description='There is the docs here -> https://github.com/enveloss/py_json_config',
+      packages=['py_json_config'],
+      author_email='stroganov400@gmail.com',
+      zip_safe=False)
