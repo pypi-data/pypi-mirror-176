a09a7100dba4ceaa624929683058370dc82096e8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:11:17 +0000

    Add package movie-bot-api/0.0.12/movie_bot_api-0.0.12-py3-none-any.whl

---
+++ packages/movie-bot-api/movie_bot_api-0.0.12-py3-none-any.whl/moviebotapi/__init__.py
@@ -0,0 +1,40 @@
+from moviebotapi.core.session import Session
+from moviebotapi.douban import DoubanApi
+from moviebotapi.meta import MetaApi
+from moviebotapi.notify import NotifyApi
+from moviebotapi.scraper import ScraperApi
+from moviebotapi.site import SiteApi
+from moviebotapi.subscribe import SubscribeApi
+from moviebotapi.tmdb import TmdbApi
+from moviebotapi.user import UserApi
+from moviebotapi.configapi import ConfigApi
+from moviebotapi.core.common import CommonApi
+
+
+class MovieBotServer:
+    user: UserApi
+    subscribe: SubscribeApi
+    scraper: ScraperApi
+    douban: DoubanApi
+    tmdb: TmdbApi
+    site: SiteApi
+    notify: NotifyApi
+    config: ConfigApi
+    meta: MetaApi
+    common: CommonApi
+
+    def __init__(self, session: Session = None):
+        if session:
+            self.set_session(session)
+
+    def set_session(self, session: Session):
+        self.config = ConfigApi(session)
+        self.user = UserApi(session)
+        self.subscribe = SubscribeApi(session)
+        self.scraper = ScraperApi(session)
+        self.douban = DoubanApi(session)
+        self.tmdb = TmdbApi(session)
+        self.site = SiteApi(session)
+        self.notify = NotifyApi(session)
+        self.meta = MetaApi(session)
+        self.common = CommonApi(session)
