edcb149e224f47f9b782c9a198e844725c81b227
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:38:01 +0000

    Add package replitkey/2.0/replitkey-2.0.tar.gz

---
+++ packages/replitkey/replitkey-2.0.tar.gz/replitkey-2.0/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup
+
+long_description = """
+# REPLIT Database Connecter Module
+
+## How to install
+
+<p>pip install replitkey</p>
+
+## More information
+
+More information about this module is on GitHub Use the link below:
+https://github.com/mas6y6/Replitkey/tree/main"""
+
+setup(name='replitkey',
+      version='2.0',
+      description='ReplitKey can get database values from a repl project',
+      author='mas6y6',
+      packages=['replitkey'],
+      long_description=long_description,
+      zip_safe=False)
