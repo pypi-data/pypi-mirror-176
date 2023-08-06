d0b70c39830afd48e4eeafd83e1c99d46da34d1f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:06:28 +0000

    Add package georetriever/0.1.1/georetriever-0.1.1.tar.gz

---
+++ packages/georetriever/georetriever-0.1.1.tar.gz/georetriever-0.1.1/georetriever/data.py
@@ -0,0 +1,176 @@
+import pandas as pd
+import xarray as xr
+import os
+from numpy import atleast_1d
+from tempfile import mkstemp, mkdtemp
+from shutil import rmtree
+from functools import wraps
+from dask import delayed, compute
+from dask.utils import SerializableLock
+from dask.diagnostics import ProgressBar
+
+import logging
+
+logger = logging.getLogger(__name__)
+
+from .datasets import modules as datamodules
+from .utils.geo_utils import Lith
+
+feature_mapping = {
+    "temperature": "era5",
+    "lithology": "macrostrat",
+}
+
+
+def get_feature(geocutout, module, feature, tmpdir=None):
+    """
+    Load the feature data for a given module.
+    This get the data for a set of features from a module. All modules in
+    `atlite.datasets` are allowed.
+    """
+
+    parameters = geocutout.data.attrs
+    lock = SerializableLock()
+    datasets = list()
+
+    get_data = datamodules[module].get_data
+
+    feature_data = delayed(get_data)(
+        geocutout, feature, tmpdir=tmpdir, lock=lock, **parameters
+    )
+    datasets.append(feature_data)
+
+    datasets = compute(*datasets)
+
+    ds = xr.merge(datasets, compat="equals")
+
+    for v in ds:
+
+        ds[v].attrs["module"] = module
+        fd = datamodules[module].features.items()
+        ds[v].attrs["features"] = [k for k, l in fd if v in l].pop()
+
+    return ds
+
+
+def maybe_remove_tmpdir(func):
+    "Use this wrapper to make tempfile deletion compatible with windows machines."
+
+    @wraps(func)
+    def wrapper(*args, **kwargs):
+        if kwargs.get("tmpdir", None):
+            res = func(*args, **kwargs)
+        else:
+            kwargs["tmpdir"] = mkdtemp()
+            try:
+                res = func(*args, **kwargs)
+            finally:
+                rmtree(kwargs["tmpdir"])
+        return res
+
+    return wrapper
+
+
+def non_bool_dict(d):
+    """Convert bool to int for netCDF4 storing"""
+    return {k: v if not isinstance(v, bool) else int(v) for k, v in d.items()}
+
+
+def make_storable(ds):
+    """
+    Ensures xr.Dataset can be stored as netcdf
+    This entails:
+        - transforming xr.DataArray of Lith objects into a xr.Dataset of str
+
+    Args:
+        ds(xr.Dataset): dataset to be made convertible into netcdf
+
+    Returns:
+        xr.Dataset
+    """
+
+    if "lithology" in ds.variables:
+        lith = ds["lithology"]
+        ds = ds.drop("lithology")
+        ds = xr.merge([ds, Lith.to_dataset(lith)])
+
+    return ds
+
+
+@maybe_remove_tmpdir
+def geocutout_prepare(geocutout, features=None, tmpdir=None, overwrite=False):
+
+    """
+    Parameters
+    ----------
+    cutout : geo_retriever.GeoCutout
+    features : str/list, optional
+        Feature(s) to be prepared. The default slice(None) results in all
+        available features.
+    tmpdir : str/Path, optional
+        Directory in which temporary files (for example retrieved ERA5 netcdf
+        files) are stored. If set, the directory will not be deleted and the
+        intermediate files can be examined.
+    overwrite : bool, optional
+        Whether to overwrite variables which are already included in the
+        cutout. The default is False.
+    Returns
+    -------
+    geocutout : geo_retriever.GeoCutout
+        GeoCutout with prepared data. The variables are stored in `geocutout.data`.
+    """
+
+    logger.warning("Overwrite not yet implemented")
+
+    if geocutout.prepared and not overwrite:
+        logger.info("GeoCutout already prepared")
+        return geocutout
+
+    logger.info(f"Storing temporary files in {tmpdir}")
+
+    features = atleast_1d(features) if features else slice(None)
+    prepared = list(atleast_1d(geocutout.data.attrs["prepared_features"]))
+
+    logging.warning("Double download of prepared features not yet prevented")
+
+    for feature in features:
+        assert feature in feature_mapping, (
+            f"No module for feature {feature} "
+            + f"\n Available features: {feature_mapping}"
+        )
+
+    logging.warning("Double importing not prevented yet")
+    for feature in features:
+        module = feature_mapping[feature]
+
+        logging.info(f"Calculating {feature} with module {module}:")
+
+        ds = get_feature(geocutout, module, feature, tmpdir=tmpdir)
+
+        prepared += [key for key in ds.keys()]
+
+        geocutout.data.attrs.update(dict(prepared_features=list(prepared)))
+
+        attrs = non_bool_dict(geocutout.data.attrs)
+        attrs.update(ds.attrs)
+
+        ds = geocutout.data.merge(ds).assign_attrs(**attrs)
+
+        directory, filename = os.path.split(str(geocutout.path))
+        fd, tmp = mkstemp(suffix=filename, dir=directory)
+
+        os.close(fd)
+
+        with ProgressBar():
+            make_storable(ds).to_netcdf(tmp)
+
+        if geocutout.path.exists():
+            geocutout.data.close()
+            geocutout.path.unlink()
+        os.rename(tmp, geocutout.path)
+
+        geocutout.data = xr.open_dataset(geocutout.path, chunks=geocutout.chunks)
+
+    geocutout.to_object_mode()
+
+    return geocutout
---
+++ packages/georetriever/georetriever-0.1.1.tar.gz/georetriever-0.1.1/setup.py
@@ -0,0 +1,65 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+here = os.path.abspath(os.path.dirname(__file__))
+
+with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
+    long_description = "\n" + fh.read()
+
+VERSION = "0.1.1"
+DESCRIPTION = (
+    "Retrieve geological data for geothermal systems from coordinates in Python!"
+)
+LONG_DESCRIPTION = "See README.me at github.com/LukasFrankenQ/georetriever"
+
+# Setting up
+setup(
+    name="georetriever",
+    version=VERSION,
+    author="Lukas Franken, Hui Ben, Heather Kennedy, Nikolaos Reppas",
+    author_email="<lukas.franken@ed.ac.uk>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=long_description,
+    packages=find_packages(),
+    install_requires=[
+        "numpy",
+        "scipy",
+        "pandas>=0.25",
+        "bottleneck",
+        "numexpr",
+        "xarray>=0.20",
+        "netcdf4",
+        "dask>=2021.10.0",
+        "toolz",
+        "requests",
+        "pyyaml",
+        "rasterio>1.2.10",
+        "shapely",
+        "progressbar2",
+        "tqdm",
+        "pyproj>=2",
+        "geopandas",
+        "cdsapi",
+        "Pillow",
+    ],
+    extras_require={"dev": ["pre-commit", "pytest", "pytest-cov"]},
+    keywords=[
+        "python",
+        "geo",
+        "geological",
+        "heatflow",
+        "thermal",
+        "lithography",
+        "conductivity",
+    ],
+    classifiers=[
+        "Development Status :: 1 - Planning",
+        "Intended Audience :: Developers",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ],
+)
