828e7ffd536397a0ea0940c86012e24ab92137e2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:04:44 +0000

    Add package jejudo/0.0.2b4/jejudo-0.0.2b4.tar.gz

---
+++ packages/jejudo/jejudo-0.0.2b4.tar.gz/jejudo-0.0.2b4/setup.py
@@ -0,0 +1,24 @@
+import os
+import setuptools
+from jejudo._version import __version__
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="jejudo", ## 소문자 영단어
+    version=__version__, ##
+    author="月 ઇ 달토끼 。✿#6777, gawi#9537", ## ex) Sunkyeong Lee
+    author_email=None, ##
+    description="discord is py-cord bot debug tools", ##
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url=None, ##
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3.9",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.9',
+)
