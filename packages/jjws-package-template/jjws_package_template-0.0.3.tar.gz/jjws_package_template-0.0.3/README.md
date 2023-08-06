586bac104fad31d07bfc21f3aeb89ca033b443a9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:24:08 +0000

    Add package jjws-package-template/0.0.3/jjws_package_template-0.0.3.tar.gz

---
+++ packages/jjws-package-template/jjws_package_template-0.0.3.tar.gz/jjws_package_template-0.0.3/README.md
@@ -0,0 +1,4 @@
+# JJW's Python Package Template
+
+My personal package template.
+I wrote this to learn how to do it, don't expect it to be good
---
+++ packages/jjws-package-template/jjws_package_template-0.0.3.tar.gz/jjws_package_template-0.0.3/src/jjws_package_template/__init__.py
@@ -0,0 +1,14 @@
+"""
+Test package for learning pip
+~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+:license: MIT, see LICENSE for more details.
+"""
+
+__title__ = 'test_package_JJWRoeloffs'
+__author__ = 'JJWRoeloffs'
+__license__ = 'MIT'
+__version__ = '0.0.3'
+
+from . import (
+    example_module as example_module
+)
