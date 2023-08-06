14808ca2a77f9112d0b254aa821af2973cf72413
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:17:52 +0000

    Add package e621py-wrapper/0.1.9/e621py_wrapper-0.1.9.tar.gz

---
+++ packages/e621py-wrapper/e621py_wrapper-0.1.9.tar.gz/e621py_wrapper-0.1.9/src/e621py_wrapper/e621_wrapper.py
@@ -0,0 +1,39 @@
+import requests
+import json
+#import e621_endpoints as e621_endpoints
+import e621_endpoints
+headers = {
+    'User-Agent': 'E621 api wrapper',
+}
+class client:
+    def __init__(self):
+        self.loggedin = False
+        self.username = ""
+        self.apikey = ""
+        self.clientjson = ""
+        self.id = ""
+        #add all endpoints
+        self.posts = e621_endpoints.posts(self)
+        self.pools = e621_endpoints.pools(self)
+        self.users = e621_endpoints.users(self)
+        self.tags = e621_endpoints.tags(self)
+        self.notes = e621_endpoints.notes(self)
+        self.util = e621_endpoints.util(self)
+        self.wiki = e621_endpoints.wiki(self)
+        self.forum = e621_endpoints.forum(self)
+    
+    def login(self, username, apikey):
+        #try user login
+        url = "https://e621.net/users/" + username + ".json?login=" + username + "&api_key=" + apikey
+        response = requests.get(url, headers=headers).json()
+        try:
+            if response["success"] == False:
+                return(response)
+        except:
+            self.username = username
+            self.apikey = apikey
+            self.loggedin = True
+            self.clientjson = response
+            self.id = response["id"]
+            response.update({"success": True})
+            return response
