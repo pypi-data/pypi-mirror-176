f8b95cc56f41d7a003f74aa181cf2c7760ae2d28
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:09:29 +0000

    Add package satnogs-network/1.97/satnogs-network-1.97.tar.gz

---
+++ packages/satnogs-network/satnogs-network-1.97.tar.gz/satnogs-network-1.97/network/static/js/station_register_step1.js
@@ -0,0 +1,10 @@
+var hash = $('#choices').data('hash');
+
+$('button').on('click', function(){
+    if(this.id == 'new-station'){
+        window.location.href = '/stations/register/step2/?hash=' + hash;
+    } else {
+        var station = this.dataset.station;
+        window.location.href = '/stations/register/step2/' + station + '/?hash=' + hash;
+    }
+});
