f945f0bc3a10b5743dce7b142d0e59a3f4490454
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:41:06 +0000

    Add package runtimepy/0.8.0/runtimepy-0.8.0-py3-none-any.whl

---
+++ packages/runtimepy/runtimepy-0.8.0-py3-none-any.whl/runtimepy/__init__.py
@@ -0,0 +1,13 @@
+# =====================================
+# generator=datazen
+# version=3.1.0
+# hash=cb9f9636128a06f7bb90199741274cc6
+# =====================================
+
+"""
+Useful defaults and other package metadata.
+"""
+
+DESCRIPTION = "A framework for implementing Python services."
+PKG_NAME = "runtimepy"
+VERSION = "0.8.0"
