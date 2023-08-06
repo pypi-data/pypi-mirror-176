50466cc24275671098fad6a0fece49a6592036b8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:11:41 +0000

    Add package skoufas-dbf-reader/0.0.1rc39.post1/skoufas-dbf-reader-0.0.1rc39.post1.tar.gz

---
+++ packages/skoufas-dbf-reader/skoufas-dbf-reader-0.0.1rc39.post1.tar.gz/skoufas-dbf-reader-0.0.1rc39.post1/tests/test_field_extractors.py
@@ -0,0 +1,239 @@
+from __future__ import annotations
+
+import pytest
+
+from skoufas_dbf_reader.field_extractors import (
+    author_part_from_a01,
+    dewey_from_a04,
+    edition_year_from_a09_a10,
+    edition_from_a07,
+    editor_from_a08_a09,
+    entry_numbers_from_a05_a06_a07_a08,
+    has_author,
+    has_language,
+    language_codes,
+    language_from_a01,
+    no_author_values,
+    pages_from_a11,
+    subtitle_from_a03,
+    title_from_a02,
+    topics_from_a12_to_a15,
+    translator_from_a06,
+)
+
+
+def test_no_author():
+    no_author = no_author_values()
+    assert no_author
+    assert type(no_author) == list
+    assert len(no_author) > 0
+
+
+def test_has_author():
+    assert not has_author(None)
+    assert not has_author("")
+    assert has_author("BLA")
+    assert not has_author("ΧΣ")
+    assert not has_author("X,S")
+    assert not has_author("X.S.")
+    assert not has_author("X.S")
+    assert not has_author("X . Σ.")
+    assert not has_author("X.Σ.")
+    assert not has_author("X.Σ")
+    assert not has_author("Χ.Σ")
+    assert not has_author("Χ.Σ.")
+    assert not has_author("Χ. Σ .")
+    assert not has_author("Χ,Σ")
+    assert not has_author("Χ. Σ.")
+    assert not has_author("Χ.Χ")
+    assert not has_author("Χ.Χ.")
+
+
+def test_language_codes():
+    assert language_codes()
+    assert type(language_codes()) == dict
+    assert len(language_codes()) > 0
+
+
+def test_has_language():
+    assert not has_language(None)
+    assert not has_language("")
+    assert not has_language("GAL")
+    assert not has_language("ΨΑΡΟΜΗΛΙΓΚΟΣ , ΛΑΖΟΥ , ΚΑΡΤΑΛΗΣ ")
+    assert has_language("ΩUENEAU RAYMOND                    GAL")
+    assert not has_language("ΚΕΛΕΣΙΔΗΣ, ΤΕΛΗΣ                       Ι")
+    assert has_language("BITSIOS,DIMITRIS                  AGL")
+    assert has_language("FINLAY GEORG                        GER")
+
+
+def test_author_part_from_a01():
+    assert not author_part_from_a01(None)
+    assert not author_part_from_a01("")
+    assert not author_part_from_a01("X,S")
+    assert author_part_from_a01("GAL") == "GAL"
+    assert author_part_from_a01("ΨΑΡΟΜΗΛΙΓΚΟΣ , ΛΑΖΟΥ , ΚΑΡΤΑΛΗΣ ") == "ΨΑΡΟΜΗΛΙΓΚΟΣ , ΛΑΖΟΥ , ΚΑΡΤΑΛΗΣ"
+    assert author_part_from_a01("ΚΕΛΕΣΙΔΗΣ, ΤΕΛΗΣ                       Ι") == "ΚΕΛΕΣΙΔΗΣ, ΤΕΛΗΣ"
+    assert author_part_from_a01("ΩUENEAU RAYMOND                    GAL") == "ΩUENEAU RAYMOND"
+    assert author_part_from_a01("BITSIOS,DIMITRIS                  AGL") == "BITSIOS,DIMITRIS"
+    assert author_part_from_a01("FINLAY GEORG                        GER") == "FINLAY GEORG"
+    assert author_part_from_a01("ΑΝΑΓΝΩΣΤΑΚΗΣ,ΗΛΙΑΣ    .") == "ΑΝΑΓΝΩΣΤΑΚΗΣ,ΗΛΙΑΣ"
+
+
+def test_language_from_a01():
+    assert not language_from_a01(None)
+    assert not language_from_a01("")
+    assert not language_from_a01("GAL")
+    assert not language_from_a01("ΨΑΡΟΜΗΛΙΓΚΟΣ , ΛΑΖΟΥ , ΚΑΡΤΑΛΗΣ ")
+    assert not language_from_a01("ΚΕΛΕΣΙΔΗΣ, ΤΕΛΗΣ                       Ι")
+    assert language_from_a01("ΩUENEAU RAYMOND                    GAL") == "FR"
+    assert language_from_a01("BITSIOS,DIMITRIS                  AGL") == "EN"
+    assert language_from_a01("FINLAY GEORG                        GER") == "DE"
+
+
+def test_title_from_a02():
+    assert title_from_a02(None) is None
+    assert title_from_a02("") is None
+    assert title_from_a02(" ") is None
+    assert title_from_a02("GAL ") == "GAL"
+    assert title_from_a02(" GAL") == "GAL"
+
+
+def test_subtitle_from_a03():
+    assert subtitle_from_a03(None) is None
+    assert subtitle_from_a03("") is None
+    assert subtitle_from_a03(" ") is None
+    assert subtitle_from_a03("GAL ") == "GAL"
+    assert subtitle_from_a03(" GAL") == "GAL"
+
+
+def test_dewey_from_a04():
+    assert dewey_from_a04(None) is None
+    assert dewey_from_a04("") is None
+    assert dewey_from_a04(" ") is None
+    assert dewey_from_a04("001A") == "001 A"
+    assert dewey_from_a04("001.009ΚΟΝ ") == "001.009 ΚΟΝ"
+    assert dewey_from_a04(" 001.009ΚΟΝ") == "001.009 ΚΟΝ"
+    assert dewey_from_a04("O.50ΠΕΡ") == "050 ΠΕΡ"
+    assert dewey_from_a04("624.183Χ.Σ") == "624.183 ΧΣ"
+    assert dewey_from_a04("320ΤΣΟ") == "320 ΤΣΟ"
+
+    assert dewey_from_a04("HOEMANN") is None
+
+
+def test_entry_numbers_from_a05_a06_a07_a08():
+    assert entry_numbers_from_a05_a06_a07_a08(None, None, None, None) == []
+    assert entry_numbers_from_a05_a06_a07_a08("", "", "", "") == []
+    assert entry_numbers_from_a05_a06_a07_a08(" ", None, "", "") == []
+    assert entry_numbers_from_a05_a06_a07_a08(None, "", "", "") == []
+    assert entry_numbers_from_a05_a06_a07_a08("2710-2709", None, None, None) == ["2710", "2709"]
+    assert entry_numbers_from_a05_a06_a07_a08("2710-2709", "foobar", "baz", "yum") == ["2710", "2709"]
+    assert entry_numbers_from_a05_a06_a07_a08("2710-2709", "-", "baz", "yum") == ["2710", "2709"]
+    assert entry_numbers_from_a05_a06_a07_a08("2710-2709", "-10450", "baz", "yum") == ["2710", "2709", "10450"]
+    assert entry_numbers_from_a05_a06_a07_a08("2710-2709", "1747-1746-1745-", "baz", "yum") == [
+        "2710",
+        "2709",
+        "1747",
+        "1746",
+        "1745",
+    ]
+    assert entry_numbers_from_a05_a06_a07_a08("10-1", "4", "baz", "yum") == ["10", "14"]
+    assert entry_numbers_from_a05_a06_a07_a08("10-1", "5", "baz", "yum") == ["10", "15"]
+    assert entry_numbers_from_a05_a06_a07_a08("10-1", "448 ΚΩΣΤΑΣ ΦΙΛΙΝΗΣ", "baz", "yum") == ["10", "1448"]
+    assert entry_numbers_from_a05_a06_a07_a08(
+        "4225-4226-4228-4229-", "4227-4290-4536-4535-", "4537", "ΕΥΡΩΠΑΙΚ.ΚΕΝΤΡ.ΤΕΧΝΗ"
+    ) == ["4225", "4226", "4228", "4229", "4227", "4290", "4536", "4535", "4537"]
+    assert entry_numbers_from_a05_a06_a07_a08(
+        "5280-5285-5286-5283-",
+        "5284-5278-5277-5279-",
+        "5281-",
+        "5282-6548-6547",
+    ) == "5280-5285-5286-5283-5284-5278-5277-5279-5281-5282-6548-6547".split("-")
+
+
+def test_translator_from_a06():
+    assert translator_from_a06(None) is None
+    assert translator_from_a06("") is None
+    assert translator_from_a06(" ") is None
+    assert translator_from_a06("hello") == "hello"
+    # Part of field06_corrections
+    assert translator_from_a06("-") is None
+    assert translator_from_a06("1203") is None
+    assert translator_from_a06("3ΕΚΔ") is None
+    assert translator_from_a06("2847 ΠΑΠΑΡΡΟΔΟΥ,ΝΙΚ") == "ΠΑΠΑΡΡΟΔΟΥ,ΝΙΚΟΛΑΟΣ"
+
+
+def test_edition_from_a07():
+    assert edition_from_a07(None) is None
+    assert edition_from_a07("") is None
+    assert edition_from_a07(" ") is None
+    assert edition_from_a07("2") == "2"
+    assert edition_from_a07("6ΕΚΔ") == "6"
+    # invalid
+    assert edition_from_a07("ΠΡΟΣΚΗ") is None
+    # year
+    assert edition_from_a07("1932") is None
+    # series
+    assert edition_from_a07("4537") is None
+
+
+def test_editor_from_a08_a09():
+    assert editor_from_a08_a09(None, None) is None
+    assert editor_from_a08_a09("", "") is None
+    assert editor_from_a08_a09(" ", " ") is None
+    assert editor_from_a08_a09("hello", " ") == ("hello", None)
+    assert editor_from_a08_a09("", "city") == (None, "city")
+    assert editor_from_a08_a09("5282-6548-6547", "city") == (None, "city")
+    assert editor_from_a08_a09('"Η ΔΑΜΑΣΚΟΣ"', "city") == ("Η ΔΑΜΑΣΚΟΣ", "city")
+    assert editor_from_a08_a09("X.E", "city") == (None, "city")
+    assert editor_from_a08_a09("hello", "1867") == ("hello", None)
+    assert editor_from_a08_a09("hello", "AMSTERDAN") == ("hello", "AMSTERDAM")
+    assert editor_from_a08_a09("hello", "ΑΘΗΝΑ 1984") == ("hello", "ΑΘΗΝΑ")
+    assert editor_from_a08_a09("hello", "X.T") == ("hello", None)
+
+
+def test_edition_year_from_a09_a10():
+    assert edition_year_from_a09_a10(None, None) is None
+    assert edition_year_from_a09_a10("", None) is None
+    assert edition_year_from_a09_a10(" ", "") is None
+    assert edition_year_from_a09_a10("", "Χ.Τ") is None
+    assert edition_year_from_a09_a10("", "ΑΡΤΑ") is None
+    assert edition_year_from_a09_a10("", "Α989") == 1989
+    assert edition_year_from_a09_a10("", "MCMX") == 1910
+    assert edition_year_from_a09_a10("", "2010") == 2010
+    assert edition_year_from_a09_a10("", "197Ο") == 1970
+    assert edition_year_from_a09_a10("1867", "197Ο") == 1867
+
+    with pytest.raises(Exception) as e_info:
+        edition_year_from_a09_a10("", "foobar")
+    assert e_info.exconly() == "Exception: invalid date [foobar]"
+
+
+def test_pages_from_a11():
+    assert pages_from_a11(None) is None
+    assert pages_from_a11("") is None
+    assert pages_from_a11(" ") is None
+    with pytest.raises(Exception) as e_info:
+        pages_from_a11("foobar")
+    assert e_info.exconly() == "Exception: invalid pages [foobar]"
+    assert pages_from_a11("127Σ") == 127
+    assert pages_from_a11("447Σ Ε") == 447
+    assert pages_from_a11("256ΣΕΓ") == 256
+    assert pages_from_a11("29ΙΣ") == 291
+
+
+def test_topics_from_a12_to_a15():
+    assert topics_from_a12_to_a15(None) == []
+    assert topics_from_a12_to_a15([]) == []
+    assert topics_from_a12_to_a15(["", ""]) == []
+    assert topics_from_a12_to_a15(["\\"]) == []
+    assert topics_from_a12_to_a15(["'"]) == []
+    assert topics_from_a12_to_a15(["A", "B"]) == ["A", "B"]
+    assert topics_from_a12_to_a15(["A-B", "B"]) == ["A", "B"]
+    assert topics_from_a12_to_a15(["19 ΑΙΩΝΑ", "B"]) == ["19 ΑΙΩΝΑΣ", "B"]
+    assert topics_from_a12_to_a15(["foobar-(12-13)"]) == ["12-13", "foobar"]
+    assert topics_from_a12_to_a15(["foobar(12-13)"]) == ["12-13", "foobar"]
+    assert topics_from_a12_to_a15(["foo bar(12-13)"]) == ["12-13", "foo bar"]
+    assert topics_from_a12_to_a15(["foo-bar-(12-13)"]) == ["12-13", "foo", "bar"]
+    assert topics_from_a12_to_a15(["foo bar(1213)"]) == ["1213", "foo bar"]
+    assert topics_from_a12_to_a15(["foo bar(1213qw)"]) == ["1213qw", "foo bar"]
+    assert topics_from_a12_to_a15(["foobar-(19 ΑΙΩΝΑ)"]) == ["19 ΑΙΩΝΑΣ", "foobar"]
---
+++ packages/skoufas-dbf-reader/skoufas-dbf-reader-0.0.1rc39.post1.tar.gz/skoufas-dbf-reader-0.0.1rc39.post1/tests/test_reports.py
@@ -0,0 +1,178 @@
+from __future__ import annotations
+
+import os
+import pytest
+import yaml
+from skoufas_dbf_reader.utilities import all_entries, romanize
+from skoufas_dbf_reader.field_extractors import *
+from collections import defaultdict
+
+
+@pytest.fixture
+def reports_directory() -> str:
+    r = os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir, "reports"))
+    os.makedirs(r, exist_ok=True)
+    return r
+
+
+def minimal_entry(input_entry: dict[int, str], fields: list[int]) -> dict[int, str]:
+    return {i: input_entry[i] for i in fields}
+
+
+def test_report_single_fields(reports_directory: str):
+    field_values: list[set[str]] = [set() for _ in range(31)]
+    for entry in all_entries():
+        for i in range(1, 31):
+            if i in entry and entry[i] and entry[i].strip():
+                field_values[i].add(entry[i].strip())
+    for i in range(1, 31):
+        with open(os.path.join(reports_directory, f"fields_{i:02}.yml"), "w", encoding="utf-8") as outfile:
+            yaml.dump(list(sorted(field_values[i])), outfile, default_flow_style=False, allow_unicode=True)
+
+
+def test_report_single_extracted_fields(reports_directory: str):
+    field_values: defaultdict[str, list[str | list[str]]] = defaultdict(list)
+    for entry in all_entries():
+        author = author_part_from_a01(entry[1])
+        if author:
+            field_values["author"].append(author)
+            if " " in author or "-" in author or author.count(",") != 1:
+                field_values["weird_author"].append(author)
+            else:
+                field_values["plain_author"].append(author)
+        language = language_from_a01(entry[1])
+        if language:
+            field_values["language"].append(language)
+        title = title_from_a02(entry[2])
+        if title:
+            field_values["title"].append(title)
+        subtitle = subtitle_from_a03(entry[3])
+        if subtitle:
+            field_values["subtitle"].append(subtitle)
+        dewey = dewey_from_a04(entry[4])
+        if dewey:
+            field_values["dewey"].append(dewey)
+        entry_numbers = entry_numbers_from_a05_a06_a07_a08(entry[5], entry[6], entry[7], entry[8])
+        field_values["entry_number_lists"].append(entry_numbers)
+        for entry_number in entry_numbers:
+            field_values["entry_numbers"].append(entry_number)
+        translator = translator_from_a06(entry[6])
+        if translator:
+            translators = translator.split("!!")
+            for single_translator in translators:
+                field_values["translator"].append(single_translator)
+                translator_surname_name = single_translator.split(",", maxsplit=1)
+                if len(translator_surname_name) == 2:
+                    field_values["translator_family_name"].append(translator_surname_name[0])
+                    if translator_surname_name[1].endswith("."):
+                        field_values["translator_name_abbreviations"].append(translator_surname_name[1])
+                    else:
+                        field_values["translator_names"].append(translator_surname_name[1])
+        edition = edition_from_a07(entry[7])
+        if edition:
+            field_values["edition"].append(edition)
+        editor = editor_from_a08_a09(entry[8], entry[9])
+        if editor:
+            if not editor[0] or not editor[1]:
+                field_values["editor"].append(f"{editor[0]} // {editor[1]} ({entry[0]})")
+            else:
+                field_values["editor"].append(f"{editor[0]} // {editor[1]}")
+        edition_year = edition_year_from_a09_a10(entry[9], entry[10])
+        if edition_year:
+            field_values["edition_year"].append(str(edition_year))
+        pages = pages_from_a11(entry[11])
+        if pages:
+            field_values["pages"].append(str(pages))
+        topic_list = topics_from_a12_to_a15(
+            [
+                entry[12],
+                entry[13],
+                entry[14],
+                entry[15],
+            ]
+        )
+        field_values["topic_lists"].append(topic_list)
+        for topic in topic_list:
+            field_values["topics"].append(topic)
+
+    for k, values in field_values.items():
+        with open(os.path.join(reports_directory, f"calculated_field_{k}.yml"), "w", encoding="utf-8") as outfile:
+            if values and isinstance(values[0], list):
+                yaml.dump(values, outfile, default_flow_style=False, allow_unicode=True)
+            else:
+                yaml.dump(
+                    list(sorted(set(values), key=lambda x: romanize(x))),
+                    outfile,
+                    default_flow_style=False,
+                    allow_unicode=True,
+                )
+
+
+def is_valid_dewey_strict(d: str):
+    strict_dewey_res = [
+        re.compile(r"[0-9]{3}\.[0-9]+ [^0-9]+"),
+        re.compile(r"[0-9]{3}\.[0-9]+"),
+        re.compile(r"[0-9]{3} [^0-9]+"),
+        re.compile(r"[0-9]{3}"),
+    ]
+    for dewey_re in strict_dewey_res:
+        if dewey_re.fullmatch(d):
+            return True
+    return False
+
+
+def test_report_dewey(reports_directory: str):
+    weird_dewey: list[dict[int, str]] = list()
+    for entry in all_entries():
+        dewey = dewey_from_a04(entry[4])
+        if dewey:
+            if not is_valid_dewey_strict(dewey):
+                weird_dewey.append(minimal_entry(entry, [0, 1, 2, 4, 5, 6]))
+        else:
+            if none_if_empty_or_stripped(entry[4]):
+                weird_dewey.append(minimal_entry(entry, [0, 1, 2, 4, 5, 6]))
+    with open(os.path.join(reports_directory, f"weird_dewey.yml"), "w", encoding="utf-8") as outfile:
+        yaml.dump(weird_dewey, outfile, default_flow_style=False, allow_unicode=True)
+
+
+def test_report_entry_numbers(reports_directory: str):
+    all_entry_numbers: dict[str, dict[int, str]] = dict()
+    no_entry_numbers: list[dict[int, str]] = list()
+    non_numeric: defaultdict[str, list[dict[int, str]]] = defaultdict(list)
+    duplicate_entry_numbers: defaultdict[str, list[dict[int, str]]] = defaultdict(list)
+    for entry in all_entries():
+        entry_numbers = entry_numbers_from_a05_a06_a07_a08(entry[5], entry[6], entry[7], entry[8])
+        if not entry_numbers:
+            no_entry_numbers.append(minimal_entry(entry, [0, 1, 2, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 17, 18, 19]))
+        else:
+            for n in entry_numbers:
+                if n in all_entry_numbers:
+                    if not duplicate_entry_numbers[n]:
+                        duplicate_entry_numbers[n].append(all_entry_numbers[n])
+                    duplicate_entry_numbers[n].append(
+                        minimal_entry(entry, [0, 1, 2, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 17, 18, 19])
+                    )
+                else:
+                    all_entry_numbers[n] = minimal_entry(entry, [0, 1, 2, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 17, 18, 19])
+                if not n.isnumeric():
+                    non_numeric[n].append(minimal_entry(entry, [0, 1, 2, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 17, 18, 19]))
+    with open(os.path.join(reports_directory, f"entry_numbers_no_entry_numbers.yml"), "w", encoding="utf-8") as outfile:
+        yaml.dump(no_entry_numbers, outfile, default_flow_style=False, allow_unicode=True)
+    with open(os.path.join(reports_directory, f"entry_numbers_non_numeric.yml"), "w", encoding="utf-8") as outfile:
+        yaml.dump(dict(non_numeric), outfile, default_flow_style=False, allow_unicode=True)
+    with open(os.path.join(reports_directory, f"entry_numbers_duplicate.yml"), "w", encoding="utf-8") as outfile:
+        yaml.dump(dict(duplicate_entry_numbers), outfile, default_flow_style=False, allow_unicode=True)
+
+
+def test_report_translators(reports_directory: str):
+    weird_translators: list[str] = []
+    valid_translator_re = re.compile(r"[Α-Ω\-]+,[Α-Ω\.]*\.?")
+    for entry in all_entries():
+        translator = translator_from_a06(entry[6])
+        if translator:
+            translators = translator.split("!!")
+            for translator in translators:
+                if not valid_translator_re.fullmatch(translator):
+                    weird_translators.append(translator)
+    with open(os.path.join(reports_directory, f"weird_translators.yml"), "w", encoding="utf-8") as outfile:
+        yaml.dump(weird_translators, outfile, default_flow_style=False, allow_unicode=True)
