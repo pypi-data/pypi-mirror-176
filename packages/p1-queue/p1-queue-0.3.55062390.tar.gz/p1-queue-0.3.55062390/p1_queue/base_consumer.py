eb705f9a06cc40207aef285bd3d04220316ecafb
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:19:41 +0000

    Add package p1-queue/0.3.55062390/p1-queue-0.3.55062390.tar.gz

---
+++ packages/p1-queue/p1-queue-0.3.55062390.tar.gz/p1-queue-0.3.55062390/p1_queue/base_consumer.py
@@ -0,0 +1,52 @@
+# -*- coding: utf-8 -*-
+
+from __future__ import absolute_import
+from builtins import str
+from builtins import object
+import logging
+import os
+import json
+
+from google.cloud import pubsub_v1
+
+LOGGER = logging.getLogger(__name__)
+
+
+class BaseConsumer(object):
+    TOPIC_ID = None
+    subscription_name = None
+    is_running = False
+
+    def __init__(self):
+        self.subscription_name = 'projects/{project_id}/subscriptions/{sub}'.format(
+            project_id=os.getenv('PUBSUB_PROJECT_ID'),
+            sub=self.TOPIC_ID
+        )
+        self.instance = pubsub_v1.SubscriberClient()
+
+    def on_message(self, message_id, data, attributes, publish_time):
+        pass
+
+    def run(self):
+        self.is_running = True
+
+        def handle_message(message):
+            LOGGER.info('Received message # %s: %s, %s, %s' % (
+                message.message_id, message.data, message.attributes, message.publish_time))
+
+            try:
+                self.on_message(message.message_id, json.loads(message.data),
+                                message.attributes, message.publish_time)
+                message.ack()
+            except Exception as e:
+                LOGGER.exception(
+                    'Error occured when handling message: %s', str(e))
+                message.nack()
+
+        self.instance.subscribe(self.subscription_name, handle_message)
+
+        while self.is_running:
+            pass
+
+    def stop(self):
+        self.is_running = False
---
+++ packages/p1-queue/p1-queue-0.3.55062390.tar.gz/p1-queue-0.3.55062390/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup, find_packages
+
+setup(name='p1-queue',
+      version='0.3.55062390',
+      description='Messaging abstraction for AMQP',
+      author='Turfa Auliarachman',
+      author_email='turfa_auliarachman@rocketmail.com',
+      license='MIT',
+      packages=find_packages(),
+      install_requires=[
+          'google-cloud-pubsub==1.7.2'
+      ],
+      zip_safe=False,
+      include_package_data=True,
+      classifiers=[
+          'Intended Audience :: Developers',
+          'Programming Language :: Python :: 2',
+          'Programming Language :: Python :: 3'
+      ])
