7ce2048eebb3db83bedc7cc01d3203162474b309
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:29:43 +0000

    Add package dotshow/1.2.0/dotshow-1.2.0-py3-none-any.whl

---
+++ packages/dotshow/dotshow-1.2.0-py3-none-any.whl/dotshow/__init__.py
@@ -0,0 +1,18 @@
+# __init__.py
+# Copyright (C) 2019 (gnyontu39@gmail.com) and contributors
+#
+
+import inspect
+import os
+import sys
+
+real_path = os.path.dirname(os.path.abspath(__file__)).replace("\\","/")
+sys.path.append(real_path)
+
+__version__ = '1.2.0'
+
+from .matching import dotshow as dotshow
+from .matching import colorshow as colorshow
+from .matching import loadshow as loadshow
+
+__all__ = [dotshow, loadshow, colorshow]
