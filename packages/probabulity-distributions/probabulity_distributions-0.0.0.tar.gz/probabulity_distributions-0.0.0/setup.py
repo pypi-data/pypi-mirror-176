8e70f0927dde06cd8caac92582c3dd7082a97a06
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:02:04 +0000

    Add package probabulity-distributions/0.0.0/probabulity_distributions-0.0.0.tar.gz

---
+++ packages/probabulity-distributions/probabulity_distributions-0.0.0.tar.gz/probabulity_distributions-0.0.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(
+    name = "probabulity_distributions",
+    versions = '0.1',
+    description= 'Gaussian and Binomial Distributions Distributions',
+    packages = ['probability_distributions'],
+    author = 'Vinlaw ZP Mudehwe',
+    author_email = 'vmudehwe3661@gmail.com',
+    zip_safe = False)
