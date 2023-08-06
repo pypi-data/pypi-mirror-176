a7f5bc548c39b98015437b9e4957e20b331d8a66
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:17:51 +0000

    Add package yosh/0.0.1/yosh-0.0.1-py3-none-any.whl

---
+++ packages/yosh/yosh-0.0.1-py3-none-any.whl/yosh.py
@@ -0,0 +1,7 @@
+yosh = int(input('Yoshingiz nechida? '))
+if yosh<=4:
+    print('Sizga kirish bepul.')
+elif yosh<=12:
+    print('Sizga kirish 5000 so\'m')
+else:
+    print('Sizga kirish 10000 so\'m')
