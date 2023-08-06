18c895bc026a4365f73934b5d46c320fdfa0a797
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:38:41 +0000

    Add package readalongs/0.2.20221114/readalongs-0.2.20221114.tar.gz

---
+++ packages/readalongs/readalongs-0.2.20221114.tar.gz/readalongs-0.2.20221114/readalongs/text/make_dict.py
@@ -0,0 +1,54 @@
+##################################################
+#
+# make_dict.py
+#
+# This module takes a text file, marked up with
+# units (e.g. w for word, m for morpheme) and ids
+# and converted to IPA, and produces a
+# .dict file for processing by PocketSphinx.
+#
+##################################################
+
+import chevron
+
+from readalongs.log import LOGGER
+
+DICT_TEMPLATE = """{{#items}}
+{{id}}\t{{pronunciation}}
+{{/items}}
+"""
+
+
+def generate_dict_entries(word_elements, input_filename, unit):
+    nwords = 0
+    for e in word_elements:
+        if "id" not in e.attrib:
+            LOGGER.error(
+                "%s-type element without id in file %s" % (unit, input_filename)
+            )
+        text = e.attrib.get("ARPABET", "").strip()
+        if not text:
+            continue
+        nwords += 1
+        yield e.attrib["id"], text
+    if nwords == 0:
+        raise RuntimeError("No words in dictionary!")
+
+
+def make_dict_object(word_elements, input_filename="'in-memory'", unit="m"):
+    return {
+        word_id: text
+        for word_id, text in generate_dict_entries(word_elements, input_filename, unit)
+    }
+
+
+def make_dict(word_elements, input_filename="'in-memory'", unit="m"):
+    data = {
+        "items": [
+            {"id": word_id, "pronunciation": text}
+            for word_id, text in generate_dict_entries(
+                word_elements, input_filename, unit
+            )
+        ]
+    }
+    return chevron.render(DICT_TEMPLATE, data)
