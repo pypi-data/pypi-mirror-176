48328527cbe68b04193267f035dfc9d8042ffcff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:13:16 +0000

    Add package python-mcs-sdk/0.1.5/python_mcs_sdk-0.1.5-py3-none-any.whl

---
+++ packages/python-mcs-sdk/python_mcs_sdk-0.1.5-py3-none-any.whl/mcs/common/constants.py
@@ -0,0 +1,24 @@
+MCS_POLYGON_MAIN_API = "https://api.multichain.storage/api/"
+MCS_POLYGON_MUMBAI_API = "https://calibration-mcs-api.filswan.com/api/"
+MCS_BSC_API = 'https://calibration-mcs-bsc.filswan.com/api/'
+REST_API_VERSION = "v1"
+GET = "GET"
+POST = "POST"
+DELETE = "DELETE"
+FIL_PRICE_API = "https://api.filswan.com/stats/storage"
+# mcs api
+MCS_PARAMS = "/common/system/params"
+PRICE_RATE = "/billing/price/filecoin"
+PAYMENT_INFO = "/billing/deal/lockpayment/info"
+TASKS_DEALS = "/storage/tasks/deals"
+MINT_INFO = "/storage/mint/info"
+UPLOAD_FILE = "/storage/ipfs/upload"
+DEAL_DETAIL = "/storage/deal/detail/"
+USER_REGISTER = "/user/register"
+USER_LOGIN = "/user/login_by_metamask_signature"
+# contract
+USDC_ABI = "ERC20.json"
+SWAN_PAYMENT_ABI = "SwanPayment.json"
+MINT_ABI = "SwanNFT.json"
+
+CONTRACT_TIME_OUT = 300
