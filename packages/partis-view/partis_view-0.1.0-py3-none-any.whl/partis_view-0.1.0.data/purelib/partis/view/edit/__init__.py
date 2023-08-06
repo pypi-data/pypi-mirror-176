d2de90e6d9738a8189f098ee0b938460df5868de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:38:58 +0000

    Add package partis-view/0.1.0/partis_view-0.1.0-py3-none-any.whl

---
+++ packages/partis-view/partis_view-0.1.0-py3-none-any.whl/partis_view-0.1.0.data/purelib/partis/view/edit/__init__.py
@@ -0,0 +1,24 @@
+# -*- coding: UTF-8 -*-
+
+#+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+from .var_tree import (
+  VariableTree,
+  VariableTreeItem )
+
+from .file_editor import (
+  FileEditor,
+  TextFileEditor )
+
+from .text import (
+  FindReplace,
+  CodeEdit,
+  PlainTextEditor )
+
+from .schema_editor import (
+  SchemaFileEditor,
+  SchemaTreeFileEditor,
+  SchemaStructTreeFileEditor )
+
+from .project import Project
+
+from .editor_map import editor_map
