80127ca69881d41908b01c7681f82b35a26a8917
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:12:34 +0000

    Add package damo/1.5.5/damo-1.5.5-py3-none-any.whl

---
+++ packages/damo/damo-1.5.5-py3-none-any.whl/damo/damo_start.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python3
+# SPDX-License-Identifier: GPL-2.0
+
+"""
+Start DAMON with given parameters.
+"""
+
+import argparse
+
+import _damon
+import _damo_paddr_layout
+
+def set_argparser(parser):
+    _damon.set_explicit_target_no_default_schemes_argparser(parser)
+
+def main(args=None):
+    if not args:
+        parser = argparse.ArgumentParser()
+        set_argparser(parser)
+        args = parser.parse_args()
+
+    _damon.ensure_root_permission()
+    err = _damon.ensure_initialized(args, False)
+    if err != None:
+        print(err)
+        exit(1)
+
+    err, ctx = _damon.turn_explicit_args_damon_on(args)
+    if err:
+        print('could not turn on damon')
+
+if __name__ == '__main__':
+    main()
