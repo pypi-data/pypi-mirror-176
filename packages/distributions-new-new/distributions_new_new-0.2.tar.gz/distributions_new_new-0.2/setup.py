6e0900fa4ff28e4183146424d2b52cbebd86fcb0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:24:31 +0000

    Add package distributions-new-new/0.2/distributions_new_new-0.2.tar.gz

---
+++ packages/distributions-new-new/distributions_new_new-0.2.tar.gz/distributions_new_new-0.2/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='distributions_new_new',
+      version='0.2',
+      description='Gaussian distributions',
+      requirements="python 3.9.13",
+      packages=['distributions_new_new'],
+      zip_safe=False)
