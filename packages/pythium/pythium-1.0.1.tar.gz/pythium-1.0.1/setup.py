42fe4235e2b4199da1c5237d27379c28131da268
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:15:25 +0000

    Add package pythium/1.0.1/pythium-1.0.1.tar.gz

---
+++ packages/pythium/pythium-1.0.1.tar.gz/pythium-1.0.1/setup.py
@@ -0,0 +1,39 @@
+# @Project: pythium
+# @Author：Lucas Liu
+# @Time: 2022/11/15 1:53 PM
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as f:
+    long_description = f.read()
+
+setuptools.setup(
+    name="pythium",
+    version="1.0.1",
+    author="lucas",
+    author_email="ly_liubo@163.com",
+    description="Python based Page Factory.",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/lucas234/pythium",
+    zip_safe=False,
+    license='MIT',
+    install_requires=[
+        'selenium==4.1',
+        'Appium-Python-Client==2.7.1',
+    ],
+    packages=['pythium'],
+    python_requires=">=3",
+    classifiers=[
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Operating System :: Microsoft :: Windows',
+        'Operating System :: POSIX :: Linux',
+        'Operating System :: MacOS :: MacOS X',
+        'Topic :: Internet :: WWW/HTTP :: Browsers',
+        'Topic :: Software Development :: Testing',
+    ],
+)
