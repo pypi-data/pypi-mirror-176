7b0c7d4c179cb8c3417f5c70b1c29573703e57d9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:43:00 +0000

    Add package cloudsen12/0.0.7/cloudsen12-0.0.7-py3-none-any.whl

---
+++ packages/cloudsen12/cloudsen12-0.0.7-py3-none-any.whl/cloudsen12/__init__.py
@@ -0,0 +1,6 @@
+from maskay import Predictor, TensorSat
+from maskay.download.s2 import SAFE as download
+from maskay.library.unetmobv2 import UnetMobV2
+from maskay.utils import MaskayDict
+
+from .main import clean, get_stac_path, load
