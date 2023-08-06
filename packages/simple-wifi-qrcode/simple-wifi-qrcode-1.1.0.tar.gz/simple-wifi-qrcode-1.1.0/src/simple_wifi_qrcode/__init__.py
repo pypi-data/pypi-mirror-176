6b1acecfeebac6c30868956229d07f12d6bf34e0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:47:40 +0000

    Add package simple-wifi-qrcode/1.1.0/simple-wifi-qrcode-1.1.0.tar.gz

---
+++ packages/simple-wifi-qrcode/simple-wifi-qrcode-1.1.0.tar.gz/simple-wifi-qrcode-1.1.0/src/simple_wifi_qrcode/__init__.py
@@ -0,0 +1,3 @@
+"""Provides utilities for creating a QR Code to join a network"""
+
+__version__ = "1.1.0"
---
+++ packages/simple-wifi-qrcode/simple-wifi-qrcode-1.1.0.tar.gz/simple-wifi-qrcode-1.1.0/src/simple_wifi_qrcode/__main__.py
@@ -0,0 +1,62 @@
+import argparse
+from simple_wifi_qrcode import qrcode
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        "--output",
+        action="store",
+        default=False,
+        required=True,
+        help="The PNG file that will store the QR Code",
+    )
+    parser.add_argument(
+        "--ssid",
+        action="store",
+        default=None,
+        required=True,
+        help="The SSID of the network",
+    )
+    parser.add_argument(
+        "--password",
+        action="store",
+        default=None,
+        required=True,
+        help="The password of the network",
+    )
+    parser.add_argument(
+        "--alg",
+        action="store",
+        default="WPA",
+        required=True,
+        help="The authentication algorithm",
+        choices=("WEP", "WPA", "nopass"),
+    )
+    parser.add_argument(
+        "--random-password",
+        action="store_true",
+        default=False,
+        required=False,
+        help="Generate a random password for the network",
+    )
+    parser.add_argument(
+        "--verbose",
+        action="store_true",
+        default=False,
+        help="Print verbose information",
+    )
+    (args, _) = parser.parse_known_args()
+
+    qrcode.generate_wifi_qrcode(
+        ssid=args.ssid,
+        password=args.password,
+        alg=args.alg,
+        output=args.output,
+        verbose=args.verbose,
+        random_password=args.random_password,
+    )
+
+
+if __name__ == "__main__":
+    main()
