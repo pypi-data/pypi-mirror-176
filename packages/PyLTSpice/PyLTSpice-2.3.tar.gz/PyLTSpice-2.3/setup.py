d1aa8c8785da7433fed4b359f223c8f9500e7669
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:43:08 +0000

    Add package PyLTSpice/2.3/PyLTSpice-2.3.tar.gz

---
+++ packages/PyLTSpice/PyLTSpice-2.3.tar.gz/PyLTSpice-2.3/setup.py
@@ -0,0 +1,34 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name='PyLTSpice',
+    # version='2.3',
+    scripts=['PyLTSpice/__init__.py',
+             'PyLTSpice/detect_encoding.py',
+             'PyLTSpice/Histogram.py',
+             'PyLTSpice/LTSpice_RawRead.py',
+             'PyLTSpice/LTSpice_RawWrite.py',
+             'PyLTSpice/LTSpice_SemiDevOpReader.py',
+             'PyLTSpice/LTSpiceBatch.py',
+             'PyLTSpice/LTSteps.py',
+             'PyLTSpice/sim_stepping.py',
+             'PyLTSpice/SpiceEditor.py',
+             'PyLTSpice/sweep_iterators.py',
+             ],
+    # install_requires = [],
+    author="Nuno Brum",
+    author_email="me@nunobrum.com",
+    description="An set of tools to Automate LTSpice simulations",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/nunobrum/PyLTSpice",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+    ],
+)
