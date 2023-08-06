e540e82c9ddf0a7e45bec9b0bfc267a3cfcaefa7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:11:14 +0000

    Add package nonebot-plugin-imagetools/0.1.4/nonebot_plugin_imagetools-0.1.4.tar.gz

---
+++ packages/nonebot-plugin-imagetools/nonebot_plugin_imagetools-0.1.4.tar.gz/nonebot_plugin_imagetools-0.1.4/README.md
@@ -0,0 +1,53 @@
+# nonebot-plugin-imagetools
+
+[Nonebot2](https://github.com/nonebot/nonebot2) 插件，用于一些简单图片操作
+
+
+### 安装
+
+- 使用 nb-cli
+
+```
+nb plugin install nonebot_plugin_imagetools
+```
+
+- 使用 pip
+
+```
+pip install nonebot_plugin_imagetools
+```
+
+本插件使用了 [go-cqhttp](https://github.com/Mrs4s/go-cqhttp) 的 `send_group_forward_msg` 和 `send_private_forward_msg` 接口 来发送合并转发消息，
+
+发送私聊合并转发消息需要使用 `v1.0.0-rc2` 版本以上的 go-cqhttp
+
+
+### 使用
+
+**以下命令需要加[命令前缀](https://v2.nonebot.dev/docs/api/config#Config-command_start) (默认为`/`)，可自行设置为空**
+
+操作名 + [图片] 或 回复图片
+
+发送“图片操作”可显示支持的指令列表
+
+
+#### 支持的操作
+ - 水平翻转/左翻/右翻
+ - 竖直翻转/上翻/下翻
+ - 旋转 + 角度
+ - 缩放 + 尺寸或百分比，如：`缩放 100x100`；`缩放 200x`；`缩放 150%`
+ - 裁剪 + 尺寸或比例，如：`裁剪 100x100`；`裁剪 2:1`
+ - 反相/反色
+ - 灰度图/黑白
+ - 轮廓
+ - 浮雕
+ - 模糊
+ - 锐化
+ - 像素化 + 像素尺寸，默认为 8
+ - 颜色滤镜 + 16进制颜色代码 或 颜色名称，如：`颜色滤镜 #66ccff`；`颜色滤镜 green`
+ - 纯色图 + 16进制颜色代码 或 颜色名称
+ - gif倒放/倒放
+ - gif正放倒放/正放倒放
+ - gif分解
+ - 九宫格
+ - 文字转图 + 文字，支持少量BBcode
---
+++ packages/nonebot-plugin-imagetools/nonebot_plugin_imagetools-0.1.4.tar.gz/nonebot_plugin_imagetools-0.1.4/nonebot_plugin_imagetools/utils.py
@@ -0,0 +1,82 @@
+import httpx
+import asyncio
+from io import BytesIO
+from dataclasses import dataclass
+from PIL.Image import Image as IMG
+from typing import Callable, List, Protocol, Tuple
+
+from nonebot.log import logger
+from nonebot_plugin_imageutils import BuildImage
+
+
+@dataclass
+class Command:
+    keywords: Tuple[str, ...]
+    func: Callable
+
+
+def save_gif(frames: List[IMG], duration: float) -> BytesIO:
+    output = BytesIO()
+    frames[0].save(
+        output,
+        format="GIF",
+        save_all=True,
+        append_images=frames[1:],
+        duration=duration * 1000,
+        loop=0,
+        disposal=2,
+    )
+    return output
+
+
+async def download_url(url: str) -> bytes:
+    async with httpx.AsyncClient() as client:
+        for i in range(3):
+            try:
+                resp = await client.get(url, timeout=20)
+                resp.raise_for_status()
+                return resp.content
+            except Exception as e:
+                logger.warning(f"Error downloading {url}, retry {i}/3: {e}")
+                await asyncio.sleep(3)
+    raise Exception(f"{url} 下载失败！")
+
+
+class Maker(Protocol):
+    def __call__(self, img: BuildImage) -> BuildImage:
+        ...
+
+
+def get_avg_duration(image: IMG) -> float:
+    if not getattr(image, "is_animated", False):
+        return 0
+    total_duration = 0
+    for i in range(image.n_frames):
+        image.seek(i)
+        total_duration += image.info["duration"]
+    return total_duration / image.n_frames
+
+
+def make_jpg_or_gif(
+    img: BuildImage, func: Maker, keep_transparency: bool = True
+) -> BytesIO:
+    """
+    制作静图或者动图
+    :params
+      * ``img``: 输入图片
+      * ``func``: 图片处理函数，输入img，返回处理后的图片
+      * ``keep_transparency``: 传入gif时，是否保留该gif的透明度
+    """
+    image = img.image
+    if not getattr(image, "is_animated", False):
+        return func(img.convert("RGBA")).save_jpg()
+    else:
+        duration = get_avg_duration(image) / 1000
+        frames: List[IMG] = []
+        for i in range(image.n_frames):
+            image.seek(i)
+            frames.append(func(BuildImage(image).convert("RGBA")).image)
+        if keep_transparency:
+            image.seek(0)
+            frames[0].info["transparency"] = image.info.get("transparency", 0)
+        return save_gif(frames, duration)
