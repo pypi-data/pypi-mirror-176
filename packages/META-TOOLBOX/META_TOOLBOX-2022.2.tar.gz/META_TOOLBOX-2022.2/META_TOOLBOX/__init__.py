0b673f91925afcb55bac5c80e8a08ba200390e72
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:40:19 +0000

    Add package META-TOOLBOX/2022.2/META_TOOLBOX-2022.2.tar.gz

---
+++ packages/META-TOOLBOX/META_TOOLBOX-2022.2.tar.gz/META_TOOLBOX-2022.2/META_TOOLBOX/__init__.py
@@ -0,0 +1,3 @@
+from .META import *
+from .META_CO_LIBRARY import *
+from .META_HC_LIBRARY import *
---
+++ packages/META-TOOLBOX/META_TOOLBOX-2022.2.tar.gz/META_TOOLBOX-2022.2/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    	name = 'META_TOOLBOX',
+    	version = '2022.2',
+		url='https://wmpjrufg.github.io/META_TOOLBOX/',
+    	license='Apache License',
+    	author_email = 'wanderlei_junior@ufcat.edu.br',
+    	packages = ['META_TOOLBOX']
+    )
+
