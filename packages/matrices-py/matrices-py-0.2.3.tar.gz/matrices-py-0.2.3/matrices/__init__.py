0480e65f094601d1b9edd272942c804b85d5da7e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:48:22 +0000

    Add package matrices-py/0.2.3/matrices-py-0.2.3.tar.gz

---
+++ packages/matrices-py/matrices-py-0.2.3.tar.gz/matrices-py-0.2.3/matrices/__init__.py
@@ -0,0 +1,15 @@
+"""Dynamic, vectorized sequence types in ≤ 2 dimensions.
+
+Contains the `Matrix` definition, along with its related utilities and
+variants.
+
+Author: Braedyn L
+Version: 0.2.3
+Documentation: https://github.com/braedynl/matrices-py/wiki
+"""
+
+from .matrices import *
+from .protocols import *
+from .rule import *
+from .shape import *
+from .utilities import *
