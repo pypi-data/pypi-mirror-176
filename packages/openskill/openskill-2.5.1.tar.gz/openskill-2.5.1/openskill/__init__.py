47dbeee880d1596501e3b521d54a17c4dbdca039
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:11:16 +0000

    Add package openskill/2.5.1/openskill-2.5.1.tar.gz

---
+++ packages/openskill/openskill-2.5.1.tar.gz/openskill-2.5.1/openskill/__init__.py
@@ -0,0 +1,12 @@
+from openskill.rate import ordinal
+from openskill.rate import (
+    rate,
+    Rating,
+    create_rating,
+    team_rating,
+    predict_win,
+    predict_draw,
+)
+
+
+__version__ = "2.5.1"
