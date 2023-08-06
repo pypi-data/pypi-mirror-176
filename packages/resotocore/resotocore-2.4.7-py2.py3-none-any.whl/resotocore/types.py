43b849c7be7a183b43476c4f1b909d4bc962de09
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:34:52 +0000

    Add package resotocore/2.4.7/resotocore-2.4.7-py2.py3-none-any.whl

---
+++ packages/resotocore/resotocore-2.4.7-py2.py3-none-any.whl/resotocore/types.py
@@ -0,0 +1,12 @@
+from typing import Any, Optional, Callable, Dict, Sequence, Union, Mapping, Literal
+
+JsonElement = Union[str, int, float, bool, None, Mapping[str, Any], Sequence[Any]]
+JsonArray = Sequence[JsonElement]
+# See discussion here: https://github.com/python/typing/issues/182
+Json = Dict[str, Any]
+
+
+ValidationResult = Optional[Any]
+ValidationFn = Callable[[Any], ValidationResult]
+
+EdgeType = Literal["default", "delete"]
