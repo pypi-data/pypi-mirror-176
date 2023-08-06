b4e8ea75ebceb5854d17af40f3164e62f730d0d0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:35:46 +0000

    Add package har2locust/0.6.0/har2locust-0.6.0.tar.gz

---
+++ packages/har2locust/har2locust-0.6.0.tar.gz/har2locust-0.6.0/har2locust/plugin.py
@@ -0,0 +1,45 @@
+from typing import Callable
+from argparse import Namespace
+import ast
+
+# For examples of how to write a plugin, see default_plugins/ or tests/plugin_example.py
+# The processors allow you to interact with your recording at various stages,
+
+# immediately after reading the HAR JSON
+class entriesprocessor:
+    processors: list[Callable[[list[dict]], None]] = []
+
+    def __init__(self, func: Callable[[list[dict]], None]):
+        self.processors.append(func)
+
+
+# immediately after reading the HAR JSON, with access to command line arguments
+class entriesprocessor_with_args:
+    processors: list[Callable[[list[dict], Namespace], None]] = []
+
+    def __init__(self, func: Callable[[list[dict], Namespace], None]):
+        self.processors.append(func)
+
+
+# just before passing values to jinja2 template
+class valuesprocessor:
+    processors: list[Callable[[dict], None]] = []
+
+    def __init__(self, func: Callable[[dict], None]):
+        self.processors.append(func)
+
+
+# after rendering template, working with the syntax tree
+class astprocessor:
+    processors: list[Callable[[ast.Module, dict], None]] = []
+
+    def __init__(self, func: Callable[[ast.Module, dict], None]):
+        self.processors.append(func)
+
+
+# just before printing output, working with the string output
+class outputstringprocessor:
+    processors: list[Callable[[str], str]] = []
+
+    def __init__(self, func: Callable[[str], str]):
+        self.processors.append(func)
---
+++ packages/har2locust/har2locust-0.6.0.tar.gz/har2locust-0.6.0/tests/test_har2locust.py
@@ -0,0 +1,137 @@
+# pylint: disable=redefined-outer-name
+import json
+import pathlib
+import subprocess
+import os
+import pytest
+import re
+from har2locust.__main__ import __main__, process, rendering
+from har2locust.argument_parser import get_parser
+
+inputs_dir = pathlib.Path(__file__).parents[0] / "inputs"
+outputs_dir = pathlib.Path(__file__).parents[0] / "outputs"
+
+har_files = list(inputs_dir.glob("*.har"))
+py_files = [outputs_dir / f.with_suffix(".py").name for f in har_files]
+
+
+def h2l(*arguments):
+    return subprocess.Popen(
+        arguments,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        text=True,
+        encoding="utf-8",
+        cwd=os.path.dirname(__file__),
+    )
+
+
+with open(inputs_dir / "login.har") as f:
+    har = json.load(f)
+
+
+def test_har_file_not_found():
+    with pytest.raises(FileNotFoundError):
+        __main__(str(inputs_dir / "foo.har"))
+
+
+def test_helptext():
+    proc = h2l("har2locust", "--resource-types xhr,foo", "tests/inputs/login.har")
+    _stdout, stderr = proc.communicate()
+    assert proc.returncode == 1, f"Unexpected return code {proc.returncode}, stderr: {stderr}"
+    assert "are not supported" in stderr
+
+
+def test_rendering_syntax_error():
+    with pytest.raises(
+        SyntaxError,
+        match=re.escape("invalid syntax (<unknown>, line 1)"),
+    ):
+        __main__([str(inputs_dir / "login.har"), "--template", "tests/broken_template.jinja2"])
+
+
+def test_rendering_missing_template():
+    with pytest.raises(
+        Exception,
+        match="Template this_doesnt_exist.jinja2 does not exist, neither in current directory nor as built in",
+    ):
+        __main__([str(inputs_dir / "login.har"), "--template", "this_doesnt_exist.jinja2"])
+
+
+# writing py file in tests/output for manual inspection
+@pytest.mark.parametrize("har_file, py_file", zip(har_files, py_files))
+def test_output(har_file, py_file):
+    with open(py_file, encoding="utf-8") as f:
+        expected_output = f.read()
+    proc = subprocess.Popen(
+        ["har2locust", har_file],
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        text=True,
+        encoding="utf-8",
+        cwd=os.path.join(os.path.dirname(__file__), "../"),
+    )
+    stdout, stderr = proc.communicate()
+    assert proc.returncode == 0, f"Bad return code {proc.returncode}, stderr: {stderr}"
+    assert stdout.strip() == expected_output.strip()
+
+
+def test_helptext():
+    proc = h2l("har2locust", "--help")
+    stdout, stderr = proc.communicate()
+    assert proc.returncode == 0, f"Bad return code {proc.returncode}, stderr: {stderr}"
+    assert "usage: har2locust" in stdout
+
+
+def test_plugins():
+    har_file = "tests/inputs/login.har"
+    py_file = "tests/outputs/login_plugin.py"
+    with open(py_file, encoding="utf-8") as f:
+        expected_output = f.read()
+    proc = subprocess.Popen(
+        ["har2locust", har_file, "--plugins", "tests/plugin_example.py"],
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        text=True,
+        encoding="utf-8",
+        cwd=os.path.join(os.path.dirname(__file__), "../"),
+    )
+    stdout, stderr = proc.communicate()
+    assert proc.returncode == 0, f"Bad return code {proc.returncode}, stderr: {stderr}"
+    assert stdout.strip() == expected_output.strip()
+    assert "self.reader.user" in stdout
+    assert "self.customer" in stdout
+    # test url timestamp rewriting function
+    assert "self.rest_" in stdout
+    assert "&_" not in stdout
+
+
+def test_plugins_run_as_module():  # same as above test, but run as module
+    har_file = "tests/inputs/login.har"
+    py_file = "tests/outputs/login_plugin.py"
+    with open(py_file, encoding="utf-8") as f:
+        expected_output = f.read()
+    proc = subprocess.Popen(
+        ["python3", "-m", "har2locust", har_file, "--plugins", "tests/plugin_example.py"],
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE,
+        text=True,
+        encoding="utf-8",
+        cwd=os.path.join(os.path.dirname(__file__), "../"),
+    )
+    stdout, stderr = proc.communicate()
+    assert proc.returncode == 0, f"Bad return code {proc.returncode}, stderr: {stderr}"
+    assert stdout.strip() == expected_output.strip()
+    assert "self.reader.user" in stdout
+    assert "self.customer" in stdout
+    # test url timestamp rewriting function
+    assert "self.rest_" in stdout
+    assert "&_" not in stdout
+
+
+# this test is intended to be run AFTER regenerating the output using make update_tests
+def test_locust_run():
+    proc = h2l("locust", "-f", "outputs/reqres.in.py", "-i", "1", "--headless")
+    _, stderr = proc.communicate()
+    assert proc.returncode == 0, f"Bad return code {proc.returncode}, stderr: {stderr}"
+    assert "Iteration limit reached" in stderr, stderr
