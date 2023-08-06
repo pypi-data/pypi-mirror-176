b5d516e7047de442dcde5a3976f27c4070dffbc4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:30:01 +0000

    Add package soil/0.30.0rc4/soil-0.30.0rc4-py3-none-any.whl

---
+++ packages/soil/soil-0.30.0rc4-py3-none-any.whl/soil/exporters.py
@@ -0,0 +1,256 @@
+import os
+import sys
+from time import time as current_time
+from io import BytesIO
+from sqlalchemy import create_engine
+from textwrap import dedent, indent
+
+
+import matplotlib.pyplot as plt
+import networkx as nx
+
+
+from .serialization import deserialize
+from .utils import try_backup, open_or_reuse, logger, timer
+
+
+from . import utils, network
+
+
+class DryRunner(BytesIO):
+    def __init__(self, fname, *args, copy_to=None, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.__fname = fname
+        self.__copy_to = copy_to
+
+    def write(self, txt):
+        if self.__copy_to:
+            self.__copy_to.write("{}:::{}".format(self.__fname, txt))
+        try:
+            super().write(txt)
+        except TypeError:
+            super().write(bytes(txt, "utf-8"))
+
+    def close(self):
+        content = "(binary data not shown)"
+        try:
+            content = self.getvalue().decode()
+        except UnicodeDecodeError:
+            pass
+        logger.info(
+            "**Not** written to {} (dry run mode):\n\n{}\n\n".format(
+                self.__fname, content
+            )
+        )
+        super().close()
+
+
+class Exporter:
+    """
+    Interface for all exporters. It is not necessary, but it is useful
+    if you don't plan to implement all the methods.
+    """
+
+    def __init__(self, simulation, outdir=None, dry_run=None, copy_to=None):
+        self.simulation = simulation
+        outdir = outdir or os.path.join(os.getcwd(), "soil_output")
+        self.outdir = os.path.join(outdir, simulation.group or "", simulation.name)
+        self.dry_run = dry_run
+        if copy_to is None and dry_run:
+            copy_to = sys.stdout
+        self.copy_to = copy_to
+
+    def sim_start(self):
+        """Method to call when the simulation starts"""
+        pass
+
+    def sim_end(self):
+        """Method to call when the simulation ends"""
+        pass
+
+    def trial_start(self, env):
+        """Method to call when a trial start"""
+        pass
+
+    def trial_end(self, env):
+        """Method to call when a trial ends"""
+        pass
+
+    def output(self, f, mode="w", **kwargs):
+        if self.dry_run:
+            f = DryRunner(f, copy_to=self.copy_to)
+        else:
+            try:
+                if not os.path.isabs(f):
+                    f = os.path.join(self.outdir, f)
+            except TypeError:
+                pass
+        return open_or_reuse(f, mode=mode, **kwargs)
+
+    def get_dfs(self, env):
+        yield from get_dc_dfs(env.datacollector, trial_id=env.id)
+
+
+def get_dc_dfs(dc, trial_id=None):
+    dfs = {
+        "env": dc.get_model_vars_dataframe(),
+        "agents": dc.get_agent_vars_dataframe(),
+    }
+    for table_name in dc.tables:
+        dfs[table_name] = dc.get_table_dataframe(table_name)
+    if trial_id:
+        for (name, df) in dfs.items():
+            df["trial_id"] = trial_id
+    yield from dfs.items()
+
+
+class SQLite(Exporter):
+    """Writes sqlite results"""
+
+    def sim_start(self):
+        if self.dry_run:
+            logger.info("NOT dumping results")
+            return
+        self.dbpath = os.path.join(self.outdir, f"{self.simulation.name}.sqlite")
+        logger.info("Dumping results to %s", self.dbpath)
+        try_backup(self.dbpath, remove=True)
+
+    def trial_end(self, env):
+        if self.dry_run:
+            logger.info("Running in DRY_RUN mode, the database will NOT be created")
+            return
+
+        with timer(
+            "Dumping simulation {} trial {}".format(self.simulation.name, env.id)
+        ):
+
+            engine = create_engine(f"sqlite:///{self.dbpath}", echo=False)
+
+            for (t, df) in self.get_dfs(env):
+                df.to_sql(t, con=engine, if_exists="append")
+
+class csv(Exporter):
+
+    """Export the state of each environment (and its agents) in a separate CSV file"""
+
+    def trial_end(self, env):
+        with timer(
+            "[CSV] Dumping simulation {} trial {} @ dir {}".format(
+                self.simulation.name, env.id, self.outdir
+            )
+        ):
+            for (df_name, df) in self.get_dfs(env):
+                with self.output("{}.{}.csv".format(env.id, df_name)) as f:
+                    df.to_csv(f)
+
+
+# TODO: reimplement GEXF exporting without history
+class gexf(Exporter):
+    def trial_end(self, env):
+        if self.dry_run:
+            logger.info("Not dumping GEXF in dry_run mode")
+            return
+
+        with timer(
+            "[GEXF] Dumping simulation {} trial {}".format(self.simulation.name, env.id)
+        ):
+            with self.output("{}.gexf".format(env.id), mode="wb") as f:
+                network.dump_gexf(env.history_to_graph(), f)
+                self.dump_gexf(env, f)
+
+
+class dummy(Exporter):
+    def sim_start(self):
+        with self.output("dummy", "w") as f:
+            f.write("simulation started @ {}\n".format(current_time()))
+
+    def trial_start(self, env):
+        with self.output("dummy", "w") as f:
+            f.write("trial started@ {}\n".format(current_time()))
+
+    def trial_end(self, env):
+        with self.output("dummy", "w") as f:
+            f.write("trial ended@ {}\n".format(current_time()))
+
+    def sim_end(self):
+        with self.output("dummy", "a") as f:
+            f.write("simulation ended @ {}\n".format(current_time()))
+
+
+class graphdrawing(Exporter):
+    def trial_end(self, env):
+        # Outside effects
+        f = plt.figure()
+        nx.draw(
+            env.G,
+            node_size=10,
+            width=0.2,
+            pos=nx.spring_layout(env.G, scale=100),
+            ax=f.add_subplot(111),
+        )
+        with open("graph-{}.png".format(env.id)) as f:
+            f.savefig(f)
+
+
+class summary(Exporter):
+    """Print a summary of each trial to sys.stdout"""
+
+    def trial_end(self, env):
+        msg = ""
+        for (t, df) in self.get_dfs(env):
+            if not len(df):
+                continue
+            tabs = "\t" * 2
+            description = indent(str(df.describe()), tabs)
+            last_line = indent(str(df.iloc[-1:]), tabs)
+            # value_counts = indent(str(df.value_counts()), tabs)
+            value_counts = indent(str(df.apply(lambda x: x.value_counts()).T.stack()), tabs)
+
+            msg += dedent("""
+            Dataframe {t}:
+                Last line: :
+            {last_line}
+
+                Description:
+            {description}
+
+                Value counts:
+            {value_counts}
+
+            """).format(**locals())
+        logger.info(msg)
+
+class YAML(Exporter):
+    """Writes the configuration of the simulation to a YAML file"""
+
+    def sim_start(self):
+        if self.dry_run:
+            logger.info("NOT dumping results")
+            return
+        with self.output(self.simulation.name + ".dumped.yml") as f:
+            logger.info(f"Dumping simulation configuration to {self.outdir}")
+            f.write(self.simulation.to_yaml())
+
+class default(Exporter):
+    """Default exporter. Writes sqlite results, as well as the simulation YAML"""
+
+    def __init__(self, *args, exporter_cls=[], **kwargs):
+        exporter_cls = exporter_cls or [YAML, SQLite, summary]
+        self.inner = [cls(*args, **kwargs) for cls in exporter_cls]
+
+    def sim_start(self):
+        for exporter in self.inner:
+            exporter.sim_start()
+
+    def sim_end(self):
+        for exporter in self.inner:
+            exporter.sim_end()
+
+    def trial_start(self, env):
+        for exporter in self.inner:
+            exporter.trial_start(env)
+
+
+    def trial_end(self, env):
+        for exporter in self.inner:
+            exporter.trial_end(env)
---
+++ packages/soil/soil-0.30.0rc4-py3-none-any.whl/soil/simulation.py
@@ -0,0 +1,291 @@
+import os
+from time import time as current_time, strftime
+import importlib
+import sys
+import yaml
+import traceback
+import inspect
+import logging
+import networkx as nx
+
+from textwrap import dedent
+
+from dataclasses import dataclass, field, asdict
+from typing import Any, Dict, Union, Optional, List
+
+
+from networkx.readwrite import json_graph
+from functools import partial
+import pickle
+
+from . import serialization, exporters, utils, basestring, agents
+from .environment import Environment
+from .utils import logger, run_and_return_exceptions
+from .config import Config, convert_old
+
+
+# TODO: change documentation for simulation
+@dataclass
+class Simulation:
+    """
+    Parameters
+    ---------
+    config (optional): :class:`config.Config`
+        name of the Simulation
+
+    kwargs: parameters to use to initialize a new configuration, if one not been provided.
+    """
+
+    version: str = "2"
+    name: str = "Unnamed simulation"
+    description: Optional[str] = ""
+    group: str = None
+    model_class: Union[str, type] = "soil.Environment"
+    model_params: dict = field(default_factory=dict)
+    seed: str = field(default_factory=lambda: current_time())
+    dir_path: str = field(default_factory=lambda: os.getcwd())
+    max_time: float = float("inf")
+    max_steps: int = -1
+    interval: int = 1
+    num_trials: int = 1
+    num_processes: Optional[int] = 1
+    parallel: Optional[bool] = False
+    exporters: Optional[List[str]] = field(default_factory=lambda: [exporters.default])
+    model_reporters: Optional[Dict[str, Any]] = field(default_factory=dict)
+    agent_reporters: Optional[Dict[str, Any]] = field(default_factory=dict)
+    tables: Optional[Dict[str, Any]] = field(default_factory=dict)
+    outdir: Optional[str] = None
+    exporter_params: Optional[Dict[str, Any]] = field(default_factory=dict)
+    dry_run: bool = False
+    extra: Dict[str, Any] = field(default_factory=dict)
+    skip_test: Optional[bool] = False
+
+    @classmethod
+    def from_dict(cls, env, **kwargs):
+
+        ignored = {
+            k: v for k, v in env.items() if k not in inspect.signature(cls).parameters
+        }
+
+        d = {k: v for k, v in env.items() if k not in ignored}
+        if ignored:
+            d.setdefault("extra", {}).update(ignored)
+        if ignored:
+            logger.warning(f'Ignoring these parameters (added to "extra"): { ignored }')
+        d.update(kwargs)
+
+        return cls(**d)
+
+    def run_simulation(self, *args, **kwargs):
+        return self.run(*args, **kwargs)
+
+    def run(self, *args, **kwargs):
+        """Run the simulation and return the list of resulting environments"""
+        logger.info(
+            dedent(
+                """
+        Simulation:
+        ---
+        """
+            )
+            + self.to_yaml()
+        )
+        return list(self.run_gen(*args, **kwargs))
+
+    def run_gen(
+        self,
+        num_processes=1,
+        dry_run=None,
+        exporters=None,
+        outdir=None,
+        exporter_params={},
+        log_level=None,
+        **kwargs,
+    ):
+        """Run the simulation and yield the resulting environments."""
+        if log_level:
+            logger.setLevel(log_level)
+        outdir = outdir or self.outdir
+        logger.info("Using exporters: %s", exporters or [])
+        logger.info("Output directory: %s", outdir)
+        if dry_run is None:
+            dry_run = self.dry_run
+        if exporters is None:
+            exporters = self.exporters
+        if not exporter_params:
+            exporter_params = self.exporter_params
+
+        exporters = serialization.deserialize_all(
+            exporters,
+            simulation=self,
+            known_modules=[
+                "soil.exporters",
+            ],
+            dry_run=dry_run,
+            outdir=outdir,
+            **exporter_params,
+        )
+
+        with utils.timer("simulation {}".format(self.name)):
+            for exporter in exporters:
+                exporter.sim_start()
+
+            for env in utils.run_parallel(
+                func=self.run_trial,
+                iterable=range(int(self.num_trials)),
+                num_processes=num_processes,
+                log_level=log_level,
+                **kwargs,
+            ):
+
+                for exporter in exporters:
+                    exporter.trial_start(env)
+
+                for exporter in exporters:
+                    exporter.trial_end(env)
+
+                yield env
+
+            for exporter in exporters:
+                exporter.sim_end()
+
+    def get_env(self, trial_id=0, model_params=None, **kwargs):
+        """Create an environment for a trial of the simulation"""
+
+        def deserialize_reporters(reporters):
+            for (k, v) in reporters.items():
+                if isinstance(v, str) and v.startswith("py:"):
+                    reporters[k] = serialization.deserialize(v.split(":", 1)[1])
+            return reporters
+
+        params = self.model_params.copy()
+        if model_params:
+            params.update(model_params)
+        params.update(kwargs)
+
+        agent_reporters = self.agent_reporters.copy()
+        agent_reporters.update(deserialize_reporters(params.pop("agent_reporters", {})))
+        model_reporters = self.model_reporters.copy()
+        model_reporters.update(deserialize_reporters(params.pop("model_reporters", {})))
+        tables = self.tables.copy()
+        tables.update(deserialize_reporters(params.pop("tables", {})))
+
+        env = serialization.deserialize(self.model_class)
+        return env(
+            id=f"{self.name}_trial_{trial_id}",
+            seed=f"{self.seed}_trial_{trial_id}",
+            dir_path=self.dir_path,
+            agent_reporters=agent_reporters,
+            model_reporters=model_reporters,
+            tables=tables,
+            **params,
+        )
+
+    def run_trial(
+        self, trial_id=None, until=None, log_file=False, log_level=logging.INFO, **opts
+    ):
+        """
+        Run a single trial of the simulation
+
+        """
+        if log_level:
+            logger.setLevel(log_level)
+        model = self.get_env(trial_id, **opts)
+        trial_id = trial_id if trial_id is not None else current_time()
+        with utils.timer("Simulation {} trial {}".format(self.name, trial_id)):
+            return self.run_model(
+                model=model, trial_id=trial_id, until=until, log_level=log_level
+            )
+
+    def run_model(self, model, until=None, **opts):
+        # Set-up trial environment and graph
+        until = float(until or self.max_time or "inf")
+
+        # Set up agents on nodes
+        def is_done():
+            return not model.running
+
+        if until and hasattr(model.schedule, "time"):
+            prev = is_done
+
+            def is_done():
+                return prev() or model.schedule.time >= until
+
+        if self.max_steps and self.max_steps > 0 and hasattr(model.schedule, "steps"):
+            prev_steps = is_done
+
+            def is_done():
+                return prev_steps() or model.schedule.steps >= self.max_steps
+
+        newline = "\n"
+        logger.info(
+            dedent(
+                f"""
+Model stats:
+  Agents (total: { model.schedule.get_agent_count() }):
+      - { (newline + '      - ').join(str(a) for a in model.schedule.agents) }
+
+  Topology size: { len(model.G) if hasattr(model, "G") else 0 }
+        """
+            )
+        )
+
+        while not is_done():
+            utils.logger.debug(
+                f'Simulation time {model.schedule.time}/{until}. Next: {getattr(model.schedule, "next_time", model.schedule.time + self.interval)}'
+            )
+            model.step()
+
+        if (
+            model.schedule.time < until
+        ):  # Simulation ended (no more steps) before the expected time
+            model.schedule.time = until
+        return model
+
+    def to_dict(self):
+        d = asdict(self)
+        return serialization.serialize_dict(d)
+
+    def to_yaml(self):
+        return yaml.dump(self.to_dict())
+
+
+def iter_from_config(*cfgs, **kwargs):
+    for config in cfgs:
+        configs = list(serialization.load_config(config))
+        for config, path in configs:
+            d = dict(config)
+            if "dir_path" not in d:
+                d["dir_path"] = os.path.dirname(path)
+            yield Simulation.from_dict(d, **kwargs)
+
+
+def from_config(conf_or_path):
+    lst = list(iter_from_config(conf_or_path))
+    if len(lst) > 1:
+        raise AttributeError("Provide only one configuration")
+    return lst[0]
+
+def iter_from_py(pyfile, module_name='custom_simulation'):
+    """Try to load every Simulation instance in a given Python file"""
+    import importlib
+    import inspect
+    spec = importlib.util.spec_from_file_location(module_name, pyfile)
+    module = importlib.util.module_from_spec(spec)
+    sys.modules[module_name] = module
+    spec.loader.exec_module(module)
+    # import pdb;pdb.set_trace()
+    for (_name, sim) in inspect.getmembers(module, lambda x: isinstance(x, Simulation)):
+        yield sim
+    del sys.modules[module_name]
+
+
+def from_py(pyfile):
+    return next(iter_from_py(pyfile))
+
+
+
+def run_from_config(*configs, **kwargs):
+    for sim in iter_from_config(*configs):
+        logger.info(f"Using config(s): {sim.name}")
+        sim.run_simulation(**kwargs)
