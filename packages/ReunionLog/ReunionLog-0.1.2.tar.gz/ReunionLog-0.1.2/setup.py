9a02fdbc2bf40e8451a137589562ea21311490af
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:14:31 +0000

    Add package ReunionLog/0.1.2/ReunionLog-0.1.2.tar.gz

---
+++ packages/ReunionLog/ReunionLog-0.1.2.tar.gz/ReunionLog-0.1.2/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+VERSION = '0.1.2'
+DESCRIPTION = 'A small package to access and use WarcraftLogs API'
+LONG_DESCRIPTION = 'A small package that can use the WarcraftLogs api and from there call function to retrive data fram WarcraftLogs'
+
+setup (
+    name= 'ReunionLog',
+    version= VERSION,
+    author= 'nickgismokato',
+    author_email= 'nickvillumlaursen@gmail.com',
+    packages= find_packages(),
+    license= 'LICENCE.txt',
+    description= DESCRIPTION,
+    long_description= LONG_DESCRIPTION,
+    install_requires=[
+        "gql-query-builder >= 0.1.7",
+    ],
+    keywords= ['python', 'WarcraftLogs-API', 'package'],
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "Intended Audience :: End Users/Desktop",
+        "Intended Audience :: Other Audience",
+        "Programming Language :: Python :: 3",
+        "Operating System :: OS Independent",
+    ],
+)
