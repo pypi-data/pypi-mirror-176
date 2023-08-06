101e25c34e285769122d399e34d32ced6cda6395
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:35:47 +0000

    Add package hei-textpreprocessor/0.0.2/hei-textpreprocessor-0.0.2.tar.gz

---
+++ packages/hei-textpreprocessor/hei-textpreprocessor-0.0.2.tar.gz/hei-textpreprocessor-0.0.2/README.md
@@ -0,0 +1,15 @@
+# Introduction 
+A simple python wrapper for usual text tasks (preprocessing, tokenization, and vectorization.)
+
+# Getting Started
+Soon, the library can be installed via pip (pip install hei-textpreprocessor)
+
+# Build and Test
+The library includes one notebook (demo.ipynb) that demonstrates the use of the library
+
+# Content
+The library includes the following classes:
+1. Language - holds all information (language-related) that is needed for further text processing.
+2. Tokenizer - defines how to text needs to be tokenized
+3. Preprocessor - applies standard preprocessing techniques to text.
+4. vectorizer - defines how to text needs to be vectorized
---
+++ packages/hei-textpreprocessor/hei-textpreprocessor-0.0.2.tar.gz/hei-textpreprocessor-0.0.2/setup.py
@@ -0,0 +1,27 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+
+setuptools.setup(
+    name="hei-textpreprocessor",
+    version="0.0.2",
+    author="Angeliki Kouka",
+    author_email="akouka@heijmans.nl",
+    description="A simple python wrapper for usual text tasks (preprocessing, tokenization, and vectorization.)",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    package_dir={"": "src"},
+    packages=setuptools.find_packages(where="src"), #, exclude=["tests"]
+    python_requires=">=3.7",
+    install_requires=[
+        "spacy>=3.4.2",
+        "nltk>=3.7",
+        "pandas>=1.5.1",
+        "deep_translator>=1.8",
+        "tokenizers>=0.12.1",
+        "scikit-learn>=1.1.3"
+    ],
+    test_suite="tests",
+)
