741f44d3ea1086a25b564e8a6604ac9adfaac8d3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:38:19 +0000

    Add package tonic/1.2.3.dev10/tonic-1.2.3.dev10.tar.gz

---
+++ packages/tonic/tonic-1.2.3.dev10.tar.gz/tonic-1.2.3.dev10/docs/gallery/representations/plot_toimage.py
@@ -0,0 +1,26 @@
+"""
+=======
+ToImage
+=======
+
+:class:`~tonic.transforms.ToImage` bins all the events to a single image.
+Typically used in conjunction with SlicedDataset, to chunk recordings
+into smaller samples.
+
+"""
+
+import tonic
+import matplotlib.pyplot as plt
+
+nmnist = tonic.datasets.NMNIST("../../tutorials/data", train=False)
+events, label = nmnist[0]
+
+transform = tonic.transforms.ToImage(
+    sensor_size=nmnist.sensor_size,
+)
+
+image = transform(events[:1000])
+
+plt.imshow(image[1] - image[0])
+plt.axis(False)
+plt.show()
