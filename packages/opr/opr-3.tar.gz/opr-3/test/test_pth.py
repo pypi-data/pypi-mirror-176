d872d47ef5454158565ae44ca8a59240d424834a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:23:30 +0000

    Add package opr/3/opr-3.tar.gz

---
+++ packages/opr/opr-3.tar.gz/opr-3/test/test_pth.py
@@ -0,0 +1,20 @@
+# This file is placed in the Public Domain.
+
+
+"path tests"
+
+
+import unittest
+
+
+from opr.obj import fntime
+
+
+FN = "store/oper.evt.Event/2022-04-11/22:40:31.259218"
+
+
+class TestPath(unittest.TestCase):
+
+    def test_path(self):
+        fnt = fntime(FN)
+        self.assertEqual(fnt, 1649709631.259218)
