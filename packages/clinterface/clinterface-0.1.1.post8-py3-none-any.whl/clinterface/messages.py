5c58aa026fe6686622a6e454eeb9752abda80c7d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:17:49 +0000

    Add package clinterface/0.1.1.post8/clinterface-0.1.1.post8-py3-none-any.whl

---
+++ packages/clinterface/clinterface-0.1.1.post8-py3-none-any.whl/clinterface/messages.py
@@ -0,0 +1,44 @@
+import sys
+from .colors import DEFAULT, GREEN, YELLOW, RED
+
+def join_args(f):
+    def wrapper(*args, **kwargs):
+        return f(' '.join(args), ', '.join('{}={}'.format(k, v) for k, v in kwargs.items()))
+    return wrapper
+
+@join_args
+def success(message, details):
+    if details:
+        formatted_message = '{} ({})'.format(message, details)
+    else:
+        formatted_message = message
+    print(GREEN + formatted_message + DEFAULT)
+
+@join_args
+def warning(message, details):
+    if details:
+        formatted_message = 'Warning: {} ({})'.format(message, details)
+    else:
+        formatted_message = 'Warning: {}'.format(message)
+    print(YELLOW + formatted_message + DEFAULT)
+
+@join_args
+def failure(message, details):
+    if details:
+        formatted_message = '{} ({})'.format(message, details)
+    else:
+        formatted_message = message
+    print(RED + formatted_message + DEFAULT)
+
+@join_args
+def error(message, details):
+    if details:
+        formatted_message = '{} ({})'.format(message, details)
+    else:
+        formatted_message = message
+    raise SystemExit(RED + formatted_message + DEFAULT)
+
+@join_args
+def unknown_error(message):
+    fcode = sys._getframe(1).f_code
+    raise SystemExit(RED + '{}:{} {}'.format(fcode.co_filename, fcode.co_name, message) + DEFAULT)
