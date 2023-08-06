8c454e48836aa4c25a3cef55db46e5baa0d9873a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:12:49 +0000

    Add package via-varejo/0.1.0/via-varejo-0.1.0.tar.gz

---
+++ packages/via-varejo/via-varejo-0.1.0.tar.gz/via-varejo-0.1.0/setup.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['via_varejo']
+
+package_data = \
+{'': ['*']}
+
+setup_kwargs = {
+    'name': 'via-varejo',
+    'version': '0.1.0',
+    'description': 'library com as funcoes utilitarias',
+    'long_description': '',
+    'author': 'Jaderson Macedo',
+    'author_email': 'jaderson.macedo@viavarejo.com.br',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'packages': packages,
+    'package_data': package_data,
+    'python_requires': '>=3.10,<4.0',
+}
+
+
+setup(**setup_kwargs)
