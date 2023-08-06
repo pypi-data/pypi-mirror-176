4737e155d41df714e2b6ce6bb4194992f4849c43
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:27:23 +0000

    Add package pycollab/0.1.1/pycollab-0.1.1-py3-none-any.whl

---
+++ packages/pycollab/pycollab-0.1.1-py3-none-any.whl/pycollab/demo/poppy.py
@@ -0,0 +1,22 @@
+def add_nums(a, b):
+    return a + b
+
+
+def sub_nums(a, b):
+    return a - b
+
+
+def div_nums(a, b):
+    return a / b
+
+
+def mul_nums(a, b):
+    return a * b
+
+
+def mod_nums(a, b):
+    return a % b
+
+
+def printHello():
+    return "Hello World!"
