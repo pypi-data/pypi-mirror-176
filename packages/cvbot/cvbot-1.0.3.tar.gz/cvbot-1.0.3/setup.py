687f323ad96664c35b3219c8c7b0b5a6608f8e6e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:36:32 +0000

    Add package cvbot/1.0.3/cvbot-1.0.3.tar.gz

---
+++ packages/cvbot/cvbot-1.0.3.tar.gz/cvbot-1.0.3/setup.py
@@ -0,0 +1,18 @@
+from distutils.core import setup
+from setuptools import find_packages
+import os
+
+setup(
+    name='cvbot',
+    package=find_packages(','),
+    version='1.0.3',
+    license='LICENSE',
+    description='An Assistant to help and make computer vision tasks easier',
+    author='Hisham Moe',
+    install_requires=[
+        'opencv-python',
+        'numpy',
+        'mss',
+        'pywin32'
+        ]
+    )
