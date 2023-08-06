ef0ff5b9ee9786a68b4150b29b2cdd517260a259
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:14:12 +0000

    Add package gallicaGetter/0.0.5/gallicagetter-0.0.5-py3-none-any.whl

---
+++ packages/gallicaGetter/gallicagetter-0.0.5-py3-none-any.whl/gallicaGetter/tests/test_get.py
@@ -0,0 +1,19 @@
+from gallicaGetter.fetch.get import Get
+from gallicaGetter.fetch.response import Response
+from unittest.mock import MagicMock
+from unittest import TestCase
+
+
+class TestGet(TestCase):
+
+    def setUp(self) -> None:
+        self.getter = Get('test')
+
+    def test_get(self):
+        self.getter.http = MagicMock(request=MagicMock(
+            return_value=MagicMock(status=200)))
+        response = self.getter.get(MagicMock())
+        self.assertIsInstance(response, Response)
+
+
+
