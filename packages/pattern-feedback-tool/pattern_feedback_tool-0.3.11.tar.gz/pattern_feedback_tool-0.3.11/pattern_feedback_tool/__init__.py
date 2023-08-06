100a18d9be47628dffdbd004e1bcc64147431a30
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:49:22 +0000

    Add package pattern-feedback-tool/0.3.11/pattern_feedback_tool-0.3.11.tar.gz

---
+++ packages/pattern-feedback-tool/pattern_feedback_tool-0.3.11.tar.gz/pattern_feedback_tool-0.3.11/pattern_feedback_tool/__init__.py
@@ -0,0 +1,11 @@
+"""pattern_feedback_tool."""
+
+from warnings import filterwarnings
+
+from beartype.roar import BeartypeDecorHintPep585DeprecationWarning
+
+__version__ = '0.3.11'
+__pkg_name__ = 'pattern_feedback_tool'
+
+# FYI: https://github.com/beartype/beartype#are-we-on-the-worst-timeline
+filterwarnings('ignore', category=BeartypeDecorHintPep585DeprecationWarning)
