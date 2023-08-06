99103750586df4a67ac3f00cbe99a041a944b16c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:12:16 +0000

    Add package tkp-finder/0.1/tkp_finder-0.1.tar.gz

---
+++ packages/tkp-finder/tkp_finder-0.1.tar.gz/tkp_finder-0.1/tests/__init__.py
@@ -0,0 +1,3 @@
+# SPDX-FileCopyrightText: 2022-present Ivan Reveguk <edikedikedikedik@gmail.com>
+#
+# SPDX-License-Identifier: MIT
---
+++ packages/tkp-finder/tkp_finder-0.1.tar.gz/tkp_finder-0.1/tkp_finder/__init__.py
@@ -0,0 +1,5 @@
+# SPDX-FileCopyrightText: 2022-present Ivan Reveguk <edikedikedikedik@gmail.com>
+#
+# SPDX-License-Identifier: MIT
+
+from .tkp_finder import find_tkps, discover_and_annotate, VARIABLES
