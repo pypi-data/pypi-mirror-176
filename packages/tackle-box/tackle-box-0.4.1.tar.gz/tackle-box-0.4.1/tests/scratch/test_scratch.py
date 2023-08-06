a56497cf80039ace811cb233a10943c5210a86de
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:06:33 +0000

    Add package tackle-box/0.4.1/tackle-box-0.4.1.tar.gz

---
+++ packages/tackle-box/tackle-box-0.4.1.tar.gz/tackle-box-0.4.1/tests/scratch/test_scratch.py
@@ -0,0 +1,18 @@
+from tackle import tackle
+from tackle.cli import main
+
+
+# def test_lazy_error(change_curdir_fixtures):
+#     main(["lazy.yaml", "-p"])
+#
+#
+# def test_lazy_error_main(change_curdir_fixtures):
+#     output = tackle("lazy.yaml")
+#     assert output
+#
+#
+# def test_if_issue(change_curdir_fixtures):
+#     main(["ifdefined.yaml"])
+
+    # output = tackle("ifdefined.yaml")
+    # assert output
