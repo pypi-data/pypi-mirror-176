0b08cda798e27756c4d84a56b9c0351d53d2c51f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:14:28 +0000

    Add package foxmetrics/1.0.6/foxmetrics-1.0.6.tar.gz

---
+++ packages/foxmetrics/foxmetrics-1.0.6.tar.gz/foxmetrics-1.0.6/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='foxmetrics',
+    version='1.0.6',
+    packages=['foxmetrics'],
+    url='',
+    license='',
+    author='jizong',
+    author_email='jizong.peng.ca@gmail.com',
+    description='metrics for decoupled training'
+)
