b2d81758b0677aea1f21f7a607612cef3325371f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:23:10 +0000

    Add package stdocker/1.0.12/stdocker-1.0.12.tar.gz

---
+++ packages/stdocker/stdocker-1.0.12.tar.gz/stdocker-1.0.12/stdocker/config.py
@@ -0,0 +1,17 @@
+import os
+
+# Shinetech Docker installation directory
+install_dir = '/opt/shinetech/stdocker'
+
+# The directory where the stdocker command is currently executed
+current_dir = os.getcwd()
+
+base_domain = '.dev.php9.cc'
+
+php_platforms = ['generic', 'magento', 'zend', 'symfony', 'laravel', 'yii',
+                 'slim', 'cakephp', 'codeigniter', 'wordpress', 'drupal']
+
+js_platforms = ['angular', 'react', 'vue', 'nextjs', 'nestjs']
+js_languages = ['javascript', 'typescript']
+
+projects = ['hp']
