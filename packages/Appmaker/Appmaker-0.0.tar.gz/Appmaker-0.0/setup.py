854a53e8e0788e98205f37ecf1fd50ccc7c78797
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:38:41 +0000

    Add package Appmaker/0.0/Appmaker-0.0.tar.gz

---
+++ packages/Appmaker/Appmaker-0.0.tar.gz/Appmaker-0.0/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+setup(
+    name='Appmaker',
+    version='0.0',
+    packages=['appmaker'],
+    url='https://gitlab.com/Pixel-Mqster/File-Find',
+    license='GNU GPL v3.0',
+    author='Pixel Master',
+    author_email='',
+    description='Command line tool to package executable into an .app bundle',
+    long_description=''.join(open('README.rst', encoding='utf-8').readlines()),
+    keywords=['cmd', 'executable', 'macOS'],
+    include_package_data=True,
+    install_requires=[''],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+        "Operating System :: MacOS",
+    ],
+    entry_points={'gui_scripts': ['appmaker=appmaker.appmaker.py:run', 'mkapp=appmaker.appmaker.py:run'], }
+
+)
