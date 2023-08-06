149d3111609395d9eabc93f87e12561bc296477d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:05:22 +0000

    Add package litewax/0.1.7/litewax-0.1.7.tar.gz

---
+++ packages/litewax/litewax-0.1.7.tar.gz/litewax-0.1.7/litewax/exceptions.py
@@ -0,0 +1,26 @@
+class AuthNotFound(Exception):
+    pass
+
+class SessionExpired(Exception):
+    pass
+
+class SignError(Exception):
+    pass
+
+class UnknownError(Exception):
+    pass
+
+class CPUlimit(Exception):
+    pass
+
+class ExpiredTransaction(Exception):
+    pass
+
+class NotImplementedError(Exception):
+    pass
+
+class CookiesExpired(Exception):
+    pass
+
+class PayWithPushError(Exception):
+    pass
---
+++ packages/litewax/litewax-0.1.7.tar.gz/litewax-0.1.7/litewax/types.py
@@ -0,0 +1,18 @@
+class Payers:
+    NEFTY = "nefty"
+    NEFTYBLOCKS = "nefty"
+    NeftyBlocks = "nefty"
+    nefty = "nefty"
+    neftyblocks = "nefty"
+    Nefty = "nefty"
+
+    ATOMICHUB = "atomichub"
+    ATOMIC_HUB = "atomichub"
+    AtomicHub = "atomichub"
+    atomichub = "atomichub"
+
+    CUSTOM = "custom"
+    Custom = "custom"
+    custom = "custom"
+    
+
