f536e889cfa7a9491b88b4fe78d0b06d3d885836
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:11:33 +0000

    Add package liana/0.1.1/liana-0.1.1-py3-none-any.whl

---
+++ packages/liana/liana-0.1.1-py3-none-any.whl/liana/tests/test_methods.py
@@ -0,0 +1,50 @@
+import pandas
+
+from liana.method import cellphonedb, singlecellsignalr as sca, natmi, connectome, logfc
+from scanpy.datasets import pbmc68k_reduced
+
+adata = pbmc68k_reduced()
+expected_shape = adata.shape
+
+
+def test_cellphonedb():
+    cellphonedb(adata, groupby='bulk_labels', use_raw=True, n_perms=2)
+    assert 'liana_res' in adata.uns.keys()
+    assert isinstance(adata.uns['liana_res'], pandas.DataFrame)
+    assert 'lr_means' in adata.uns['liana_res'].columns
+    assert 'pvals' in adata.uns['liana_res'].columns
+    assert adata.shape == expected_shape
+
+
+def test_natmi():
+    natmi(adata, groupby='bulk_labels', use_raw=True)
+    assert 'liana_res' in adata.uns.keys()
+    assert isinstance(adata.uns['liana_res'], pandas.DataFrame)
+    assert 'expr_prod' in adata.uns['liana_res'].columns
+    assert 'spec_weight' in adata.uns['liana_res'].columns
+    assert adata.shape == expected_shape
+
+
+def test_sca():
+    sca(adata, groupby='bulk_labels', use_raw=True)
+    assert 'liana_res' in adata.uns.keys()
+    assert isinstance(adata.uns['liana_res'], pandas.DataFrame)
+    assert 'lrscore' in adata.uns['liana_res'].columns
+    assert adata.shape == expected_shape
+
+
+def test_logfc():
+    logfc(adata, groupby='bulk_labels', use_raw=True)
+    assert 'liana_res' in adata.uns.keys()
+    assert isinstance(adata.uns['liana_res'], pandas.DataFrame)
+    assert 'lr_logfc' in adata.uns['liana_res'].columns
+    assert adata.shape == expected_shape
+
+
+def test_connectome():
+    connectome(adata, groupby='bulk_labels', use_raw=True)
+    assert 'liana_res' in adata.uns.keys()
+    assert isinstance(adata.uns['liana_res'], pandas.DataFrame)
+    assert 'expr_prod' in adata.uns['liana_res'].columns
+    assert 'scaled_weight' in adata.uns['liana_res'].columns
+    assert adata.shape == expected_shape
