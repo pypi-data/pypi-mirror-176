e9ea43c325e546bc2fc12f775f9adb03f1815363
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:17:49 +0000

    Add package piknik/0.1.2/piknik-0.1.2.tar.gz

---
+++ packages/piknik/piknik-0.1.2.tar.gz/piknik-0.1.2/piknik/msg.py
@@ -0,0 +1,104 @@
+# standard imports
+import logging
+import uuid
+import mimetypes
+from base64 import b64encode
+import time
+
+#from email.message import EmailMessage as Message
+from email.message import Message
+from email import message_from_string
+from email.policy import Compat32
+from email.utils import formatdate
+
+
+logg = logging.getLogger(__name__)
+
+
+class IssueMessage:
+
+    def __init__(self, issue):
+        self.__m = Message()
+
+        self.__m.add_header('Subject', issue.title)
+        self.__m.add_header('X-Piknik-Id', issue.id)
+        self.__m.add_header('Date', formatdate(time.time()))
+        self.__m.set_payload(None)
+        self.__m.set_type('multipart/relative')
+        self.__m.set_boundary(str(uuid.uuid4()))
+
+
+    @classmethod
+    def parse(cls, issue, v, verifier=None):
+        o = cls(issue)
+        m = message_from_string(v)
+        if verifier != None:
+            verifier(m)
+        o.__m
+        return o
+
+
+    def from_text(self, v):
+        m = Message()
+        m.add_header('Content-Transfer-Encoding', 'QUOTED-PRINTABLE')
+        m.set_charset('UTF-8')
+        m.set_payload(str(v))
+        return m
+
+
+    def detect_file(self, v):
+        return mimetypes.guess_type(v)
+
+
+    def from_file(self, v):
+        mime_type = self.detect_file(v)
+
+        m = Message()
+        m.set_type(mime_type[0])
+        if mime_type[1] != None:
+            m.set_charset(mime-type[1])
+        m.add_header('Content-Transfer-Encoding', 'BASE64')
+
+        f = open(v, 'rb')
+        r = f.read()
+        f.close()
+        r = b64encode(r)
+        m.set_payload(str(r))
+
+        return m
+
+
+    def add(self, *args, related_id=None, wrapper=None):
+        m_id = uuid.uuid4()
+        m = Message()
+        m.add_header('X-Piknik-Msg-Id', str(m_id))
+        m.add_header('Date', formatdate(time.time()))
+        if related_id != None:
+            m.add_header('In-Reply-To', related_id)
+        m.set_payload(None)
+        m.set_type('multipart/mixed')
+        m.set_boundary(str(uuid.uuid4()))
+        for a in args:
+            p = a[:2]
+            v = a[2:]
+            r = None
+            if p == 'f:':
+                r = self.from_file(v)
+            elif p == 's:':
+                r = self.from_text(v)
+            m.attach(r)
+        if wrapper:
+            m = wrapper(m)
+        self.__m.attach(m)
+
+
+    def as_string(self, **kwargs):
+        return self.__m.as_string(**kwargs)
+
+
+    def as_bytes(self, **kwargs):
+        return self.__m.as_bytes(**kwargs)
+
+
+    def __str__(self):
+        return self.as_string()
