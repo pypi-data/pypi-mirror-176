4f88be6d21225268248b52568c4cfcaf3ff33498
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:17:52 +0000

    Add package wsslack.py/1.0.0/wsslack.py-1.0.0-py3-none-any.whl

---
+++ packages/wsslack.py/wsslack.py-1.0.0-py3-none-any.whl/slack/__init__.py
@@ -0,0 +1,11 @@
+from .channel import *
+from .client import *
+from .httpclient import *
+from .member import *
+from .message import *
+from .route import *
+from .state import *
+from .team import *
+from .ws import *
+
+version = "1.0.0"
