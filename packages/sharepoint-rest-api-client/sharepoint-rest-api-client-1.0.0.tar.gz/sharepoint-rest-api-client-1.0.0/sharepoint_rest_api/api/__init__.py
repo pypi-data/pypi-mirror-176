6f711790a3fc0794dc9efa3fc589056e59b4c976
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:13:46 +0000

    Add package sharepoint-rest-api-client/1.0.0/sharepoint-rest-api-client-1.0.0.tar.gz

---
+++ packages/sharepoint-rest-api-client/sharepoint-rest-api-client-1.0.0.tar.gz/sharepoint-rest-api-client-1.0.0/sharepoint_rest_api/api/__init__.py
@@ -0,0 +1,8 @@
+from __future__ import absolute_import
+
+# flake8: noqa
+
+# import apis into api package
+from sharepoint_rest_api.api.lists_api import ListsApi
+from sharepoint_rest_api.api.sites_api import SitesApi
+from sharepoint_rest_api.api.webs_api import WebsApi
