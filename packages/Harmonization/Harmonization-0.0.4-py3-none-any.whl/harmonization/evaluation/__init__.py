5824a822ac0c2cfce3f33a5593411cc8f8233fd2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:38:35 +0000

    Add package Harmonization/0.0.4/Harmonization-0.0.4-py3-none-any.whl

---
+++ packages/Harmonization/Harmonization-0.0.4-py3-none-any.whl/harmonization/evaluation/__init__.py
@@ -0,0 +1,6 @@
+"""
+Module related to the Human Alignment benchmark
+"""
+
+from .click_me import load_clickme_val, evaluate_clickme
+from .metrics import dice, spearman_correlation, intersection_over_union
