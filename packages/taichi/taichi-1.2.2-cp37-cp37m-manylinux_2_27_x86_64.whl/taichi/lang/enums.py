2e5c03b1914efc8f570b39ebc276e64a6ccb24d6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:21:38 +0000

    Add package taichi/1.2.2/taichi-1.2.2-cp37-cp37m-manylinux_2_27_x86_64.whl

---
+++ packages/taichi/taichi-1.2.2-cp37-cp37m-manylinux_2_27_x86_64.whl/taichi/lang/enums.py
@@ -0,0 +1,7 @@
+from taichi._lib import core as _ti_core
+
+Layout = _ti_core.Layout
+AutodiffMode = _ti_core.AutodiffMode
+SNodeGradType = _ti_core.SNodeGradType
+
+__all__ = ['Layout', 'AutodiffMode', 'SNodeGradType']
