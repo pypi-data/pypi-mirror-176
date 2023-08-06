3df33ec7a0a390152fedb0af80205096c69d7965
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:16:08 +0000

    Add package binarybrain/4.2.3/binarybrain-4.2.3-cp36-cp36m-win_amd64.whl

---
+++ packages/binarybrain/binarybrain-4.2.3-cp36-cp36m-win_amd64.whl/binarybrain/__init__.py
@@ -0,0 +1,23 @@
+# coding: utf-8
+
+
+from binarybrain.system       import *
+
+from binarybrain.dtype        import *
+
+from binarybrain.object       import *
+
+from binarybrain.tensor       import *
+from binarybrain.frame_buffer import *
+from binarybrain.variables    import *
+
+from binarybrain.models       import *
+
+from binarybrain.losses       import *
+from binarybrain.metrics      import *
+from binarybrain.optimizer    import *
+
+from binarybrain.storage      import *
+from binarybrain.verilog      import *
+from binarybrain.hls          import *
+
