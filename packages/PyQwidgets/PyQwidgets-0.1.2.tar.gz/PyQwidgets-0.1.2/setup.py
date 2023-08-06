a1e9846a50329cb509103b59d7d4d63610a33366
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:24:21 +0000

    Add package PyQwidgets/0.1.2/PyQwidgets-0.1.2.tar.gz

---
+++ packages/PyQwidgets/PyQwidgets-0.1.2.tar.gz/PyQwidgets-0.1.2/setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+setuptools.setup(
+	name='PyQwidgets',
+	version='0.1.2',
+	author='Georg8528',
+	author_email='zadvornow2908@gmail.com',
+	description='No info',
+	packages=['PyQwidgets'],
+	classifiers=[
+		"Programming Language :: Python :: 3",
+		"License :: OSI Approved :: MIT License",
+		"Operating System :: OS Independent",
+	],
+	python_requires='>=3.6',
+)
