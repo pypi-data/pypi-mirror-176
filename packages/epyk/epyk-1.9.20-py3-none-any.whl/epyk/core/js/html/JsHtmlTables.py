c7a9be6c341efa20c8e88a9f29a700c87066e5e6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:11:47 +0000

    Add package epyk/1.9.20/epyk-1.9.20-py3-none-any.whl

---
+++ packages/epyk/epyk-1.9.20-py3-none-any.whl/epyk/core/js/html/JsHtmlTables.py
@@ -0,0 +1,91 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+from epyk.core.py import primitives
+
+from epyk.core.js.html import JsHtml
+from epyk.core.js.objects import JsNodeDom
+from epyk.core.js.primitives import JsObjects
+
+
+class JsHtmlTabulatorCell(JsHtml.JsHtml):
+
+  def _init__(self, js_code: str, page: primitives.PageModel, component: primitives.HtmlModel = None):
+    self.tableId = js_code
+    self.js_code = js_code
+    self.page = page
+    self.component = component
+
+  def getElement(self):
+    """
+
+    """
+    return JsNodeDom.JsDoms.get("cell.getElement()")
+
+  def getColumnField(self):
+    """
+
+    """
+    return JsObjects.JsObject.JsObject.get("cell.getColumn().getField()")
+
+  def getColumnTitle(self):
+    """
+
+    """
+    return JsObjects.JsObject.JsObject.get("cell.getColumn().getDefinition().title")
+
+  def getValue(self):
+    """
+
+    """
+    return JsObjects.JsObject.JsObject.get("cell.getValue()")
+
+  def getRow(self):
+    """
+
+    """
+    return JsObjects.JsObject.JsObject.get("cell.getRow()")
+
+  def getData(self):
+    """
+
+    """
+    return JsObjects.JsObject.JsObject.get("cell.getRow().getData()")
+
+
+class JsHtmlTabulator(JsHtml.JsHtml):
+
+  @property
+  def val(self):
+    """   Return a Javascript val object
+    """
+    return JsObjects.JsObjects.get(
+      "{%s: {value: %s, timestamp: Date.now(), offset: new Date().getTimezoneOffset()}}" % (
+        self.htmlCode, self.content.toStr()))
+
+  @property
+  def content(self):
+    """   
+
+    """
+    return JsHtml.ContentFormatters(self.page, "%s.getData()" % self.component.tableId)
+
+  @property
+  def headers(self):
+    """   
+
+    """
+    return JsObjects.JsObjects.get("%s.getColumnDefinitions()" % self.component.tableId)
+
+  def empty(self):
+    return self.component.js.clearData()
+
+
+class JsHtmlAggrid(JsHtml.JsHtml):
+
+  @property
+  def content(self):
+    """   
+
+    """
+    return JsHtml.ContentFormatters(self.page, self.component.js.getRowsData().toStr())
