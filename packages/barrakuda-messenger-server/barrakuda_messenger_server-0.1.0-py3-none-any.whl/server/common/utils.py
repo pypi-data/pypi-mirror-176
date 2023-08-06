125e25d98b9de0595c15d206932913436f9ec85d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:20:40 +0000

    Add package barrakuda-messenger-server/0.1.0/barrakuda_messenger_server-0.1.0-py3-none-any.whl

---
+++ packages/barrakuda-messenger-server/barrakuda_messenger_server-0.1.0-py3-none-any.whl/server/common/utils.py
@@ -0,0 +1,34 @@
+from server_dist.server.common.variables import *
+import json
+import sys
+sys.path.append('../../../')
+from server_dist.server.common.decos import log
+
+
+@log
+def get_message(client):
+    """
+    :param client: сокет с которого должно прийти сообщение
+    Функция, осуществляющая приём сообщения с определенного сокета
+    """
+
+    encoded_response = client.recv(MAX_PACKAGE_LENGTH)
+    json_response = encoded_response.decode(ENCODING)
+    response = json.loads(json_response)
+    if isinstance(response, dict):
+        return response
+    else:
+        raise TypeError
+
+
+@log
+def send_message(sock, message):
+    """
+    :param sock: сокет, на который нужно отправить сообщение
+    :param message: словарь сообщения для отправки
+    Функция, осуществляющая отправку сообщений на определённый сокет
+    """
+
+    js_message = json.dumps(message)
+    encoded_message = js_message.encode(ENCODING)
+    sock.send(encoded_message)
