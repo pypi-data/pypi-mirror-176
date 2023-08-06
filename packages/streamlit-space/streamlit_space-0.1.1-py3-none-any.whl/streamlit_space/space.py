a1088cf84ef9181560a40438b141ae14c1b13ad7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:18:54 +0000

    Add package streamlit-space/0.1.1/streamlit_space-0.1.1-py3-none-any.whl

---
+++ packages/streamlit-space/streamlit_space-0.1.1-py3-none-any.whl/streamlit_space/space.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""Spacing component for Streamlit"""
+
+from typing import Any, Optional
+
+import streamlit as st
+
+
+def space(container: Optional[Any] = None, lines: int = 1) -> None:
+    """Add blank lines to Streamlit app.
+
+    Args:
+        container: Streamlit container
+        lines: number of blank lines to be added
+    """
+    if container is None:
+        container = st
+    for _ in range(lines):
+        container.write('')
