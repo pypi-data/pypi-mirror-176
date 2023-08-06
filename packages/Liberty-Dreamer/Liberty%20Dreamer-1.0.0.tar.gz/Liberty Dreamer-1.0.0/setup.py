a763ea1c40d54f80fcd01c498b4de40741e82b9f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:37:07 +0000

    Add package Liberty-Dreamer/1.0.0/Liberty%20Dreamer-1.0.0.tar.gz

---
+++ packages/Liberty-Dreamer/Liberty%20Dreamer-1.0.0.tar.gz/Liberty Dreamer-1.0.0/setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='Liberty Dreamer',
+    version='1.0.0',
+    license='MIT License',
+    author='Benfica',
+    keywords=['chatbot', 'liberty', 'freedom', 'bot', 'chat'],
+    description='Free yourself with tools that increase the ease and possibilities of your project.',
+    packages=['datasets'],
+    install_requires=['fuzzywuzzy', 'python-Levenshtein'],
+)
