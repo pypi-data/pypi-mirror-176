309e5a136bff0a72323eb93ebbb7ec58d57e3832
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:37:05 +0000

    Add package simplepygamemenus/0.1.4/simplepygamemenus-0.1.4.tar.gz

---
+++ packages/simplepygamemenus/simplepygamemenus-0.1.4.tar.gz/simplepygamemenus-0.1.4/setup.py
@@ -0,0 +1,21 @@
+import setuptools
+from setuptools import find_packages
+  
+with open("README.md", "r") as fh:
+    description = fh.read()
+  
+setuptools.setup(
+    name="simplepygamemenus",
+    version="0.1.4",
+    author="Leonardo Ferrisi",
+    author_email="ferrisil@union.edu",
+    packages=find_packages(where='src'),
+    include_package_data=True,
+    description="A package for making simple menus for your python games!",
+    long_description=description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/LeonardoFerrisi/simplepygamemenus-gitrepo",
+    license='MIT',
+    python_requires='>=3.8',
+    install_requires=["pygame"]
+)
