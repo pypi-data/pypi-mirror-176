5fb13f0a5d37bbbebe165b819714524a43e7d334
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:15:59 +0000

    Add package viam-sdk/0.2.0/viam_sdk-0.2.0-py3-none-manylinux2014_aarch64.whl

---
+++ packages/viam-sdk/viam_sdk-0.2.0-py3-none-manylinux2014_aarch64.whl/viam/components/pose_tracker/client.py
@@ -0,0 +1,35 @@
+from typing import Any, Dict, List, Optional
+
+from grpclib.client import Channel
+
+from viam.components.generic.client import do_command
+from viam.proto.common import PoseInFrame
+from viam.proto.component.posetracker import (
+    GetPosesRequest,
+    GetPosesResponse,
+    PoseTrackerServiceStub,
+)
+
+from .pose_tracker import PoseTracker
+
+
+class PoseTrackerClient(PoseTracker):
+    """
+    gRPC client for the PoseTracker component.
+    """
+
+    def __init__(self, name: str, channel: Channel):
+        self.channel = channel
+        self.client = PoseTrackerServiceStub(channel)
+        super().__init__(name)
+
+    async def get_poses(self, body_names: List[str], *, timeout: Optional[float] = None) -> Dict[str, PoseInFrame]:
+        request = GetPosesRequest(
+            name=self.name,
+            body_names=body_names,
+        )
+        response: GetPosesResponse = await self.client.GetPoses(request, timeout=timeout)
+        return {key: response.body_poses[key] for key in response.body_poses.keys()}
+
+    async def do_command(self, command: Dict[str, Any], *, timeout: Optional[float] = None) -> Dict[str, Any]:
+        return await do_command(self.channel, self.name, command, timeout=timeout)
