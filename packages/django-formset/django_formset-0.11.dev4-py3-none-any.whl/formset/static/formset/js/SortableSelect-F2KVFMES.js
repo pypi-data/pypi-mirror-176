2b7aa9872f704dc38231b5b2a19e592d4a601a55
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:34:05 +0000

    Add package django-formset/0.11.dev4/django_formset-0.11.dev4-py3-none-any.whl

---
+++ packages/django-formset/django_formset-0.11.dev4-py3-none-any.whl/formset/static/formset/js/SortableSelect-F2KVFMES.js
@@ -0,0 +1,2 @@
+import{a}from"./chunk-O7FCP67S.js";import"./chunk-SNXKFZ2Y.js";import"./chunk-XXGBPDQQ.js";import"./chunk-ON5OQYWL.js";export{a as SortableSelectElement};
+//# sourceMappingURL=SortableSelect-F2KVFMES.js.map
