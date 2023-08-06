262bbc87c92a80fc886a66d2a8d7a5784087fde1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:04:43 +0000

    Add package git-sim/0.0.1/git_sim-0.0.1-py3-none-any.whl

---
+++ packages/git-sim/git_sim-0.0.1-py3-none-any.whl/git_sim/git_sim.py
@@ -0,0 +1,35 @@
+from manim import *
+import git, sys, numpy
+
+from git_sim_reset import *
+from git_sim_revert import *
+from git_sim_branch import *
+from git_sim_tag import *
+from git_sim_status import *
+from git_sim_add import *
+
+class GitSim(MovingCameraScene):
+    def __init__(self, args):
+        super().__init__()
+        self.args = args
+
+        if ( self.args.light_mode ):
+            self.fontColor = BLACK
+        else:
+            self.fontColor = WHITE
+
+    def construct(self):
+        if self.args.subcommand == 'reset':
+            self.command = GitSimReset(self)
+        elif self.args.subcommand == 'revert':
+            self.command = GitSimRevert(self)
+        elif self.args.subcommand == 'branch':
+            self.command = GitSimBranch(self)
+        elif self.args.subcommand == 'tag':
+            self.command = GitSimTag(self)
+        elif self.args.subcommand == 'status':
+            self.command = GitSimStatus(self)
+        elif self.args.subcommand == 'add':
+            self.command = GitSimAdd(self)
+
+        self.command.execute()
