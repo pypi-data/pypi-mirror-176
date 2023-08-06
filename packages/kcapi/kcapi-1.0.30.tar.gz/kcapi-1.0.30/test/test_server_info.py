5e874e1dbe04fb07c1b18ca77c816fdace8084ea
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:11:49 +0000

    Add package kcapi/1.0.30/kcapi-1.0.30.tar.gz

---
+++ packages/kcapi/kcapi-1.0.30.tar.gz/kcapi-1.0.30/test/test_server_info.py
@@ -0,0 +1,29 @@
+﻿import unittest
+
+from .testbed import TestBed
+
+
+class TestServerInfo(unittest.TestCase):
+    def test_serverinfo_api(self):
+        kc = self.testbed.getKeycloak()
+        serverinfo_api = kc.build_serverinfo()
+
+        serverinfo = serverinfo_api.get(None).verify().resp().json()
+
+        self.assertIsInstance(serverinfo, dict)
+        self.assertEqual("15.0.2", serverinfo["systemInfo"]["version"])
+        self.assertEqual("community", serverinfo["profileInfo"]["name"])
+        self.assertEqual("15.0.2", kc.server_info.version)
+        self.assertEqual("community", kc.server_info.profile_name)
+
+    @classmethod
+    def setUpClass(self):
+        self.testbed = TestBed()
+        # self.testbed.createRealms()
+        # self.testbed.createUsers()
+        self.REALM = self.testbed.REALM
+
+    @classmethod
+    def tearDownClass(self):
+        # self.testbed.goodBye()
+        return 1
