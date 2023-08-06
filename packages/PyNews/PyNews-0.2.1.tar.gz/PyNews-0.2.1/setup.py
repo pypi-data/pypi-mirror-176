6f1296301ad1bd3f90fb56ff93c2d31a2397eb61
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:06:43 +0000

    Add package PyNews/0.2.1/PyNews-0.2.1.tar.gz

---
+++ packages/PyNews/PyNews-0.2.1.tar.gz/PyNews-0.2.1/setup.py
@@ -0,0 +1,48 @@
+from pathlib import Path
+
+from setuptools import find_packages, setup
+
+this_directory = Path(__file__).parent
+long_description = (this_directory / "README.rst").read_text()
+description = "PyNews is a simple Python CLI to browse news from HN website"
+author_name = "Patrick Mazulo"
+author_email = "pmazulo@gmail.com"
+dependencies = [
+    "curses-menu==0.5.0",
+    "requests==2.11.1",
+    "alive-progress==2.4.1",
+]
+
+setup(
+    name="PyNews",
+    version="0.2.1",
+    description=description,
+    long_description=long_description,
+    long_description_content_type="text/x-rst",
+    url="https://github.com/mazulo/pynews_cli",
+    author=author_name,
+    author_email=author_email,
+    maintainer=author_name,
+    maintainer_email=author_email,
+    license="MIT",
+    classifiers=[
+        "Intended Audience :: Developers",
+        "Development Status :: 5 - Production/Stable",
+        "Environment :: Console",
+        "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.7",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Topic :: Utilities",
+        "Topic :: System :: Shells",
+    ],
+    keywords="pynews cli shell news hackernews",
+    download_url="https://github.com/mazulo/pynews_cli/archive/master.zip",
+    packages=find_packages(exclude=["tests*"]),
+    install_requires=dependencies,
+    entry_points={"console_scripts": ["pynews = pynews.pynews:main"]},
+    platforms="windows linux",
+)
