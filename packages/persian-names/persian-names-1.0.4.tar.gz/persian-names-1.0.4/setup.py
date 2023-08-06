4cdd4269cdb1e1e2308fd9536c4097c730478109
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:14:45 +0000

    Add package persian-names/1.0.4/persian-names-1.0.4.tar.gz

---
+++ packages/persian-names/persian-names-1.0.4.tar.gz/persian-names-1.0.4/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup
+
+with open('./README.md', 'r') as f:
+    readme = f.read()
+
+setup(
+    name='persian-names',
+    version='1.0.4',
+    packages=['persian_names'],
+    url='https://github.com/armanyazdi/persian-names',
+    license='MIT',
+    author='Arman Yazdi',
+    description='A simple Python library for generating random Persian (Farsi) names.',
+    long_description=readme,
+    long_description_content_type='text/markdown',
+    install_requires=['requests'],
+    keywords='persian names, farsi names, iranian names, name generator',
+    classifiers=[
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Programming Language :: Python :: 3.10',
+        'Programming Language :: Python :: 3.11',
+    ],
+    project_urls={
+        'Source': 'https://github.com/armanyazdi/persian-names',
+        'Documentation': 'https://pypi.org/project/persian-names',
+    },
+)
