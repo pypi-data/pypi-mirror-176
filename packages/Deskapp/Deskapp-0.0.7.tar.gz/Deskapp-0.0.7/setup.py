8d5d1cd24a51f5845ccdb26edc3ba052c2c725e1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:05:18 +0000

    Add package Deskapp/0.0.7/Deskapp-0.0.7.tar.gz

---
+++ packages/Deskapp/Deskapp-0.0.7.tar.gz/Deskapp-0.0.7/setup.py
@@ -0,0 +1,33 @@
+# New Version of Setup.py
+# now using distutils.
+# last updated 7-26-22.
+
+
+from distutils.core import setup
+
+setup(
+    name='Deskapp',
+	version = "0.0.7",
+    description='An Open Source Terminal basesd Program Manager. Write and manage your own apps in this framework.',
+    long_description_content_type='text/markdown',
+    long_description=open('README.md').read(),
+    author='Ruckusist',
+    author_email='ruckusist@outlook.com',
+    url='https://ruckusist.com/deskapp',
+    packages=['deskapp', 'deskapp.mods','deskapp.src'],
+    classifiers=[
+          'Development Status :: 4 - Beta',
+          'Environment :: Console',
+          'Intended Audience :: End Users/Desktop',
+          'Intended Audience :: Developers',
+          'Intended Audience :: System Administrators',
+          'Operating System :: MacOS :: MacOS X',
+          'Operating System :: POSIX',
+          'Programming Language :: Python',
+          'Topic :: Office/Business',
+          ],
+    install_requires=['jinja2'],
+    entry_points = {
+        'console_scripts': ['deskapp=deskapp.__main__:main']
+    },
+)
