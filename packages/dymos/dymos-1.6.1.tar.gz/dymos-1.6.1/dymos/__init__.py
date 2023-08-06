73be6efa46254160ba4eaaa7c64c35c5313e11c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:39:28 +0000

    Add package dymos/1.6.1/dymos-1.6.1.tar.gz

---
+++ packages/dymos/dymos-1.6.1.tar.gz/dymos-1.6.1/dymos/__init__.py
@@ -0,0 +1,8 @@
+__version__ = '1.6.1'
+
+from .phase import Phase, AnalyticPhase
+from .transcriptions import GaussLobatto, Radau, ExplicitShooting, Analytic
+from .trajectory.trajectory import Trajectory
+from .run_problem import run_problem
+from .load_case import load_case
+from .options import options
---
+++ packages/dymos/dymos-1.6.1.tar.gz/dymos-1.6.1/dymos/phase/__init__.py
@@ -0,0 +1,2 @@
+from .analytic_phase import AnalyticPhase
+from .phase import Phase
