29288813b4b6bd7c09ca6324685bafe9d6b3c0bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:12:14 +0000

    Add package histoclean/0.0.1/histoclean-0.0.1.tar.gz

---
+++ packages/histoclean/histoclean-0.0.1.tar.gz/histoclean-0.0.1/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+setup(
+    name='histoclean',
+    version='0.0.1',
+    description='Histoclean (initial package setup)',
+    py_modules=["histoclean"],
+    package_dir={'': 'src'},
+    install_requires=[
+        "Pillow", "opencv-python", "imageio",
+        "numpy", "numba", "imagecorruptions",
+        "openslide-python", "scipy", "imgaug"
+    ],
+)
