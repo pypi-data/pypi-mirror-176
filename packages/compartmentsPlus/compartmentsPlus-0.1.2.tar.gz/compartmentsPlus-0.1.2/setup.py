0d5c39cbf91d1e54da18207b8b7e657181c6d2f8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:04:20 +0000

    Add package compartmentsPlus/0.1.2/compartmentsPlus-0.1.2.tar.gz

---
+++ packages/compartmentsPlus/compartmentsPlus-0.1.2.tar.gz/compartmentsPlus-0.1.2/setup.py
@@ -0,0 +1,32 @@
+from setuptools import setup, find_packages
+import codecs
+import os
+
+VERSION = '0.1.2'
+DESCRIPTION = 'Unified API for compartmental modeling'
+
+from pathlib import Path
+this_directory = Path(__file__).parent
+LONG_DESCRIPTION = (this_directory / "README.md").read_text()
+
+# Setting up
+setup(
+    name="compartmentsPlus",
+    version=VERSION,
+    author="ThreeStrings",
+    author_email="<zack@ouhermans.com>",
+    description=DESCRIPTION,
+    long_description_content_type="text/markdown",
+    long_description=LONG_DESCRIPTION,
+    packages=find_packages(),
+    install_requires=['streamlit', 'openpyxl', 'pandas', 'matplotlib'],
+    keywords=['python', 'compartment', 'modeling', 'epidemiology', 'networks'],
+    classifiers=[
+        "Development Status :: 5 - Production/Stable",
+        "Intended Audience :: Science/Research",
+        "Programming Language :: Python :: 3",
+        "Operating System :: Unix",
+        "Operating System :: MacOS :: MacOS X",
+        "Operating System :: Microsoft :: Windows",
+    ]
+)
