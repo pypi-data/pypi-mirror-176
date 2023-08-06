4d4e6365f7b2cb5e75ae5df8d62903f3086eea24
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:12:43 +0000

    Add package trtimeextractor/0.1.1/trtimeextractor-0.1.1.tar.gz

---
+++ packages/trtimeextractor/trtimeextractor-0.1.1.tar.gz/trtimeextractor-0.1.1/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+def readme():
+    with open('README.rst') as f:
+        return f.read()
+
+setup(name='trtimeextractor',
+      version='0.1.1',
+      description='Time Extractor NLP project - locate dates and times in text documents',
+      long_description=readme(),
+      keywords='NLP text extraction time date',
+      url='https://github.com/westeropsml/time-extractor-tr',
+      author='WesterOps',
+      author_email='ml@westerops.com',
+      license='WesterOps',
+      install_requires=[
+          'pyjnius==1.1.1',
+      ],
+      packages=['trtimeextractor'],
+      include_package_data=True,
+      test_suite='nose.collector',
+      tests_require=['nose'],
+      zip_safe=False)
