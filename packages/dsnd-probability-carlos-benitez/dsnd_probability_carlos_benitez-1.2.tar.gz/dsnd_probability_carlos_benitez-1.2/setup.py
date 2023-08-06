69808a8861684941bcf42443fec2049fc2bfadbc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:23:53 +0000

    Add package dsnd-probability-carlos-benitez/1.2/dsnd_probability_carlos_benitez-1.2.tar.gz

---
+++ packages/dsnd-probability-carlos-benitez/dsnd_probability_carlos_benitez-1.2.tar.gz/dsnd_probability_carlos_benitez-1.2/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name='dsnd_probability_carlos_benitez',
+    version='1.2',
+    description='Gaussian an Binomial distributions',
+    packages=['dsnd_probability_carlos_benitez'],
+    author='Carlos Benitez',
+    author_email='carlitos.benitez@yahoo.com',
+    zip_file=False
+)
