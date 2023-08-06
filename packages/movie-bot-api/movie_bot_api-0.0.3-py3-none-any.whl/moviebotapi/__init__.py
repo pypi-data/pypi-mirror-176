ed3f3c86b634c71faf1778f18c2c8feed489cf69
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:23:10 +0000

    Add package movie-bot-api/0.0.3/movie_bot_api-0.0.3-py3-none-any.whl

---
+++ packages/movie-bot-api/movie_bot_api-0.0.3-py3-none-any.whl/moviebotapi/__init__.py
@@ -0,0 +1,28 @@
+from moviebotapi.core.session import Session
+from moviebotapi.douban import DoubanApi
+from moviebotapi.scraper import ScraperApi
+from moviebotapi.site import SiteApi
+from moviebotapi.subscribe import SubscribeApi
+from moviebotapi.tmdb import TmdbApi
+from moviebotapi.user import UserApi
+
+
+class MovieBotServer:
+    user: UserApi
+    subscribe: SubscribeApi
+    scraper: ScraperApi
+    douban: DoubanApi
+    tmdb: TmdbApi
+    site: SiteApi
+
+    def __init__(self, session: Session = None):
+        if session:
+            self.set_session(session)
+
+    def set_session(self, session: Session):
+        self.user = UserApi(session)
+        self.subscribe = SubscribeApi(session)
+        self.scraper = ScraperApi(session)
+        self.douban = DoubanApi(session)
+        self.tmdb = TmdbApi(session)
+        self.site = SiteApi(session)
