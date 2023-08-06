cc0ab8b9293866bf4dc2d0bfed1ccc3055792aff
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:11:27 +0000

    Add package tap-zenloop/0.1.1/tap-zenloop-0.1.1.tar.gz

---
+++ packages/tap-zenloop/tap-zenloop-0.1.1.tar.gz/tap-zenloop-0.1.1/setup.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+from setuptools import setup
+
+setup(
+    name="tap-zenloop",
+    version="0.1.1",
+    description="Singer tap to extract Zenloop data",
+    author="cargo.one",
+    url="https://cargo.one",
+    classifiers=["Programming Language :: Python :: 3 :: Only"],
+    py_modules=["tap_zenloop"],
+    install_requires=[
+        "singer-python==5.13.*",
+        "requests>=2.27.0",
+        "pendulum==2.1.*",
+    ],
+    entry_points="""
+    [console_scripts]
+    tap-zenloop=tap_zenloop:main
+    """,
+    packages=["tap_zenloop"],
+    package_data = {
+        "schemas": ["tap_zenloop/schemas/*.json"]
+    },
+    include_package_data=True,
+)
