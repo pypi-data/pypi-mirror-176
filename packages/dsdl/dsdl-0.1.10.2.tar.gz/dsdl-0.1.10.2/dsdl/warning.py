b552782a62d94f91b0144968da807f9ff4404cc6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:13:47 +0000

    Add package dsdl/0.1.10.2/dsdl-0.1.10.2.tar.gz

---
+++ packages/dsdl/dsdl-0.1.10.2.tar.gz/dsdl-0.1.10.2/dsdl/warning.py
@@ -0,0 +1,14 @@
+class DuplicateDefineWarning(SyntaxWarning):
+    pass
+
+
+class InvalidLabelWarning(SyntaxWarning):
+    pass
+
+
+class FieldNotFoundWarning(SyntaxWarning):
+    pass
+
+
+class ClassHasDefinedWarning(SyntaxWarning):
+    pass
