7c8e7a8991744b4691a3076b533538220cb8f138
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:36:29 +0000

    Add package replitkey/1.0/replitkey-1.0.tar.gz

---
+++ packages/replitkey/replitkey-1.0.tar.gz/replitkey-1.0/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='replitkey',
+      version='1.0',
+      description='ReplitKey can get database values from a repl project',
+      author='mas6y6',
+      packages=['replitkey'],
+      long_description="For More info Use the Link: https://github.com/mas6y6/Replitkey/tree/main")
