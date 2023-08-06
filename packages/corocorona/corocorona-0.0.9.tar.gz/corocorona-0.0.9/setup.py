323c9c86f76bbf9479455fa6ed64114af35fabf4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:03:01 +0000

    Add package corocorona/0.0.9/corocorona-0.0.9.tar.gz

---
+++ packages/corocorona/corocorona-0.0.9.tar.gz/corocorona-0.0.9/setup.py
@@ -0,0 +1,30 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="corocorona",
+    version="0.0.9",
+    install_requires=[
+        "requests",
+    ],
+    entry_points={
+        'console_scripts': [
+            'corona=corona:printCorona',
+        ],
+    },
+    author="syoshika",
+    author_email="syoshika@student.42tokyo.jp",
+    description="A covoid-19 in Japan",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/sho-yoshikawa/corocorona",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.9',
+)
