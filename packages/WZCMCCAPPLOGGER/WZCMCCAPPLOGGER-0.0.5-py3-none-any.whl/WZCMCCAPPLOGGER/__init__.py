d1fc0b99a9dbb8e2ae8133f330eb503e8d2eddc4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:22:55 +0000

    Add package WZCMCCAPPLOGGER/0.0.5/WZCMCCAPPLOGGER-0.0.5-py3-none-any.whl

---
+++ packages/WZCMCCAPPLOGGER/WZCMCCAPPLOGGER-0.0.5-py3-none-any.whl/WZCMCCAPPLOGGER/__init__.py
@@ -0,0 +1,25 @@
+
+import WZCMCCAPPLOGGER.GetNetWorkInfo
+import WZCMCCAPPLOGGER.LogUploader
+
+name = "WZCMCCAPPLOGGER"
+
+#API  DB  Redis
+log_type="DB"
+dbhost ='10.77.18.248,8433';
+dbuser ='cmcclogger'
+dbpwd= 'log$zj123'
+dbname = 'WZCMCCAPPLOGGER'
+
+def InsertLog(APPID):
+
+    mac = WZCMCCAPPLOGGER.GetNetWorkInfo.get_mac_address();
+    pcname = WZCMCCAPPLOGGER.GetNetWorkInfo.get_hostName();
+    ip = WZCMCCAPPLOGGER.GetNetWorkInfo.get_hostip();
+
+    sql ="""insert into CMCCAPPLOG(APPID,PCNAME,MAC,IP)  values ('{APPID}','{pcname}','{mac}','{ip}')""".format(APPID=APPID,pcname=pcname,mac=mac,ip=ip)
+    _Obdcsql = LogUploader.Obdcsql(dbhost,dbuser,dbpwd,dbname)
+    _Obdcsql.insert_record(sql)
+
+__version__ = "0.78.0"
+
