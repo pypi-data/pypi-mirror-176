c2372a954b3584000c21125e065a9c688a039305
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:40:38 +0000

    Add package xedocs/0.2.0/xedocs-0.2.0.tar.gz

---
+++ packages/xedocs/xedocs-0.2.0.tar.gz/xedocs-0.2.0/xedocs/__init__.py
@@ -0,0 +1,24 @@
+"""Top-level package for xedocs."""
+
+__author__ = """Yossi Mosbacher"""
+__email__ = "joe.mosbacher@gmail.com"
+__version__ = "0.2.0"
+import logging
+
+logger = logging.getLogger(__name__)
+
+from . import api
+from ._settings import settings
+from .datasources import *
+from . import schemas
+from .utils import *
+from .xedocs import *
+
+from .contexts import *
+
+try:
+    from . import widgets
+
+    gui = widgets.XedocsEditor()
+except ImportError:
+    logger.warning("Could not import editors, GUI not available.")
