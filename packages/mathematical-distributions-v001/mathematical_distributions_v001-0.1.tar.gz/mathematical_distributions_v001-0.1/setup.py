c6dfeb1677b6f70390022c0e557354acaed80cc6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:14:12 +0000

    Add package mathematical-distributions-v001/0.1/mathematical_distributions_v001-0.1.tar.gz

---
+++ packages/mathematical-distributions-v001/mathematical_distributions_v001-0.1.tar.gz/mathematical_distributions_v001-0.1/setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup
+
+setup(name='mathematical_distributions_v001',
+      version='0.1',
+      description='Gaussian and Binomial distributions',
+      packages=['mathematical_distributions_v001'],
+      author = "Evren Arslan",
+      author_email= "earslan3e@gmail.com",
+      zip_safe=False)
