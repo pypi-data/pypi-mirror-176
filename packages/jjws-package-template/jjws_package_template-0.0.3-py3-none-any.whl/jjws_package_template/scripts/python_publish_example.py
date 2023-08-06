7b3dd2a5243513a6424c164817b358451601c474
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:24:08 +0000

    Add package jjws-package-template/0.0.3/jjws_package_template-0.0.3-py3-none-any.whl

---
+++ packages/jjws-package-template/jjws_package_template-0.0.3-py3-none-any.whl/jjws_package_template/scripts/python_publish_example.py
@@ -0,0 +1,7 @@
+from jjws_package_template.example_module import example_function
+
+def python_publish_example_script():
+    print(example_function())
+
+if __name__ == "__main__":
+    python_publish_example_script()
