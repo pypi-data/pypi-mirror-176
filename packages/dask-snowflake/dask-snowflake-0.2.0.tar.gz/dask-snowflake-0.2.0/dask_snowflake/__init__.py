5f581c37356cad30214ab867f8ebc5b268457049
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:12:23 +0000

    Add package dask-snowflake/0.2.0/dask-snowflake-0.2.0.tar.gz

---
+++ packages/dask-snowflake/dask-snowflake-0.2.0.tar.gz/dask-snowflake-0.2.0/dask_snowflake/__init__.py
@@ -0,0 +1,3 @@
+from .core import read_snowflake, to_snowflake
+
+__version__ = "0.2.0"
---
+++ packages/dask-snowflake/dask-snowflake-0.2.0.tar.gz/dask-snowflake-0.2.0/setup.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+
+from setuptools import setup
+
+setup(
+    name="dask-snowflake",
+    version="0.2.0",
+    description="Dask + Snowflake intergration",
+    license="BSD",
+    maintainer="James Bourbeau",
+    maintainer_email="james@coiled.io",
+    packages=["dask_snowflake"],
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    python_requires=">=3.7",
+    install_requires=open("requirements.txt").read().strip().split("\n"),
+    include_package_data=True,
+    zip_safe=False,
+)
