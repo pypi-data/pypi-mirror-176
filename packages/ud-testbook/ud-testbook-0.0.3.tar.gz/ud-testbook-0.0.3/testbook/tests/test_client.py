21e99eddebc817500c395786343e0d3228082590
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:35:15 +0000

    Add package ud-testbook/0.0.3/ud-testbook-0.0.3.tar.gz

---
+++ packages/ud-testbook/ud-testbook-0.0.3.tar.gz/ud-testbook-0.0.3/testbook/tests/test_client.py
@@ -0,0 +1,117 @@
+import pytest
+from textwrap import dedent
+
+from ..testbook import testbook
+from ..client import TestbookNotebookClient
+from ..exceptions import (
+    TestbookCellTypeError,
+    TestbookCellTagNotFoundError,
+    TestbookExecuteResultNotFoundError,
+)
+
+
+@pytest.fixture(scope='module')
+def notebook():
+    with testbook('testbook/tests/resources/inject.ipynb', execute=True) as tb:
+        yield tb
+
+
+@pytest.mark.parametrize("cell_index_args, expected_result", [(2, [2]), ('hello', [1])])
+def test_cell_index(cell_index_args, expected_result, notebook):
+    assert notebook._cell_indexes(cell_index_args) == expected_result
+
+
+@pytest.mark.parametrize(
+    "cell_index_args, expected_error",
+    [([1, 2, 3], TypeError), ('non-existent-tag', TestbookCellTagNotFoundError)],
+)
+def test_cell_index_raises_error(cell_index_args, expected_error, notebook):
+    with pytest.raises(expected_error):
+        notebook._cell_indexes(cell_index_args)
+
+
+@pytest.mark.parametrize(
+    "var_name, expected_result",
+    [
+        ('sample_dict', {'foo': 'bar'}),
+        ('sample_list', ['foo', 'bar']),
+        ('sample_list + ["hello world"]', ['foo', 'bar', 'hello world']),
+        ('sample_int', 42),
+        ('sample_int * 2', 84),
+        ('sample_str', 'hello world'),
+        ('sample_str + " foo"', 'hello world foo'),
+    ],
+)
+def test_value(var_name, expected_result, notebook):
+    assert notebook.value(var_name) == expected_result
+
+
+@pytest.mark.parametrize("code", [('sample_int *= 2'), ('print(sample_int)'), ('')])
+def test_value_raises_error(code, notebook):
+    with pytest.raises(TestbookExecuteResultNotFoundError):
+        notebook.value(code)
+
+
+@pytest.mark.parametrize(
+    "cell, expected_result",
+    [
+        (
+            {
+                "cell_type": "code",
+                "execution_count": 9,
+                "metadata": {},
+                "outputs": [
+                    {
+                        "name": "stdout",
+                        "output_type": "stream",
+                        "text": "hello world\n" "foo\n" "bar\n",
+                    }
+                ],
+            },
+            """
+            hello world
+            foo
+            bar
+            """,
+        ),
+        ({"cell_type": "code", "execution_count": 9, "metadata": {}, "outputs": []}, ""),
+    ],
+)
+def test_output_text(cell, expected_result):
+    assert TestbookNotebookClient._output_text(cell) == dedent(expected_result).strip()
+
+
+@pytest.mark.parametrize(
+    "cell", [{}, {"cell_type": "markdown", "metadata": {}, "source": ["# Hello"]}]
+)
+def test_output_text_raises_error(cell):
+    with pytest.raises(TestbookCellTypeError):
+        assert TestbookNotebookClient._output_text(cell)
+
+
+def test_cell_execute_result_index(notebook_factory):
+    nb = notebook_factory()
+    with testbook(nb, execute="test") as tb:
+        assert tb.cell_execute_result(1) == [{"text/plain": "22"}]
+        assert tb.cell_execute_result(2) == [{"text/plain": "text"}]
+
+
+def test_cell_execute_result_tag(notebook_factory):
+    nb = notebook_factory()
+    with testbook(nb, execute="test") as tb:
+        assert tb.cell_execute_result("test") == [{"text/plain": "22"}]
+        assert tb.cell_execute_result("dummy-outputs") == [{"text/plain": "text"}]
+
+
+def test_cell_execute_result_indexerror(notebook_factory):
+    nb = notebook_factory([])
+    with testbook(nb) as tb:
+        with pytest.raises(IndexError):
+            tb.cell_execute_result(1)
+
+
+def test_cell_execute_result_tagnotfound(notebook_factory):
+    nb = notebook_factory([])
+    with testbook(nb) as tb:
+        with pytest.raises(TestbookCellTagNotFoundError):
+            tb.cell_execute_result("test")
---
+++ packages/ud-testbook/ud-testbook-0.0.3.tar.gz/ud-testbook-0.0.3/testbook/tests/test_inject.py
@@ -0,0 +1,80 @@
+import pytest
+
+from ..testbook import testbook
+
+
+@pytest.fixture(scope='module')
+def notebook():
+    with testbook('testbook/tests/resources/inject.ipynb', execute=True) as tb:
+        yield tb
+
+
+def inject_helper(*args, **kwargs):
+    pass
+
+
+@pytest.mark.parametrize(
+    "args, kwargs",
+    [
+        (None, None),
+        ([1, 2], None),
+        ((1, 2), None),
+        ((True, False), None),
+        (['a', 'b'], None),
+        ([1.1, float('nan'), float('inf'), float('-inf')], None),
+        ([{'key1': 'value1'}, {'key2': 'value2'}], None),
+        ((1, 2, False), {'key2': 'value2'}),
+        ((None, None, False), {'key2': 'value2'}),
+    ],
+)
+def test_inject(args, kwargs, notebook):
+    assert notebook.inject(inject_helper, args=args, kwargs=kwargs, pop=True)
+
+
+@pytest.mark.parametrize(
+    "code_block, expected_text",
+    [
+        (
+            '''
+            def foo():
+                print('I ran in the code block')
+            foo()
+        ''',
+            "I ran in the code block",
+        ),
+        (
+            '''
+            def foo(arg):
+                print(f'You passed {arg}')
+            foo('bar')
+        ''',
+            "You passed bar",
+        ),
+    ],
+)
+def test_inject_code_block(code_block, expected_text, notebook):
+    assert notebook.inject(code_block, pop=True).output_text == expected_text
+
+
+def test_inject_raises_exception(notebook):
+    values = [3, {'key': 'value'}, ['a', 'b', 'c'], (1, 2, 3), {1, 2, 3}]
+
+    for value in values:
+        with pytest.raises(TypeError):
+            notebook.inject(value)
+
+
+def test_inject_before_after(notebook):
+    notebook.inject("say_hello()", run=False, after="hello")
+    assert notebook.cells[notebook._cell_indexes("hello")[0] + 1].source == "say_hello()"
+
+    notebook.inject("say_bye()", before="hello")
+    assert notebook.cells[notebook._cell_indexes("hello")[0] - 1].source == "say_bye()"
+
+    with pytest.raises(ValueError):
+        notebook.inject("say_hello()", before="hello", after="bye")
+
+
+def test_inject_pop(notebook):
+    assert notebook.inject("1+1", pop=True).execute_result == [{'text/plain': '2'}]
+    assert notebook.cells[-1].source != "1+1"
