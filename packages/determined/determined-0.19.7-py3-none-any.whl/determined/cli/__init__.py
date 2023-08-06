1fd5b865869333bca925232128f72d0708f7c907
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:17:14 +0000

    Add package determined/0.19.7/determined-0.19.7-py3-none-any.whl

---
+++ packages/determined/determined-0.19.7-py3-none-any.whl/determined/cli/__init__.py
@@ -0,0 +1,27 @@
+from determined.cli._util import (
+    output_format_args,
+    make_pagination_args,
+    default_pagination_args,
+    setup_session,
+    require_feature_flag,
+)
+from determined.cli import (
+    agent,
+    checkpoint,
+    cli,
+    command,
+    experiment,
+    master,
+    model,
+    notebook,
+    project,
+    remote,
+    render,
+    resources,
+    shell,
+    template,
+    tensorboard,
+    trial,
+    user,
+    workspace,
+)
