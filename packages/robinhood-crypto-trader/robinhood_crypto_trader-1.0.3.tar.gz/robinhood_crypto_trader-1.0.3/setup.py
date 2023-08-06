b782e3987a8a457b4461628d61a69bc080df49b1
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:15:41 +0000

    Add package robinhood-crypto-trader/1.0.3/robinhood_crypto_trader-1.0.3.tar.gz

---
+++ packages/robinhood-crypto-trader/robinhood_crypto_trader-1.0.3.tar.gz/robinhood_crypto_trader-1.0.3/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="robinhood_crypto_trader",
+    version="1.0.3",
+    author="Tyler Pruitt",
+    description="A Python package for an automated cryptocurrency trader on the popular trading platform Robinhood",
+    url="https://github.com/tyler-pruitt/robinhood_crypto_trader",
+    packages=find_packages(),
+    requires=[
+        "numpy",
+        "robin_stocks",
+        "pandas",
+        "matplotlib",
+        "pandas_ta",
+        "discord_webhook"
+    ],
+    install_requires=[
+        "numpy",
+        "robin_stocks",
+        "pandas",
+        "matplotlib",
+        "pandas_ta",
+        "discord_webhook"
+    ],
+    keywords=["robinhood", "crypto", "cryptocurrency", "trading", "autotrading", "investing", "robinhood crypto trader"],
+    license="MIT"
+)
