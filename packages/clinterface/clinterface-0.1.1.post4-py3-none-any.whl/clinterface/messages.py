be5d7de156822323c6650cfbc3a2959932e09815
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 16:24:24 +0000

    Add package clinterface/0.1.1.post4/clinterface-0.1.1.post4-py3-none-any.whl

---
+++ packages/clinterface/clinterface-0.1.1.post4-py3-none-any.whl/clinterface/messages.py
@@ -0,0 +1,36 @@
+import sys
+from . import colors
+
+def join_args(f):
+    def wrapper(*args, **kwargs):
+        return f(' '.join(args), ', '.join('{}={}'.format(k, v) for k, v in kwargs.items()))
+    return wrapper
+
+@join_args
+def success(message, details):
+    if details:
+        message = '{} ({})'.format(message, details)
+    print(colors.GREEN + message + colors.DEFAULT)
+
+@join_args
+def warning(message, details):
+    if details:
+        message = 'Warning: {} ({})'.format(message, details)
+    print(colors.YELLOW + message + colors.DEFAULT)
+
+@join_args
+def failure(message, details):
+    if details:
+        message = 'Failure: {} ({})'.format(message, details)
+    print(colors.RED + message + colors.DEFAULT)
+
+@join_args
+def error(message, details):
+    if details:
+        message = '{} ({})'.format(message, details)
+    raise SystemExit(colors.RED + message + colors.DEFAULT)
+
+@join_args
+def unknown_error(message):
+    fcode = sys._getframe(1).f_code
+    raise SystemExit(colors.RED + '{}:{} {}'.format(fcode.co_filename, fcode.co_name, message) + colors.DEFAULT)
