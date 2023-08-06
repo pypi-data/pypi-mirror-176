746f48c97b6f89c6fce473cdb0c827a5aa384765
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:04:06 +0000

    Add package djoserfoodgram/2.1.3/djoserfoodgram-2.1.3.tar.gz

---
+++ packages/djoserfoodgram/djoserfoodgram-2.1.3.tar.gz/djoserfoodgram-2.1.3/setup.py
@@ -0,0 +1,31 @@
+import setuptools
+
+setuptools.setup(
+	name='djoserfoodgram',
+	version='2.1.3',
+	author='avnosov',
+	author_email='nosov1995@gmail.com',
+	description='',
+	packages=['djoserfoodgram',
+ 'djoserfoodgram.social',
+ 'djoserfoodgram.social.backends',
+ 'djoserfoodgram.social.token',
+ 'djoserfoodgram.urls'],
+	classifiers=[
+		"Programming Language :: Python :: 3",
+		"License :: OSI Approved :: MIT License",
+		"Operating System :: OS Independent",
+	],
+        package_data = \
+{'': ['*'],
+ 'djoserfoodgram': ['locale/de/LC_MESSAGES/django.po',
+            'locale/es/LC_MESSAGES/django.po',
+            'locale/fr/LC_MESSAGES/django.po',
+            'locale/ka/LC_MESSAGES/django.po',
+            'locale/pl/LC_MESSAGES/django.po',
+            'locale/pt_BR/LC_MESSAGES/django.po',
+            'locale/ru_RU/LC_MESSAGES/django.po',
+            'templates/email/*']},
+
+	python_requires='>=3.6',
+)
