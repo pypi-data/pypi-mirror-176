b3f1bac68d7c1914b7502adb93f80a4b564053e3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:17:28 +0000

    Add package gron/1.3.0/gron-1.3.0-py3-none-any.whl

---
+++ packages/gron/gron-1.3.0-py3-none-any.whl/gron/__init__.py
@@ -0,0 +1,9 @@
+"""
+
+All imports here are unused in this module but exported as public
+interface.
+
+"""
+
+from gron.gron import gron as gron  # noqa
+from gron.version import __VERSION__ as __VERSION__  # noqa
