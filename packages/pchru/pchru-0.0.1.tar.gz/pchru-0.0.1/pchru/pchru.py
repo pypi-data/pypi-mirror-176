f5424dc2813b57b6e81639c764717a2b201aec0b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:06:14 +0000

    Add package pchru/0.0.1/pchru-0.0.1.tar.gz

---
+++ packages/pchru/pchru-0.0.1.tar.gz/pchru-0.0.1/pchru/pchru.py
@@ -0,0 +1,14 @@
+import hashlib
+import datetime
+import json
+import uuid
+
+def get_hash_uuid(payload: dict) -> uuid:
+    payload["date"] = str(datetime.date.today())
+    hash_object = hashlib.sha256(json.dumps(payload, sort_keys=True).encode())
+    hex_dig = hash_object.hexdigest()
+    return uuid.uuid5(uuid.NAMESPACE_DNS, hex_dig)
+
+
+if __name__ == "__main__":
+    get_uuid({"test": "test"})
---
+++ packages/pchru/pchru-0.0.1.tar.gz/pchru-0.0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='pchru',
+      version='0.0.1',
+      description='Generate a hash of the provided dictionary',
+      packages=['pchru'],
+      author_email='sagrutyanin4@gmail.com',
+      zip_safe=False)
