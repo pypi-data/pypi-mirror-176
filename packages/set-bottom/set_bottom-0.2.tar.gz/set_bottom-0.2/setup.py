22d81588a3327476b28af5f7a808e482f47c6a29
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:30:14 +0000

    Add package set-bottom/0.2/set_bottom-0.2.tar.gz

---
+++ packages/set-bottom/set_bottom-0.2.tar.gz/set_bottom-0.2/setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup, find_packages
+
+
+setup(
+    name='set_bottom',
+    version='0.2',
+    license='MIT',
+    author="James Mattison",
+    author_email='james.mattison7@gmail.com',
+    packages=find_packages('.'),
+    url='https://github.com/james-mattison',
+    keywords='bottombar loadingbar',
+    install_requires=[
+      ]
+)
