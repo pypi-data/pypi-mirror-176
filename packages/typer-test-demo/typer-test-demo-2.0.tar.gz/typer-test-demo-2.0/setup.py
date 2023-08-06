f123e41bd05f1c921fee59ff777d97eb1a3cd027
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:15:29 +0000

    Add package typer-test-demo/2.0/typer-test-demo-2.0.tar.gz

---
+++ packages/typer-test-demo/typer-test-demo-2.0.tar.gz/typer-test-demo-2.0/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+import os
+
+# pull long description from README
+this_directory = os.path.abspath(os.path.dirname(__file__))
+with open(os.path.join(this_directory, "README.md")) as f:
+    long_description = f.read()
+
+setup(
+    name='typer-test-demo',
+    version='2.0',
+    packages=['cli'],
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    include_package_data=True,
+    install_requires=[
+        'typer'
+    ],
+    entry_points='''
+        [console_scripts]
+        clickctl=cli.main:main
+    ''',
+)
