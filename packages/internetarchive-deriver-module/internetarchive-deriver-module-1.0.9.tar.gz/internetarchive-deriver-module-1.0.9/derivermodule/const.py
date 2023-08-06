988799a42ac53df6bd09391d6152cf9b768b3cf8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:36:38 +0000

    Add package internetarchive-deriver-module/1.0.9/internetarchive-deriver-module-1.0.9.tar.gz

---
+++ packages/internetarchive-deriver-module/internetarchive-deriver-module-1.0.9.tar.gz/internetarchive-deriver-module-1.0.9/derivermodule/const.py
@@ -0,0 +1,10 @@
+#: Directory of the item files within the container
+PB_ITEM = '/item'
+#: Directory of the task related JSON files within the container
+PB_TASK = '/task'
+#: Temporary (hard disk backed) temporary directory (auto cleaned upon exit)
+PB_TMP = '/tmp'
+#: Temporary fast (memory backed) temporary directory (auto cleaned upon exit)
+#: Limited in size, currently 1.5G
+PB_FAST = '/var/tmp/fast'
+
