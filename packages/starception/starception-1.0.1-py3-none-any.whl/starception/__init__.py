c8bfa790a5dee579f5108ddb03199da44e60b6a5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:11:34 +0000

    Add package starception/1.0.1/starception-1.0.1-py3-none-any.whl

---
+++ packages/starception/starception-1.0.1-py3-none-any.whl/starception/__init__.py
@@ -0,0 +1,10 @@
+from starception.exception_handler import add_link_template, exception_handler, install_error_handler, set_editor
+from starception.middleware import StarceptionMiddleware
+
+__all__ = [
+    'StarceptionMiddleware',
+    'exception_handler',
+    'set_editor',
+    'add_link_template',
+    'install_error_handler',
+]
