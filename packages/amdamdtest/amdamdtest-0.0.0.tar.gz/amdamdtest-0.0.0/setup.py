377a421ad8e7755ec76c613fa074e5b072366db3
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:10:38 +0000

    Add package amdamdtest/0.0.0/amdamdtest-0.0.0.tar.gz

---
+++ packages/amdamdtest/amdamdtest-0.0.0.tar.gz/amdamdtest-0.0.0/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup
+
+
+def readme():
+    with open('README.md') as f:
+        return f.read()
+
+
+setup(
+    name='amdamdtest',
+    description='demo example',
+    long_description=readme(),
+    long_description_content_type='text/markdown',
+    classifiers=['Development Status :: 1 - Planning', 'Programming Language :: Python :: 3.10'],
+    url='https://github.com/moralesveratom/locatePath',
+    author='tmorales',
+    author_email='moralesveratomas@gmail.com',
+    keywords=['python', 'path', 'locate', 'locator', 'pathLocator', 'pathLocate', 'locatorPath'],
+    license='MIT',
+    install_requires=[],
+    include_package_data=True,
+    zip_safe=False
+)
