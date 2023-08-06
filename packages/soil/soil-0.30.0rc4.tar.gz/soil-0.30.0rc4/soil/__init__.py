a366316808fa3a4fa9f96624229f1a4b7f2641ce
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:30:02 +0000

    Add package soil/0.30.0rc4/soil-0.30.0rc4.tar.gz

---
+++ packages/soil/soil-0.30.0rc4.tar.gz/soil-0.30.0rc4/soil/__init__.py
@@ -0,0 +1,256 @@
+from __future__ import annotations
+
+import importlib
+from importlib.resources import path
+import sys
+import os
+import logging
+import traceback
+from contextlib import contextmanager
+
+from .version import __version__
+
+try:
+    basestring
+except NameError:
+    basestring = str
+
+from pathlib import Path
+from .agents import *
+from . import agents
+from .simulation import *
+from .environment import Environment, EventedEnvironment
+from .datacollection import SoilCollector
+from . import serialization
+from .utils import logger
+from .time import *
+
+
+def main(
+    cfg="simulation.yml",
+    exporters=None,
+    parallel=None,
+    output="soil_output",
+    *,
+    do_run=False,
+    debug=False,
+    pdb=False,
+    **kwargs,
+):
+
+    sim = None
+    if isinstance(cfg, Simulation):
+        sim = cfg
+
+    import argparse
+    from . import simulation
+
+    logger.info("Running SOIL version: {}".format(__version__))
+
+    parser = argparse.ArgumentParser(description="Run a SOIL simulation")
+    parser.add_argument(
+        "file",
+        type=str,
+        nargs="?",
+        default=cfg if sim is None else "",
+        help="Configuration file for the simulation (e.g., YAML or JSON)",
+    )
+    parser.add_argument(
+        "--version", action="store_true", help="Show version info and exit"
+    )
+    parser.add_argument(
+        "--module",
+        "-m",
+        type=str,
+        help="file containing the code of any custom agents.",
+    )
+    parser.add_argument(
+        "--dry-run",
+        "--dry",
+        action="store_true",
+        help="Do not store the results of the simulation to disk, show in terminal instead.",
+    )
+    parser.add_argument(
+        "--pdb", action="store_true", help="Use a pdb console in case of exception."
+    )
+    parser.add_argument(
+        "--debug",
+        action="store_true",
+        help="Run a customized version of a pdb console to debug a simulation.",
+    )
+    parser.add_argument(
+        "--graph",
+        "-g",
+        action="store_true",
+        help="Dump each trial's network topology as a GEXF graph. Defaults to false.",
+    )
+    parser.add_argument(
+        "--csv",
+        action="store_true",
+        help="Dump all data collected in CSV format. Defaults to false.",
+    )
+    parser.add_argument("--level", type=str, help="Logging level")
+    parser.add_argument(
+        "--output",
+        "-o",
+        type=str,
+        default=output or "soil_output",
+        help="folder to write results to. It defaults to the current directory.",
+    )
+    if parallel is None:
+        parser.add_argument(
+            "--synchronous",
+            action="store_true",
+            help="Run trials serially and synchronously instead of in parallel. Defaults to false.",
+        )
+
+    parser.add_argument(
+        "-e",
+        "--exporter",
+        action="append",
+        default=[],
+        help="Export environment and/or simulations using this exporter",
+    )
+
+    parser.add_argument(
+        "--only-convert",
+        "--convert",
+        action="store_true",
+        help="Do not run the simulation, only convert the configuration file(s) and output them.",
+    )
+
+    parser.add_argument(
+        "--set",
+        metavar="KEY=VALUE",
+        action="append",
+        help="Set a number of parameters that will be passed to the simulation."
+        "(do not put spaces before or after the = sign). "
+        "If a value contains spaces, you should define "
+        "it with double quotes: "
+        'foo="this is a sentence". Note that '
+        "values are always treated as strings.",
+    )
+
+    args = parser.parse_args()
+    logger.setLevel(getattr(logging, (args.level or "INFO").upper()))
+
+    if args.version:
+        return
+
+    if parallel is None:
+        parallel = not args.synchronous
+
+    exporters = exporters or [
+        "default",
+    ]
+    for exp in args.exporter:
+        if exp not in exporters:
+            exporters.append(exp)
+    if args.csv:
+        exporters.append("csv")
+    if args.graph:
+        exporters.append("gexf")
+
+    if os.getcwd() not in sys.path:
+        sys.path.append(os.getcwd())
+    if args.module:
+        importlib.import_module(args.module)
+    if output is None:
+        output = args.output
+
+    debug = debug or args.debug
+
+    if args.pdb or debug:
+        args.synchronous = True
+        os.environ["SOIL_POSTMORTEM"] = "true"
+
+    res = []
+    try:
+        exp_params = {}
+
+        if sim:
+            logger.info("Loading simulation instance")
+            sim.dry_run = args.dry_run
+            sim.exporters = exporters
+            sim.parallel = parallel
+            sim.outdir = output
+            sims = [
+                sim,
+            ]
+        else:
+            logger.info("Loading config file: {}".format(args.file))
+            if not os.path.exists(args.file):
+                logger.error("Please, input a valid file")
+                return
+
+            sims = list(
+                simulation.iter_from_config(
+                    args.file,
+                    dry_run=args.dry_run,
+                    exporters=exporters,
+                    parallel=parallel,
+                    outdir=output,
+                    exporter_params=exp_params,
+                    **kwargs,
+                )
+            )
+
+        for sim in sims:
+
+            if args.set:
+                for s in args.set:
+                    k, v = s.split("=", 1)[:2]
+                    v = eval(v)
+                    tail, *head = k.rsplit(".", 1)[::-1]
+                    target = sim
+                    if head:
+                        for part in head[0].split("."):
+                            try:
+                                target = getattr(target, part)
+                            except AttributeError:
+                                target = target[part]
+                    try:
+                        setattr(target, tail, v)
+                    except AttributeError:
+                        target[tail] = v
+
+            if args.only_convert:
+                print(sim.to_yaml())
+                continue
+            if do_run:
+                res.append(sim.run())
+            else:
+                print("not running")
+                res.append(sim)
+
+    except Exception as ex:
+        if args.pdb:
+            from .debugging import post_mortem
+
+            print(traceback.format_exc())
+            post_mortem()
+        else:
+            raise
+    if debug:
+        from .debugging import set_trace
+
+        os.environ["SOIL_DEBUG"] = "true"
+        set_trace()
+    return res
+
+
+@contextmanager
+def easy(cfg, pdb=False, debug=False, **kwargs):
+    try:
+        yield main(cfg, debug=debug, pdb=pdb, **kwargs)[0]
+    except Exception as e:
+        if os.environ.get("SOIL_POSTMORTEM"):
+            from .debugging import post_mortem
+
+            print(traceback.format_exc())
+            post_mortem()
+        raise
+
+
+if __name__ == "__main__":
+    main(do_run=True)
---
+++ packages/soil/soil-0.30.0rc4.tar.gz/soil-0.30.0rc4/soil/environment.py
@@ -0,0 +1,349 @@
+from __future__ import annotations
+
+import os
+import sqlite3
+import math
+import logging
+import inspect
+
+from typing import Any, Dict, Optional, Union, List
+from collections import namedtuple
+from time import time as current_time
+from copy import deepcopy
+from networkx.readwrite import json_graph
+
+
+import networkx as nx
+
+from mesa import Model
+
+from . import agents as agentmod, config, datacollection, serialization, utils, time, network, events
+
+
+class BaseEnvironment(Model):
+    """
+    The environment is key in a simulation. It controls how agents interact,
+    and what information is available to them.
+
+    This is an opinionated version of `mesa.Model` class, which adds many
+    convenience methods and abstractions.
+
+    The environment parameters and the state of every agent can be accessed
+    both by using the environment as a dictionary and with the environment's
+    :meth:`soil.environment.Environment.get` method.
+    """
+
+    def __init__(
+        self,
+        id="unnamed_env",
+        seed="default",
+        schedule_class=time.TimedActivation,
+        dir_path=None,
+        interval=1,
+        agent_class=None,
+        agents: List[tuple[type, Dict[str, Any]]] = {},
+        collector_class: type = datacollection.SoilCollector,
+        agent_reporters: Optional[Any] = None,
+        model_reporters: Optional[Any] = None,
+        tables: Optional[Any] = None,
+        **env_params,
+    ):
+
+        super().__init__(seed=seed)
+
+        self.current_id = -1
+
+        self.id = id
+
+        self.dir_path = dir_path or os.getcwd()
+
+        if schedule_class is None:
+            schedule_class = time.TimedActivation
+        else:
+            schedule_class = serialization.deserialize(schedule_class)
+        self.schedule = schedule_class(self)
+
+        self.agent_class = agent_class or agentmod.BaseAgent
+
+        self.interval = interval
+        self.init_agents(agents)
+
+        self.logger = utils.logger.getChild(self.id)
+
+        collector_class = serialization.deserialize(collector_class)
+        self.datacollector = collector_class(
+            model_reporters=model_reporters,
+            agent_reporters=agent_reporters,
+            tables=tables,
+        )
+        for (k, v) in env_params.items():
+            self[k] = v
+
+    def _agent_from_dict(self, agent):
+        """
+        Translate an agent dictionary into an agent
+        """
+        agent = dict(**agent)
+        cls = agent.pop("agent_class", None) or self.agent_class
+        unique_id = agent.pop("unique_id", None)
+        if unique_id is None:
+            unique_id = self.next_id()
+
+        return serialization.deserialize(cls)(unique_id=unique_id, model=self, **agent)
+
+    def init_agents(self, agents: Union[config.AgentConfig, List[Dict[str, Any]]] = {}):
+        """
+        Initialize the agents in the model from either a `soil.config.AgentConfig` or a list of
+        dictionaries that each describes an agent.
+
+        If given a list of dictionaries, an agent will be created for each dictionary. The agent
+        class can be specified through the `agent_class` key. The rest of the items will be used
+        as parameters to the agent.
+        """
+        if not agents:
+            return
+
+        lst = agents
+        override = []
+        if not isinstance(lst, list):
+            if not isinstance(agents, config.AgentConfig):
+                lst = config.AgentConfig(**agents)
+            if lst.override:
+                override = lst.override
+            lst = self._agent_dict_from_config(lst)
+
+        # TODO: check override is working again. It cannot (easily) be part of agents.from_config anymore,
+        # because it needs attribute such as unique_id, which are only present after init
+        new_agents = [self._agent_from_dict(agent) for agent in lst]
+
+        for a in new_agents:
+            self.schedule.add(a)
+
+        for rule in override:
+            for agent in agentmod.filter_agents(self.schedule._agents, **rule.filter):
+                for attr, value in rule.state.items():
+                    setattr(agent, attr, value)
+
+    def _agent_dict_from_config(self, cfg):
+        return agentmod.from_config(cfg, random=self.random)
+
+    @property
+    def agents(self):
+        return agentmod.AgentView(self.schedule._agents)
+
+    def find_one(self, *args, **kwargs):
+        return agentmod.AgentView(self.schedule._agents).one(*args, **kwargs)
+
+    def count_agents(self, *args, **kwargs):
+        return sum(1 for i in self.agents(*args, **kwargs))
+
+    @property
+    def now(self):
+        if self.schedule:
+            return self.schedule.time
+        raise Exception(
+            "The environment has not been scheduled, so it has no sense of time"
+        )
+
+    def add_agent(self, unique_id=None, **kwargs):
+        if unique_id is None:
+            unique_id = self.next_id()
+
+        kwargs["unique_id"] = unique_id
+        a = self._agent_from_dict(kwargs)
+
+        self.schedule.add(a)
+        return a
+
+    def log(self, message, *args, level=logging.INFO, **kwargs):
+        if not self.logger.isEnabledFor(level):
+            return
+        message = message + " ".join(str(i) for i in args)
+        message = " @{:>3}: {}".format(self.now, message)
+        for k, v in kwargs:
+            message += " {k}={v} ".format(k, v)
+        extra = {}
+        extra["now"] = self.now
+        extra["id"] = self.id
+        return self.logger.log(level, message, extra=extra)
+
+    def step(self):
+        """
+        Advance one step in the simulation, and update the data collection and scheduler appropriately
+        """
+        super().step()
+        # self.logger.info(
+        #     "--- Step: {:^5} - Time: {now:^5} ---", steps=self.schedule.steps, now=self.now
+        # )
+        self.schedule.step()
+        self.datacollector.collect(self)
+
+    def __getitem__(self, key):
+        try:
+            return getattr(self, key)
+        except AttributeError:
+            raise KeyError(f"key {key}  not found in environment")
+
+    def __delitem__(self, key):
+        return delattr(self, key)
+
+    def __contains__(self, key):
+        return hasattr(self, key)
+
+    def __setitem__(self, key, value):
+        setattr(self, key, value)
+
+    def __str__(self):
+        return str(dict(self))
+
+    def __len__(self):
+        return sum(1 for n in self.keys())
+
+    def __iter__(self):
+        return iter(self.agents())
+
+    def get(self, key, default=None):
+        return self[key] if key in self else default
+
+    def keys(self):
+        return (k for k in self.__dict__ if k[0] != "_")
+
+class NetworkEnvironment(BaseEnvironment):
+    """
+    The NetworkEnvironment is an environment that includes one or more networkx.Graph intances
+    and methods to associate agents to nodes and vice versa.
+    """
+
+    def __init__(
+        self, *args, topology: Union[config.NetConfig, nx.Graph] = None, **kwargs
+    ):
+        agents = kwargs.pop("agents", None)
+        super().__init__(*args, agents=None, **kwargs)
+
+        if topology is None:
+            topology = nx.Graph()
+        elif not isinstance(topology, nx.Graph):
+            topology = network.from_config(topology, dir_path=self.dir_path)
+
+        self.G = topology
+
+        self.init_agents(agents)
+
+    def init_agents(self, *args, **kwargs):
+        """Initialize the agents from a"""
+        super().init_agents(*args, **kwargs)
+        for agent in self.schedule._agents.values():
+            self._init_node(agent)
+
+    def _init_node(self, agent):
+        """
+        Make sure the node for a given agent has the proper attributes.
+        """
+        if hasattr(agent, "node_id"):
+            self.G.nodes[agent.node_id]["agent"] = agent
+
+    def _agent_dict_from_config(self, cfg):
+        return agentmod.from_config(cfg, topology=self.G, random=self.random)
+
+    def _agent_from_dict(self, agent, unique_id=None):
+        agent = dict(agent)
+
+        if not agent.get("topology", False):
+            return super()._agent_from_dict(agent)
+
+        if unique_id is None:
+            unique_id = self.next_id()
+        node_id = agent.get("node_id", None)
+        if node_id is None:
+            node_id = network.find_unassigned(self.G, random=self.random)
+        self.G.nodes[node_id]["agent"] = None
+        agent["node_id"] = node_id
+        agent["unique_id"] = unique_id
+        agent["topology"] = self.G
+        node_attrs = self.G.nodes[node_id]
+        node_attrs.pop('agent', None)
+        node_attrs.update(agent)
+        agent = node_attrs
+
+        a = super()._agent_from_dict(agent)
+        self._init_node(a)
+
+        return a
+
+    @property
+    def network_agents(self):
+        for a in self.schedule._agents.values():
+            if isinstance(a, agentmod.NetworkAgent):
+                yield a
+
+    def add_node(self, agent_class, unique_id=None, node_id=None, **kwargs):
+        if unique_id is None:
+            unique_id = self.next_id()
+        if node_id is None:
+            node_id = network.find_unassigned(
+                G=self.G, shuffle=True, random=self.random
+            )
+            if node_id is None:
+                node_id = f"node_for_{unique_id}"
+
+        if node_id not in self.G.nodes:
+            self.G.add_node(node_id)
+
+        assert "agent" not in self.G.nodes[node_id]
+        self.G.nodes[node_id]["agent"] = None  # Reserve
+
+        a = self.add_agent(
+            unique_id=unique_id,
+            agent_class=agent_class,
+            topology=self.G,
+            node_id=node_id,
+            **kwargs,
+        )
+        a["visible"] = True
+        return a
+
+    def add_agent(self, *args, **kwargs):
+        a = super().add_agent(*args, **kwargs)
+        if hasattr(a, "node_id"):
+            assert self.G.nodes[a.node_id]["agent"] == a
+        return a
+
+    def agent_for_node_id(self, node_id):
+        return self.G.nodes[node_id].get("agent")
+
+    def populate_network(self, agent_class, weights=None, **agent_params):
+        if not hasattr(agent_class, "len"):
+            agent_class = [agent_class]
+            weights = None
+        for (node_id, node) in self.G.nodes(data=True):
+            if "agent" in node:
+                continue
+            a_class = self.random.choices(agent_class, weights)[0]
+            self.add_agent(node_id=node_id, topology=self.G,  agent_class=a_class, **agent_params)
+
+
+class EventedEnvironment(BaseEnvironment):
+    def broadcast(self, msg, sender=None, expiration=None, ttl=None, **kwargs):
+        for agent in self.agents(**kwargs):
+            if agent == sender:
+                continue
+            self.logger.info(f"Telling {repr(agent)}: {msg} ttl={ttl}")
+            try:
+                inbox = agent._inbox
+            except AttributeError:
+                self.logger.info(
+                    f"Agent {agent.unique_id} cannot receive events because it does not have an inbox"
+                )
+                continue
+            # Allow for AttributeError exceptions in this part of the code
+            inbox.append(
+                events.Tell(
+                    payload=msg,
+                    sender=sender,
+                    expiration=expiration if ttl is None else self.now + ttl,
+                )
+            )
+
+
+class Environment(NetworkEnvironment, EventedEnvironment):
+    """Default environment class, has both network and event capabilities"""
