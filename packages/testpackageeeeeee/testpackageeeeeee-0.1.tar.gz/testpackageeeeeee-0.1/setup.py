d9ec363a2e71c5faf78785b22af4ed54bb03b136
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:06:15 +0000

    Add package testpackageeeeeee/0.1/testpackageeeeeee-0.1.tar.gz

---
+++ packages/testpackageeeeeee/testpackageeeeeee-0.1.tar.gz/testpackageeeeeee-0.1/setup.py
@@ -0,0 +1,11 @@
+import setuptools
+setuptools.setup(
+    name="testpackageeeeeee",
+    version="0.01",
+    author="bharat",
+    author_email="bharat.singh.1908@gmail.com",
+    description="package for testing",
+    packages=setuptools.find_packages(),
+    classifiers=["Programming Language :: Python :: 3",
+                 "License :: OSI Approved :: MIT License", "Operating System :: OS Independent"],
+)
