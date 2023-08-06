ebefbeaa2b254dcf40f479d7962a29f448417f2f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:37:06 +0000

    Add package typer-cloup/0.8.0/typer-cloup-0.8.0.tar.gz

---
+++ packages/typer-cloup/typer-cloup-0.8.0.tar.gz/typer-cloup-0.8.0/docs_src/subcommands/tutorial001/main.py
@@ -0,0 +1,11 @@
+import typer_cloup as typer
+
+import items
+import users
+
+app = typer.Typer()
+app.add_typer(users.app, name="users")
+app.add_typer(items.app, name="items")
+
+if __name__ == "__main__":
+    app()
