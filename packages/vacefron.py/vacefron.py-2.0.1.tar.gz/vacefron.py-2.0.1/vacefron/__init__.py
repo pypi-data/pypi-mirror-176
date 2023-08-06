69c5409f0a47edf2156601a596277aaaaa152a41
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:16:27 +0000

    Add package vacefron.py/2.0.1/vacefron.py-2.0.1.tar.gz

---
+++ packages/vacefron.py/vacefron.py-2.0.1.tar.gz/vacefron.py-2.0.1/vacefron/__init__.py
@@ -0,0 +1,8 @@
+from .client import Client
+from .errors import *
+from .models import *
+from .enums import Badges, CrewmateColour
+
+__license__ = "MLP-2.0"
+__author__ = "Soheab"
+__version__ = "2.0.1"
