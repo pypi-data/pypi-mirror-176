23c10628380b2ab5d6a61d1072d2517b0d10221d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:37:05 +0000

    Add package kuqezeze/2.0.0/kuqezeze-2.0.0.tar.gz

---
+++ packages/kuqezeze/kuqezeze-2.0.0.tar.gz/kuqezeze-2.0.0/kuqezeze/guess_module.py
@@ -0,0 +1,9 @@
+import random
+
+def guess_the_munber(user_nr, range_nr):
+    rand_nr = random.randint(1, range_nr)
+
+    if user_nr == rand_nr:
+        return True
+    else:
+        return False
---
+++ packages/kuqezeze/kuqezeze-2.0.0.tar.gz/kuqezeze-2.0.0/kuqezeze/main.py
@@ -0,0 +1,11 @@
+from kuqezeze.guess_module import guess_the_munber
+
+range_nr = int(input("Vendos range nga 1 deri ne n: "))
+user_nr = int(input("Mendoni nje number: "))
+
+rezult = guess_the_munber(user_nr, range_nr)
+
+if rezult:
+    print('YOU WIN !!!')
+else:
+    print('Go home loser!!')
