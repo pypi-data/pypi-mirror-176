67e96d09776a138a97ad6496fd2ceba24d2ea0e9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 19:15:02 +0000

    Add package kit-py/0.5.45/kit_py-0.5.45-py3-none-any.whl

---
+++ packages/kit-py/kit_py-0.5.45-py3-none-any.whl/kit/rpc/broker/rmq.py
@@ -0,0 +1,31 @@
+# -*- coding: utf-8 -*-
+import json
+
+from kit.rpc.store import get_store
+from .base import Broker
+
+
+class RMQBroker(Broker):
+
+    def __init__(self, queue, backend_store=None, middlewares=None):
+        super().__init__(queue, backend_store, middlewares)
+        self.backend_store = backend_store or self._get_default_store()
+
+    def send(self, message):
+        self.backend_store.send(self.queue, message)
+
+    def consume(self, *args, **kwargs):
+        return self.backend_store.consume(self.queue, *args, **kwargs)
+
+    def format_message(self, message):
+        """转换message的数据类型"""
+        if not isinstance(message, str):
+            return json.dumps(message)
+        return message
+
+    @staticmethod
+    def _get_default_store():
+        return get_store()
+
+    def __repr__(self):
+        return f"<RMQBroker {self.queue}>"
