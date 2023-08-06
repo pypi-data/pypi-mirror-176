b15bd587f8d35beb551e5c1aaf9437c4644dd663
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:34:08 +0000

    Add package miskibin/1.0.7/miskibin-1.0.7.tar.gz

---
+++ packages/miskibin/miskibin-1.0.7.tar.gz/miskibin-1.0.7/setup.py
@@ -0,0 +1,19 @@
+import setuptools
+
+
+setuptools.setup(
+    name="miskibin",
+    version="1.0.7",
+    author="Michał Skibiński",
+    author_email="mskibinski109@gmail.com",
+    description="My personal package for colored logs. Highly customizable.",
+    long_description="My personal package for colored logs. Highly customizable.",
+    url="https://github.com/michalskibinski109/miskibin",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.8",
+)
