5300fc96149102c4dd4040e354cd3d60e878cf89
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:34:31 +0000

    Add package example-package-YG-HERE/1.1.3/example_package_YG_HERE-1.1.3.tar.gz

---
+++ packages/example-package-YG-HERE/example_package_YG_HERE-1.1.3.tar.gz/example_package_YG_HERE-1.1.3/example_package_YG_HERE/example.py
@@ -0,0 +1,9 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov 15 17:16:26 2022
+
+@author: Administrator
+"""
+
+def add_one(number):
+    return number + 1
---
+++ packages/example-package-YG-HERE/example_package_YG_HERE-1.1.3.tar.gz/example_package_YG_HERE-1.1.3/setup.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+from io import open
+from setuptools import setup, find_packages
+setup(
+    name='example_package_YG_HERE',
+    version='1.1.3',
+    description='a package for your test using',
+    long_description='测试发布自己的包',
+    author='xiaoguzai',
+    author_email='474551240@qq.com',
+    license='Apache License 2.0',
+    url='https://github.com/boss2020/tfbert',
+    download_url='https://github.com/boss2020/tfbert/master.zip',
+    packages=find_packages(),
+    
+)
+
