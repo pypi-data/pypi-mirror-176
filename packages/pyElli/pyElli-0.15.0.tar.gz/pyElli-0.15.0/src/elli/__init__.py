5ab57a6da05e5475e83da5913ad2c13ad09f8e0e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:10:48 +0000

    Add package pyElli/0.15.0/pyElli-0.15.0.tar.gz

---
+++ packages/pyElli/pyElli-0.15.0.tar.gz/pyElli-0.15.0/src/elli/__init__.py
@@ -0,0 +1,30 @@
+# Encoding: utf-8
+import sys
+
+from . import database as db
+from .database.materials_db import AIR
+from .dispersions import *
+from .dispersions.base_dispersion import *
+from .experiment import Experiment
+from .importer.nexus import *
+from .importer.spectraray import *
+from .materials import *
+from .result import Result, ResultList
+from .solver2x2 import Solver2x2
+from .solver4x4 import *
+from .structure import *
+from .utils import *
+
+if sys.version_info.major >= 3 and sys.version_info.minor > 7:
+    from importlib.metadata import PackageNotFoundError, version
+else:
+    from importlib_metadata import PackageNotFoundError, version
+
+try:
+    # Change here if project is renamed and does not equal the package name
+    dist_name = __name__
+    __version__ = version(dist_name)
+except PackageNotFoundError:  # pragma: no cover
+    __version__ = "unknown"
+finally:
+    del version, PackageNotFoundError
