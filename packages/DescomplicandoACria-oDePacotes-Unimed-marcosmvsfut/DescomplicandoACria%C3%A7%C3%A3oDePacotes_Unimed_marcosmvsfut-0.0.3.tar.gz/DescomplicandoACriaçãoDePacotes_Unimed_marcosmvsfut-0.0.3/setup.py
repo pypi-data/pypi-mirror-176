d8c5b8d0d286ce5be856583d778cfc13f19b0885
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:13:20 +0000

    Add package DescomplicandoACria-oDePacotes-Unimed-marcosmvsfut/0.0.3/DescomplicandoACria%C3%A7%C3%A3oDePacotes_Unimed_marcosmvsfut-0.0.3.tar.gz

---
+++ packages/DescomplicandoACria-oDePacotes-Unimed-marcosmvsfut/DescomplicandoACria%C3%A7%C3%A3oDePacotes_Unimed_marcosmvsfut-0.0.3.tar.gz/DescomplicandoACriaçãoDePacotes_Unimed_marcosmvsfut-0.0.3/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as f:
+    page_description = f.read()
+
+with open("requirements.txt") as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name="DescomplicandoACriaçãoDePacotes_Unimed_marcosmvsfut",
+    version="0.0.3",
+    author="Marcos Vinicius",
+    author_email="marcos_mvs99@hotmail.com",
+    description="DIO project process",
+    long_description=page_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/marcosmvsfut/Dio_Unimed_Bootcamp",
+    packages=find_packages(),
+    install_requires=requirements,
+    python_requires='>=3.8',
+)
