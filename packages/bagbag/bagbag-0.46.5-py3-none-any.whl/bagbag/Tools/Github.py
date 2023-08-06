e5eb917243f047613e010feef9c136b4d53a7b78
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:10:32 +0000

    Add package bagbag/0.46.5/bagbag-0.46.5-py3-none-any.whl

---
+++ packages/bagbag/bagbag-0.46.5-py3-none-any.whl/bagbag/Tools/Github.py
@@ -0,0 +1,88 @@
+from __future__ import annotations
+
+from github import Github as githubclient
+
+try:
+    from .. import Http
+    from .Ratelimit import RateLimit
+    from ..String import String
+except:
+    from Ratelimit import RateLimit
+    import sys
+    sys.path.append("..")
+    import Http
+    from String import String
+
+# class GithuException(Exception):
+#     pass 
+
+# class NoNewItem(GithuException):
+#     pass 
+
+class GithubSearchResult():
+    def __init__(self):
+        self.url:str = ""
+        self.content:str = ""
+
+    def __str__(self) -> str:
+        content = String(self.content.replace("\n", "\\n")).Ommit(160)
+        return f"GithubSearchResult(url={self.url}, content={content})"
+    
+    def __iter__(self):
+        # first start by grabbing the Class items
+        iters = dict((x,y) for x,y in GithubSearchResult.__dict__.items() if x[:2] != '__')
+
+        # then update the class items with the instance items
+        iters.update(self.__dict__)
+
+        # now 'yield' through the items
+        for x,y in iters.items():
+            yield x,y
+
+class GithubSearchResults():
+    def __init__(self, github:Github, pattern:str) -> None:
+        self.token = github.token 
+        self.g = github.g 
+        self.rl = github.rl 
+
+        self.repos = self.g.search_code(pattern)
+
+        self.rl.Take()
+        self.total = self.repos.totalCount
+        self.pages = [i for i in range(0, self.total)]
+
+    def Get(self) -> GithubSearchResult | None:
+        if len(self.pages) == 0:
+            return None 
+
+        page = self.pages.pop(0)
+
+        self.rl.Take()
+        url = self.repos.get_page(page)[0].download_url
+        content = Http.Get(url).Content
+
+        res = GithubSearchResult()
+        res.url = url 
+        res.content = content 
+        
+        return res
+
+    def __iter__(self) -> GithubSearchResult:
+        while True:
+            res = self.Get()
+            if res != None:
+                yield res 
+            else:
+                return 
+
+class Github():
+    def __init__(self, token:str, ratelimit:str="30/m"):
+        self.token = token 
+        self.g = githubclient(token)
+        self.rl = RateLimit(ratelimit)
+
+        # 每页搜索结果就1个, 就不需要遍历每页的条目了, 测试不影响结果数量, 都是1000个
+        self.g.per_page = 1 
+
+    def Search(self, pattern:str) -> GithubSearchResults:
+        return GithubSearchResults(self, pattern)
