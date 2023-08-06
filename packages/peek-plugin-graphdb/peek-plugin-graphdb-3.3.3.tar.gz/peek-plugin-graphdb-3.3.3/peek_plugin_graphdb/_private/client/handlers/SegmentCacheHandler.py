da4b153a6a642afc29a6d764f5eb40e2b85a4a5d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:27:37 +0000

    Add package peek-plugin-graphdb/3.3.3/peek-plugin-graphdb-3.3.3.tar.gz

---
+++ packages/peek-plugin-graphdb/peek-plugin-graphdb-3.3.3.tar.gz/peek-plugin-graphdb-3.3.3/peek_plugin_graphdb/_private/client/handlers/SegmentCacheHandler.py
@@ -0,0 +1,29 @@
+import logging
+from typing import Dict
+
+from peek_abstract_chunked_index.private.client.handlers.ACICacheHandlerABC import (
+    ACICacheHandlerABC,
+)
+from peek_abstract_chunked_index.private.tuples import ACIUpdateDateTupleABC
+from peek_plugin_graphdb._private.PluginNames import graphDbFilt
+from peek_plugin_graphdb._private.client.controller.SegmentCacheController import (
+    clientSegmentUpdateFromServerFilt,
+)
+from peek_plugin_graphdb._private.tuples.SegmentIndexUpdateDateTuple import (
+    SegmentIndexUpdateDateTuple,
+)
+
+logger = logging.getLogger(__name__)
+
+clientSegmentWatchUpdateFromDeviceFilt = {
+    "key": "clientSegmentWatchUpdateFromDevice"
+}
+clientSegmentWatchUpdateFromDeviceFilt.update(graphDbFilt)
+
+
+# ModelSet HANDLER
+class SegmentCacheHandler(ACICacheHandlerABC):
+    _UpdateDateTuple: ACIUpdateDateTupleABC = SegmentIndexUpdateDateTuple
+    _updateFromDeviceFilt: Dict = clientSegmentWatchUpdateFromDeviceFilt
+    _updateFromLogicFilt: Dict = clientSegmentUpdateFromServerFilt
+    _logger: logging.Logger = logger
