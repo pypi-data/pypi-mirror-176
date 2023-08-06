b52dc58f992f14e575440167cd01cee9240ba5bf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:23:04 +0000

    Add package zanestear/0.0.5/zanestear-0.0.5.tar.gz

---
+++ packages/zanestear/zanestear-0.0.5.tar.gz/zanestear-0.0.5/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup
+
+setup(name='zanestear',
+      version='0.0.5',
+      description='ZaneStear Test',
+      url='https://github.com/ZaneStear/',
+      author='ZaneStear',
+      author_email='1030349525@qq.com',
+      license='MIT',
+      packages=['zanestear'])
