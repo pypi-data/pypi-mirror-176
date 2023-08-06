ca72dce5bfbe8636233d619ffdceb0b5262cca61
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:43:55 +0000

    Add package ezstools/2.2/ezstools-2.2.tar.gz

---
+++ packages/ezstools/ezstools-2.2.tar.gz/ezstools-2.2/ezstools/__init__.py
@@ -0,0 +1,7 @@
+from .simple_kv import *
+from .html_slice import *
+from .funcs import *
+from .json_tools import *
+from .random_generator import *
+from .string_tools import *
+from .module_tools import *
