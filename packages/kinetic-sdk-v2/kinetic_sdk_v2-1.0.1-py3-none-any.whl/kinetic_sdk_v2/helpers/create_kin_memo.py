d01af52d07ffec25ff6201445dfea1869c605c47
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:38:13 +0000

    Add package kinetic-sdk-v2/1.0.1/kinetic_sdk_v2-1.0.1-py3-none-any.whl

---
+++ packages/kinetic-sdk-v2/kinetic_sdk_v2-1.0.1-py3-none-any.whl/kinetic_sdk_v2/helpers/create_kin_memo.py
@@ -0,0 +1,8 @@
+from kinetic_sdk_v2.models.transaction_type import TransactionType
+from kinetic_sdk_v2.models.kin_memo import KinMemo
+
+
+def create_kin_memo(app_index: int, type: TransactionType = TransactionType.NONE):
+    data = bytes()
+    kin_memo = KinMemo.new(1, type, app_index, data)
+    return kin_memo
