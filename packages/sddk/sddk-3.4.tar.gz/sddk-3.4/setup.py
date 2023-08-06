6e41984200a5d0b5f30f05763e05b02a88e7d295
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:13:31 +0000

    Add package sddk/3.4/sddk-3.4.tar.gz

---
+++ packages/sddk/sddk-3.4.tar.gz/sddk-3.4/setup.py
@@ -0,0 +1,34 @@
+import setuptools
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="sddk", # Replace with your own username
+    version="3.4",
+    author="Vojtech Kase",
+    author_email="vojtech.kase@gmail.com",
+    description="A package to access sciencedata.dk",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/sdam-au/sddk",
+    packages=setuptools.find_packages(),
+    install_requires=[
+        "pyarrow" # >=3",
+        "plotly",
+        #"kaleido>=0.2.1",
+        "beautifulsoup4>=4",
+        "pandas>=1",
+        "numpy>=1",
+        "requests>=2",
+        "matplotlib>=3.3.4",
+        "geopandas",
+        "shapely"
+        ],
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.4',
+)
