461fa63e16fbb29f60797f6020b6e216faa91d04
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:02:04 +0000

    Add package tacitus/0.1.3/tacitus-0.1.3.tar.gz

---
+++ packages/tacitus/tacitus-0.1.3.tar.gz/tacitus-0.1.3/tacitus/adapters/sqlalchemy/connecting.py
@@ -0,0 +1,22 @@
+import sqlalchemy.ext.asyncio
+
+
+class SqlalchemyConnectionPool:
+    def __init__(self, dsn: str, echo: bool = True, auto_commit: bool = False):
+        self._engine = sqlalchemy.ext.asyncio.create_async_engine(dsn, echo=echo)
+        self._auto_commit = auto_commit
+
+    async def get_connection(self):
+        connection = sqlalchemy.ext.asyncio.AsyncConnection(self._engine)
+        await connection.start()
+        if self._auto_commit:
+            await connection.begin()
+        return connection
+
+    async def close_connection(
+        self,
+        connection: sqlalchemy.ext.asyncio.AsyncConnection
+    ):
+        if self._auto_commit:
+            await connection.commit()
+        await connection.close()
