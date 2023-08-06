40abd4c0c8eb4c4fe4570c22feb204d4ce83a9ed
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:33:31 +0000

    Add package simassis/0.1.2/simassis-0.1.2.tar.gz

---
+++ packages/simassis/simassis-0.1.2.tar.gz/simassis-0.1.2/setup.py
@@ -0,0 +1,28 @@
+from setuptools import setup
+
+setup(
+    name='simassis',
+    version='0.1.2',
+    description='A python Package for MD calculations in solids',
+    author='Marcin Krynski',
+    author_email='marcin.krynski@pw.edu.pl',
+    url='https://pypi.org/project/simassis/',
+    license='BSD 2-clause',
+    packages=['simassis'],
+    install_requires=[
+                      'numpy',
+                      'scipy',
+                      'os',
+                      'shutli',    
+                      ],
+
+    classifiers=[
+        'Development Status :: 1 - Planning',
+        'Intended Audience :: Science/Research',
+        'License :: OSI Approved :: BSD License',  
+        'Operating System :: POSIX :: Linux',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.4',
+        'Programming Language :: Python :: 3.5',
+    ],
+)
