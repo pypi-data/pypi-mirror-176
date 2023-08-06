ccbf259027f7b447159d6504331a3fffc7983f77
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:18:12 +0000

    Add package softwaress/0.0.1/softwaress-0.0.1-py2.py3-none-any.whl

---
+++ packages/softwaress/softwaress-0.0.1-py2.py3-none-any.whl/softwares/module_test.py
@@ -0,0 +1,13 @@
+from softwares.promptware import Promptware
+
+
+class FooPromptware(Promptware):
+    def __init__(self):
+        self.fool = "fool"
+
+    def fool(self) -> str:
+        return "fool"
+
+
+class Dummy1:
+    pass
