3df87e5fdc861d09dfa5e4b5b88145ee1fd0e698
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:32:44 +0000

    Add package nspyre/0.4.8/nspyre-0.4.8-py2.py3-none-any.whl

---
+++ packages/nspyre/nspyre-0.4.8-py2.py3-none-any.whl/nspyre/config/__init__.py
@@ -0,0 +1,11 @@
+from .config_files import (
+    get_config_param,
+    load_config,
+    load_meta_config
+)
+
+__all__ = [
+    'get_config_param',
+    'load_config',
+    'load_meta_config'
+]
---
+++ packages/nspyre/nspyre-0.4.8-py2.py3-none-any.whl/nspyre/gui/__init__.py
@@ -0,0 +1,41 @@
+import logging
+import sys
+
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QApplication
+
+from nspyre.misc import nspyre_init_logger
+
+from .app import NSpyreApp
+from .main_window import NSpyreMainWindow
+
+from .colors import colors
+from .data_handling import save_data
+from .widgets.plotting import HeatmapPlotWidget, LinePlotWidget
+from .widgets.views import Plot1D, Plot2D, PlotFormatInit, PlotFormatUpdate
+
+__all__ = [
+    'HeatmapPlotWidget',
+    'LinePlotWidget',
+    'Plot1D',
+    'Plot2D',
+    'PlotFormatInit',
+    'PlotFormatUpdate',
+    'colors',
+    'save_data'
+]
+
+logger = logging.getLogger(__name__)
+
+
+def main(args=None):
+    """Entry point for the application script."""
+
+    nspyre_init_logger(logging.INFO)
+    if hasattr(Qt, 'AA_EnableHighDpiScaling'):
+        QApplication.setAttribute(Qt.AA_EnableHighDpiScaling, True)
+    if hasattr(Qt, 'AA_UseHighDpiPixmaps'):
+        QApplication.setAttribute(Qt.AA_UseHighDpiPixmaps, True)
+    app = NSpyreApp(['NSpyre', sys.argv])
+    main_window = NSpyreMainWindow()
+    sys.exit(app.exec())
