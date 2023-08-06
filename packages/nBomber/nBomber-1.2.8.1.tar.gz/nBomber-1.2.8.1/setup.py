a021a629c64de3487b190f115a3c1b1d72b9ea9b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:13:20 +0000

    Add package nBomber/1.2.8.1/nBomber-1.2.8.1.tar.gz

---
+++ packages/nBomber/nBomber-1.2.8.1.tar.gz/nBomber-1.2.8.1/setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup
+long_description = open("README.md").read()
+
+setup(name="nBomber",
+version="1.2.8.1",
+description="SMS BOMBING TOOL WITH HAVING 50,000 MESSAGE SENDING CAPACITY AT A TIME.",
+long_description=long_description,
+long_description_content_type='text/markdown',
+author="NISHANT2009",
+url="https://github.com/Nishant2009/nBomber",
+scripts=["nBomber"],
+install_requires= ['certifi>=2020.6.20', 'chardet>=3.0.4', 'colorama>=0.4.3', 'idna>=2.10', 'requests>=2.24.0'],
+classifiers=[
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python :: 3',
+], )
