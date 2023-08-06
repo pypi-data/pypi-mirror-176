a275bdd9d06131052ac444778c6c550d58df9e0c
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 03:23:34 +0000

    Add package accelbyte-py-sdk/0.27.1/accelbyte_py_sdk-0.27.1.tar.gz

---
+++ packages/accelbyte-py-sdk/accelbyte_py_sdk-0.27.1.tar.gz/accelbyte_py_sdk-0.27.1/accelbyte_py_sdk/api/dsmc/operations/pod_config/__init__.py
@@ -0,0 +1,21 @@
+# Copyright (c) 2021 AccelByte Inc. All Rights Reserved.
+# This is licensed software from AccelByte Inc, for limitations
+# and restrictions contact your company contract manager.
+#
+# Code generated. DO NOT EDIT!
+
+# template file: accelbyte_cloud_py_codegen
+
+"""Auto-generated package that contains models used by the AccelByte Cloud Dsm Controller Service."""
+
+__version__ = "3.10.1"
+__author__ = "AccelByte"
+__email__ = "dev@accelbyte.net"
+
+# pylint: disable=line-too-long
+
+from .create_pod_config import CreatePodConfig
+from .delete_pod_config import DeletePodConfig
+from .get_all_pod_config import GetAllPodConfig
+from .get_pod_config import GetPodConfig
+from .update_pod_config import UpdatePodConfig
