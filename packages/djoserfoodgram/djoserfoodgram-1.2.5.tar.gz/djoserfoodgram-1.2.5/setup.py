4419c0e986c61213ab3f5c66ac6d302841564c40
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 08:16:45 +0000

    Add package djoserfoodgram/1.2.5/djoserfoodgram-1.2.5.tar.gz

---
+++ packages/djoserfoodgram/djoserfoodgram-1.2.5.tar.gz/djoserfoodgram-1.2.5/setup.py
@@ -0,0 +1,16 @@
+import setuptools
+
+setuptools.setup(
+	name='djoserfoodgram',
+	version='1.2.5',
+	author='avnosov',
+	author_email='nosov1995@gmail.com',
+	description='',
+	packages=['djoser'],
+	classifiers=[
+		"Programming Language :: Python :: 3",
+		"License :: OSI Approved :: MIT License",
+		"Operating System :: OS Independent",
+	],
+	python_requires='>=3.6',
+)
