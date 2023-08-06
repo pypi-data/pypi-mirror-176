170952843ca89992f0bd26e8197c3bff1c307861
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:44:04 +0000

    Add package cellar-extractor/1.0.24/cellar_extractor-1.0.24.tar.gz

---
+++ packages/cellar-extractor/cellar_extractor-1.0.24.tar.gz/cellar_extractor-1.0.24/cellar_extractor/cellar_extra_extract.py
@@ -0,0 +1,13 @@
+from cellar_extractor.json_to_csv import read_csv
+from cellar_extractor.fulltext_saving import add_sections
+
+def extra_cellar(data=None,filepath=None, threads=10):
+    print(f" Starting Extra cellar with {data} as data, {filepath} as filepath")
+    if data is None:
+        data = read_csv(filepath)
+    if filepath:
+        add_sections(data, threads, filepath.replace(".csv", "_fulltext.json"))
+        data.to_csv(filepath, index=False)
+    else:
+        json = add_sections(data, threads)
+        return data, json
