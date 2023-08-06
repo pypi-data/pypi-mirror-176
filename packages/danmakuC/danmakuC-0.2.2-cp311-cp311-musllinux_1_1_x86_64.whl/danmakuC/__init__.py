a017310c54fa76bc22aa6a5330949990727ef1b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:18:54 +0000

    Add package danmakuC/0.2.2/danmakuC-0.2.2-cp311-cp311-musllinux_1_1_x86_64.whl

---
+++ packages/danmakuC/danmakuC-0.2.2-cp311-cp311-musllinux_1_1_x86_64.whl/danmakuC/__init__.py
@@ -0,0 +1,7 @@
+"""
+Faster conversion for larger Danmaku to Ass format
+https://github.com/HFrost0/danmakuC
+"""
+
+from .bilibili import *  # export all method for bilibili for now...
+from . import bilibili
