b84aa6df93888f5c061ff7c608a75a1e54e9256b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:11:35 +0000

    Add package kognic-io/1.1.0/kognic-io-1.1.0.tar.gz

---
+++ packages/kognic-io/kognic-io-1.1.0.tar.gz/kognic-io-1.1.0/kognic/io/model/input/lidars_sequence/frame.py
@@ -0,0 +1,17 @@
+from typing import List, Mapping, Union, Optional
+
+from kognic.io.model import UnixTimestampNs
+from kognic.io.model.ego import EgoVehiclePose
+from kognic.io.model.input.abstract.sequence_frame import SequenceFrame
+from kognic.io.model.input.resources import PointCloud
+
+
+class Frame(SequenceFrame):
+    point_clouds: List[PointCloud]
+    ego_vehicle_pose: Optional[EgoVehiclePose] = None
+    metadata: Mapping[str, Union[int, float, str, bool]] = {}
+    unix_timestamp: Optional[UnixTimestampNs] = None
+
+    @property
+    def resources(self) -> Mapping[str, PointCloud]:
+        return {p.resource_id: p for p in self.point_clouds}
