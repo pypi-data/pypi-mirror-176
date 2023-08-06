8b9b53017392cde77de4da0fca93286afb403531
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:13:05 +0000

    Add package fabrique-nodes-core/0.0.11/fabrique_nodes_core-0.0.11-py3-none-any.whl

---
+++ packages/fabrique-nodes-core/fabrique_nodes_core-0.0.11-py3-none-any.whl/fabrique_nodes_core/ui_params.py
@@ -0,0 +1,33 @@
+default_ui_params = {
+    'has_inputs': True,
+    'has_outputs': True,
+    'has_visible': False,
+    'has_code': False,
+    'has_special': False,
+    'has_required': False,
+    'name_from_code': True
+} 
+
+destructurer_ui_params = {
+    **default_ui_params, 
+    'has_code': True,
+    'has_required': True,
+    'has_visible': True,
+    'has_special': True
+}
+
+structurer_ui_params = {
+    **default_ui_params, 
+    'has_code': True,
+    'has_required': True,
+    'has_visible': True,
+    'has_special': True
+}
+
+@classmethod
+class StringInput:
+    default:str = ''
+
+@classmethod
+class BoolInput:
+    default:bool = False
