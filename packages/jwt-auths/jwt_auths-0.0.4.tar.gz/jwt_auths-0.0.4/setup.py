491b735ff5b2da63cae7cf5a75788d9a3f9fd08b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 22:10:54 +0000

    Add package jwt-auths/0.0.4/jwt_auths-0.0.4.tar.gz

---
+++ packages/jwt-auths/jwt_auths-0.0.4.tar.gz/jwt_auths-0.0.4/setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup
+
+# Metadata goes in setup.cfg. These are here for GitHub's dependency graph.
+setup(
+    name="jwt_auths",
+    version='0.0.4',
+    url='https://github.com/ChloeXF/jwt-auth-py',
+    packages=['jwt_auth'],
+    install_requires=[
+        "Django>=2.0.5",
+        "djangorestframework>=3.8.2",
+        "PyJWT>=1.6.4",
+    ],
+)
