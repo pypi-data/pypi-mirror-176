da9a5a7ed063f6c7f7ab81508b26d971ec5e8a77
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:36:40 +0000

    Add package agm-dict-validator/0.0.1/agm_dict_validator-0.0.1.tar.gz

---
+++ packages/agm-dict-validator/agm_dict_validator-0.0.1.tar.gz/agm_dict_validator-0.0.1/setup.py
@@ -0,0 +1,8 @@
+from setuptools import setup
+
+setup(name='agm_dict_validator',
+      version='0.0.1',
+      description='Custom dict validator',
+      packages=['agm_dict_validator'],
+      author_email='alexgeniusman@gmail.com',
+      zip_safe=False)
