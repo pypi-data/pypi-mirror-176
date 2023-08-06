095ffd388ce7cb421f8874d417e01a9bee023260
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:12:03 +0000

    Add package morgan-linter/1.0.0/morgan-linter-1.0.0.tar.gz

---
+++ packages/morgan-linter/morgan-linter-1.0.0.tar.gz/morgan-linter-1.0.0/setup.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from setuptools import setup
+
+packages = \
+['morgan_linter']
+
+package_data = \
+{'': ['*']}
+
+entry_points = \
+{'console_scripts': ['morgan-linter = morgan_linter']}
+
+setup_kwargs = {
+    'name': 'morgan-linter',
+    'version': '1.0.0',
+    'description': 'Linter for validate google docstrings',
+    'long_description': '# morgan-linter\nLinter to verify the google docstrings format in a python project\n',
+    'author': 'Edwar Girón',
+    'author_email': 'contactoedwargiron@gmail.com',
+    'maintainer': 'None',
+    'maintainer_email': 'None',
+    'url': 'None',
+    'packages': packages,
+    'package_data': package_data,
+    'entry_points': entry_points,
+    'python_requires': '>=3.9.0,<3.10.0',
+}
+
+
+setup(**setup_kwargs)
