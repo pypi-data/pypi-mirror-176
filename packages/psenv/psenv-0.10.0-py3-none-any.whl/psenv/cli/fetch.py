b4f7fb871da21589ba587f5ec25429d0fe36aaa4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:11:43 +0000

    Add package psenv/0.10.0/psenv-0.10.0-py3-none-any.whl

---
+++ packages/psenv/psenv-0.10.0-py3-none-any.whl/psenv/cli/fetch.py
@@ -0,0 +1,27 @@
+from argparse import Namespace
+from pathlib import Path
+from psenv.environment.variables import PSENV_YML
+from psenv.core.parameter_store import ParameterStore
+from psenv.core.env_file import EnvFile
+from psenv.core.helpers import parse_config
+
+
+def fetch_entrypoint(cmd: Namespace) -> None:
+
+    try:
+        config = parse_config()["environments"][cmd.env]
+    except KeyError:
+        print(f"environment {cmd.env} not found in .psenv.yml config file.")
+        exit()
+    except Exception:
+        print("unknown exception encountered... no idea why.")
+
+    # make things with parameters
+    parameter_store = ParameterStore(path=config["path"])
+    params = parameter_store.get_parameters_by_path()
+    params = parameter_store.parse_params_to_key_value_pairs(params)
+
+    env_path = Path(config["env"])
+    env_file = EnvFile(path=env_path)
+    env_file.write_params_to_env(params=params, method=cmd.method)
+    env_file.append_private_section()
