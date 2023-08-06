5cbabc11c2b05bc97a4b2909ef720b71b099a9d5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 15:41:01 +0000

    Add package devxblog-theme-kit/0.1.2/devxblog_theme_kit-0.1.2.tar.gz

---
+++ packages/devxblog-theme-kit/devxblog_theme_kit-0.1.2.tar.gz/devxblog_theme_kit-0.1.2/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='devxblog_theme_kit',
+    version='0.1.2',
+    install_requires=[
+        'click==8.1.3',
+        'django==4.0.2',
+        'rich==12.4.4',
+        'requests==2.27.1',
+        'pyyaml==6',
+        'twine==4.0.1',
+        'bump2version==1.0.1'
+    ],
+    packages=find_packages('.'),
+    entry_points={
+        'console_scripts': [
+            'devxblog-theme-kit = devxblog_theme_kit.cli:main'
+        ]
+    }
+)
