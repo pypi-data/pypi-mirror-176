216336ee4781703968bdbe092197de84ea57f877
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 17:19:38 +0000

    Add package qlat/0.7/qlat-0.7.tar.gz

---
+++ packages/qlat/qlat-0.7.tar.gz/qlat-0.7/include/qlat/coordinate.h
@@ -0,0 +1,254 @@
+#pragma once
+
+#include <qlat/setup.h>
+
+#include <iostream>
+#include <sstream>
+
+namespace qlat
+{  //
+
+struct API Coordinate : public array<int, DIMN> {
+  qacc Coordinate() { array<int, DIMN>::fill(0); }
+
+  qacc Coordinate(int first, int second, int third, int fourth)
+  {
+    int *p = data();
+    p[0] = first;
+    p[1] = second;
+    p[2] = third;
+    p[3] = fourth;
+  }
+
+  qacc long product() const
+  {
+    long ret = 1;
+    int size_ = size();
+    for (int i = 0; i < size_; i++) {
+      ret *= operator[](i);
+    }
+    return ret;
+  }
+};
+
+qacc bool operator==(const Coordinate &c1, const Coordinate &c2)
+{
+  return c1[0] == c2[0] and c1[1] == c2[1] and c1[2] == c2[2] and
+         c1[3] == c2[3];
+}
+
+qacc bool operator!=(const Coordinate &c1, const Coordinate &c2)
+{
+  return !(c1 == c2);
+}
+
+qacc Coordinate operator*(const int integer, const Coordinate &coor)
+{
+  return Coordinate(integer * coor[0], integer * coor[1], integer * coor[2],
+                    integer * coor[3]);
+}
+
+qacc Coordinate operator*(const Coordinate &coor, const int integer)
+{
+  return integer * coor;
+}
+
+qacc Coordinate operator*(const Coordinate &coor1, const Coordinate &coor2)
+{
+  return Coordinate(coor1[0] * coor2[0], coor1[1] * coor2[1],
+                    coor1[2] * coor2[2], coor1[3] * coor2[3]);
+}
+
+qacc Coordinate operator/(const Coordinate &coor, const int integer)
+{
+  return Coordinate(coor[0] / integer, coor[1] / integer, coor[2] / integer,
+                    coor[3] / integer);
+}
+
+qacc Coordinate operator/(const Coordinate &coor1, const Coordinate &coor2)
+{
+  return Coordinate(coor1[0] / coor2[0], coor1[1] / coor2[1],
+                    coor1[2] / coor2[2], coor1[3] / coor2[3]);
+}
+
+qacc Coordinate operator%(const Coordinate &coor1, const Coordinate &coor2)
+{
+  return Coordinate(coor1[0] % coor2[0], coor1[1] % coor2[1],
+                    coor1[2] % coor2[2], coor1[3] % coor2[3]);
+}
+
+qacc Coordinate operator%(const Coordinate &coor, const int integer)
+{
+  return Coordinate(coor[0] % integer, coor[1] % integer, coor[2] % integer,
+                    coor[3] % integer);
+}
+
+qacc Coordinate operator+(const Coordinate &coor1, const Coordinate &coor2)
+{
+  return Coordinate(coor1[0] + coor2[0], coor1[1] + coor2[1],
+                    coor1[2] + coor2[2], coor1[3] + coor2[3]);
+}
+
+qacc Coordinate operator-(const Coordinate &coor1, const Coordinate &coor2)
+{
+  return Coordinate(coor1[0] - coor2[0], coor1[1] - coor2[1],
+                    coor1[2] - coor2[2], coor1[3] - coor2[3]);
+}
+
+qacc Coordinate operator-(const Coordinate &coor)
+{
+  return Coordinate(-coor[0], -coor[1], -coor[2], -coor[3]);
+}
+
+qacc long product(const Coordinate &coor)
+{
+  long ret = 1;
+  for (int i = 0; i < (int)coor.size(); i++) {
+    ret *= coor[i];
+  }
+  return ret;
+}
+
+qacc int sum(const Coordinate &coor)
+{
+  int ret = 0;
+  for (int i = 0; i < (int)coor.size(); i++) {
+    ret += coor[i];
+  }
+  return ret;
+}
+
+qacc int parity(const Coordinate &coor)
+{
+  return 2 - sum(coor) % 2;  // 2 for even, 1 for odd
+}
+
+inline void regularize(Coordinate &coor, const Coordinate &regularizer)
+{
+#ifndef SUPPRESS_REG_COOR
+  // #ifndef USE_SINGLE_NODE // SINGLE_NODE mode (or !USE_MULTI_NODE) does NOT
+  // work (yet?) so this is meaningless. Thus it is commented out.
+  warn("use regular_coordinate");
+#endif
+  for (int mu = 0; mu < DIMN; mu++) {
+    coor[mu] = (coor[mu] % regularizer[mu] + regularizer[mu]) % regularizer[mu];
+  }
+}
+
+qacc Coordinate coordinate_shifts(const Coordinate &x) { return x; }
+
+qacc Coordinate coordinate_shifts(const Coordinate &x, const int dir)
+{
+  Coordinate xsh = x;
+  qassert(-DIMN <= dir && dir < DIMN);
+  if (0 <= dir) {
+    xsh[dir] += 1;
+  } else {
+    xsh[-dir - 1] -= 1;
+  }
+  return xsh;
+}
+
+qacc Coordinate coordinate_shifts(const Coordinate &x, const int dir1,
+                                  const int dir2)
+{
+  Coordinate xsh = x;
+  qassert(-DIMN <= dir1 && dir1 < DIMN);
+  qassert(-DIMN <= dir2 && dir2 < DIMN);
+  if (0 <= dir1) {
+    xsh[dir1] += 1;
+  } else {
+    xsh[-dir1 - 1] -= 1;
+  }
+  if (0 <= dir2) {
+    xsh[dir2] += 1;
+  } else {
+    xsh[-dir2 - 1] -= 1;
+  }
+  return xsh;
+}
+
+qacc Coordinate coordinate_shifts(const Coordinate &x, const int dir1,
+                                  const int dir2, const int dir3)
+{
+  Coordinate xsh = x;
+  qassert(-DIMN <= dir1 && dir1 < DIMN);
+  qassert(-DIMN <= dir2 && dir2 < DIMN);
+  qassert(-DIMN <= dir3 && dir3 < DIMN);
+  if (0 <= dir1) {
+    xsh[dir1] += 1;
+  } else {
+    xsh[-dir1 - 1] -= 1;
+  }
+  if (0 <= dir2) {
+    xsh[dir2] += 1;
+  } else {
+    xsh[-dir2 - 1] -= 1;
+  }
+  if (0 <= dir3) {
+    xsh[dir3] += 1;
+  } else {
+    xsh[-dir3 - 1] -= 1;
+  }
+  return xsh;
+}
+
+qacc Coordinate coordinate_shifts(const Coordinate &x, const int dir1,
+                                  const int dir2, const int dir3,
+                                  const int dir4)
+{
+  Coordinate xsh = x;
+  qassert(-DIMN <= dir1 && dir1 < DIMN);
+  qassert(-DIMN <= dir2 && dir2 < DIMN);
+  qassert(-DIMN <= dir3 && dir3 < DIMN);
+  qassert(-DIMN <= dir4 && dir4 < DIMN);
+  if (0 <= dir1) {
+    xsh[dir1] += 1;
+  } else {
+    xsh[-dir1 - 1] -= 1;
+  }
+  if (0 <= dir2) {
+    xsh[dir2] += 1;
+  } else {
+    xsh[-dir2 - 1] -= 1;
+  }
+  if (0 <= dir3) {
+    xsh[dir3] += 1;
+  } else {
+    xsh[-dir3 - 1] -= 1;
+  }
+  if (0 <= dir4) {
+    xsh[dir4] += 1;
+  } else {
+    xsh[-dir4 - 1] -= 1;
+  }
+  return xsh;
+}
+
+inline Coordinate coordinate_shifts(const Coordinate &x,
+                                    const std::vector<int> path)
+{
+  Coordinate ret = x;
+  for (int i = 0; i < (int)path.size(); ++i) {
+    const int dir = path[i];
+    qassert(-DIMN <= dir && dir < DIMN);
+    ret = coordinate_shifts(ret, dir);
+  }
+  return ret;
+}
+
+template <class CharT, class Traits>
+std::basic_ostream<CharT, Traits> &operator<<(
+    std::basic_ostream<CharT, Traits> &os, const Coordinate coor)
+{
+  std::basic_ostringstream<CharT, Traits> os_;
+  os_.flags(os.flags());
+  os_.imbue(os.getloc());
+  os_.precision(os.precision());
+  os_.setf(std::ios::showpos);
+  os_ << "(" << coor[0] << ", " << coor[1] << ", " << coor[2] << ", " << coor[3]
+      << ")";
+  return os << os_.str();
+}
+
+}  // namespace qlat
---
+++ packages/qlat/qlat-0.7.tar.gz/qlat-0.7/include/qlat/mvector.h
@@ -0,0 +1,190 @@
+#pragma once
+
+#include <qlat/setup.h>
+
+#include <cmath>
+
+namespace qlat
+{  //
+
+template <int DIMN, class T = ComplexT>
+struct API ALIGN MvectorT
+{
+  T p[DIMN];
+  //
+  // convert to double array
+  qacc double* d() { return (double*)p; }
+  qacc const double* d() const { return (const double*)p; }
+  //
+  // convert to Eigen Matrix
+  qacc Eigen::Matrix<T, DIMN, 1>& em()
+  {
+    return *((Eigen::Matrix<T, DIMN, 1>*)this);
+  }
+  qacc const Eigen::Matrix<T, DIMN, 1>& em() const
+  {
+    return *((Eigen::Matrix<T, DIMN, 1>*)this);
+  }
+  //
+  qacc T& operator()(int i)
+  {
+    qassert(0 <= i && i < DIMN);
+    return p[i];
+  }
+  qacc const T& operator()(int i) const
+  {
+    qassert(0 <= i && i < DIMN);
+    return p[i];
+  }
+  //
+  qacc const MvectorT& operator+=(const MvectorT& x)
+  {
+    *this = *this + x;
+    return *this;
+  }
+  //
+  qacc const MvectorT& operator-=(const MvectorT& x)
+  {
+    *this = *this - x;
+    return *this;
+  }
+  //
+  qacc const MvectorT& operator*=(const T& x)
+  {
+    *this = *this * x;
+    return *this;
+  }
+  //
+  qacc const MvectorT& operator/=(const T& x)
+  {
+    *this = *this / x;
+    return *this;
+  }
+};
+
+template <int DIMN, class T>
+qacc MvectorT<DIMN, T> operator+(const MvectorT<DIMN, T>& x,
+                                 const MvectorT<DIMN, T>& y)
+{
+  MvectorT<DIMN, T> ret;
+  ret.em() = x.em() + y.em();
+  return ret;
+}
+
+template <int DIMN, class T>
+qacc MvectorT<DIMN, T> operator-(const MvectorT<DIMN, T>& x,
+                                 const MvectorT<DIMN, T>& y)
+{
+  MvectorT<DIMN, T> ret;
+  ret.em() = x.em() - y.em();
+  return ret;
+}
+
+template <int DIMN, class T>
+qacc MvectorT<DIMN, T> operator-(const MvectorT<DIMN, T>& x)
+{
+  MvectorT<DIMN, T> ret;
+  ret.em() = -x.em();
+  return ret;
+}
+
+template <int DIMN, class T>
+qacc MvectorT<DIMN, T> operator*(const T& x, const MvectorT<DIMN, T>& y)
+{
+  MvectorT<DIMN, T> ret;
+  ret.em() = x * y.em();
+  return ret;
+}
+
+template <int DIMN, class T>
+qacc MvectorT<DIMN, T> operator*(const MvectorT<DIMN, T>& x, const T& y)
+{
+  MvectorT<DIMN, T> ret;
+  ret.em() = x.em() * y;
+  return ret;
+}
+
+template <int DIMN, class T>
+qacc MvectorT<DIMN, T> operator/(const MvectorT<DIMN, T>& x, const T& y)
+{
+  MvectorT<DIMN, T> ret;
+  ret.em() = x.em() / y;
+  return ret;
+}
+
+template <int DIMN, class T>
+qacc Complex dot_product(const MvectorT<DIMN, T>& x, const MvectorT<DIMN, T>& y)
+{
+  const Complex ret = x.em().adjoint() * y.em();
+  return ret;
+}
+
+template <int DIMN, class T>
+qacc void set_zero(MvectorT<DIMN, T>& m)
+{
+  std::memset((void*)&m, 0, sizeof(MvectorT<DIMN, T>));
+}
+
+template <int DIMN, class T>
+qacc void set_unit(MvectorT<DIMN, T>& m, const Complex& coef = 1.0)
+{
+  for (int i = 0; i < DIMN; ++i) {
+    m(i) = coef;
+  }
+}
+
+template <int DIMN, class T>
+qacc double qnorm(const MvectorT<DIMN, T>& m)
+{
+  return m.em().squaredNorm();
+}
+
+template <int DIMN, class T>
+qacc MvectorT<DIMN, T> vector_conjugate(const MvectorT<DIMN, T>& x)
+{
+  MvectorT<DIMN, T> ret;
+  ret.em() = x.em().conjugate();
+  return ret;
+}
+
+template <class T = ComplexT>
+struct API WilsonVectorT : MvectorT<4 * NUM_COLOR, T> {
+  qacc WilsonVectorT() {}
+  qacc WilsonVectorT(const MvectorT<4 * NUM_COLOR, T>& m) { *this = m; }
+  //
+  qacc const WilsonVectorT& operator=(const MvectorT<4 * NUM_COLOR, T>& m)
+  {
+    *this = (const WilsonVectorT&)m;
+    return *this;
+  }
+};
+
+template <class T = ComplexT>
+struct API SpinVectorT : MvectorT<4, T> {
+  qacc SpinVectorT() {}
+  qacc SpinVectorT(const MvectorT<4, T>& m) { *this = m; }
+  //
+  qacc const SpinVectorT& operator=(const MvectorT<4, T>& m)
+  {
+    *this = (const SpinVectorT&)m;
+    return *this;
+  }
+};
+
+#ifndef QLAT_NO_DEFAULT_TYPE
+
+typedef WilsonVectorT<> WilsonVector;
+
+typedef SpinVectorT<> SpinVector;
+
+#endif
+
+template <int DIMN, class T>
+std::string show(const MvectorT<DIMN, T>& m)
+{
+  std::ostringstream out;
+  out << m.em();
+  return out.str();
+}
+
+}  // namespace qlat
