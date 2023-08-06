75344deca65052cb678af8f2441f02db1d13b0a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 20:13:31 +0000

    Add package modelwhaleutils/0.4.9.3/modelwhaleutils-0.4.9.3.tar.gz

---
+++ packages/modelwhaleutils/modelwhaleutils-0.4.9.3.tar.gz/modelwhaleutils-0.4.9.3/setup.py
@@ -0,0 +1,17 @@
+import setuptools
+
+setuptools.setup(
+    name="modelwhaleutils",
+    version="0.4.9.3",
+    author="modalwhale team",
+    description="use in mw",
+    url="https://github.com/Kesci/modelwhaleutils",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    install_requires=["PyJWT", "requests", "pynvml", "psutil"],
+    python_requires='>=3.6',
+)
