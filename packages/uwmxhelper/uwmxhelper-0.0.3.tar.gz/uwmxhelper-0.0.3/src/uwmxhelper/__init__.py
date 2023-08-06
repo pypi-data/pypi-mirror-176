13351e9fdb89011a23101b373e1ff3a755b7d584
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 23:11:02 +0000

    Add package uwmxhelper/0.0.3/uwmxhelper-0.0.3.tar.gz

---
+++ packages/uwmxhelper/uwmxhelper-0.0.3.tar.gz/uwmxhelper-0.0.3/src/uwmxhelper/__init__.py
@@ -0,0 +1,6 @@
+from .greet import greet
+from .nextprime import nextprime
+from .adapt_ts import adapt_ts
+from .dryweatherflow import dryweatherflow
+from .plot_ts import plot_ts
+from .plot_ts_cum import plot_ts_cum
---
+++ packages/uwmxhelper/uwmxhelper-0.0.3.tar.gz/uwmxhelper-0.0.3/src/uwmxhelper/adapt_ts.py
@@ -0,0 +1,31 @@
+import numpy as np
+import pandas as pd
+
+def adapt_ts(time,value,startdate):
+    
+    """
+    
+    Parameters
+    ----------
+    time : array of timestamps
+        time data
+    value : array of numeric values
+        measurement data (usually rain data)
+    startdate : timestamp
+        date of first measurement
+
+    Returns
+    -------
+    ts_simulink : dict
+        timeseries in the required structure for simulink blocks (usually "RainReadFromWorkspace" )
+
+    """
+    
+    # Create relative time
+    time_rel = (time-pd.to_datetime(startdate))/np.timedelta64(1,'s')
+    
+    # Create simulink compatible data
+    a = eng.transpose(matlab.double(list(time_rel)))
+    b = eng.transpose(matlab.double(list(value)))
+    ts_simulink = {'time':a,'signals':{'values':b}}
+    return ts_simulink
