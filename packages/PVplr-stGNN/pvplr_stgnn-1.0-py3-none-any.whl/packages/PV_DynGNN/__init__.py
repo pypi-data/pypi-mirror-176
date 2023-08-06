8750f43d28a6ae44c4fa438b25520059ea52d0aa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 21:10:49 +0000

    Add package PVplr-stGNN/1.0/pvplr_stgnn-1.0-py3-none-any.whl

---
+++ packages/PVplr-stGNN/pvplr_stgnn-1.0-py3-none-any.whl/packages/PV_DynGNN/__init__.py
@@ -0,0 +1,8 @@
+# @Time     : xx/xx/20xx
+# @Author   : Yangxin Fan
+# @FileName : __init__.py
+# @Version  : 1.0
+# @IDE      : Jupyter Notebook
+# @Github   : https://github.com/Yangxin666/STD-GAE
+
+## In Process: reorganize codes from notebook to python packaging format
