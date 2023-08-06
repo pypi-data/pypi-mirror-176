650c93f88f15504b75eb56f9f891168e20c17bfe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 07:11:41 +0000

    Add package k9-python-logger/0.0.10/k9-python-logger-0.0.10.tar.gz

---
+++ packages/k9-python-logger/k9-python-logger-0.0.10.tar.gz/k9-python-logger-0.0.10/src/k9/internal/k9_core_client.py
@@ -0,0 +1,45 @@
+import logging
+from dataclasses import dataclass
+from json import JSONDecodeError
+from os import environ
+
+import requests
+
+
+@dataclass
+class K9CoreResponse:
+    id: str
+    message: str
+    app_name: str
+    correlation_id: str
+    span_id: str
+    log_level: str
+    
+@dataclass
+class K9CoreError():
+    payload: dict
+    status_code: int
+    message: dict
+
+
+class K9Core:
+    host: str
+
+    def __init__(self):
+        self.host = f"{environ.get('K9_CORE_HOST')}/api/v1"
+        self.token = environ.get('K9_TOKEN')
+
+    def sendRequest(self, message: dict):
+        r = requests.post(f"{self.host}/log", json=message, headers={"XK9Token": self.token})
+        if r.ok:
+            try:
+                result = r.json()
+                return K9CoreResponse(**result.data)
+            except JSONDecodeError | AttributeError:
+                logging.error("Invalid payload: ", r.text)
+                return K9CoreError(message=message,status_code=r.status_code,payload=message)
+           
+
+
+if __name__ == "__main__":
+    print(K9Core().sendRequest(dict(teste1="", teste2=123)))
