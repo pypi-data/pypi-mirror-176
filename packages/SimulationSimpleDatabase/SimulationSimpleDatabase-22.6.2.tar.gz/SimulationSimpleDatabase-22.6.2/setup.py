b9018935572d7d6e0aab546df6260a33b721d909
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 18:04:11 +0000

    Add package SimulationSimpleDatabase/22.6.2/SimulationSimpleDatabase-22.6.2.tar.gz

---
+++ packages/SimulationSimpleDatabase/SimulationSimpleDatabase-22.6.2.tar.gz/SimulationSimpleDatabase-22.6.2/setup.py
@@ -0,0 +1,31 @@
+from setuptools import setup, find_packages
+from os.path import join
+
+packages = []
+packages_dir = {}
+
+include_SOFA = True
+try:
+    import Sofa
+except ImportError:
+    include_SOFA = False
+
+project = 'SSD'
+roots = ['Core', 'SOFA'] if include_SOFA else ['Core']
+root_packages = ['storage', 'rendering']
+
+for root in roots:
+    packages.append(f'{project}.{root}')
+    packages_dir[f'{project}.{root}'] = join('src', root)
+    for package in find_packages(where=join('src', root)):
+        packages.append(f'{project}.{root}.{package}')
+        packages_dir[f'{project}.{root}.{package}'] = join('src', root, package)
+
+setup(name='SimulationSimpleDatabase',
+      version='22.06.2',
+      description='A simplified API to use SQL Databases with numerical simulation.',
+      author='R. Enjalbert, A. Odot',
+      url='https://github.com/RobinEnjalbert/SimulationSimpleDatabase',
+      packages=packages,
+      package_dir=packages_dir,
+      install_requires=['numpy', 'peewee', 'vedo'])
