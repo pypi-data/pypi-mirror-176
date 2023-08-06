d1156240c1256abbffeda3fdbc1ab1ea4d5b2e53
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2023-08-06 14:12:53 +0000

    Add package DEC-Tec-decode/0.0.2/DEC_Tec_decode-0.0.2-py3-none-any.whl

---
+++ packages/DEC-Tec-decode/DEC_Tec_decode-0.0.2-py3-none-any.whl/decode/skeleton.py
@@ -0,0 +1,18 @@
+
+import argparse
+import sys
+
+from decode import __version__
+from . import decode
+
+__author__ = "FEI YUAN"
+__copyright__ = "FEI YUAN"
+__license__ = "MIT"
+
+
+def main():
+    decode.main()
+
+
+if __name__ == "__main__":
+    main()
